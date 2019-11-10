682464531266ca2f03dd71a86e359a173261cf99
Arshankhanifar <arshankhanifar@gmail.com>
2019-11-09 19:46:12 -0500

    [MF]-2 this project tests the adc measurement on the tutorial boards

---
+++ projects/example/src/main.c
@@ -0,0 +1,60 @@
+// Example program for STM32F072 Controller board or Discovery Board.
+// Blinks the LEDs sequentially.
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "delay.h"       // For real-time delays
+#include "gpio.h"        // General Purpose I/O control.
+#include "interrupt.h"   // For enabling interrupts.
+#include "misc.h"        // Various helper functions/macros.
+#include "soft_timer.h"  // Software timers for scheduling future events.
+
+// Depending on which board you are working with you will need to (un)comment
+// the relevant block of GPIO pins. Generally these would be in a configuration
+// file somewhere and we normally write configurations only for the controller
+// board.
+
+// Controller board LEDs
+ static const GpioAddress leds[] = {
+  { .port = GPIO_PORT_B, .pin = 5 },   //
+  { .port = GPIO_PORT_B, .pin = 4 },   //
+  { .port = GPIO_PORT_B, .pin = 3 },   //
+  { .port = GPIO_PORT_A, .pin = 15 },  //
+};
+// Discovery board LEDs
+//static const GpioAddress leds[] = {
+//  { .port = GPIO_PORT_C, .pin = 8 },  //
+//  { .port = GPIO_PORT_C, .pin = 9 },  //
+//  { .port = GPIO_PORT_C, .pin = 6 },  //
+//  { .port = GPIO_PORT_C, .pin = 7 },  //
+//};
+
+int main(void) {
+  // Enable various peripherals
+  interrupt_init();
+  soft_timer_init();
+  gpio_init();
+
+  GpioSettings led_settings = {
+    .direction = GPIO_DIR_OUT,        // The pin needs to output.
+    .state = GPIO_STATE_HIGH,         // Start in the "on" state.
+    .alt_function = GPIO_ALTFN_NONE,  // No connections to peripherals.
+    .resistor = GPIO_RES_NONE,        // No need of a resistor to modify floating logic levels.
+  };
+
+  // Init all of the LED pins
+  for (size_t i = 0; i < SIZEOF_ARRAY(leds); i++) {
+    gpio_init_pin(&leds[i], &led_settings);
+  }
+
+  // Keep toggling the state of the pins from on to off with a 50 ms delay between.
+  while (true) {
+    for (size_t i = 0; i < SIZEOF_ARRAY(leds); i++) {
+      gpio_toggle_state(&leds[i]);
+      delay_ms(50);
+    }
+  }
+
+  return 0;
+}
---
+++ projects/tutorial_board_adc/src/main.c
@@ -0,0 +1,48 @@
+#include "adc.h"
+#include "gpio.h"
+#include "interrupt.h"
+#include "log.h"
+#include "delay.h"
+#include "soft_timer.h"
+
+
+int main(void) {
+  interrupt_init();
+  soft_timer_init();
+  gpio_init();
+
+  GpioAddress potentiometer_addr = {
+    .port = GPIO_PORT_A,
+    .pin = 1
+  };
+
+  GpioSettings pot_settings = {
+    GPIO_DIR_IN,        //
+    GPIO_STATE_LOW,     //
+    GPIO_RES_NONE,      //
+    GPIO_ALTFN_ANALOG,  //
+  };
+
+  gpio_init_pin(&potentiometer_addr, &pot_settings);
+  adc_init(ADC_MODE_SINGLE);
+
+  AdcChannel pot_channel = NUM_ADC_CHANNELS;
+
+  adc_get_channel(potentiometer_addr, &pot_channel);
+  adc_set_channel(pot_channel, true);
+  adc_set_channel(ADC_CHANNEL_TEMP, true);
+
+  LOG_DEBUG("Potentiometer ADC Channel: %d\n", pot_channel);
+
+  while (true) {
+    uint16_t potentiometer_data = 0;
+    uint16_t temperature_data = 0;
+    adc_read_raw(pot_channel, &potentiometer_data);
+    adc_read_raw(ADC_CHANNEL_TEMP, &temperature_data);
+    LOG_DEBUG("potentiometer data: %d\n", potentiometer_data);
+    delay_ms(200);
+  }
+
+  return 0;
+}
+
