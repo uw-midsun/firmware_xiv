d6fd4afc16eb49e17990e54e1a46a6f53dc7e31d
Arshankhanifar <arshankhanifar@gmail.com>
2019-11-22 16:41:13 -0500

    toggled LEDs and parsed message data and showed it

--- projects/can_communication/src/main.c
+++ projects/can_communication/src/main.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "can.h"
 #include "event_queue.h"
 #include "gpio.h"
@@ -15,15 +16,12 @@ typedef enum {
 
 static CanStorage s_can_storage;
 
-static StatusCode prv_rx_callback(const CanMessage *msg, void *context, CanAckStatus *ack_reply) {
-  LOG_DEBUG("Received a message!\n");
-  return STATUS_CODE_OK;
-}
+static StatusCode prv_rx_callback(const CanMessage *msg, void *context, CanAckStatus *ack_reply);
 
 void init_can(void) {
   CanSettings can_settings = {
     .device_id = TEST_CAN_DEVICE_ID,
-    .bitrate = CAN_HW_BITRATE_125KBPS,
+    .bitrate = CAN_HW_BITRATE_500KBPS,
     .rx_event = TEST_CAN_EVENT_RX,
     .tx_event = TEST_CAN_EVENT_TX,
     .fault_event = TEST_CAN_EVENT_FAULT,
@@ -36,23 +34,65 @@ void init_can(void) {
   can_register_rx_default_handler(prv_rx_callback, NULL);
 }
 
+static const GpioAddress s_leds[] = {
+  { .port = GPIO_PORT_B, .pin = 5 },   //
+  { .port = GPIO_PORT_B, .pin = 4 },   //
+  { .port = GPIO_PORT_B, .pin = 3 },   //
+  { .port = GPIO_PORT_A, .pin = 15 },  //
+};
+
+static void prv_init_leds() {
+  GpioSettings led_settings = {
+    .direction = GPIO_DIR_OUT,
+    .state = GPIO_STATE_HIGH,
+    .alt_function = GPIO_ALTFN_NONE,
+    .resistor = GPIO_RES_NONE
+  };
+  for (size_t i = 0; i < SIZEOF_ARRAY(s_leds); i++) {
+    gpio_init_pin(&s_leds[i], &led_settings);
+  }
+}
+
+static void prv_toggle_leds() {
+  for (size_t i = 0; i < SIZEOF_ARRAY(s_leds); i++) {
+    gpio_toggle_state(&s_leds[i]);
+  }
+}
+
+static StatusCode prv_rx_callback(const CanMessage *msg, void *context, CanAckStatus *ack_reply) {
+  prv_toggle_leds();
+  LOG_DEBUG("Received a message!\n");
+  char log_message[30];
+  strcpy(log_message, "Data:\n\t");
+  uint8_t i;
+  for (i = 0; i < msg->dlc; i++) {
+      uint8_t byte = 0;
+      byte = msg->data >> (i * 8);
+      char buff[5];
+      sprintf(buff, "%x ", byte);
+      strcat(log_message, buff);
+  }
+  strcat(log_message, "\n");
+  LOG_DEBUG("%s", log_message);
+  return STATUS_CODE_OK;
+}
+
 int main() {
   gpio_init();
   event_queue_init();
   interrupt_init();
   soft_timer_init();
+  
+  prv_init_leds();
 
   init_can();
-  LOG_DEBUG("Hello World!\n");
+  LOG_DEBUG("Welcome to CAN Demo!\n");
 
   Event e = { 0 };
   int i = 0;
 
   while (true) {
     while (event_process(&e) != STATUS_CODE_OK) {
-      LOG_DEBUG("Event Queue: empty%d!\r", i);
-      i++;
-      i %= 2;
     }
     can_process_event(&e);
   }
