d339cc0b6ef4b1c7b60c7bb589a43faff43dcf8a
Adam Ding <wqding@uwaterloo.ca>
2019-11-29 16:29:45 -0500

    added new files

---
+++ projects/centre_console/src/center_console.h
@@ -0,0 +1,58 @@
+
+#pragma once
+#include "can.h"
+#include "gpio.h"
+#include "interrupt.h"
+#include "gpio_it.h"
+#include "event_queue.h"
+#include "log.h"
+#include "soft_timer.h"
+#include "status.h"
+
+//implement the fsm led for when the power button is checked???
+typedef enum {
+  EVENT_BTN_IDLE = 0,
+  EVENT_BTN_PUSH,
+} BUTTON_EVENT;
+
+//CAN Events
+typedef enum {
+  EE_CENTER_CONSOLE_DIGITAL_INPUT_POWER = 0,
+  EE_CENTER_CONSOLE_DIGITAL_INPUT_DRIVE,
+  EE_CENTER_CONSOLE_DIGITAL_INPUT_NEUTRAL,
+  EE_CENTER_CONSOLE_DIGITAL_INPUT_REVERSE,
+  EE_CENTER_CONSOLE_DIGITAL_INPUT_HAZARDS,
+  EE_CENTER_CONSOLE_DIGITAL_INPUT_LOW_BEAM,
+  NUM_EE_CENTER_CONSOLE_DIGITAL_INPUTS,
+} EECenterConsoleCanEvents;
+
+typedef struct{
+  GpioAddress btn_addr;
+  EECenterConsoleCanEvents can_event;
+}CenterConsoleInputLink;
+
+typedef struct {
+  CenterConsoleInputLink power_input;
+  CenterConsoleInputLink drive_input;
+  CenterConsoleInputLink neutral_input;
+  CenterConsoleInputLink reverse_input;
+  CenterConsoleInputLink hazards_input;
+  CenterConsoleInputLink low_beam_input;
+}CenterConsoleStorage;
+
+typedef enum {
+  TEST_CAN_EVENT_RX = 10,
+  TEST_CAN_EVENT_TX,
+  TEST_CAN_EVENT_FAULT,
+} TestCanEvent;
+
+typedef struct {
+  CenterConsoleInputLink* input_link;
+  Event* e;
+}GpioItCallbackContext;
+
+static CenterConsoleStorage s_cc_storage;
+
+StatusCode initialize_center_console(CenterConsoleStorage* cc_storage);
+
+void can_emit_msg();
---
+++ projects/centre_console/src/main.c
@@ -0,0 +1,82 @@
+
+
+#include "center_console.h"
+
+
+#define TEST_CAN_DEVICE_ID 0x1
+
+static CanStorage s_can_storage = { 0 };
+
+static StatusCode prv_rx_callback(const CanMessage *msg, void *context, CanAckStatus *ack_reply) {
+  // prv_toggle_leds();
+  LOG_DEBUG("Received a message!\n");
+  char log_message[30];
+  printf("Data:\n\t");
+  uint8_t i;
+  for (i = 0; i < msg->dlc; i++) {
+    uint8_t byte = 0;
+    byte = msg->data >> (i * 8);
+    printf("%x ", byte);
+  }
+  printf("\n");
+  return STATUS_CODE_OK;
+}
+
+void init_can(void) {
+  CanSettings can_settings = {
+    .device_id = TEST_CAN_DEVICE_ID,
+    .bitrate = CAN_HW_BITRATE_125KBPS,
+    .rx_event = TEST_CAN_EVENT_RX,
+    .tx_event = TEST_CAN_EVENT_TX,
+    .fault_event = TEST_CAN_EVENT_FAULT,
+    .tx = { GPIO_PORT_A, 12 },
+    .rx = { GPIO_PORT_A, 11 },
+    .loopback = false,
+  };
+
+  StatusCode ret = can_init(&s_can_storage, &can_settings);
+  can_register_rx_default_handler(prv_rx_callback, NULL);
+}
+
+int main(void) {
+  gpio_init();
+  interrupt_init();
+  soft_timer_init();
+  event_queue_init();
+
+  init_can();
+
+  CenterConsoleStorage cc_storage = {
+    .power_input = {
+        .btn_addr = { .port = GPIO_PORT_B, .pin = 0 },
+        .can_event = EE_CENTER_CONSOLE_DIGITAL_INPUT_POWER,
+    },
+    .drive_input = {
+      .btn_addr = { .port = GPIO_PORT_B, .pin = 1 } ,
+      .can_event = EE_CENTER_CONSOLE_DIGITAL_INPUT_DRIVE,
+    },
+    .neutral_input = {
+      .btn_addr = { .port = GPIO_PORT_B, .pin = 2 },
+      .can_event = EE_CENTER_CONSOLE_DIGITAL_INPUT_NEUTRAL,
+    },
+    .reverse_input = {
+      .btn_addr = { .port = GPIO_PORT_B, .pin = 0 },
+      .can_event = EE_CENTER_CONSOLE_DIGITAL_INPUT_REVERSE,
+    },
+    .hazards_input = {
+      .btn_addr = { .port = GPIO_PORT_B, .pin = 0 },
+      .can_event = EE_CENTER_CONSOLE_DIGITAL_INPUT_HAZARDS,
+    },
+    .low_beam_input = {
+      .btn_addr = { .port = GPIO_PORT_B, .pin = 0 },
+      .can_event = EE_CENTER_CONSOLE_DIGITAL_INPUT_LOW_BEAM,
+    },
+  };
+
+  initialize_center_console(&cc_storage);
+  
+  while (true) {
+
+  }
+    return 0;
+}
