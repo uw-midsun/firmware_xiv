81dfebda82b5547beb9eb980fa062d7f8304b5a4
Arshankhanifar <arshankhanifar@gmail.com>
2020-01-10 19:15:55 -0500

    SOFT-39: added one can test that does not pass

---
+++ projects/centre_console/inc/power_on_sequence_fsm.h
@@ -0,0 +1,13 @@
+#include "fsm.h"
+#include "status.h"
+#include "centre_console_fault_reason.h"
+
+typedef struct PowerOnSequenceFsmStorage {
+  Fsm sequence_fsm;
+  CentreConsoleFaultReason ack_fault_reason;
+  EventId success_ack_next_event;
+} PowerOnSequenceFsmStorage;
+
+StatusCode power_on_sequence_fsm_init(PowerOnSequenceFsmStorage *power_fsm);
+
+bool power_on_sequence_fsm_process_event(PowerOnSequenceFsmStorage *sequence_fsm, const Event *event);
---
+++ projects/centre_console/test/test_power_on_sequence_fsm.c
@@ -0,0 +1,74 @@
+#include "unity.h"
+#include "can.h"
+#include "status.h"
+#include "can_msg_defs.h"
+#include "ms_test_helpers.h"
+#include "log.h"
+#include "event_queue.h"
+#include "centre_console_events.h"
+#include "power_on_sequence_fsm.h"
+#include "interrupt.h"
+#include "test_helpers.h"
+#include "gpio.h"
+#include <string.h>
+
+typedef struct TestCanMessageAssertion {
+  CanMessageId message_id;
+  uint64_t data;
+} TestCanMessageAssertion;
+
+static PowerOnSequenceFsmStorage s_sequence_storage;
+
+static CanStorage s_can_storage;
+
+const CanSettings s_can_settings = {
+  // clang-format on
+  .device_id = SYSTEM_CAN_DEVICE_CENTRE_CONSOLE,
+  .loopback = true,
+  .bitrate = CAN_HW_BITRATE_500KBPS,
+  .rx = { .port = GPIO_PORT_A, .pin = 11 },
+  .tx = { .port = GPIO_PORT_A, .pin = 12 },
+  .rx_event = CENTRE_CONSOLE_EVENT_CAN_RX,
+  .tx_event = CENTRE_CONSOLE_EVENT_CAN_TX,
+  .fault_event = CENTRE_CONSOLE_EVENT_CAN_FAULT
+};
+
+static TestCanMessageAssertion s_can_message_assertion;
+
+static StatusCode prv_rx_callback(const CanMessage *msg, void *context, CanAckStatus *ack_reply) {
+  s_can_message_assertion.message_id = msg->msg_id;
+  s_can_message_assertion.data = msg->data;
+  return STATUS_CODE_OK;
+}
+
+void setup_test(void) {
+  gpio_init();
+  event_queue_init();
+  interrupt_init();
+  soft_timer_init();
+
+  TEST_ASSERT_OK(can_init(&s_can_storage, &s_can_settings));
+  can_register_rx_handler(SYSTEM_CAN_MESSAGE_FRONT_POWER, 
+    prv_rx_callback, 
+    NULL
+  );
+  memset(&s_sequence_storage, 0, sizeof(s_sequence_storage));
+  memset(&s_can_message_assertion, 0, sizeof(s_can_message_assertion));
+
+  power_on_sequence_fsm_init(&s_sequence_storage);
+}
+
+void teardown_test(void) {}
+
+void test_sequence_starts_by_turning_on_driver_display(void) {
+  // given
+  Event e = { .id = POWER_ON_SEQUENCE_EVENT_BEGIN, .data = 0 };
+  // when
+  //power_on_sequence_fsm_process_event(&s_sequence_storage, &e);
+  CAN_TRANSMIT_FRONT_POWER(0);
+  MS_TEST_HELPER_CAN_TX_RX(CENTRE_CONSOLE_EVENT_CAN_TX, CENTRE_CONSOLE_EVENT_CAN_RX);
+  while (!status_ok(event_process(&e))) {}
+  // then
+  TEST_ASSERT_EQUAL(SYSTEM_CAN_MESSAGE_FRONT_POWER, s_can_message_assertion.message_id);
+}
+
