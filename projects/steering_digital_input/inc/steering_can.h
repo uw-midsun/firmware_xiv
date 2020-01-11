4438ec28eb6be484619a379411ae71d1fda51230
Avery Chiu <a27chiu@uwaterloo.ca>
2020-01-11 00:56:58 +0000

    Added unit tests

--- projects/steering_digital_input/inc/steering_can.h
+++ projects/steering_digital_input/inc/steering_can.h
@@ -1,12 +1,12 @@
 #pragma once
-//Module for sending can messages
+// Module for sending can messages
 #include "can.h"
-#include "can_fifo.h"
 #include "can_ack.h"
+#include "can_fifo.h"
 #include "can_fsm.h"
 
-//Initializes the CAN
+// Initializes the CAN
 StatusCode steering_can_init();
 
-//Proccesses a CAN event and pops an item off the queue
+// Proccesses a CAN event and pops an item off the queue
 StatusCode steering_can_process_event(Event *e);
--- projects/steering_digital_input/src/main.c
+++ projects/steering_digital_input/src/main.c
@@ -5,7 +5,6 @@
 #include "delay.h"
 #include "digital_input_config.h"
 #include "digital_input_events.h"
-#include "steering_can.h"
 #include "event_queue.h"
 #include "exported_enums.h"
 #include "gpio.h"
@@ -14,6 +13,7 @@
 #include "misc.h"
 #include "soft_timer.h"
 #include "status.h"
+#include "steering_can.h"
 #include "wait.h"
 
 int main() {
@@ -32,30 +32,30 @@ int main() {
     .bitrate = CAN_HW_BITRATE_500KBPS,
     .rx_event = STEERING_DIGITAL_INPUT_CAN_RX,
     .tx_event = STEERING_DIGITAL_INPUT_CAN_TX,
+    .tx = { GPIO_PORT_A, 11 },
+    .rx = { GPIO_PORT_A, 12 },
+    .loopback = true,
   };
   // Initialize CAN
   can_init(&storage, &settings);
   steering_can_init();
 
-  static SteeringDigitalInputConfiguration s_steering_lookup_table[NUM_STEERING_DIGITAL_INPUTS] = {
-    [STEERING_DIGITAL_INPUT_HORN] = 
-    { .event = STEERING_DIGITAL_INPUT_EVENT_HORN,
-      .address = { .port = GPIO_PORT_B, .pin = 0 } },
-    [STEERING_DIGITAL_INPUT_RADIO_PPT] = 
-    { .event = STEERING_DIGITAL_INPUT_EVENT_RADIO_PPT,
-      .address = { .port = GPIO_PORT_B, .pin = 1 } },
+  SteeringDigitalInputConfiguration s_steering_lookup_table[NUM_STEERING_DIGITAL_INPUTS] = {
+    [STEERING_DIGITAL_INPUT_HORN] = { .event = STEERING_DIGITAL_INPUT_EVENT_HORN,
+                                      .address = { .port = GPIO_PORT_B, .pin = 0 } },
+    [STEERING_DIGITAL_INPUT_RADIO_PPT] = { .event = STEERING_DIGITAL_INPUT_EVENT_RADIO_PPT,
+                                           .address = { .port = GPIO_PORT_B, .pin = 1 } },
     [STEERING_DIGITAL_INPUT_HIGH_BEAM_FORWARD] =
         { .event = STEERING_DIGITAL_INPUT_EVENT_HIGH_BEAM_FORWARD,
           .address = { .port = GPIO_PORT_B, .pin = 3 } },
-    [STEERING_DIGITAL_INPUT_HIGH_BEAM_REAR] = 
-       { .event =  STEERING_DIGITAL_INPUT_EVENT_HIGH_BEAM_REAR,
-         .address = { .port = GPIO_PORT_B, .pin = 4 } },
+    [STEERING_DIGITAL_INPUT_HIGH_BEAM_REAR] = { .event =
+                                                    STEERING_DIGITAL_INPUT_EVENT_HIGH_BEAM_REAR,
+                                                .address = { .port = GPIO_PORT_B, .pin = 4 } },
     [STEERING_DIGITAL_INPUT_REGEN_BRAKE_TOGGLE] =
         { .event = STEERING_DIGITAL_INPUT_EVENT_REGEN_BRAKE_TOGGLE,
           .address = { .port = GPIO_PORT_B, .pin = 5 } },
-    [STEERING_DIGITAL_INPUT_CC_TOGGLE] = 
-        { .event = STEERING_DIGITAL_INPUT_EVENT_CC_TOGGLE,
-          .address = { .port = GPIO_PORT_B, .pin = 6 } },
+    [STEERING_DIGITAL_INPUT_CC_TOGGLE] = { .event = STEERING_DIGITAL_INPUT_EVENT_CC_TOGGLE,
+                                           .address = { .port = GPIO_PORT_B, .pin = 6 } },
     [STEERING_DIGITAL_DIGITAL_INPUT_CC_INCREASE_SPEED] =
         { .event = STEERING_DIGITAL_DIGITAL_INPUT_EVENT_CC_INCREASE_SPEED,
           .address = { .port = GPIO_PORT_B, .pin = 7 } },
@@ -66,16 +66,16 @@ int main() {
 
   // Initialize an event
   Event e = { .id = 0, .data = 0 };
-    
+
   // Initialize the steering_digital_input to register
   // all interrupts and GPIO pins so messages
   // can be sent to CAN
-  steering_digital_input_init(&s_steering_lookup_table);
+  steering_digital_input_init(s_steering_lookup_table);
 
   while (true) {
     // Pops events off of the queue if there is an item in the queue
     // and sends a CAN message
-   steering_can_process_event(&e);
+    steering_can_process_event(&e);
   }
   return 0;
 }
