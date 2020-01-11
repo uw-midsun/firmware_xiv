f53c23b69f6ab164d65e1e321cb8abe87ef60cdd
Andrew Guo <andrewguo2001@gmail.om>
2020-01-11 14:02:55 -0500

    Revisions on Jan 11

--- projects/pedal_board/test/test_throttle.c
+++ projects/pedal_board/test/test_throttle.c
@@ -19,29 +19,30 @@ void setup_test(void) {
   interrupt_init();
   soft_timer_init();
   gpio_it_init();
+  event_queue_init(); 
+  
+  // I2CSettings i2c_settings = {
+  //   .speed = I2C_SPEED_FAST,
+  //   .scl = { .port = GPIO_PORT_B, .pin = 5 },
+  //   .sda = { .port = GPIO_PORT_B, .pin = 5 },  // Need to change later
+  // };
 
-  I2CSettings i2c_settings = {
-    .speed = I2C_SPEED_FAST,
-    .scl = { .port = GPIO_PORT_B, .pin = 5 },
-    .sda = { .port = GPIO_PORT_B, .pin = 5 },  // Need to change later
-  };
+  // i2c_init(I2C_PORT_2, &i2c_settings);
+  // GpioAddress ready_pin = { .port = GPIO_PORT_B, .pin = 2 };
 
-  i2c_init(I2C_PORT_2, &i2c_settings);
-  GpioAddress ready_pin = { .port = GPIO_PORT_B, .pin = 2 };
-
-  // Intialize ADS1015
-  ads1015_init(&ads1015_storage, I2C_PORT_2, ADS1015_ADDRESS_GND, &ready_pin);
+  // // Intialize ADS1015
+  // ads1015_init(&ads1015_storage, I2C_PORT_2, ADS1015_ADDRESS_GND, &ready_pin);
 }
 
 void teardown_test(void) {}
 
-void test_throttle_module_should_intialize_properly(void) {
-  TEST_ASSERT_EQUAL(STATUS_CODE_OK, throttle_init(&throttle_storage));
-}
+// void test_throttle_module_should_intialize_properly(void) {
+//   TEST_ASSERT_EQUAL(STATUS_CODE_OK, throttle_init(&throttle_storage));
+// }
 
-void test_throttle_should_be_enabled(void) {
-  TEST_ASSERT_EQUAL(STATUS_CODE_OK, throttle_enable(&throttle_storage));
-}
+// void test_throttle_should_be_enabled(void) {
+//   TEST_ASSERT_EQUAL(STATUS_CODE_OK, throttle_enable(&throttle_storage));
+// }
 
 void test_private_callback_should_raise_events(void) {
   throttle_init(&throttle_storage); 
