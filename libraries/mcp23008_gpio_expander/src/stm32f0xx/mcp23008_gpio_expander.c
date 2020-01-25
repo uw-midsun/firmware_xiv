f902fbbef10d35d890ba9fd1bca51ecbeab3d2d7
Ryan Dancy <ryan@keal.ca>
2020-01-25 20:56:11 +0000

    Move MCP23008 register definitions to their own header

--- libraries/mcp23008_gpio_expander/src/stm32f0xx/mcp23008_gpio_expander.c
+++ libraries/mcp23008_gpio_expander/src/stm32f0xx/mcp23008_gpio_expander.c
@@ -2,33 +2,7 @@
 
 #include <stdbool.h>
 #include "i2c.h"
-
-#define I2C_PORT I2C_PORT_2
-
-#define CONFIG_PIN_I2C_SCL \
-  { GPIO_PORT_B, 10 }
-#define CONFIG_PIN_I2C_SDA \
-  { GPIO_PORT_B, 11 }
-
-// Should the unused ones be removed?
-#define IODIR 0x00    // direction, 1 input 0 output
-#define IPOL 0x01     // polarity, corresponding GPIO bit will show inverted value on the pin
-#define GPINTEN 0x02  // interrupt on change 1 enable 0 disable
-#define DEFVAL 0x03   // default compare value for interrupt, opposite value on pin causes interrupt
-#define INTCON 0x04   // interrupt control, interrupt-on-change vs interrupt-on-previous-val
-
-// IO configuration register
-// bit 5 (SEQOP): sequential operation register, 0 if enabled. Address pointer increments.
-// (apparently good for polling) bit 4 (DISSLW): slew rate control 0 enable 1 disable bit 3 (HAEN):
-// HW addr bit enable (always enabled for this model) bit 2 (ODR): INT pin has open drain ouptut bit
-// 1 (INTPOL): polarity for INT ouput pin
-#define IOCON 0x05
-
-#define GPPU 0x06    // pull-up-resistor 1 pulled up
-#define INTF 0x07    // Interrupt flag, 1 means that pin caused interrupt
-#define INTCAP 0x08  // captures the GPIO PORT value when interrupt occurs
-#define GPIO 0x09    // read GPIO from here, write modifies the Output Latch
-#define OLAT 0x0A    // 'read' reads OLAT, write modifies the pins configured as output
+#include "mcp23008_gpio_expander_defs.h"
 
 StatusCode mcp23008_gpio_init(const Mcp23008I2CAddress i2c_address) {
   // i2c_address only used in x86 - should this even have any implementation?
