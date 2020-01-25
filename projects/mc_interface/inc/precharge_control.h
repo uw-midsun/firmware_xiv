14ac61b2bfe1df8d7f3f2d4b3aa6f978f10ff0f9
g-jessmuir <e.jessmuir@gmail.com>
2020-01-25 15:05:48 -0500

    added boilerplate and some structure for the MCI FSMs

---
+++ projects/mc_interface/inc/precharge_control.h
@@ -0,0 +1,12 @@
+#include "status.h"
+
+typedef enum {
+    PRECHARGE_STATE_INCOMPLETE = 0,
+    PRECHARGE_STATE_PRECHARGING,
+    PRECHARGE_STATE_COMPLETE,
+    NUM_PRECHARGE_STATES
+} PrechargeStates;
+
+static StatusCode precharge_init(void *context);
+
+void precharge_fsm_process_event(const Event *e);
