080402580cf766b282d0a4b1cff4616ecafbbb96
Gerald Aryeetey <aryeeteygerald@rogers.com>
2020-02-07 14:32:21 +0000

    Remove WIP motor controller files

--- projects/mc_interface/inc/.drive_can.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-// Handles drive output messages from Driver Controls
-// Requires CAN and motor controllers to be initialized
-//
-// Raises events accordingly
-#include "motor_controller.h"
-#include "status.h"
-
-StatusCode drive_can_init(MotorControllerStorage *controller);
--- projects/mc_interface/inc/.motor_event.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-typedef enum {
-  MOTOR_EVENT_CAN_TX,
-  MOTOR_EVENT_CAN_RX,
-  MOTOR_EVENT_CAN_FAULT,
-} MotorEvent;
