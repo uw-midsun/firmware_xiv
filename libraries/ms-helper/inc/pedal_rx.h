9efa57850cb70b71517413907d7633ac64ba2985
Gerald Aryeetey <aryeeteygerald@rogers.com>
2020-02-10 00:17:40 +0000

    Match Pedal multiplier
    TODO: Should move this to EE

--- libraries/ms-helper/inc/pedal_rx.h
+++ libraries/ms-helper/inc/pedal_rx.h
@@ -5,7 +5,7 @@
 #include "soft_timer.h"
 
 // Max value is 100 = 7 bits. which leaves 25 bits of precision
-#define PEDAL_RX_MSG_DENOMINATOR (1 << 25)
+#define PEDAL_RX_MSG_DENOMINATOR (1 << 12)
 #define PEDAL_RX_MAX_PEDAL_VALUE 100.0f
 
 #define PEDAL_OUTPUT_WATCHDOG_PERIOD_MS 400
