9b75dbd4c99099ecda6a2592b82446871b27c821
Arshan Khanifar <arshankhanifar@gmail.com>
2020-02-11 00:13:05 +0000

    fixed issua

--- projects/centre_console/src/mci_output_tx.c
+++ projects/centre_console/src/mci_output_tx.c
@@ -3,3 +3,8 @@
 StatusCode mci_output_init(MciOutputTxStorage *storage) {
   return STATUS_CODE_OK;
 }
+
+StatusCode mci_output_tx_drive_output(MciOutputTxStorage *storage, RetryTxRequest *request,
+                                      EEDriveOutput drive_output) {
+  return STATUS_CODE_OK;
+}
--- projects/centre_console/src/relay_tx.c
+++ projects/centre_console/src/relay_tx.c
@@ -3,3 +3,8 @@
 StatusCode relay_tx_init(RelayTxStorage *storage) {
   return STATUS_CODE_OK;
 }
+
+StatusCode relay_tx_relay_state(RelayTxStorage *storage, RetryTxRequest *request,
+                                EERelayState state) {
+  return STATUS_CODE_OK;
+}
