a11e97ea95dcb0f5becf6896995978405f0a8af7
Avery Chiu <a27chiu@uwaterloo.ca>
2020-02-21 16:53:56 +0000

    changed formatting

--- projects/steering_can/inc/steering_can.h
+++ projects/steering_can/inc/steering_can.h
@@ -3,11 +3,11 @@
 // in the event queue when triggered
 // Requires GPIO,Interrupts,Event Queue,Soft-timer, CAN
 #include "can.h"
-#include "can_fsm.h"
-#include "can_msg.h"
 #include "can_ack.h"
 #include "can_fifo.h"
+#include "can_fsm.h"
 #include "can_hw.h"
+#include "can_msg.h"
 #include "can_rx.h"
 #include "event_queue.h"
 #include "fsm.h"
@@ -19,13 +19,12 @@
 #define STEERING_CAN_DEVICE_ID 0x1
 
 typedef enum {
-    STEERING_CAN_FAULT = 0,
-    STEERING_CAN_EVENT_RX,
-    STEERING_CAN_EVENT_TX,
-}SteeringCanEvents;
+  STEERING_CAN_FAULT = 0,
+  STEERING_CAN_EVENT_RX,
+  STEERING_CAN_EVENT_TX,
+} SteeringCanEvents;
 
 StatusCode steering_can_init();
 
-//Receives an event and sends a CAN message
+// Receives an event and sends a CAN message
 StatusCode steering_can_process_event(Event e);
-
