01c81c3367fcd056b9edf7f221d99e96c1096f08
Gerald Aryeetey <aryeeteygerald@rogers.com>
2020-03-05 15:34:35 +0000

    Apply test_format changes

--- projects/mci/inc/motor_controller.h
+++ projects/mci/inc/motor_controller.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "precharge_control.h"
 #include "mci_broadcast.h"
 #include "mci_output.h"
+#include "precharge_control.h"
 
 typedef struct MotorControllerStorage {
   MotorControllerOutputStorage mci_output_storage;
