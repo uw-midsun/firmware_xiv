b86da55af4c24485417f70057c3a834fa7ab54e1
Andrew Guo <andrewguo2001@gmail.om>
2020-03-07 14:46:13 -0500

    Revised killswitch

--- projects/bms_carrier/inc/killswitch.h
+++ projects/bms_carrier/inc/killswitch.h
@@ -4,7 +4,7 @@
 //
 // Raises a fault event if the killswitch is hit as an input.
 // Allows bypassing the killswitch as an output.
-#include "bps_heartbeat.h"
+#include "battery_heartbeat.h"
 #include "debouncer.h"
 #include "gpio.h"
 
@@ -12,9 +12,20 @@ typedef struct KillswitchStorage {
   DebouncerStorage debouncer;
 } KillswitchStorage;
 
+typedef enum {
+  EE_BATTERY_HEARTBEAT_FAULT_SOURCE_KILLSWITCH = 0,
+  // EE_BPS_HEARTBEAT_FAULT_SOURCE_LTC_AFE_CELL,
+  // EE_BPS_HEARTBEAT_FAULT_SOURCE_LTC_AFE_TEMP,
+  // EE_BPS_HEARTBEAT_FAULT_SOURCE_LTC_AFE_FSM,
+  // EE_BPS_HEARTBEAT_FAULT_SOURCE_LTC_ADC,
+  // EE_BPS_HEARTBEAT_FAULT_SOURCE_ACK_TIMEOUT,
+  // NUM_EE_BPS_HEARTBEAT_FAULT_SOURCES,
+} EEBatteryHeartbeatFaultSource;
+
+
 // Set the killswitch up to fault if hit. Assumes the killswitch is active-low.
 StatusCode killswitch_init(KillswitchStorage *storage, const GpioAddress *killswitch,
-                           BpsHeartbeatStorage *bps_heartbeat);
+                           BatteryHeartbeatStorage *battery_heartbeat);
 
 // Bypass the killswitch. It does not need to be initialized.
 StatusCode killswitch_bypass(const GpioAddress *killswitch);
--- projects/bms_carrier/src/killswitch.c
+++ projects/bms_carrier/src/killswitch.c
@@ -3,22 +3,22 @@
 #include "log.h"
 
 static void prv_killswitch_handler(const GpioAddress *address, void *context) {
-  BpsHeartbeatStorage *storage = context;
+  BatteryHeartbeatStorage *storage = context;
 
   GpioState state = NUM_GPIO_STATES;
   gpio_get_state(address, &state);
 
   if (state == GPIO_STATE_LOW) {
     // Falling edge - killswitch was hit
-    bps_heartbeat_raise_fault(storage, EE_BPS_HEARTBEAT_FAULT_SOURCE_KILLSWITCH);
+    battery_heartbeat_raise_fault(storage, EE_BATTERY_HEARTBEAT_FAULT_SOURCE_KILLSWITCH);
   } else {
     // Rising edge - killswitch was released
-    bps_heartbeat_clear_fault(storage, EE_BPS_HEARTBEAT_FAULT_SOURCE_KILLSWITCH);
+    battery_heartbeat_clear_fault(storage, EE_BATTERY_HEARTBEAT_FAULT_SOURCE_KILLSWITCH);
   }
 }
 
 StatusCode killswitch_init(KillswitchStorage *storage, const GpioAddress *killswitch,
-                           BpsHeartbeatStorage *bps_heartbeat) {
+                           BatteryHeartbeatStorage *bps_heartbeat) {
   // Force update
   prv_killswitch_handler(killswitch, bps_heartbeat);
   return debouncer_init_pin(&storage->debouncer, killswitch, prv_killswitch_handler, bps_heartbeat);
