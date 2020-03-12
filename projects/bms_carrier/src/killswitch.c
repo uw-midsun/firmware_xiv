895386f94032270cf018ead80c680acb0fdb7819
Andrew Guo <andrewguo2001@gmail.om>
2020-03-11 20:48:03 -0400

    Revisions to killswitch.c and rules.mk to include ms-helper

--- projects/bms_carrier/src/killswitch.c
+++ projects/bms_carrier/src/killswitch.c
@@ -1,14 +1,11 @@
 #include "killswitch.h"
 #include "gpio_it.h"
 #include "log.h"
-// #include "debouncer.h"
 
 static void prv_killswitch_handler(const GpioAddress *address, void *context) {
   BatteryHeartbeatStorage *storage = context;
-
   GpioState state = NUM_GPIO_STATES;
   gpio_get_state(address, &state);
-
   if (state == GPIO_STATE_LOW) {
     // Falling edge - killswitch was hit
     battery_heartbeat_raise_fault(storage, EE_BATTERY_HEARTBEAT_FAULT_SOURCE_KILLSWITCH);
@@ -22,7 +19,8 @@ StatusCode killswitch_init(KillswitchStorage *storage, const GpioAddress *killsw
                            BatteryHeartbeatStorage *battery_heartbeat) {
   // Force update
   prv_killswitch_handler(killswitch, battery_heartbeat);
-  return debouncer_init_pin(&storage->debouncer, killswitch, prv_killswitch_handler, battery_heartbeat);
+  return debouncer_init_pin(&storage->debouncer, killswitch,
+                            prv_killswitch_handler, battery_heartbeat);
 }
 
 StatusCode killswitch_bypass(const GpioAddress *killswitch) {
--- projects/bms_carrier/test/test_killswitch.c
+++ projects/bms_carrier/test/test_killswitch.c
@@ -1,10 +1,7 @@
 #include "killswitch.h"
 #include "gpio_it.h"
-#include "log.h" 
+#include "log.h"
 
 void setup_test(void) {
-
-
 }
-
 void teardown_test(void) { }
