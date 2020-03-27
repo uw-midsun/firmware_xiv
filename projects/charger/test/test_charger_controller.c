de3bebdb5ff4932bb2372dfee0f339126a5f2a71
Jarvis <jarvis0910@gmail.com>
2020-03-27 02:37:32 +0000

    just charger controller tx test left

--- projects/charger/test/test_charger_controller.c
+++ projects/charger/test/test_charger_controller.c
@@ -1,7 +1,9 @@
 #include "can.h"
+#include "can_msg_defs.h"
 #include "charger_controller.h"
 #include "charger_events.h"
 #include "event_queue.h"
+#include "exported_enums.h"
 #include "generic_can_mcp2515.h"
 #include "gpio.h"
 #include "gpio_it.h"
@@ -84,18 +86,93 @@ void test_charger_tx(void) {
 }
 
 void test_charger_rx(void) {
-  charger_controller_activate();
-  MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
-
   static GenericCanMsg s_gen_can_msg = {
     .id = BCA_CCS_ID,
     .extended = false,
     .data = (uint64_t)(1 << 24),
     .dlc = 0,
   };
+
+  // hardware fault
+  charger_controller_activate();
+  MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
+
   generic_can_tx(&s_generic_can.base, &s_gen_can_msg);
 
   MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
-  //test message is fault
+  Event e = { 0 };
+  while (!status_ok(event_process(&e))) {
+  }
+  TEST_ASSERT_EQUAL(SYSTEM_CAN_MESSAGE_CHARGER_FAULT, e.id);
+  TEST_ASSERT_EQUAL(EE_CHARGER_HARDWARE_FAULT, e.data);
 
+  // temperature fault
+  s_gen_can_msg.data = (uint64_t)(1 << 25);
+  charger_controller_activate();
+  MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
+
+  generic_can_tx(&s_generic_can.base, &s_gen_can_msg);
+
+  MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
+  Event e = { 0 };
+  while (!status_ok(event_process(&e))) {
+  }
+  TEST_ASSERT_EQUAL(SYSTEM_CAN_MESSAGE_CHARGER_FAULT, e.id);
+  TEST_ASSERT_EQUAL(EE_CHARGER_TEMP_FAULT, e.data);
+
+  // temperature fault
+  s_gen_can_msg.data = (uint64_t)(1 << 25);
+  charger_controller_activate();
+  MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
+
+  generic_can_tx(&s_generic_can.base, &s_gen_can_msg);
+
+  MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
+  Event e = { 0 };
+  while (!status_ok(event_process(&e))) {
+  }
+  TEST_ASSERT_EQUAL(SYSTEM_CAN_MESSAGE_CHARGER_FAULT, e.id);
+  TEST_ASSERT_EQUAL(EE_CHARGER_TEMP_FAULT, e.data);
+
+  // input fault
+  s_gen_can_msg.data = (uint64_t)(1 << 26);
+  charger_controller_activate();
+  MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
+
+  generic_can_tx(&s_generic_can.base, &s_gen_can_msg);
+
+  MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
+  Event e = { 0 };
+  while (!status_ok(event_process(&e))) {
+  }
+  TEST_ASSERT_EQUAL(SYSTEM_CAN_MESSAGE_CHARGER_FAULT, e.id);
+  TEST_ASSERT_EQUAL(EE_CHARGER_INPUT_FAULT, e.data);
+
+  // state fault
+  s_gen_can_msg.data = (uint64_t)(1 << 27);
+  charger_controller_activate();
+  MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
+
+  generic_can_tx(&s_generic_can.base, &s_gen_can_msg);
+
+  MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
+  Event e = { 0 };
+  while (!status_ok(event_process(&e))) {
+  }
+  TEST_ASSERT_EQUAL(SYSTEM_CAN_MESSAGE_CHARGER_FAULT, e.id);
+  TEST_ASSERT_EQUAL(EE_CHARGER_STATE_FAULT, e.data);
+
+  // communication fault
+  s_gen_can_msg.data = (uint64_t)(1 << 28);
+  charger_controller_activate();
+  MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
+
+  generic_can_tx(&s_generic_can.base, &s_gen_can_msg);
+
+  MS_TEST_HELPER_CAN_TX_RX(CHARGER_CAN_EVENT_TX, CHARGER_CAN_EVENT_RX);
+  Event e = { 0 };
+  while (!status_ok(event_process(&e))) {
+  }
+  TEST_ASSERT_EQUAL(SYSTEM_CAN_MESSAGE_CHARGER_FAULT, e.id);
+  TEST_ASSERT_EQUAL(EE_CHARGER_COMMUNICATION_FAULT, e.data);
 }
