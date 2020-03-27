ec1b767163c3490aeca11d2c831d59b09374e876
Avery Chiu <a27chiu@uwaterloo.ca>
2020-03-27 22:14:22 +0000

    Added can_message script

--- projects/can_set_gpio/scripts/can_message.py
+++ projects/can_set_gpio/scripts/can_message.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
- #coding: utf-8
+# coding: utf-8
 from __future__ import print_function
+
 import can
-can.rc['interface'] = 'socketcan'
-can.rc['channel'] = 'vcan0'
-can.rc['bitrate'] = 500000
-from can.interfaces.interface import Bus
+
 
 def send_one():
-    bus = can.interface.Bus(bustype='socketcan', channel='vcan0', bitrate=500000)
-    msg = can.Message(arbitration_id=0xc0ffee,
-                      data=[0, 25, 0, 1, 3, 1, 4, 1],
-                      is_extended_id=True)
+
+    bus = can.interface.Bus(bustype='socketcan', channel='vcan0', bitrate=250000)
+
+    msg = can.Message(
+        arbitration_id=0xC0FFEE, data=[0, 25, 0, 1, 3, 1, 4, 1], is_extended_id=True
+    )
 
     try:
         bus.send(msg)
@@ -19,5 +19,6 @@ def send_one():
     except can.CanError:
         print("Message NOT sent")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     send_one()
