53069dc076cd7eed3d978a631d51697905af6f71
Ryan Dancy <ryan@keal.ca>
2020-03-29 21:43:41 -0400

    [SOFT-131] Extract pin aliases, update rear power distribution can rx config

--- projects/power_distribution/src/power_distribution_current_measurement_config.c
+++ projects/power_distribution/src/power_distribution_current_measurement_config.c
@@ -1,117 +1,93 @@
 #include "power_distribution_current_measurement_config.h"
+#include "power_distribution_pin_defs.h"
 
 // Definitions of the hardware configs declared in the header
 
 #define POWER_DISTRIBUTION_I2C_PORT I2C_PORT_2
 
-#define POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_0 0x74
-#define POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_1 0x76
-
 const PowerDistributionCurrentHardwareConfig FRONT_POWER_DISTRIBUTION_CURRENT_HW_CONFIG = {
   .i2c_port = POWER_DISTRIBUTION_I2C_PORT,
   .dsel_i2c_addresses =
       (I2CAddress[]){
-          POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_0,  //
-          POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_1,  //
+          POWER_DISTRIBUTION_I2C_ADDRESS_0,  //
+          POWER_DISTRIBUTION_I2C_ADDRESS_1,  //
       },
   .num_dsel_i2c_addresses = 2,
   .bts7200s =
       (PowerDistributionBts7200Data[]){
           {
-              // LEFT_RIGHT_CAM
-              .dsel_pin = { .i2c_address = POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_0,
-                            .pin = PCA9539R_PIN_IO0_0 },
-              .current_0 = POWER_DISTRIBUTION_CURRENT_LEFT_CAMERA,
-              .current_1 = POWER_DISTRIBUTION_CURRENT_RIGHT_CAMERA,
+              .dsel_pin = FRONT_PIN_LEFT_RIGHT_CAM_DSEL,
+              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_LEFT_CAMERA,
+              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_RIGHT_CAMERA,
               .mux_selection = 12,
           },
           {
-              // DVR_REAR_DISP
-              .dsel_pin = { .i2c_address = POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_0,
-                            .pin = PCA9539R_PIN_IO0_6 },
-              .current_0 = POWER_DISTRIBUTION_CURRENT_DRIVER_DISPLAY,
-              .current_1 = POWER_DISTRIBUTION_CURRENT_REAR_DISPLAY,
+              .dsel_pin = FRONT_PIN_DVR_REAR_DISP_DSEL,
+              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_DRIVER_DISPLAY,
+              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_REAR_DISPLAY,
               .mux_selection = 0,
           },
           {
-              // L_R_DVR_DISP
-              .dsel_pin = { .i2c_address = POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_0,
-                            .pin = PCA9539R_PIN_IO1_5 },
-              .current_0 = POWER_DISTRIBUTION_CURRENT_LEFT_DRIVER_DISPLAY,
-              .current_1 = POWER_DISTRIBUTION_CURRENT_RIGHT_DRIVER_DISPLAY,
+              .dsel_pin = FRONT_PIN_L_R_DVR_DISP_DSEL,
+              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_LEFT_DRIVER_DISPLAY,
+              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_RIGHT_DRIVER_DISPLAY,
               .mux_selection = 4,
           },
           {
-              // FRONT_TURN_LIGHT
-              .dsel_pin = { .i2c_address = POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_1,
-                            .pin = PCA9539R_PIN_IO0_3 },
-              .current_0 = POWER_DISTRIBUTION_CURRENT_LEFT_FRONT_TURN_LIGHT,
-              .current_1 = POWER_DISTRIBUTION_CURRENT_RIGHT_FRONT_TURN_LIGHT,
+              .dsel_pin = FRONT_PIN_FRONT_TURN_LIGHT_DSEL,
+              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_LEFT_FRONT_TURN_LIGHT,
+              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_RIGHT_FRONT_TURN_LIGHT,
               .mux_selection = 9,
           },
           {
-              // MAIN_REAR_PI
-              .dsel_pin = { .i2c_address = POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_0,
-                            .pin = PCA9539R_PIN_IO1_1 },
-              .current_0 = POWER_DISTRIBUTION_CURRENT_MAIN_PI,
-              .current_1 = POWER_DISTRIBUTION_CURRENT_REAR_PI,
+              .dsel_pin = FRONT_PIN_MAIN_REAR_PI_DSEL,
+              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_MAIN_PI,
+              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_REAR_PI,
               .mux_selection = 1,
           },
           {
-              // 5V_SPARE
-              .dsel_pin = { .i2c_address = POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_0,
-                            .pin = PCA9539R_PIN_IO0_3 },
-              .current_0 = POWER_DISTRIBUTION_CURRENT_5V_SPARE_1,
-              .current_1 = POWER_DISTRIBUTION_CURRENT_5V_SPARE_2,
+              .dsel_pin = FRONT_PIN_5V_SPARE_DSEL,
+              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_5V_SPARE_1,
+              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_5V_SPARE_2,
               .mux_selection = 2,
           },
           {
-              // SPARE_2_CTR_CONSL
-              .dsel_pin = { .i2c_address = POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_1,
-                            .pin = PCA9539R_PIN_IO0_0 },
-              .current_0 = POWER_DISTRIBUTION_CURRENT_SPARE_2,
-              .current_1 = POWER_DISTRIBUTION_CURRENT_CENTRE_CONSOLE,
+              .dsel_pin = FRONT_PIN_SPARE_2_CTR_CONSL_DSEL,
+              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_SPARE_2,
+              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_CENTRE_CONSOLE,
               .mux_selection = 7,
           },
           {
-              // STR_PDL
-              .dsel_pin = { .i2c_address = POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_1,
-                            .pin = PCA9539R_PIN_IO1_2 },
-              .current_0 = POWER_DISTRIBUTION_CURRENT_STEERING,
-              .current_1 = POWER_DISTRIBUTION_CURRENT_PEDAL,
+              .dsel_pin = FRONT_PIN_STR_PDL_DSEL,
+              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_STEERING,
+              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_PEDAL,
               .mux_selection = 6,
           } },
   .num_bts7200_channels = 8,
   .bts7040s =
       (PowerDistributionBts7040Data[]){
           {
-              // MAIN_DISP
-              .current = POWER_DISTRIBUTION_CURRENT_MAIN_DISPLAY,
+              .current = FRONT_POWER_DISTRIBUTION_CURRENT_MAIN_DISPLAY,
               .mux_selection = 13,
           },
           {
-              // DAYTIME
-              .current = POWER_DISTRIBUTION_CURRENT_DAYTIME_RUNNING_LIGHTS,
+              .current = FRONT_POWER_DISTRIBUTION_CURRENT_DAYTIME_RUNNING_LIGHTS,
               .mux_selection = 8,
           },
           {
-              // PARKING_BRAKE
-              .current = POWER_DISTRIBUTION_CURRENT_PARKING_BRAKE,
+              .current = FRONT_POWER_DISTRIBUTION_CURRENT_PARKING_BRAKE,
               .mux_selection = 10,
           },
           {
-              // SPARE_1
-              .current = POWER_DISTRIBUTION_CURRENT_SPARE_1,
+              .current = FRONT_POWER_DISTRIBUTION_CURRENT_SPARE_1,
               .mux_selection = 11,
           },
           {
-              // HORN - actually a BTS7008 but it's the same
-              .current = POWER_DISTRIBUTION_CURRENT_HORN,
+              .current = FRONT_POWER_DISTRIBUTION_CURRENT_HORN,
               .mux_selection = 3,
           },
           {
-              // SPEAKER
-              .current = POWER_DISTRIBUTION_CURRENT_SPEAKER,
+              .current = FRONT_POWER_DISTRIBUTION_CURRENT_SPEAKER,
               .mux_selection = 5,
           } },
   .num_bts7040_channels = 6,
@@ -136,60 +112,90 @@ const PowerDistributionCurrentHardwareConfig REAR_POWER_DISTRIBUTION_CURRENT_HW_
   .i2c_port = POWER_DISTRIBUTION_I2C_PORT,
   .dsel_i2c_addresses =
       (I2CAddress[]){
-          POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_0,  //
-          POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_1,  //
+          POWER_DISTRIBUTION_I2C_ADDRESS_0,  //
+          POWER_DISTRIBUTION_I2C_ADDRESS_1,  //
       },
   .num_dsel_i2c_addresses = 2,
   .bts7200s =
       (PowerDistributionBts7200Data[]){
           {
-              // Rear camera and placeholder (there's nothing on the second port?)
-              .dsel_pin = { .i2c_address = POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_0,
-                            .pin = PCA9539R_PIN_IO0_0 },
-              .current_0 = POWER_DISTRIBUTION_CURRENT_REAR_CAMERA,
-              .current_1 = POWER_DISTRIBUTION_CURRENT_PLACEHOLDER_REAR_CAM,
-              .mux_selection = 12,
+              .dsel_pin = REAR_PIN_SOLAR_TELEMETRY_DSEL,
+              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_SOLAR_SENSE,
+              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_TELEMETRY,
+              .mux_selection = 7,
           },
           {
-              // Charger interface and lights BPS strobe - assuming on same BTS7200 as L_R_DVR_DISP
-              .dsel_pin = { .i2c_address = POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_0,
-                            .pin = PCA9539R_PIN_IO1_5 },
-              .current_0 = POWER_DISTRIBUTION_CURRENT_CHARGER_INTERFACE,
-              .current_1 = POWER_DISTRIBUTION_CURRENT_LIGHTS_BPS_STROBE,
+              .dsel_pin = REAR_PIN_STROBE_CTR_BRK_DSEL,
+              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_STROBE,
+              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_CENTRE_BRAKE_LIGHT,
+              .mux_selection = 9,
+          },
+          {
+              .dsel_pin = REAR_PIN_REAR_TURN_LIGHT_DSEL,
+              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_LEFT_REAR_TURN_LIGHT,
+              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_RIGHT_REAR_TURN_LIGHT,
+              .mux_selection = 0,
+          },
+          {
+              .dsel_pin = REAR_PIN_REAR_BRAKE_LIGHT_DSEL,
+              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_LEFT_BRAKE_LIGHT,
+              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_RIGHT_BRAKE_LIGHT,
               .mux_selection = 4,
           },
           {
-              // Left and right rear turn lights
-              .dsel_pin = { .i2c_address = POWER_DISTRIBUTION_DSEL_I2C_ADDRESS_1,
-                            .pin = PCA9539R_PIN_IO0_3 },
-              .current_0 = POWER_DISTRIBUTION_CURRENT_LEFT_REAR_TURN_LIGHT,
-              .current_1 = POWER_DISTRIBUTION_CURRENT_RIGHT_REAR_TURN_LIGHT,
-              .mux_selection = 9,
-          } },
-  .num_bts7200_channels = 3,
+              .dsel_pin = REAR_PIN_CAM_SPARE_10_DSEL,
+              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_REAR_CAMERA,
+              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_10,
+              .mux_selection = 12,
+          },
+          {
+              .dsel_pin = REAR_PIN_SPARE_1_2_DSEL,
+              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_1,
+              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_2,
+              .mux_selection = 1,
+          },
+          {
+              .dsel_pin = REAR_PIN_SPARE_3_4_DSEL,
+              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_3,
+              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_4,
+              .mux_selection = 2,
+          },
+          {
+              .dsel_pin = REAR_PIN_SPARE_8_9_DSEL,
+              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_8,
+              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_9,
+              .mux_selection = 6,
+          },
+      },
+  .num_bts7200_channels = 8,
   .bts7040s =
       (PowerDistributionBts7040Data[]){
           {
-              // BMS carrier - assuming "infotainment monitor" is MAIN_DISP
-              .current = POWER_DISTRIBUTION_CURRENT_BMS_CARRIER,
-              .mux_selection = 13,
-          },
-          {
-              // Rear brake light
-              .current = POWER_DISTRIBUTION_CURRENT_REAR_BRAKE_LIGHT,
+              .current = REAR_POWER_DISTRIBUTION_CURRENT_BMS_CARRIER,
               .mux_selection = 8,
           },
           {
-              // Solar sense
-              .current = POWER_DISTRIBUTION_CURRENT_SOLAR_SENSE,
+              .current = REAR_POWER_DISTRIBUTION_CURRENT_MCI,
+              .mux_selection = 13,
+          },
+          {
+              .current = REAR_POWER_DISTRIBUTION_CURRENT_CHARGER,
+              .mux_selection = 11,
+          },
+          {
+              .current = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_5,
               .mux_selection = 10,
           },
           {
-              // MCI
-              .current = POWER_DISTRIBUTION_CURRENT_MCI,
+              .current = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_6,
               .mux_selection = 3,
-          } },
-  .num_bts7040_channels = 4,
+          },
+          {
+              .current = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_7,
+              .mux_selection = 5,
+          },
+      },
+  .num_bts7040_channels = 6,
   .mux_address =
       {
           .bit_width = 4,
