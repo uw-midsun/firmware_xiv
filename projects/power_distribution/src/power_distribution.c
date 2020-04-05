559ce5cc1f48f251954fdca50855be7cdffa855a
Ryan Dancy <ryan@keal.ca>
2020-04-04 21:07:04 -0400

    [SOFT-144] Detect front/rear power distribution by checking if pin 30 is shorted

--- projects/power_distribution/src/power_distribution.c
+++ projects/power_distribution/src/power_distribution.c
@@ -1,23 +1,25 @@
 // Current measurement testing main
 
-// I haven't figured out how to test if pin 31 is grounded, so as of yet to run on rear power
-// distribution change this to false.
-#define ON_FRONT_POWER_DISTRIBUTION true
+// If pins 30 and 31 haven't been grounded on rear power distribution yet, uncomment this line
+// and it'll force firmware to run as rear power distribution.
+// #define FORCE_REAR_POWER_DISTRIBUTION
 
+#include "adc.h"
+#include "interrupt.h"
+#include "log.h"
 #include "power_distribution_current_measurement.h"
 #include "power_distribution_current_measurement_config.h"
-#include "interrupt.h"
 #include "soft_timer.h"
-#include "adc.h"
-#include "log.h"
 
 #define I2C_PORT I2C_PORT_2
-#define I2C_SCL_PIN { GPIO_PORT_B, 10 }
-#define I2C_SDA_PIN { GPIO_PORT_B, 11 }
+#define I2C_SCL_PIN \
+  { GPIO_PORT_B, 10 }
+#define I2C_SDA_PIN \
+  { GPIO_PORT_B, 11 }
 
 #define READ_INTERVAL_US 500000
 
-static const char * const s_current_names[] = {
+static const char *const s_current_names[] = {
   // Currents for front power distribution
   [FRONT_POWER_DISTRIBUTION_CURRENT_LEFT_CAMERA] = "Left camera",
   [FRONT_POWER_DISTRIBUTION_CURRENT_RIGHT_CAMERA] = "Right camera",
@@ -67,19 +69,42 @@ static const char * const s_current_names[] = {
   [REAR_POWER_DISTRIBUTION_CURRENT_SPARE_10] = "Spare 10",
 };
 
-void print_current_measurements(void *context) {
+static bool s_is_front_power_distribution;
+
+static bool prv_detect_is_front_power_distribution(void) {
+#ifdef FORCE_REAR_POWER_DISTRIBUTION
+  return false;
+#else
+  // initialize pin 30 (PC13) as pull-up, it's shorted on rear
+  GpioAddress board_test_pin = { .port = GPIO_PORT_C, .pin = 13 };
+  GpioSettings board_test_settings = {
+    .direction = GPIO_DIR_IN,
+    .resistor = GPIO_RES_PULLUP,
+    .alt_function = GPIO_ALTFN_NONE,
+  };
+  gpio_init_pin(&board_test_pin, &board_test_settings);
+
+  // we're on front if it's high and rear if it's low
+  GpioState state = GPIO_STATE_HIGH;  // default to front for x86 since we can force rear
+  gpio_get_state(&board_test_pin, &state);
+
+  return state == GPIO_STATE_HIGH;
+#endif
+}
+
+static void prv_print_current_measurements(void *context) {
   PowerDistributionCurrentStorage *storage = power_distribution_current_measurement_get_storage();
-  
+
   LOG_DEBUG("--- Begin current measurements on ");
-  if (ON_FRONT_POWER_DISTRIBUTION) {
+  if (s_is_front_power_distribution) {
     LOG_DEBUG("front power distribution");
   } else {
     LOG_DEBUG("rear power distribution");
   }
   LOG_DEBUG(" ---\r\n");
-  
+
   // limits here must be updated if the current array changes
-  if (ON_FRONT_POWER_DISTRIBUTION) {
+  if (s_is_front_power_distribution) {
     for (PowerDistributionCurrent current = FRONT_POWER_DISTRIBUTION_CURRENT_LEFT_CAMERA;
          current < REAR_POWER_DISTRIBUTION_CURRENT_BMS_CARRIER; current++) {
       LOG_DEBUG("%s", s_current_names[current]);
@@ -100,21 +125,23 @@ int main(void) {
   interrupt_init();
   soft_timer_init();
   adc_init(ADC_MODE_SINGLE);
-  
+
   I2CSettings i2c_settings = {
     .speed = I2C_SPEED_FAST,
     .scl = I2C_SCL_PIN,
     .sda = I2C_SDA_PIN,
   };
   i2c_init(I2C_PORT, &i2c_settings);
-  
+
+  s_is_front_power_distribution = prv_detect_is_front_power_distribution();
+
   PowerDistributionCurrentSettings settings = {
-    .hw_config = ON_FRONT_POWER_DISTRIBUTION ? FRONT_POWER_DISTRIBUTION_CURRENT_HW_CONFIG
-      : REAR_POWER_DISTRIBUTION_CURRENT_HW_CONFIG,
+    .hw_config = s_is_front_power_distribution ? FRONT_POWER_DISTRIBUTION_CURRENT_HW_CONFIG
+                                               : REAR_POWER_DISTRIBUTION_CURRENT_HW_CONFIG,
     .interval_us = READ_INTERVAL_US,
-    .callback = &print_current_measurements,
+    .callback = &prv_print_current_measurements,
   };
   power_distribution_current_measurement_init(&settings);
-  
+
   return 0;
 }
