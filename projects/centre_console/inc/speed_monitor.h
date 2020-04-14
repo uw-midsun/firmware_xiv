bfe10a5a3f58748d9c6c49a09427b6e5586dcdb6
Arshankhanifar <arshankhanifar@gmail.com>
2020-04-13 23:20:43 -0400

    added speed monitor module

---
+++ projects/centre_console/inc/speed_monitor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "status.h"
+#include "watchdog.h"
+#include "can.h"
+#include "can_msg_defs.h"
+
+typedef enum { 
+  SPEED_STATE_STATIONARY = 0,
+  SPEED_STATE_MOVING,
+  NUM_SPEED_STATES
+} SpeedState;
+
+#define STATIONARY_VELOCITY_THRESHOLD 100 // units: cm/s (3.6 km/h)
+
+StatusCode speed_monitor_init(WatchdogTimeout timeout);
+
+SpeedState *get_global_speed_state(void);
---
+++ projects/centre_console/src/speed_monitor.c
@@ -0,0 +1,52 @@
+#include "speed_monitor.h"
+#include "can_unpack.h"
+#include "log.h"
+
+
+static SpeedState s_speed_state = NUM_SPEED_STATES;
+
+typedef struct SpeedMonitorStorage {
+  WatchdogTimeout timeout;
+  WatchdogStorage watchdog_storage;
+} SpeedMonitorStorage;
+
+static SpeedMonitorStorage s_storage = { 0 };
+
+static void prv_did_not_receive_speed_message(void *context) {
+  s_speed_state = NUM_SPEED_STATES;
+}
+
+static StatusCode prv_receive_velocity(const CanMessage *msg, 
+                                       void *context,
+                                       CanAckStatus *ack_reply) {
+  uint16_t u_velocity_left, u_velocity_right = 0;
+  CAN_UNPACK_MOTOR_VELOCITY(msg, &u_velocity_left, 
+                                  &u_velocity_right);
+  float velocity_left = (float) u_velocity_left,
+        velocity_right = (float) u_velocity_right;
+
+  if (velocity_right > STATIONARY_VELOCITY_THRESHOLD || 
+      velocity_left > STATIONARY_VELOCITY_THRESHOLD) {
+    s_speed_state = SPEED_STATE_MOVING;
+  } else {
+    s_speed_state = SPEED_STATE_STATIONARY;
+  }
+  watchdog_kick(&s_storage.watchdog_storage);
+  return STATUS_CODE_OK;
+}
+
+StatusCode speed_monitor_init(WatchdogTimeout timeout) {
+  s_storage = (SpeedMonitorStorage) {
+    .timeout = timeout
+  };
+  status_ok_or_return(
+      can_register_rx_handler(SYSTEM_CAN_MESSAGE_MOTOR_VELOCITY, 
+      prv_receive_velocity, NULL));
+  watchdog_start(&s_storage.watchdog_storage, timeout, 
+    prv_did_not_receive_speed_message, NULL);
+  return STATUS_CODE_OK;
+}
+
+SpeedState *get_global_speed_state(void) {
+  return &s_speed_state;
+}
