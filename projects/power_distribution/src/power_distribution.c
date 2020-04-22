df17f3eb7e90f7bbc6bfffbf15ef39e22eef07e9
Ryan Dancy <ryan@keal.ca>
2020-04-22 19:07:40 -0400

    [SOFT-144] Use lights_signal_fsm and strobe blinker in testing can rx and gpio

--- projects/power_distribution/src/power_distribution.c
+++ projects/power_distribution/src/power_distribution.c
@@ -4,9 +4,11 @@
 // uncomment this and it'll force firmware to configure as rear.
 // #define FORCE_REAR_POWER_DISTRIBUTION
 
+#include "can_msg_defs.h"
 #include "gpio.h"
 #include "i2c.h"
 #include "interrupt.h"
+#include "lights_signal_fsm.h"
 #include "log.h"
 #include "power_distribution_can_rx_event_mapper.h"
 #include "power_distribution_can_rx_event_mapper_config.h"
@@ -14,6 +16,7 @@
 #include "power_distribution_gpio.h"
 #include "power_distribution_gpio_config.h"
 #include "power_distribution_pin_defs.h"
+#include "rear_power_distribution_strobe_blinker.h"
 
 #define I2C_PORT I2C_PORT_2
 #define I2C_SCL_PIN \
@@ -26,9 +29,11 @@
 #define CAN_RX_PIN \
   { GPIO_PORT_A, 11 }
 
-#define CAN_DEVICE_ID 0x5
+#define SIGNAL_BLINK_INTERVAL_US 500000  // 0.5s
+#define STROBE_BLINK_INTERVAL_US 100000  // 0.1s
 
 static CanStorage s_can_storage;
+static SignalFsmStorage s_signal_fsm_storage;
 
 static bool s_is_front_power_distribution;
 
@@ -70,8 +75,12 @@ int main(void) {
   pca9539r_gpio_init(I2C_PORT, POWER_DISTRIBUTION_I2C_ADDRESS_0);
   pca9539r_gpio_init(I2C_PORT, POWER_DISTRIBUTION_I2C_ADDRESS_1);
 
+  // test if it's front or rear power distribution
+  s_is_front_power_distribution = prv_detect_is_front_power_distribution();
+
   CanSettings can_settings = {
-    .device_id = CAN_DEVICE_ID,
+    .device_id = s_is_front_power_distribution ? SYSTEM_CAN_DEVICE_POWER_DISTRIBUTION_FRONT
+                                               : SYSTEM_CAN_DEVICE_POWER_DISTRIBUTION_REAR,
     .loopback = true,
     .bitrate = CAN_HW_BITRATE_500KBPS,
     .rx_event = POWER_DISTRIBUTION_CAN_EVENT_RX,
@@ -82,9 +91,6 @@ int main(void) {
   };
   can_init(&s_can_storage, &can_settings);
 
-  // test if it's front or rear power distribution
-  s_is_front_power_distribution = prv_detect_is_front_power_distribution();
-
   // initialize can rx and gpio
   power_distribution_can_rx_event_mapper_init(s_is_front_power_distribution
                                                   ? FRONT_POWER_DISTRIBUTION_CAN_RX_CONFIG
@@ -92,12 +98,36 @@ int main(void) {
   power_distribution_gpio_init(s_is_front_power_distribution ? FRONT_POWER_DISTRIBUTION_GPIO_CONFIG
                                                              : REAR_POWER_DISTRIBUTION_GPIO_CONFIG);
 
+  // initialize lights_signal_fsm
+  SignalFsmSettings signal_fsm_settings = {
+    .signal_left_input_event = POWER_DISTRIBUTION_SIGNAL_EVENT_LEFT,
+    .signal_right_input_event = POWER_DISTRIBUTION_SIGNAL_EVENT_RIGHT,
+    .signal_hazard_input_event = POWER_DISTRIBUTION_SIGNAL_EVENT_HAZARD,
+    .signal_left_output_event = POWER_DISTRIBUTION_GPIO_EVENT_SIGNAL_LEFT,
+    .signal_right_output_event = POWER_DISTRIBUTION_GPIO_EVENT_SIGNAL_RIGHT,
+    .signal_hazard_output_event = POWER_DISTRIBUTION_GPIO_EVENT_SIGNAL_HAZARD,
+    .blink_interval_us = SIGNAL_BLINK_INTERVAL_US,
+  };
+  lights_signal_fsm_init(&s_signal_fsm_storage, &signal_fsm_settings);
+
+  if (!s_is_front_power_distribution) {
+    // initialize the strobe blinker on rear
+    RearPowerDistributionStrobeBlinkerSettings strobe_settings = {
+      .strobe_blink_delay_us = STROBE_BLINK_INTERVAL_US,
+    };
+    rear_power_distribution_strobe_blinker_init(&strobe_settings);
+  }
+
   // process events
   Event e = { 0 };
   while (true) {
     while (event_process(&e) != STATUS_CODE_OK) {
       can_process_event(&e);
       power_distribution_gpio_process_event(&e);
+      lights_signal_fsm_process_event(&s_signal_fsm_storage, &e);
+      if (!s_is_front_power_distribution) {
+        rear_power_distribution_strobe_blinker_process_event(&e);
+      }
     }
   }
 
