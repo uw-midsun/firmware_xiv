7ab579548e62d2b9ce932595c3afdc6bcba4b5f8
g-jessmuir <e.jessmuir@gmail.com>
2020-05-18 21:27:32 -0400

    removed unnecessary comments

--- projects/bms_carrier/inc/killswitch.h
+++ projects/bms_carrier/inc/killswitch.h
@@ -12,16 +12,6 @@ typedef struct KillswitchStorage {
   DebouncerStorage debouncer;
 } KillswitchStorage;
 
-// typedef enum {
-//   EE_BATTERY_HEARTBEAT_FAULT_SOURCE_KILLSWITCH = 0,
-//   // EE_BPS_HEARTBEAT_FAULT_SOURCE_LTC_AFE_CELL,
-//   // EE_BPS_HEARTBEAT_FAULT_SOURCE_LTC_AFE_TEMP,
-//   // EE_BPS_HEARTBEAT_FAULT_SOURCE_LTC_AFE_FSM,
-//   // EE_BPS_HEARTBEAT_FAULT_SOURCE_LTC_ADC,
-//   // EE_BPS_HEARTBEAT_FAULT_SOURCE_ACK_TIMEOUT,
-//   // NUM_EE_BPS_HEARTBEAT_FAULT_SOURCES,
-// } EEBatteryHeartbeatFaultSource;
-
 // Set the killswitch up to fault if hit. Assumes the killswitch is active-low.
 StatusCode killswitch_init(KillswitchStorage *storage, const GpioAddress *killswitch,
                            BatteryHeartbeatStorage *battery_heartbeat);
