48a4a130930c062ecf016116a543315cc7c56015
AveryChiu64 <a27chiu@uwaterloo.ca>
2020-05-20 22:14:27 +0000

    removed duplicate adc_periodic_reader implementation on stm32f0xx

--- projects/steering/inc/steering_can.h
+++ projects/steering/inc/steering_can.h
@@ -2,14 +2,9 @@
 // Module to process events on the queue and send CAN messages
 // Requires GPIO,Interrupts,Event Queue, and CAN to be initialized
 #include "can.h"
-#include "can_ack.h"
-#include "can_fifo.h"
-#include "can_fsm.h"
 #include "event_queue.h"
-#include "fsm.h"
 #include "gpio.h"
 #include "interrupt.h"
-#include "soft_timer.h"
 #include "status.h"
 
 // Receives an event and sends a CAN message
