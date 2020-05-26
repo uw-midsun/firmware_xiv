f99a34588e6e84fe0f643514448e02cd41f9eb2d
tristanle22 <tristan.tuanle@gmail.com>
2020-05-26 19:13:28 +0000

    Fixed #define name, struct initialization and un-static functions :)

--- projects/counter/inc/counter.h
+++ projects/counter/inc/counter.h
@@ -4,7 +4,7 @@
 #include "soft_timer.h"
 
 // Callback function for counter 1
-static void counter_a_timeout(SoftTimerId timer_id, void *context);
+void counter_a_timeout(SoftTimerId timer_id, void *context);
 
 // Callback function for counter 2
-static void counter_b_timeout(SoftTimerId timer_id, void *context);
+void counter_b_timeout(SoftTimerId timer_id, void *context);
--- projects/counter/src/main.c
+++ projects/counter/src/main.c
@@ -4,40 +4,37 @@
 #include "soft_timer.h"
 #include "wait.h"
 
-#define COUNTER_TIMEOUT_MILI_SECS 500
+#define COUNTER_TIMEOUT_MS 500
 
 typedef struct {
   uint8_t count_a;
   uint8_t count_b;
 } Counters;
 
-static void counter_a_timeout(SoftTimerId timer_id, void *context) {
+void counter_a_timeout(SoftTimerId timer_id, void *context) {
   Counters *counter = context;
   counter->count_a++;
   LOG_DEBUG("Counter A: %d\n", counter->count_a);
 
-  soft_timer_start_millis(COUNTER_TIMEOUT_MILI_SECS, counter_b_timeout, counter, NULL);
+  soft_timer_start_millis(COUNTER_TIMEOUT_MS, counter_b_timeout, counter, NULL);
 }
 
-static void counter_b_timeout(SoftTimerId timer_id, void *context) {
+void counter_b_timeout(SoftTimerId timer_id, void *context) {
   Counters *counter = context;
   counter->count_a++;
   counter->count_b++;
   LOG_DEBUG("Counter A: %d\n", counter->count_a);
   LOG_DEBUG("Counter B: %d\n", counter->count_b);
 
-  soft_timer_start_millis(COUNTER_TIMEOUT_MILI_SECS, counter_a_timeout, counter, NULL);
+  soft_timer_start_millis(COUNTER_TIMEOUT_MS, counter_a_timeout, counter, NULL);
 }
 
 int main(void) {
   interrupt_init();
   soft_timer_init();
 
-  Counters counter = {
-    .count_a = 0,
-    .count_b = 0,
-  };
-  soft_timer_start_millis(COUNTER_TIMEOUT_MILI_SECS, counter_a_timeout, &counter, NULL);
+  Counters counter = { 0 };
+  soft_timer_start_millis(COUNTER_TIMEOUT_MS, counter_a_timeout, &counter, NULL);
 
   while (true) {
     wait();
