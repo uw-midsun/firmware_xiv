c0fe4cc620229f75df941d3206becc982872e264
James Carr-Pries <jcarrpries@gmail.com>
2020-05-27 19:45:21 +0000

    Formatting (Automated commit)

--- projects/hello_world/src/main.c
+++ projects/hello_world/src/main.c
@@ -1,47 +1,46 @@
+#include "delay.h"  // For real-time delays
+#include "interrupt.h"
 #include "log.h"
 #include "soft_timer.h"  // Software timers for scheduling future events.
-#include "delay.h"       // For real-time delays
-#include "interrupt.h"
 #include "wait.h"
 
 typedef struct Counters {
-    uint8_t counter_a;
-    uint8_t counter_b;
-    bool half_second;
+  uint8_t counter_a;
+  uint8_t counter_b;
+  bool half_second;
 } Counters;
 
-void callback(SoftTimerId timer_id, void *context){
-    Counters *counters = context;
+void callback(SoftTimerId timer_id, void *context) {
+  Counters *counters = context;
 
-    if (counters->half_second == true){
-        counters->counter_a++;
-        LOG_DEBUG("Counter A: %i\n", counters->counter_a);
-    } else {
-        counters->counter_a++;
-        counters->counter_b++;
-        LOG_DEBUG("Counter A: %i\n", counters->counter_a);
-        LOG_DEBUG("Counter B: %i\n", counters->counter_b);
-    }
+  if (counters->half_second == true) {
+    counters->counter_a++;
+    LOG_DEBUG("Counter A: %i\n", counters->counter_a);
+  } else {
+    counters->counter_a++;
+    counters->counter_b++;
+    LOG_DEBUG("Counter A: %i\n", counters->counter_a);
+    LOG_DEBUG("Counter B: %i\n", counters->counter_b);
+  }
 
-    counters->half_second = ! counters->half_second;
+  counters->half_second = !counters->half_second;
 
-    soft_timer_start_millis(500, callback, counters, NULL);
-
+  soft_timer_start_millis(500, callback, counters, NULL);
 }
 
 int main(void) {
-    interrupt_init();
-    soft_timer_init();
+  interrupt_init();
+  soft_timer_init();
 
-    Counters counters;
-    counters.counter_a = 0;
-    counters.counter_b = 0;
-    counters.half_second = false;
+  Counters counters;
+  counters.counter_a = 0;
+  counters.counter_b = 0;
+  counters.half_second = false;
 
-    soft_timer_start_millis(500, callback, &counters, NULL);
+  soft_timer_start_millis(500, callback, &counters, NULL);
 
-    while(true){
-        wait();
-    }
-    return 0;
+  while (true) {
+    wait();
+  }
+  return 0;
 }
