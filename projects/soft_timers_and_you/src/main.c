5b15ab7f9a8f2c9707eabf7b90250ad102a3a930
ufshk <ufsheikh@live.ca>
2020-05-30 18:00:56 +0000

    counting with soft timers

---
+++ projects/soft_timers_and_you/src/main.c
@@ -0,0 +1,41 @@
+#include "log.h"
+#include "soft_timer.h"
+#include "interrupt.h"
+#include "wait.h"
+
+#include <stdint.h>
+
+#define CONST_MILLIS_COUNTER 500
+
+typedef struct {
+  uint8_t counter_a; 
+  uint8_t counter_b;
+} Counters; // instructions say to put this in main function fyi
+
+static void prv_counter(SoftTimerId timer_id, void *context) {
+  Counters *counter = context;
+  counter->counter_a++;
+  if (counter->counter_a % 2 == 0) {
+    LOG_DEBUG("Counter A: %i\n", counter->counter_a);
+    counter->counter_b++;
+    LOG_DEBUG("Counter B: %i\n", counter->counter_b);
+    soft_timer_start_millis(CONST_MILLIS_COUNTER, prv_counter, counter, NULL);
+  } else {
+    LOG_DEBUG("Counter A: %i\n", counter->counter_a);
+    soft_timer_start_millis(CONST_MILLIS_COUNTER, prv_counter, counter, NULL);
+  }
+}
+
+int main(void) {
+  interrupt_init();
+  soft_timer_init();
+
+  Counters counters = { 0 };
+  soft_timer_start_millis(CONST_MILLIS_COUNTER, prv_counter, &counters, NULL);
+
+  while (true) {
+    wait();
+  }
+
+  return 0;
+}
