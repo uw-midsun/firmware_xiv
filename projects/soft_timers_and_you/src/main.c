d6f29b288fa3b096e6314816f42b9376a974c98f
ufshk <ufsheikh@live.ca>
2020-06-01 23:37:05 +0000

    changes requested are complete

--- projects/soft_timers_and_you/src/main.c
+++ projects/soft_timers_and_you/src/main.c
@@ -2,24 +2,23 @@
 #include "log.h"
 #include "soft_timer.h"
 #include "wait.h"
-#define CONST_MILLIS_COUNTER 500
+#define CONST_COUNTER_MS 500
 
 typedef struct {
   uint8_t counter_a;
   uint8_t counter_b;
 } Counters;
 
-static void prv_counter(SoftTimerId timer_id, void *context) {
+static void prv_counter_timer_callback(SoftTimerId timer_id, void *context) {
   Counters *counter = context;
   counter->counter_a++;
+  LOG_DEBUG("Counter A: %i\n", counter->counter_a);
   if (counter->counter_a % 2 == 0) {
-    LOG_DEBUG("Counter A: %i\n", counter->counter_a);
     counter->counter_b++;
     LOG_DEBUG("Counter B: %i\n", counter->counter_b);
-    soft_timer_start_millis(CONST_MILLIS_COUNTER, prv_counter, counter, NULL);
+    soft_timer_start_millis(CONST_COUNTER_MS, prv_counter, counter, NULL);
   } else {
-    LOG_DEBUG("Counter A: %i\n", counter->counter_a);
-    soft_timer_start_millis(CONST_MILLIS_COUNTER, prv_counter, counter, NULL);
+    soft_timer_start_millis(CONST_COUNTER_MS, prv_counter, counter, NULL);
   }
 }
 
@@ -28,7 +27,7 @@ int main(void) {
   soft_timer_init();
 
   Counters counters = { 0 };
-  soft_timer_start_millis(CONST_MILLIS_COUNTER, prv_counter, &counters, NULL);
+  soft_timer_start_millis(CONST_COUNTER_MS, prv_counter, &counters, NULL);
 
   while (true) {
     wait();
