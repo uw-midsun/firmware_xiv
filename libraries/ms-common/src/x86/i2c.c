988995a55c24ddbbaf2c49cdcae58067dcc39e3d
AveryChiu64 <a27chiu@uwaterloo.ca>
2020-06-08 23:59:32 +0000

    First commit

--- libraries/ms-common/src/x86/i2c.c
+++ libraries/ms-common/src/x86/i2c.c
@@ -1,23 +1,41 @@
 #include "i2c.h"
-
+#include "log.h"
 StatusCode i2c_init(I2CPort i2c, const I2CSettings *settings) {
+  if (i2c >= NUM_I2C_PORTS) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
+  } else if (settings->speed >= NUM_I2C_SPEEDS) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C speed.");
+  }
+  LOG_DEBUG("Note this is an x86 version of SPI");
   return STATUS_CODE_UNIMPLEMENTED;
 }
 
 StatusCode i2c_read(I2CPort i2c, I2CAddress addr, uint8_t *rx_data, size_t rx_len) {
+  if (i2c >= NUM_I2C_PORTS) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
+  }
   return STATUS_CODE_UNIMPLEMENTED;
 }
 
 StatusCode i2c_write(I2CPort i2c, I2CAddress addr, uint8_t *tx_data, size_t tx_len) {
+  if (i2c >= NUM_I2C_PORTS) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
+  }
   return STATUS_CODE_UNIMPLEMENTED;
 }
 
 StatusCode i2c_read_reg(I2CPort i2c, I2CAddress addr, uint8_t reg, uint8_t *rx_data,
                         size_t rx_len) {
+  if (i2c >= NUM_I2C_PORTS) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
+  }
   return STATUS_CODE_UNIMPLEMENTED;
 }
 
 StatusCode i2c_write_reg(I2CPort i2c, I2CAddress addr, uint8_t reg, uint8_t *tx_data,
                          size_t tx_len) {
+  if (i2c >= NUM_I2C_PORTS) {
+    return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
+  }
   return STATUS_CODE_UNIMPLEMENTED;
 }
