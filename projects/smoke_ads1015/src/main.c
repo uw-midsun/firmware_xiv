b6694ba2ef472618bcdc240c0bcf43b0d69c3f44
tristanle22 <tristan.tuanle@gmail.com>
2020-06-10 22:46:43 +0000

    Addressed PR issues

--- projects/smoke_ads1015/src/main.c
+++ projects/smoke_ads1015/src/main.c
@@ -13,13 +13,14 @@
 
 #define SMOKE_READING_WAIT_MS 1000  // Set wait time between each set of readings
 #define SMOKE_ADS1015_ADDR ADS1015_ADDRESS_GND
-#define SMOKE_ADS1015_I2C_PORT I2C_PORT_1  // I2C_PORT_2
-#define NUM_TEST_CHANNELS 4                // Number of channels to be tested
+#define SMOKE_ADS1015_I2C_PORT I2C_PORT_2  // I2C_PORT_1
 
 static Ads1015Storage s_storage = { 0 };
-static bool READ_CONVERTED = false;
+static bool READ_CONVERTED = true;
 
-// To test a channel, uncomment that channel in this array, has to match NUM_TEST_CHANNELS
+// Number of channels to be tested, MUST match the number of elements in test_channels
+#define NUM_TEST_CHANNELS 4
+// Set of channels to be tested, the number of elements MUST match NUM_TEST_CHANNELS
 const Ads1015Channel test_channels[NUM_TEST_CHANNELS] = { ADS1015_CHANNEL_0, ADS1015_CHANNEL_1,
                                                           ADS1015_CHANNEL_2, ADS1015_CHANNEL_3 };
 
@@ -28,21 +29,21 @@ static void prv_read_and_log(SoftTimerId timer_id, void *context) {
   int16_t reading = 0;
   StatusCode status = STATUS_CODE_OK;
 
-  if (!READ_CONVERTED) {
-    for (size_t i = 0; i < SIZEOF_ARRAY(test_channels) && status == STATUS_CODE_OK; i++) {
-      status = ads1015_read_raw(storage, test_channels[i], &reading);
-      if (status == STATUS_CODE_OK)
-        LOG_DEBUG("Channel: %d; Raw value: %d\n", test_channels[i], reading);
-      else
-        LOG_DEBUG("Fail to take raw reading\n");
-    }
-  } else {
+  if (READ_CONVERTED) {
     for (size_t i = 0; i < SIZEOF_ARRAY(test_channels) && status == STATUS_CODE_OK; i++) {
       status = ads1015_read_converted(storage, test_channels[i], &reading);
       if (status == STATUS_CODE_OK)
         LOG_DEBUG("Channel: %d; Converted value: %d mVolt\n", test_channels[i], reading);
       else
         LOG_DEBUG("Fail to take converted reading\n");
+    }
+  } else {
+    for (size_t i = 0; i < SIZEOF_ARRAY(test_channels) && status == STATUS_CODE_OK; i++) {
+      status = ads1015_read_raw(storage, test_channels[i], &reading);
+      if (status == STATUS_CODE_OK)
+        LOG_DEBUG("Channel: %d; Raw value: %d\n", test_channels[i], reading);
+      else
+        LOG_DEBUG("Fail to take raw reading\n");
     }
   }
   soft_timer_start_millis(SMOKE_READING_WAIT_MS, prv_read_and_log, &s_storage, NULL);
