7b390edaa743870dd66043fbd6033f1aa0a23c44
tristanle22 <tristan.tuanle@gmail.com>
2020-06-10 15:41:35 +0000

    Added iterative reading, modifed make file

--- projects/smoke_ads1015/src/main.c
+++ projects/smoke_ads1015/src/main.c
@@ -1,18 +1,17 @@
 // A simple smoke test for ADS1015
 
 // Periodically take reading from user selected channels and log the result
-// Configuration: channels to be tested, enable conversion, wait time
-
+// Configurable items: channels to be tested, conversion enable, wait time
 #include "ads1015.h"
-#include "ads1015_def.h"
 #include "gpio.h"
 #include "i2c.h"
 #include "interrupt.h"
 #include "log.h"
 #include "soft_timer.h"
+#include "status.h"
 #include "wait.h"
 
-#define SMOKE_READING_WAIT_MS 100  // Set wait time between each set of readings
+#define SMOKE_READING_WAIT_MS 1000  // Set wait time between each set of readings
 #define SMOKE_ADS1015_ADDR ADS1015_ADDRESS_GND
 #define SMOKE_ADS1015_I2C_PORT I2C_PORT_1  // I2C_PORT_2
 #define NUM_TEST_CHANNELS 4                // Number of channels to be tested
@@ -20,26 +19,35 @@
 static Ads1015Storage s_storage = { 0 };
 static bool READ_CONVERTED = false;
 
-// To test a channel, uncomment that channel in this array
+// To test a channel, uncomment that channel in this array, has to match NUM_TEST_CHANNELS
 const Ads1015Channel test_channels[NUM_TEST_CHANNELS] = { ADS1015_CHANNEL_0, ADS1015_CHANNEL_1,
                                                           ADS1015_CHANNEL_2, ADS1015_CHANNEL_3 };
 
 static void prv_read_and_log(SoftTimerId timer_id, void *context) {
   Ads1015Storage *storage = context;
   int16_t reading = 0;
+  StatusCode status = STATUS_CODE_OK;
+
   if (!READ_CONVERTED) {
-    for (size_t i = 0; i < SIZEOF_ARRAY(test_channels); i++) {
-      ads1015_read_raw(storage, test_channels[i], reading);
-      LOG_DEBUG("Channel: %d; Raw value: %d", test_channels[i], reading);
+    for (size_t i = 0; i < SIZEOF_ARRAY(test_channels) && status == STATUS_CODE_OK; i++) {
+      status = ads1015_read_raw(storage, test_channels[i], &reading);
+      if (status == STATUS_CODE_OK)
+        LOG_DEBUG("Channel: %d; Raw value: %d\n", test_channels[i], reading);
+      else
+        LOG_DEBUG("Fail to take raw reading\n");
     }
   } else {
-    for (size_t i = 0; i < SIZEOF_ARRAY(test_channels); i++) {
-      ads1015_read_converted(storage, test_channels[i], reading);
-      LOG_DEBUG("Channel: %d; Converted value: %d mVolt", test_channels[i], reading);
+    for (size_t i = 0; i < SIZEOF_ARRAY(test_channels) && status == STATUS_CODE_OK; i++) {
+      status = ads1015_read_converted(storage, test_channels[i], &reading);
+      if (status == STATUS_CODE_OK)
+        LOG_DEBUG("Channel: %d; Converted value: %d mVolt\n", test_channels[i], reading);
+      else
+        LOG_DEBUG("Fail to take converted reading\n");
     }
   }
   soft_timer_start_millis(SMOKE_READING_WAIT_MS, prv_read_and_log, &s_storage, NULL);
 }
+
 int main(void) {
   gpio_init();
   interrupt_init();
