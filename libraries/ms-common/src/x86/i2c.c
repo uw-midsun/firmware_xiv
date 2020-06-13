2d25a3b00bf145122a57ab323c3768fb0102cb9a
AveryChiu64 <a27chiu@uwaterloo.ca>
2020-06-13 00:49:42 +0000

    Added more I2C functionality on x86

--- libraries/ms-common/src/x86/i2c.c
+++ libraries/ms-common/src/x86/i2c.c
@@ -6,22 +6,34 @@ StatusCode i2c_init(I2CPort i2c, const I2CSettings *settings) {
   } else if (settings->speed >= NUM_I2C_SPEEDS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C speed.");
   }
-  LOG_DEBUG("Note this is an x86 version of SPI");
-  return STATUS_CODE_UNIMPLEMENTED;
+  LOG_DEBUG("Note this is an x86 version of I2C");
+  return STATUS_CODE_OK;
 }
 
 StatusCode i2c_read(I2CPort i2c, I2CAddress addr, uint8_t *rx_data, size_t rx_len) {
   if (i2c >= NUM_I2C_PORTS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
   }
-  return STATUS_CODE_UNIMPLEMENTED;
+  LOG_DEBUG("Reading Data...\n");
+  for (size_t i = 0; i < rx_len; i++) {
+    // Insert dummy data
+    if (i % 2 == 0) {
+      rx_data[i] = 1;
+    }
+    LOG_DEBUG("0x%x\n", rx_data[i]);
+  }
+  return STATUS_CODE_OK;
 }
 
 StatusCode i2c_write(I2CPort i2c, I2CAddress addr, uint8_t *tx_data, size_t tx_len) {
   if (i2c >= NUM_I2C_PORTS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
   }
-  return STATUS_CODE_UNIMPLEMENTED;
+  LOG_DEBUG("Sending Data...\n");
+  for (size_t i = 0; i < tx_len; i++) {
+    LOG_DEBUG("0x%x\n", tx_data[i]);
+  }
+  return STATUS_CODE_OK;
 }
 
 StatusCode i2c_read_reg(I2CPort i2c, I2CAddress addr, uint8_t reg, uint8_t *rx_data,
@@ -29,7 +41,15 @@ StatusCode i2c_read_reg(I2CPort i2c, I2CAddress addr, uint8_t reg, uint8_t *rx_d
   if (i2c >= NUM_I2C_PORTS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
   }
-  return STATUS_CODE_UNIMPLEMENTED;
+  LOG_DEBUG("Reading Register Address...\n");
+  for (size_t i = 0; i < rx_len; i++) {
+    // Insert dummy data
+    if (i % 2 == 0) {
+      rx_data[i] = 1;
+    }
+    LOG_DEBUG("0x%x\n", rx_data[i]);
+  }
+  return STATUS_CODE_OK;
 }
 
 StatusCode i2c_write_reg(I2CPort i2c, I2CAddress addr, uint8_t reg, uint8_t *tx_data,
@@ -37,5 +57,9 @@ StatusCode i2c_write_reg(I2CPort i2c, I2CAddress addr, uint8_t reg, uint8_t *tx_
   if (i2c >= NUM_I2C_PORTS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
   }
-  return STATUS_CODE_UNIMPLEMENTED;
+  LOG_DEBUG("Writing Register Address...\n");
+  for (size_t i = 0; i < tx_len; i++) {
+    LOG_DEBUG("0x%x\n", tx_data[i]);
+  }
+  return STATUS_CODE_OK;
 }
