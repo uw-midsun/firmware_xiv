76dd3fa4eb5012dd36554ce6ed750cb3e02ac73e
Avery Chiu <a27chiu@uwaterloo.ca>
2020-06-14 16:37:29 +0000

    Resolved changes

--- libraries/ms-common/src/x86/i2c.c
+++ libraries/ms-common/src/x86/i2c.c
@@ -1,12 +1,13 @@
 #include "i2c.h"
 #include "log.h"
+
 StatusCode i2c_init(I2CPort i2c, const I2CSettings *settings) {
   if (i2c >= NUM_I2C_PORTS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
   } else if (settings->speed >= NUM_I2C_SPEEDS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C speed.");
   }
-  LOG_DEBUG("Note this is an x86 version of I2C");
+  LOG_DEBUG("Note this is an x86 version of I2C\n");
   return STATUS_CODE_OK;
 }
 
@@ -14,13 +15,10 @@ StatusCode i2c_read(I2CPort i2c, I2CAddress addr, uint8_t *rx_data, size_t rx_le
   if (i2c >= NUM_I2C_PORTS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
   }
-  LOG_DEBUG("Reading Data...\n");
+  LOG_DEBUG("Reading %ld bytes from I2C\n", rx_len);
   for (size_t i = 0; i < rx_len; i++) {
     // Insert dummy data
-    if (i % 2 == 0) {
-      rx_data[i] = 1;
-    }
-    LOG_DEBUG("0x%x\n", rx_data[i]);
+    rx_data[i] = i % 2;
   }
   return STATUS_CODE_OK;
 }
@@ -29,10 +27,11 @@ StatusCode i2c_write(I2CPort i2c, I2CAddress addr, uint8_t *tx_data, size_t tx_l
   if (i2c >= NUM_I2C_PORTS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
   }
-  LOG_DEBUG("Sending Data...\n");
+  LOG_DEBUG("Sending %ld bytes over I2C\n", tx_len);
   for (size_t i = 0; i < tx_len; i++) {
-    LOG_DEBUG("0x%x\n", tx_data[i]);
+    LOG_DEBUG("0x%x", tx_data[i]);
   }
+  LOG_DEBUG("\n");
   return STATUS_CODE_OK;
 }
 
@@ -41,13 +40,10 @@ StatusCode i2c_read_reg(I2CPort i2c, I2CAddress addr, uint8_t reg, uint8_t *rx_d
   if (i2c >= NUM_I2C_PORTS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
   }
-  LOG_DEBUG("Reading Register Address...\n");
+  LOG_DEBUG("Reading %ld bytes from register %d over I2C\n", rx_len, reg);
   for (size_t i = 0; i < rx_len; i++) {
     // Insert dummy data
-    if (i % 2 == 0) {
-      rx_data[i] = 1;
-    }
-    LOG_DEBUG("0x%x\n", rx_data[i]);
+    rx_data[i] = i % 2;
   }
   return STATUS_CODE_OK;
 }
@@ -57,9 +53,10 @@ StatusCode i2c_write_reg(I2CPort i2c, I2CAddress addr, uint8_t reg, uint8_t *tx_
   if (i2c >= NUM_I2C_PORTS) {
     return status_msg(STATUS_CODE_INVALID_ARGS, "Invalid I2C port.");
   }
-  LOG_DEBUG("Writing Register Address...\n");
+  LOG_DEBUG("Writing %ld bytes from register %d over I2C\n", tx_len, reg);
   for (size_t i = 0; i < tx_len; i++) {
-    LOG_DEBUG("0x%x\n", tx_data[i]);
+    LOG_DEBUG("0x%x", tx_data[i]);
   }
+  LOG_DEBUG("\n");
   return STATUS_CODE_OK;
 }
