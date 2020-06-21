c7b9a7c5e74adc33daec7f933e200ba50b86e22e
Ryan Dancy <ryan@keal.ca>
2020-06-21 21:50:40 +0000

    [SOFT-208] Add basic sense_current callback implementation

--- projects/solar/src/sense_current.c
+++ projects/solar/src/sense_current.c
@@ -1,15 +1,32 @@
 #include "sense_current.h"
+#include "data_store.h"
+#include "log.h"
 #include "mcp3427_adc.h"
 #include "sense.h"
 #include "solar_config.h"
 
-static Mcp3427Storage s_mcp3427_storage;
+// we only use one channel on the MCP3427
+#define CURRENT_MCP3427_CHANNEL MCP3427_CHANNEL_1
 
-static void prv_mcp3427_callback(int16_t value_ch1, int16_t value_ch2, void *context) {}
+static Mcp3427Storage s_mcp3427_storage;
+static int16_t s_value;
+static bool s_has_value;  // to prevent sending uninitialized garbage to data_store
+
+static void prv_mcp3427_callback(int16_t value_ch1, int16_t value_ch2, void *context) {
+  s_value = (CURRENT_MCP3427_CHANNEL == MCP3427_CHANNEL_1) ? value_ch1 : value_ch2;
+  s_has_value = true;
+}
 
 static void prv_mcp3427_fault_callback(void *context) {}
 
-static void prv_sense_cycle_callback(void *context) {}
+static void prv_sense_cycle_callback(void *context) {
+  if (s_has_value) {
+    StatusCode status = data_store_set(DATA_POINT_CURRENT, (uint16_t)s_value);
+    if (!status_ok(status)) {
+      LOG_CRITICAL("sense_current could not data_store_set with DATA_POINT_CURRENT\n");
+    }
+  }
+}
 
 StatusCode sense_current_init(void) {
   // initialize the MCP3427
@@ -20,5 +37,6 @@ StatusCode sense_current_init(void) {
   status_ok_or_return(mcp3427_start(&s_mcp3427_storage));
 
   status_ok_or_return(sense_register(prv_sense_cycle_callback, NULL));
+  s_has_value = false;
   return STATUS_CODE_OK;
 }
