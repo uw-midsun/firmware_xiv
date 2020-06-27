47f2a061d910412a24f9dc77e1bfb83a66b796a9
JARVIS <jarvisweng@hotmail.com>
2020-06-27 15:04:16 -0400

    fix merge conflicts

--- projects/solar/rules.mk
+++ projects/solar/rules.mk
@@ -9,3 +9,7 @@
 $(T)_DEPS := ms-common ms-drivers
 
 $(T)_test_mppt_MOCKS := mux_set
+
+$(T)_test_sense_MOCKS := data_store_done
+$(T)_test_sense_current_MOCKS := data_store_set sense_register mcp3427_register_callback \
+	mcp3427_register_fault_callback mcp3427_start
