478d724bb4f4293ab8a40b39b9917c0edc9b14e6
max-mz <max.zhu.sss@gmail.com>
2020-06-29 00:10:57 +0000

    draft pr comments, changed fan inputs

--- libraries/ms-drivers/inc/adt_7476a_fan_controller.h
+++ libraries/ms-drivers/inc/adt_7476a_fan_controller.h
@@ -1,16 +1,11 @@
 #pragma once
 
-#include "adc.h"
-#include "gpio.h"
-#include "gpio_it.h"
-#include "i2c.h"
-#include "interrupt.h"
-#include "soft_timer.h"
+// Requires interrupts, soft timers, GPIO, and GPIO interrupts to be initialized
 
 typedef void (*Adt7476ADataCallback)(void *context);
 
 typedef struct {
-  GpioAddress *smbalert_pin;
+  GpioAddress smbalert_pin;
   uint32_t interval_ms;
   Adt7476ADataCallback callback;  // set to NULL for no callback
   void *callback_context;
@@ -21,23 +16,23 @@ typedef struct {
 } Adt7476aSettings;
 
 typedef struct {
-  GpioAddress *smbalert_pin;
+  GpioAddress smbalert_pin;
   uint32_t interval_ms;
   SoftTimerId timer_id;
   Adt7476ADataCallback callback;
   void *callback_context;
   I2CPort i2c;
-  I2CAddress i2c_read_addr;
-  I2CAddress i2c_write_addr;
-  I2CSettings i2c_settings;
 } Adt7476aStorage;
 
-// probably a better name for these
-typedef enum { FAN_1 = 0, FAN_2, FAN_3, FAN_4 } FAN_NUM;
+// 2 pwm outputs, each controlling 2 fans
+typedef enum { ADT_FAN_GROUP_1, ADT_FAN_GROUP_2, NUM_FANS } FAN_GROUP;
 
 // Initialize the Adt7476a with the given settings; the select pin is an STM32 GPIO pin.
 StatusCode adt7476a_init(Adt7476aStorage *storage, Adt7476aSettings *settings);
 
 // Translate and write the new speed
-StatusCode adt7476a_set_speed(I2CPort port, uint8_t speed, uint8_t FAN_PWM_ADDR,
-                              uint8_t ADR7470_I2C_ADDRESS);
+StatusCode adt7476a_set_speed(I2CPort port, uint8_t speed, FAN_GROUP fan_group,
+                              uint8_t adt_7476a_i2c_addr);
+
+StatusCode get_status(I2CPort port, uint8_t adt_7476a_i2c_addr, uint8_t *register_1_data,
+                      uint8_t *register_2_data);
--- libraries/ms-drivers/inc/adt_7476a_fan_controller_defs.h
+++ libraries/ms-drivers/inc/adt_7476a_fan_controller_defs.h
@@ -10,18 +10,15 @@
 #define ADT7476A_INTERRUPT_STATUS_REGISTER_1 0x41
 #define ADT7476A_INTERRUPT_STATUS_REGISTER_2 0x42
 
-#define ADT7470_FAN_MODE_REGISTER_1 0x68  // bits 6 and 7 contorl the mode for PWM2 and PWM1
-#define ADT7470_FAN_MODE_REGISTER_2 0x69  // bits 6 and 7 contorl the mode for PWM4 and PWM3
-
 #define ADT7476A_FAN_MODE_REGISTER_1 0x5C  // bits 7:5 control mode - 111 for manual
 #define ADT7476A_FAN_MODE_REGISTER_3 0x5E  // bits 7:5 control mode - 111 for manual
 
 #define ADT7476A_INTERRUPT_MASK_REGISTER_1 0x74
 #define ADT7476A_INTERRUPT_MASK_REGISTER_2 0x75
 
-#define ADT7476A_PWM_1 \
-  0x30  // default 0xFF, recommend using 1 and 3 as PWM outputs so that pwm2 can be used for TACH3
-#define ADT7476A_PWM_3 0x32  //
+// default 0xFF, recommend using 1 and 3 as PWM outputs so that pwm2 can be used for TACH3
+#define ADT7476A_PWM_1 0x30
+#define ADT7476A_PWM_3 0x32
 
 #define ADT7476A_TACH_1_LOW 0x28   // default 0xFF
 #define ADT7476A_TACH_1_HIGH 0x29  // default 0xFF
@@ -35,10 +32,5 @@
 #define ADT7476A_MANUAL_MODE_MASK 0b11100010
 #define ADT7476A_CONFIG_REG_3_MASK 0b00000001
 
-#define NUM_GPIO_FAN_PINS \
-  4  // The ADT7470 has four pins that can be configured as either general-purpose logic pins or as
-     // PWM outputs
-
-#define NUM_BYTES_TO_READ 1
-#define NUM_BYTES_TO_WRITE 1
+#define ADT7476A_REG_SIZE 1
 #define SET_SPEED_NUM_BYTES 1
