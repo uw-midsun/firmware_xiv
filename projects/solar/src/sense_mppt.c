176eaee19ff0622d914570ef2f8c086e3cc07de0
Ryan Dancy <ryan@keal.ca>
2020-07-05 19:32:47 +0000

    [SOFT-211] Restructure sense_mppt

--- projects/solar/src/sense_mppt.c
+++ projects/solar/src/sense_mppt.c
@@ -13,34 +13,7 @@
 static SpiPort s_spi_port;
 static uint8_t s_indices[MAX_SOLAR_BOARD_MPPTS];
 
-static void prv_sense_cycle_callback(void *context) {
-  uint8_t mppt = *(uint8_t *)context;
-
-  uint16_t current, vin, pwm;
-  uint8_t status;
-  if (!status_ok(mppt_read_current(s_spi_port, &current, mppt))) {
-    LOG_WARN("Error reading current from MPPT %d\n", mppt);
-    return;
-  }
-  if (!status_ok(mppt_read_voltage_in(s_spi_port, &vin, mppt))) {
-    LOG_WARN("Error reading voltage from MPPT %d\n", mppt);
-    return;
-  }
-  if (!status_ok(mppt_read_pwm(s_spi_port, &pwm, mppt))) {
-    LOG_WARN("Error reading PWM from MPPT %d\n", mppt);
-    return;
-  }
-  if (!status_ok(mppt_read_status(s_spi_port, &status, mppt))) {
-    LOG_WARN("Error reading status from MPPT %d\n", mppt);
-    return;
-  }
-
-  // this is almost certainly not the best way to get the |mppt|th data point
-  data_store_set(DATA_POINT_MPPT_CURRENT_1 + mppt, current);
-  data_store_set(DATA_POINT_MPPT_VOLTAGE_1 + mppt, vin);
-  data_store_set(DATA_POINT_MPPT_PWM_1 + mppt, pwm);
-  data_store_set(DATA_POINT_CR_BIT_1 + mppt, spv1020_is_cr_bit_set(status));
-
+static void prv_check_status_for_faults(uint8_t mppt, uint8_t status) {
   uint8_t ovc_branch_bitmask;
   if (spv1020_is_overcurrent(status, &ovc_branch_bitmask)) {
     uint16_t ovc_data = (ovc_branch_bitmask << 8) | mppt;
@@ -51,6 +24,38 @@ static void prv_sense_cycle_callback(void *context) {
   }
   if (spv1020_is_overtemperature(status)) {
     event_raise(SOLAR_FAULT_EVENT_MPPT_OVERTEMPERATURE, mppt);
+  }
+}
+
+static void prv_sense_cycle_callback(void *context) {
+  uint8_t mppt = *(uint8_t *)context;
+
+  uint16_t current, vin, pwm;
+  uint8_t status;
+
+  if (status_ok(mppt_read_current(s_spi_port, &current, mppt))) {
+    data_store_set(DATA_POINT_MPPT_CURRENT(mppt), current);
+  } else {
+    LOG_WARN("Error reading current from MPPT %d\n", mppt);
+  }
+
+  if (status_ok(mppt_read_voltage_in(s_spi_port, &vin, mppt))) {
+    data_store_set(DATA_POINT_MPPT_VOLTAGE(mppt), vin);
+  } else {
+    LOG_WARN("Error reading voltage from MPPT %d\n", mppt);
+  }
+
+  if (status_ok(mppt_read_pwm(s_spi_port, &pwm, mppt))) {
+    data_store_set(DATA_POINT_MPPT_PWM(mppt), pwm);
+  } else {
+    LOG_WARN("Error reading PWM from MPPT %d\n", mppt);
+  }
+
+  if (status_ok(mppt_read_status(s_spi_port, &status, mppt))) {
+    data_store_set(DATA_POINT_CR_BIT(mppt), spv1020_is_cr_bit_set(status));
+    prv_check_status_for_faults(mppt, status);
+  } else {
+    LOG_WARN("Error reading status from MPPT %d\n", mppt);
   }
 }
 
