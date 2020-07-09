ff2f1c231cba116c9df45e8fa37b5ff0bca5d21c
Ryan Dancy <ryan@keal.ca>
2020-07-09 02:39:45 +0000

    [SOFT-209] Resolve incompatibilities in sense_mppt

--- projects/solar/inc/solar_config.h
+++ projects/solar/inc/solar_config.h
@@ -3,12 +3,7 @@
 // Provides all hardware settings and configuration for solar.
 
 #include "sense_mcp3427.h"
-
-typedef enum SolarMpptCount {
-  SOLAR_BOARD_5_MPPTS = 5,
-  SOLAR_BOARD_6_MPPTS = 6,
-  MAX_SOLAR_BOARD_MPPTS = 6,
-} SolarMpptCount;
+#include "solar_boards.h"
 
 StatusCode config_get_sense_mcp3427_settings(SolarMpptCount mppt_count,
                                              SenseMcp3427Settings *settings);
