f296ed3d97af6c839a9180e214b3077be3ff80b3
Hewitt McGaughey <58054834+hewittmcg@users.noreply.github.com>
2020-07-09 20:18:58 -0400

    [SOFT-189] DRV120 relay driver (#123)
    * added files for driver, test sequence, and formatted
    
    * changed pin used in test to correct value, clarified variable name
    
    * made requested changes, added some comments to increase clarity
    
    * made requested changes: formatting/clarity
    
    Co-authored-by: Ryan Dancy <41841896+copperium@users.noreply.github.com>

---
+++ libraries/ms-drivers/src/drv120_relay.c
@@ -0,0 +1,26 @@
+#include "drv120_relay.h"
+
+// Storage for EN pin on DRV120 to open/close relay
+static GpioAddress s_relay_pin;
+
+StatusCode drv120_relay_init(GpioAddress *pin) {
+  s_relay_pin = *pin;
+
+  // Pin high on initialization
+  GpioSettings drv120_pin_settings = {
+    .direction = GPIO_DIR_OUT,
+    .state = GPIO_STATE_HIGH,
+    .resistor = GPIO_RES_NONE,
+    .alt_function = GPIO_ALTFN_NONE,
+  };
+
+  return gpio_init_pin(&s_relay_pin, &drv120_pin_settings);
+}
+
+StatusCode drv120_relay_close(void) {
+  return gpio_set_state(&s_relay_pin, GPIO_STATE_HIGH);
+}
+
+StatusCode drv120_relay_open(void) {
+  return gpio_set_state(&s_relay_pin, GPIO_STATE_LOW);
+}
