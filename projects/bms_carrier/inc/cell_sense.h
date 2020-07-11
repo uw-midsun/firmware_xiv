a532ad157ac21d137f5a7acf450e20c0bc80a330
Gerald Aryeetey <aryeeteygerald@rogers.com>
2020-07-11 04:59:23 +0000

    Apply autoformat fixes

--- projects/bms_carrier/inc/cell_sense.h
+++ projects/bms_carrier/inc/cell_sense.h
@@ -3,9 +3,9 @@
 #include <stdint.h>
 
 #include "event_queue.h"
+#include "ltc_afe.h"
 #include "spi.h"
 #include "status.h"
-#include "ltc_afe.h"
 
 // Requires ltc_afe to be initialized
 
@@ -24,8 +24,8 @@ typedef struct CellSenseSettings {
 
 typedef struct AfeReadings {
   // TODO(SOFT-9): total_voltage used to be stored here as well
-  uint16_t voltages[NUM_AFES*NUM_CELL_MODULES_PER_AFE];
-  uint16_t temps[NUM_AFES*NUM_THERMISTORS_PER_AFE];
+  uint16_t voltages[NUM_AFES * NUM_CELL_MODULES_PER_AFE];
+  uint16_t temps[NUM_AFES * NUM_THERMISTORS_PER_AFE];
 } AfeReadings;
 
 typedef struct CellSenseStorage {
@@ -35,6 +35,7 @@ typedef struct CellSenseStorage {
   CellSenseSettings settings;
 } CellSenseStorage;
 
-StatusCode cell_sense_init(const CellSenseSettings *settings, AfeReadings *readings, LtcAfeStorage *afe);
+StatusCode cell_sense_init(const CellSenseSettings *settings, AfeReadings *readings,
+                           LtcAfeStorage *afe);
 
 StatusCode cell_sense_process_event(const Event *e);
--- projects/bms_carrier/src/cell_sense.c
+++ projects/bms_carrier/src/cell_sense.c
@@ -19,7 +19,8 @@ static void prv_extract_cell_result(uint16_t *result_arr, size_t len, void *cont
   bool fault = false;
   for (size_t i = 0; i < len; i++) {
     // s_storage.total_voltage += s_storage.readings->voltages[i];
-    if (s_storage.readings->voltages[i] < s_storage.settings.undervoltage_dmv || s_storage.readings->voltages[i] > s_storage.settings.overvoltage_dmv) {
+    if (s_storage.readings->voltages[i] < s_storage.settings.undervoltage_dmv ||
+        s_storage.readings->voltages[i] > s_storage.settings.overvoltage_dmv) {
       fault = true;
     }
   }
@@ -36,7 +37,7 @@ static void prv_extract_aux_result(uint16_t *result_arr, size_t len, void *conte
 
   uint16_t threshold = s_storage.settings.discharge_overtemp_dmv;
   if (current_sense_is_charging()) threshold = s_storage.settings.charge_overtemp_dmv;
-  
+
   for (size_t i = 0; i < len; ++i) {
     if (s_storage.readings->temps[i] > threshold) {
       fault_bps(EE_BPS_FAULT_SOURCE_CURRENT_SENSE_AFE_TEMP, false);
@@ -47,7 +48,8 @@ static void prv_extract_aux_result(uint16_t *result_arr, size_t len, void *conte
   fault_bps(EE_BPS_FAULT_SOURCE_CURRENT_SENSE_AFE_TEMP, true);
 }
 
-StatusCode cell_sense_init(const CellSenseSettings *settings, AfeReadings* afe_readings, LtcAfeStorage *afe) {
+StatusCode cell_sense_init(const CellSenseSettings *settings, AfeReadings *afe_readings,
+                           LtcAfeStorage *afe) {
   s_storage.afe = afe;
   s_storage.readings = afe_readings;
   memset(afe_readings, 0, sizeof(AfeReadings));
@@ -57,24 +59,23 @@ StatusCode cell_sense_init(const CellSenseSettings *settings, AfeReadings* afe_r
 }
 
 StatusCode cell_sense_process_event(const Event *e) {
-  switch (e->id)
-  {
-  case BMS_AFE_EVENT_FAULT:
-    // TODO(SOFT-9): Logic about when to trigger a fault could be exported to fault_bps
-    if (s_storage.num_afe_faults > MAX_AFE_FAULTS) {
-      fault_bps(EE_BPS_FAULT_SOURCE_CURRENT_SENSE_AFE_FSM, false);
-    }
-    s_storage.num_afe_faults++;
-    ltc_afe_request_cell_conversion(s_storage.afe);
-    break;
+  switch (e->id) {
+    case BMS_AFE_EVENT_FAULT:
+      // TODO(SOFT-9): Logic about when to trigger a fault could be exported to fault_bps
+      if (s_storage.num_afe_faults > MAX_AFE_FAULTS) {
+        fault_bps(EE_BPS_FAULT_SOURCE_CURRENT_SENSE_AFE_FSM, false);
+      }
+      s_storage.num_afe_faults++;
+      ltc_afe_request_cell_conversion(s_storage.afe);
+      break;
 
-  case BMS_AFE_EVENT_CALLBACK_RUN:
-    s_storage.num_afe_faults = 0;
-    fault_bps(EE_BPS_FAULT_SOURCE_CURRENT_SENSE_AFE_FSM, true);
-    break;
+    case BMS_AFE_EVENT_CALLBACK_RUN:
+      s_storage.num_afe_faults = 0;
+      fault_bps(EE_BPS_FAULT_SOURCE_CURRENT_SENSE_AFE_FSM, true);
+      break;
 
-  default:
-    return status_code(STATUS_CODE_EMPTY);
+    default:
+      return status_code(STATUS_CODE_EMPTY);
   }
 
   return STATUS_CODE_OK;
