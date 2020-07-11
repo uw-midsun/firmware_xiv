6234967a4674e0d9a72131c517251506ed694b91
JarvisWeng <jarvis0910@gmail.com>
2020-07-11 13:25:42 -0400

    finished smoke test

--- projects/smoke_spv1020/src/main.c
+++ projects/smoke_spv1020/src/main.c
@@ -7,24 +7,47 @@
 #include "gpio_it.h"
 #include "interrupt.h"
 #include "log.h"
+#include "mppt.h"
 #include "soft_timer.h"
 #include "spi.h"
-#include "mppt.h"
 
-#define TEST_SPI_PORT SPI_PORT_2
+#define SPI_PORT SPI_PORT_2
+#define SPV1020 0
 
-#define TEST_BAUDRATE 60000
-#define TEST_MOSI_PIN \
+#define SMOKETEST_WAIT_TIME_MS 1000
+
+#define BAUDRATE 60000
+#define MOSI_PIN \
   { .port = GPIO_PORT_B, 15 }
-#define TEST_MISO_PIN \
+#define MISO_PIN \
   { .port = GPIO_PORT_B, 14 }
-#define TEST_SCLK_PIN \
+#define SCLK_PIN \
   { .port = GPIO_PORT_B, 13 }
-#define TEST_CS_PIN \
+#define CS_PIN \
   { .port = GPIO_PORT_B, 12 }
 
+uint8_t status = 0xFF;      // 8th bit is set
+uint16_t pwm = 0xFFFF;      // over 900
+uint16_t vin = 0xFFFF;      // over 10 bits
+uint16_t current = 0xFFFF;  // over 10 bits
+
 static void prv_spv1020_check(SoftTimerId timer_id, void *context) {
-  
+  status = 0xFF;
+  pwm = 0xFFFF;
+  vin = 0xFFFF;
+  current = 0xFFFF;
+  mppt_turn_on(SPI_PORT, SPV1020);
+
+  mppt_read_status(SPI_PORT, &status, SPV1020);
+  LOG_DEBUG("SPV1020 # %d status is: %x\r\n", SPV1020, status);
+  mppt_read_pwm(SPI_PORT, &pwm, SPV1020);
+  LOG_DEBUG("SPV1020 # %d pwm is: %x\r\n", SPV1020, pwm);
+  mppt_read_voltage_in(SPI_PORT, &vin, SPV1020);
+  LOG_DEBUG("SPV1020 # %d status is: %x\r\n", SPV1020, vin);
+  mppt_read_current(SPI_PORT, &current, SPV1020);
+  LOG_DEBUG("SPV1020 # %d status is: %x\r\n", SPV1020, current);
+
+  mppt_shut(SPI_PORT, SPV1020);
 }
 
 int main(void) {
@@ -37,19 +60,19 @@ int main(void) {
   SpiSettings spi_settings = {
     .baudrate = 60000,
     .mode = SPI_MODE_3,
-    .mosi = TEST_MOSI_PIN,
-    .miso = TEST_MISO_PIN,
-    .sclk = TEST_SCLK_PIN,
-    .cs = TEST_CS_PIN,
+    .mosi = MOSI_PIN,
+    .miso = MISO_PIN,
+    .sclk = SCLK_PIN,
+    .cs = CS_PIN,
   };
-  spi_init(TEST_SPI_PORT, &spi_settings);
+  spi_init(SPI_PORT, &spi_settings);
 
   mppt_init();
 
   LOG_DEBUG("Initializing spv1020 smoke test\n");
 
   Event e = { 0 };
-  soft_timer_start_millis(500, prv_spv1020_check, NULL, NULL);
+  soft_timer_start_millis(SMOKETEST_WAIT_TIME_MS, prv_spv1020_check, NULL, NULL);
   while (true) {
     while (event_process(&e) != STATUS_CODE_OK) {
     }
