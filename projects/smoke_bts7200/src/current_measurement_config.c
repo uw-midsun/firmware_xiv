43c3fe0b8c13d4a3365376a916971f07f92f37e1
tristanle22 <tristan.tuanle@gmail.com>
2020-07-12 17:43:02 +0000

    [soft-224] - Clean up unused code and addressed PR comments

--- projects/smoke_bts7200/src/current_measurement_config.c
+++ projects/smoke_bts7200/src/current_measurement_config.c
@@ -17,80 +17,53 @@ const PowerDistributionCurrentHardwareConfig FRONT_POWER_DISTRIBUTION_CURRENT_HW
       (PowerDistributionBts7200Data[]){
           {
               .dsel_pin = FRONT_PIN_LEFT_RIGHT_CAM_DSEL,
-              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_LEFT_CAMERA,
-              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_RIGHT_CAMERA,
+              //   .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_LEFT_CAMERA,
+              //   .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_RIGHT_CAMERA,
               .mux_selection = 12,
           },
           {
               .dsel_pin = FRONT_PIN_DVR_REAR_DISP_DSEL,
-              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_DRIVER_DISPLAY,
-              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_REAR_DISPLAY,
+              //   .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_DRIVER_DISPLAY,
+              //   .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_REAR_DISPLAY,
               .mux_selection = 0,
           },
           {
               .dsel_pin = FRONT_PIN_L_R_DVR_DISP_DSEL,
-              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_LEFT_DRIVER_DISPLAY,
-              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_RIGHT_DRIVER_DISPLAY,
+              //   .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_LEFT_DRIVER_DISPLAY,
+              //   .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_RIGHT_DRIVER_DISPLAY,
               .mux_selection = 4,
           },
           {
               .dsel_pin = FRONT_PIN_FRONT_TURN_LIGHT_DSEL,
-              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_LEFT_FRONT_TURN_LIGHT,
-              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_RIGHT_FRONT_TURN_LIGHT,
+              //   .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_LEFT_FRONT_TURN_LIGHT,
+              //   .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_RIGHT_FRONT_TURN_LIGHT,
               .mux_selection = 9,
           },
           {
               .dsel_pin = FRONT_PIN_MAIN_REAR_PI_DSEL,
-              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_MAIN_PI,
-              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_REAR_PI,
+              //   .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_MAIN_PI,
+              //   .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_REAR_PI,
               .mux_selection = 1,
           },
           {
               .dsel_pin = FRONT_PIN_5V_SPARE_DSEL,
-              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_5V_SPARE_1,
-              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_5V_SPARE_2,
+              //   .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_5V_SPARE_1,
+              //   .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_5V_SPARE_2,
               .mux_selection = 2,
           },
           {
               .dsel_pin = FRONT_PIN_SPARE_2_CTR_CONSL_DSEL,
-              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_SPARE_2,
-              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_CENTRE_CONSOLE,
+              //   .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_SPARE_2,
+              //   .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_CENTRE_CONSOLE,
               .mux_selection = 7,
           },
           {
               .dsel_pin = FRONT_PIN_STR_PDL_DSEL,
-              .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_STEERING,
-              .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_PEDAL,
+              //   .current_0 = FRONT_POWER_DISTRIBUTION_CURRENT_STEERING,
+              //   .current_1 = FRONT_POWER_DISTRIBUTION_CURRENT_PEDAL,
               .mux_selection = 6,
           } },
   .num_bts7200_channels = 8,
-  .bts7040s =
-      (PowerDistributionBts7040Data[]){
-          {
-              .current = FRONT_POWER_DISTRIBUTION_CURRENT_MAIN_DISPLAY,
-              .mux_selection = 13,
-          },
-          {
-              .current = FRONT_POWER_DISTRIBUTION_CURRENT_DAYTIME_RUNNING_LIGHTS,
-              .mux_selection = 8,
-          },
-          {
-              .current = FRONT_POWER_DISTRIBUTION_CURRENT_PARKING_BRAKE,
-              .mux_selection = 10,
-          },
-          {
-              .current = FRONT_POWER_DISTRIBUTION_CURRENT_SPARE_1,
-              .mux_selection = 11,
-          },
-          {
-              .current = FRONT_POWER_DISTRIBUTION_CURRENT_HORN,
-              .mux_selection = 3,
-          },
-          {
-              .current = FRONT_POWER_DISTRIBUTION_CURRENT_SPEAKER,
-              .mux_selection = 5,
-          } },
-  .num_bts7040_channels = 6,
   .mux_address =
       {
           .bit_width = 4,
@@ -120,82 +93,54 @@ const PowerDistributionCurrentHardwareConfig REAR_POWER_DISTRIBUTION_CURRENT_HW_
       (PowerDistributionBts7200Data[]){
           {
               .dsel_pin = REAR_PIN_SOLAR_TELEMETRY_DSEL,
-              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_SOLAR_SENSE,
-              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_TELEMETRY,
+              //   .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_SOLAR_SENSE,
+              //   .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_TELEMETRY,
               .mux_selection = 7,
           },
           {
               .dsel_pin = REAR_PIN_STROBE_CTR_BRK_DSEL,
-              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_STROBE,
-              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_CENTRE_BRAKE_LIGHT,
+              //   .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_STROBE,
+              //   .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_CENTRE_BRAKE_LIGHT,
               .mux_selection = 9,
           },
           {
               .dsel_pin = REAR_PIN_REAR_TURN_LIGHT_DSEL,
-              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_LEFT_REAR_TURN_LIGHT,
-              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_RIGHT_REAR_TURN_LIGHT,
+              //   .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_LEFT_REAR_TURN_LIGHT,
+              //   .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_RIGHT_REAR_TURN_LIGHT,
               .mux_selection = 0,
           },
           {
               .dsel_pin = REAR_PIN_REAR_BRAKE_LIGHT_DSEL,
-              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_LEFT_BRAKE_LIGHT,
-              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_RIGHT_BRAKE_LIGHT,
+              //   .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_LEFT_BRAKE_LIGHT,
+              //   .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_RIGHT_BRAKE_LIGHT,
               .mux_selection = 4,
           },
           {
               .dsel_pin = REAR_PIN_CAM_SPARE_10_DSEL,
-              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_REAR_CAMERA,
-              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_10,
+              //   .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_REAR_CAMERA,
+              //   .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_10,
               .mux_selection = 12,
           },
           {
               .dsel_pin = REAR_PIN_SPARE_1_2_DSEL,
-              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_1,
-              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_2,
+              //   .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_1,
+              //   .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_2,
               .mux_selection = 1,
           },
           {
               .dsel_pin = REAR_PIN_SPARE_3_4_DSEL,
-              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_3,
-              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_4,
+              //   .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_3,
+              //   .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_4,
               .mux_selection = 2,
           },
           {
               .dsel_pin = REAR_PIN_SPARE_8_9_DSEL,
-              .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_8,
-              .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_9,
+              //   .current_0 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_8,
+              //   .current_1 = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_9,
               .mux_selection = 6,
           },
       },
   .num_bts7200_channels = 8,
-  .bts7040s =
-      (PowerDistributionBts7040Data[]){
-          {
-              .current = REAR_POWER_DISTRIBUTION_CURRENT_BMS_CARRIER,
-              .mux_selection = 8,
-          },
-          {
-              .current = REAR_POWER_DISTRIBUTION_CURRENT_MCI,
-              .mux_selection = 13,
-          },
-          {
-              .current = REAR_POWER_DISTRIBUTION_CURRENT_CHARGER,
-              .mux_selection = 11,
-          },
-          {
-              .current = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_5,
-              .mux_selection = 10,
-          },
-          {
-              .current = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_6,
-              .mux_selection = 3,
-          },
-          {
-              .current = REAR_POWER_DISTRIBUTION_CURRENT_SPARE_7,
-              .mux_selection = 5,
-          },
-      },
-  .num_bts7040_channels = 6,
   .mux_address =
       {
           .bit_width = 4,
--- projects/smoke_bts7200/src/main.c
+++ projects/smoke_bts7200/src/main.c
@@ -3,8 +3,8 @@
 // Periodically take reading from user selected channels and log the result
 // Configurable items: wait time, FRONT or REAR power distro selection, channels to be tested
 #include "bts_7200_current_sense.h"
-#include "current_measurement.h"
 #include "current_measurement_config.h"
+#include "pca9539r_gpio_expander.h"
 
 #include "gpio.h"
 #include "interrupt.h"
@@ -17,23 +17,25 @@
 #define CURRENT_MEASURE_INTERVAL_MS 500  // Set wait time between each set of readings
 #define IS_FRONT_POWER_DISTRO true       // Set whether to test FRONT or REAR power distro
 
-// Number of channels to be tested, MUST match the number of elements in test_channels
-#define NUM_TEST_CHANNELS 8
-// Set of channels to be tested, the number of elements MUST match NUM_TEST_CHANNELS.
-// Range of channels: 0-7
-static uint8_t test_channels[NUM_TEST_CHANNELS] = { 0, 1, 2, 3, 4, 5, 6, 7 };
+// Maximum number of channels that can be tested,
+// which is the same number of bts7200 used in front/rear power distribution
+#define MAX_TEST_CHANNELS 8
+// Set of channels to be tested, the array contains the indices of the BTS7200 in the front/rear HW
+// config Details at
+// https://uwmidsun.atlassian.net/wiki/spaces/ELEC/pages/1419740028/BTS7200+smoke+test+user+guide
+static uint8_t s_test_channels[] = { 0, 1, 2, 3, 4, 5, 6, 7 };
 
-static Bts7200Storage s_bts7200_storages[NUM_TEST_CHANNELS];
+static Bts7200Storage s_bts7200_storages[MAX_TEST_CHANNELS];
 
 static void prv_read_and_log(SoftTimerId timer_id, void *context) {
   PowerDistributionCurrentHardwareConfig *s_hw_config = context;
   uint16_t current_0, current_1;
 
-  for (uint8_t i = 0; i < SIZEOF_ARRAY(test_channels); i++) {
-    mux_set(&s_hw_config->mux_address, s_hw_config->bts7200s[test_channels[i]].mux_selection);
+  for (uint8_t i = 0; i < SIZEOF_ARRAY(s_test_channels); i++) {
+    mux_set(&s_hw_config->mux_address, s_hw_config->bts7200s[s_test_channels[i]].mux_selection);
     bts_7200_get_measurement(&s_bts7200_storages[i], &current_0, &current_1);
 
-    LOG_DEBUG("Channel: %d; current_0: %d, current_1: %d\n", test_channels[i], current_0,
+    LOG_DEBUG("Channel: %d; current_0: %d, current_1: %d\n", s_test_channels[i], current_0,
               current_1);
   }
   soft_timer_start_millis(CURRENT_MEASURE_INTERVAL_MS, prv_read_and_log, s_hw_config, NULL);
@@ -48,26 +50,16 @@ int main() {
       IS_FRONT_POWER_DISTRO ? FRONT_POWER_DISTRIBUTION_CURRENT_HW_CONFIG
                             : REAR_POWER_DISTRIBUTION_CURRENT_HW_CONFIG;
 
-  // Initialize the PCA9539R on every I2C address specified
-  for (uint8_t i = 0; i < s_hw_config.num_dsel_i2c_addresses; i++) {
-    status_ok_or_return(
-        pca9539r_gpio_init(s_hw_config.i2c_port, s_hw_config.dsel_i2c_addresses[i]));
-  }
   status_ok_or_return(mux_init(&s_hw_config.mux_address));
 
   // Initialize and start the BTS7200s
   Bts7200Pca9539rSettings bts_7200_settings = {
     .sense_pin = &s_hw_config.mux_address.mux_output_pin,
+    .i2c_port = s_hw_config.i2c_port,
   };
 
-  for (uint8_t i = 0; i < NUM_TEST_CHANNELS; i++) {
-    // check that the currents are valid
-    if (s_hw_config.bts7200s[test_channels[i]].current_0 >= NUM_POWER_DISTRIBUTION_CURRENTS ||
-        s_hw_config.bts7200s[test_channels[i]].current_1 >= NUM_POWER_DISTRIBUTION_CURRENTS) {
-      return status_code(STATUS_CODE_INVALID_ARGS);
-    }
-
-    bts_7200_settings.select_pin = &s_hw_config.bts7200s[test_channels[i]].dsel_pin;
+  for (uint8_t i = 0; i < SIZEOF_ARRAY(s_test_channels); i++) {
+    bts_7200_settings.select_pin = &s_hw_config.bts7200s[s_test_channels[i]].dsel_pin;
     status_ok_or_return(bts_7200_init_pca9539r(&s_bts7200_storages[i], &bts_7200_settings));
   }
 
