c3da6b46d9b2fd43760600b35e3527007cf69ddf
max-mz <max.zhu.sss@gmail.com>
2020-07-15 03:30:18 +0000

    added pins to smoke test

--- projects/smoke_adt7476a/src/main.c
+++ projects/smoke_adt7476a/src/main.c
@@ -1,3 +1,5 @@
+#include <stdio.h> 
+#include <stdlib.h> 
 #include "adt7476a_fan_controller.h"
 #include "adt7476a_fan_controller_defs.h"
 #include "i2c.h"
@@ -8,49 +10,46 @@
 #include "soft_timer.h"
 
 #define SET_SPEED_INTERVAL 5
-#define I2C_WRITE_ADDR_1
-#define I2C_WRITE_ADDR_2
+#define I2C_WRITE_ADDR_1 0x5E
+#define I2C_WRITE_ADDR_2 0x58
+
 static Adt7476aStorage s_storage;
 
 static void prv_periodic_set_speed(SoftTimerId id, void *context) {
 
-    adt7476a_set_speed(I2C_PORT_1, )
+    uint8_t random_speed = rand() % 254;
+
+    adt7476a_set_speed(I2C_PORT_2, random_speed, ADT_FAN_GROUP_1, I2C_WRITE_ADDR_1);
+    adt7476a_set_speed(I2C_PORT_2, random_speed, ADT_FAN_GROUP_2, I2C_WRITE_ADDR_1);
+
+    soft_timer_start_seconds(SET_SPEED_INTERVAL, prv_periodic_set_speed, NULL, NULL);
 
 }
 
 int main() {
   
   GpioAddress smbalert = {
-      .pin = 10,
-  };
-
-  GpioAddress sda = {
-      .pin = 10,
-  };
-
-  GpioAddress scl = {
-      .pin = 10,
+      .port = GPIO_PORT_A,
+      .pin = 9,
   };
 
   I2CSettings i2c_settings = {
-    .speed = I2C_SPEED_FAST,         //
-    .sda = sda, //
-    .scl = scl,  //
+    .speed = I2C_SPEED_FAST,       
+    .scl = { .port = GPIO_PORT_B, .pin = 10 },
+    .sda = { .port = GPIO_PORT_B, .pin = 11 },
   };
 
   const Adt7476aSettings settings = {
     .smbalert_pin = smbalert,
     .callback = NULL,  // set to NULL for no callback
     .callback_context = NULL,
-    .i2c = ,
-    .i2c_settings =,
+    .i2c = I2C_PORT_2,
+    .i2c_settings = i2c_settings,
   };
   interrupt_init();
   gpio_init();
   soft_timer_init();
   adt7476a_init(&s_storage, &settings);
-
-
 
   while (1) {
     soft_timer_start_seconds(SET_SPEED_INTERVAL, prv_periodic_set_speed, NULL, NULL);
