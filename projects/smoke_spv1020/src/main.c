73bdd521d88d156b725f8381890dc59301532857
JarvisWeng <jarvis0910@gmail.com>
2020-07-16 22:31:45 -0400

    fixed changes

--- projects/smoke_spv1020/src/main.c
+++ projects/smoke_spv1020/src/main.c
@@ -1,17 +1,21 @@
 // Simple smoketest project for the solar boards
 // specifically for the spv1020s
 
+//
 #include "delay.h"
 #include "event_queue.h"
 #include "gpio.h"
 #include "gpio_it.h"
 #include "interrupt.h"
 #include "log.h"
-#include "mppt.h"
 #include "soft_timer.h"
 #include "spi.h"
+#include "mux.h"
+#include "spv1020_mppt.h"
 
 #define SPI_PORT SPI_PORT_2
+// this is the mux output pin
+// or the address of the spv1020, change it according to which spv1020 you want to read
 #define SPV1020 0
 
 #define SMOKETEST_WAIT_TIME_MS 1000
@@ -26,28 +30,78 @@
 #define CS_PIN \
   { .port = GPIO_PORT_B, 12 }
 
-uint8_t status = 0xFF;      // 8th bit is set
-uint16_t pwm = 0xFFFF;      // over 900
-uint16_t vin = 0xFFFF;      // over 10 bits
-uint16_t current = 0xFFFF;  // over 10 bits
+#define MUX_ENABLE                \
+  {                               \
+    .port = GPIO_PORT_B, .pin = 1 \
+  }  // need to check hardward for correct pin
+     // this pin is unused
+#define MUX_OUTPUT                \
+  {                               \
+    .port = GPIO_PORT_B, .pin = 1 \
+  }  // need to check hardward for correct pin
+     // this pin is unused
+#define SEL_PIN_0 \
+  { .port = GPIO_PORT_B, .pin = 3 }
+#define SEL_PIN_1 \
+  { .port = GPIO_PORT_B, .pin = 4 }
+#define SEL_PIN_2 \
+  { .port = GPIO_PORT_B, .pin = 5 }
+
+#define DISCONNECTED_MUX_OUTPUT 7
+
+// will get the address and size of the demultiplexer
+static MuxAddress s_mux_address = {
+  .bit_width = 3,
+  .sel_pins[0] = SEL_PIN_0,
+  .sel_pins[1] = SEL_PIN_1,
+  .sel_pins[2] = SEL_PIN_2,
+  .mux_enable_pin = MUX_ENABLE,
+  .mux_output_pin = MUX_OUTPUT,
+};
 
 static void prv_spv1020_check(SoftTimerId timer_id, void *context) {
-  status = 0xFF;
-  pwm = 0xFFFF;
-  vin = 0xFFFF;
-  current = 0xFFFF;
-  mppt_turn_on(SPI_PORT, SPV1020);
+  uint8_t status = 0xFF;      // 8th bit is set
+  uint16_t pwm = 0xFFFF;      // over 900
+  uint16_t vin = 0xFFFF;      // over 10 bits
+  uint16_t current = 0xFFFF;  // over 10 bits
 
-  mppt_read_status(SPI_PORT, &status, SPV1020);
-  LOG_DEBUG("SPV1020 # %d status is: %x\r\n", SPV1020, status);
-  mppt_read_pwm(SPI_PORT, &pwm, SPV1020);
-  LOG_DEBUG("SPV1020 # %d pwm is: %x\r\n", SPV1020, pwm);
-  mppt_read_voltage_in(SPI_PORT, &vin, SPV1020);
-  LOG_DEBUG("SPV1020 # %d status is: %x\r\n", SPV1020, vin);
-  mppt_read_current(SPI_PORT, &current, SPV1020);
-  LOG_DEBUG("SPV1020 # %d status is: %x\r\n", SPV1020, current);
+  mux_set(&s_mux_address, SPV1020);
 
-  mppt_shut(SPI_PORT, SPV1020);
+  spv1020_turn_on(SPI_PORT);
+
+  spv1020_read_status(SPI_PORT, &status);
+  if (status != 0) {
+    LOG_DEBUG("Reading Status failed");
+  } else {
+    LOG_DEBUG("SPV1020 #%d status is: 0x%x\r\n", SPV1020, status);
+  }
+
+  spv1020_read_pwm(SPI_PORT, &pwm);
+  if (pwm == 0xFFFF) {
+    LOG_DEBUG("Reading PWM failed");
+  } else {
+    LOG_DEBUG("SPV1020 #%d pwm is: 0x%x\r\n", SPV1020, pwm);
+  }
+
+  spv1020_read_voltage_in(SPI_PORT, &vin);
+  if (vin == 0xFFFF) {
+    LOG_DEBUG("Reading Voltage failed");
+  } else {
+    LOG_DEBUG("SPV1020 #%d voltage_in is: 0x%x\r\n", SPV1020, vin);
+  }
+
+  spv1020_read_current(SPI_PORT, &current);
+  if (current == 0xFFFF) {
+    LOG_DEBUG("Reading Current failed");
+  } else {
+    LOG_DEBUG("SPV1020 #%d current is: 0x%x\r\n", SPV1020, current);
+  }
+
+  spv1020_shut(SPI_PORT);
+
+  status_ok_or_return(mux_set(&s_mux_address, DISCONNECTED_MUX_OUTPUT));
+
+  soft_timer_start_millis(SMOKETEST_WAIT_TIME_MS, prv_spv1020_check, NULL, NULL);
 }
 
 int main(void) {
@@ -67,15 +121,14 @@ int main(void) {
   };
   spi_init(SPI_PORT, &spi_settings);
 
-  mppt_init();
+  mux_init(&s_mux_address);
 
   LOG_DEBUG("Initializing spv1020 smoke test\n");
 
   Event e = { 0 };
   soft_timer_start_millis(SMOKETEST_WAIT_TIME_MS, prv_spv1020_check, NULL, NULL);
   while (true) {
-    while (event_process(&e) != STATUS_CODE_OK) {
-    }
+    wait();
   }
   return 0;
 }
