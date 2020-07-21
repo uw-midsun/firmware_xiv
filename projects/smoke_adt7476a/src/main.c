b61edf5cdfc824509b0bc7a8c7fba077f8710aa9
max-mz <max.zhu.sss@gmail.com>
2020-07-21 22:48:13 +0000

    added callback, support for single controller

--- projects/smoke_adt7476a/src/main.c
+++ projects/smoke_adt7476a/src/main.c
@@ -7,69 +7,90 @@
 #include "soft_timer.h"
 #include "wait.h"
 
-#define SET_SPEED_INTERVAL 5
-#define FAN_SPEED_INCREMENT 10
-#define I2C_WRITE_ADDR_1 0x5E
-#define I2C_WRITE_ADDR_2 0x58
-#define I2C_READ_ADDR_1 0x5F
-#define I2C_READ_ADDR_2 0x59
+/*
 
-// 2 storages for 2 components
-static Adt7476aStorage s_storage_1;
-static Adt7476aStorage s_storage_2;
+  this teset will increment the fan speed by 10% every second until at full speed,
+  and then loop back to 0% speed and continue.
+
+  the speed interval and increment defines can be changed for further testing
+
+  the i2c port, i2c write and read address, and i2c settings should be changed as needed
+  depending on the board.
+
+*/
+
+#define ADT_7476A_NUM_FANS 4
+#define ADT_7476A_INTERRUPT_MASK_OFFSET 2
+#define SET_SPEED_INTERVAL_S 1
+#define FAN_SPEED_INCREMENT 10
+#define I2C_WRITE_ADDR 0x5E
+#define I2C_READ_ADDR 0x5F
+#define I2C_PORT I2C_PORT_2
+#define ADT_PWM_PORT ADT_PWM_PORT_1
+#define SMBALERT_ADDR \
+  { GPIO_PORT_A, 9 }
+#define SDA \
+  { GPIO_PORT_B, 11 }
+#define SCL \
+  { GPIO_PORT_B, 10 }
+static Adt7476aStorage s_storage;
+
 int s_current_speed;
 
 static void prv_periodic_set_speed(SoftTimerId id, void *context) {
   s_current_speed += FAN_SPEED_INCREMENT;
   s_current_speed = s_current_speed % 101;
   LOG_DEBUG("SETTING SPEED: %d PERCENT\n", s_current_speed);
-  adt7476a_set_speed(I2C_PORT_2, s_current_speed, ADT_PWM_PORT_1, I2C_WRITE_ADDR_1);
-  adt7476a_set_speed(I2C_PORT_2, s_current_speed, ADT_PWM_PORT_2, I2C_WRITE_ADDR_1);
+  adt7476a_set_speed(I2C_PORT, s_current_speed, ADT_PWM_PORT, I2C_WRITE_ADDR);
+  soft_timer_start_seconds(SET_SPEED_INTERVAL_S, prv_periodic_set_speed, NULL, NULL);
+}
 
-  soft_timer_start_seconds(SET_SPEED_INTERVAL, prv_periodic_set_speed, NULL, NULL);
+static void prv_callback(GpioAddress *address, void *context) {
+  LOG_DEBUG("FAN OUT OF RANGE");
+
+  uint8_t rx_interrupt_status_reg_1;
+  uint8_t rx_interrupt_status_reg_2;
+
+  uint8_t adt_7476a_fan_status[4];
+
+  adt7476a_get_status(I2C_PORT, I2C_READ_ADDR, &rx_interrupt_status_reg_1,
+                      &rx_interrupt_status_reg_2);
+
+  // bit positions 2 - 5 indicate out of range fan speeds for fans 1 - 4
+  for (int i = 0; i < ADT_7476A_NUM_FANS; i++) {
+    if ((rx_interrupt_status_reg_2 >> (ADT_7476A_INTERRUPT_MASK_OFFSET + i)) && 1) {
+      LOG_DEBUG("FAN %d HAS STALLED\n", i + 1);
+    }
+  }
 }
 
 int main() {
-  GpioAddress smbalert = {
-    .port = GPIO_PORT_A,
-    .pin = 9,
-  };
-
   I2CSettings i2c_settings = {
     .speed = I2C_SPEED_FAST,
-    .scl = { .port = GPIO_PORT_B, .pin = 10 },
-    .sda = { .port = GPIO_PORT_B, .pin = 11 },
+    .scl = SCL,
+    .sda = SDA,
   };
 
-  const Adt7476aSettings settings_1 = {
-    .smbalert_pin = smbalert,
-    .callback = NULL,  // set to NULL for no callback
+  const Adt7476aSettings settings = {
+    .smbalert_pin = SMBALERT_ADDR,
+    .callback = (GpioItCallback)prv_callback,  // set to NULL for no callback
     .callback_context = NULL,
-    .i2c_write_addr = I2C_WRITE_ADDR_1,
-    .i2c_read_addr = I2C_READ_ADDR_1,
-    .i2c = I2C_PORT_2,
-    .i2c_settings = i2c_settings,
-  };
-
-  const Adt7476aSettings settings_2 = {
-    .smbalert_pin = smbalert,
-    .callback = NULL,  // set to NULL for no callback
-    .callback_context = NULL,
-    .i2c_write_addr = I2C_WRITE_ADDR_2,
-    .i2c_read_addr = I2C_READ_ADDR_2,
-    .i2c = I2C_PORT_2,
+    .i2c_write_addr = I2C_WRITE_ADDR,
+    .i2c_read_addr = I2C_READ_ADDR,
+    .i2c = I2C_PORT,
     .i2c_settings = i2c_settings,
   };
 
   interrupt_init();
   gpio_init();
+  gpio_it_init();
   soft_timer_init();
-  adt7476a_init(&s_storage_1, &settings_1);
-  adt7476a_init(&s_storage_2, &settings_2);
+  adt7476a_init(&s_storage, &settings);
+
   s_current_speed = 0;
 
-  soft_timer_start_seconds(SET_SPEED_INTERVAL, prv_periodic_set_speed, NULL, NULL);
-
+  soft_timer_start_seconds(SET_SPEED_INTERVAL_S, prv_periodic_set_speed, NULL, NULL);
+  prv_periodic_set_speed(SOFT_TIMER_INVALID_TIMER, NULL);
   while (true) {
     wait();
   }
