c85f6371e5ab7d9c75fdbb24a9d4f56ea6512ae3
Ryan Dancy <ryan@keal.ca>
2020-07-30 02:41:57 +0000

    [SOFT-212] Implement logger

---
+++ projects/solar/inc/logger.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// Logs all the data in the data store to the console every cycle.
+// Requires the event queue and the data store to be initialized.
+
+#include <stdbool.h>
+
+#include "event_queue.h"
+#include "solar_boards.h"
+#include "status.h"
+
+// Initialize the module, logging data from the given number of MPPTs on the board.
+StatusCode logger_init(SolarMpptCount mppt_count);
+
+// Process the event and return whether the event was processed.
+bool logger_process_event(Event *e);
