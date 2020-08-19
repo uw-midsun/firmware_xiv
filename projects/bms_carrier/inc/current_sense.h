a6c4c883caa6f1037455b83f9a95b30abd6c0ee6
g-jessmuir <e.jessmuir@gmail.com>
2020-08-19 18:26:33 -0400

    [SOFT-172] Implement current sense
    Current sense stores the last 20 readings, and faults
    bps in case of overcurrent.

--- projects/bms_carrier/inc/current_sense.h
+++ projects/bms_carrier/inc/current_sense.h
@@ -7,11 +7,13 @@
 #include "status.h"
 
 #define NUM_STORED_CURRENT_READINGS 20
+#define CURRENT_SENSE_SPI_PORT SPI_PORT_2
 
 typedef struct CurrentReadings {
   int16_t readings[NUM_STORED_CURRENT_READINGS];
+  int16_t average;
 } CurrentReadings;
 
-bool current_sense_is_charging();
+bool current_sense_is_charging(readings);
 
-StatusCode current_sense_init(SpiSettings *settings);
+StatusCode current_sense_init(CurrentReadings *readings, SpiSettings *settings);
--- projects/bms_carrier/src/current_sense.c
+++ projects/bms_carrier/src/current_sense.c
@@ -1,6 +1,77 @@
 #include "current_sense.h"
 
-// TODO(SOFT-172): Actually implement this method
+#include "ads1259_adc.h"
+#include "bms.h"
+#include "exported_enums.h"
+#include "soft_timer.h"
+
+// slightly larger than conversion time of adc
+#define CONVERSION_TIME_MS 18
+// placeholder - 100 amps
+#define OVERCURRENT_MA (100 * 1000)
+
+static CurrentReadings *s_readings;
+static Ads1259Storage s_storage;
+static uint16_t s_ring_idx = 0;
+
+static void prv_ads_error_cb(Ads1259StatusCode code, void *context) {
+  if (code == ADS1259_STATUS_CODE_OUT_OF_RANGE) {
+    LOG_WARN("ADS1259 ERROR: OUT OF RANGE\n");
+  } else if (code == ADS1259_STATUS_CODE_CHECKSUM_FAULT) {
+    LOG_WARN("ADS1259 ERROR: CHECKSUM FAULT\n");
+  }
+  fault_bps(EE_BPS_STATE_FAULT_CURRENT_SENSE, false);
+}
+
+void prv_update_average(void) {
+  int32_t sum = 0;
+  for (uint16_t i = 0; i < NUM_STORED_CURRENT_READINGS; i++) {
+    sum += s_readings->readings[i];
+  }
+  s_readings->average = sum / NUM_STORED_CURRENT_READINGS;
+}
+
+int16_t prv_voltage_to_current(double reading) {
+  // placeholder - to be filled in after calibration
+  return 0;
+}
+
+void prv_periodic_ads_read(SoftTimerId id, void *context) {
+  ads1259_get_conversion_data(&s_storage);
+  double reading = s_storage.reading;
+  int16_t val = prv_voltage_to_current(reading);
+  CurrentReadings *readings = context;
+  readings->readings[s_ring_idx];
+  s_ring_idx = (s_ring_idx + 1) % NUM_STORED_CURRENT_READINGS;
+  soft_timer_start_millis(CONVERSION_TIME_MS, prv_periodic_ads_read, context, NULL);
+  
+  prv_update_average();
+
+  // check faults
+  if (s_readings->average > OVERCURRENT_MA) {
+    fault_bps(EE_BPS_STATE_FAULT_CURRENT_SENSE, false);
+  } else {
+    fault_bps(EE_BPS_STATE_FAULT_CURRENT_SENSE, true);
+  }
+}
+
 bool current_sense_is_charging() {
-  return false;
+  return s_readings->average > 0;
+}
+
+StatusCode current_sense_init(CurrentReadings *readings, SpiSettings *settings) {
+  const Ads1259Settings settings = {
+    .spi_port = CURRENT_SENSE_SPI_PORT,
+    .spi_baudrate = settings->baudrate,
+    .mosi = settings->mosi,
+    .miso = settings->miso,
+    .sclk = settings->miso,
+    .cs = settings->cs,
+    .handler = prv_ads_error_cb,
+    .error_context = NULL,
+  };
+  s_readings = readings;
+  status_ok_or_return(ads1259_init(&s_storage, &settings));
+  prv_periodic_ads_read(SOFT_TIMER_INVALID_TIMER, NULL);
+  return STATUS_CODE_OK;
 }
