1efa5766da06f091021739a1aa74ef773da1bebb
Ryan Dancy <ryan@keal.ca>
2020-09-01 20:02:36 +0000

    Change write register format

--- libraries/ms-drivers/src/stm32f0xx/pca9539r_gpio_expander.c
+++ libraries/ms-drivers/src/stm32f0xx/pca9539r_gpio_expander.c
@@ -1,6 +1,7 @@
 #include "pca9539r_gpio_expander.h"
 
 #include <stdbool.h>
+
 #include "pca9539r_gpio_expander_defs.h"
 
 // The I2C port used for all operations - won't change on each board
@@ -24,19 +25,31 @@ StatusCode pca9539r_gpio_init(const I2CPort i2c_port, const I2CAddress i2c_addre
   return STATUS_CODE_OK;
 }
 
+static void prv_read_reg(const Pca9539rGpioAddress *address, uint8_t reg0, uint8_t reg1,
+                         uint8_t *rx_data) {
+  uint8_t reg = prv_select_reg(address->pin, reg0, reg1);
+  i2c_read_reg(s_i2c_port, address->i2c_address, reg, rx_data, 1);
+}
+
+static void prv_write_reg(const Pca9539rGpioAddress *address, uint8_t reg0, uint8_t reg1,
+                          uint8_t tx_data) {
+  // PCA9539R expects the register ("command byte") as just a data byte (see figs 10, 11)
+  uint8_t reg = prv_select_reg(address->pin, reg0, reg1);
+  uint8_t data[] = { reg, tx_data };
+  i2c_write(s_i2c_port, address->i2c_address, data, SIZEOF_ARRAY(data));
+}
+
 static void prv_set_reg_bit(const Pca9539rGpioAddress *address, uint8_t reg0, uint8_t reg1,
                             bool val) {
-  uint8_t reg = prv_select_reg(address->pin, reg0, reg1);
   uint8_t bit = prv_pin_bit(address->pin);
-
   uint8_t data = 0;
-  i2c_read_reg(s_i2c_port, address->i2c_address, reg, &data, 1);
+  prv_read_reg(address, reg0, reg1, &data);
   if (val) {
     data |= 1 << bit;
   } else {
     data &= ~(1 << bit);
   }
-  i2c_write_reg(s_i2c_port, address->i2c_address, reg, &data, 1);
+  prv_write_reg(address, reg0, reg1, data);
 }
 
 StatusCode pca9539r_gpio_init_pin(const Pca9539rGpioAddress *address,
@@ -85,11 +98,9 @@ StatusCode pca9539r_gpio_toggle_state(const Pca9539rGpioAddress *address) {
 
   // optimization: instead of using set_state and get_state, we read only once
   uint8_t gpio_data = 0;
-  i2c_read_reg(s_i2c_port, address->i2c_address, prv_select_reg(address->pin, INPUT0, INPUT1),
-               &gpio_data, 1);
+  prv_read_reg(address, INPUT0, INPUT1, &gpio_data);
   gpio_data ^= 1 << prv_pin_bit(address->pin);
-  i2c_write_reg(s_i2c_port, address->i2c_address, prv_select_reg(address->pin, OUTPUT0, OUTPUT1),
-                &gpio_data, 1);
+  prv_write_reg(address, OUTPUT0, OUTPUT1, gpio_data);
 
   return STATUS_CODE_OK;
 }
@@ -105,8 +116,7 @@ StatusCode pca9539r_gpio_get_state(const Pca9539rGpioAddress *address,
   }
 
   uint8_t gpio_data = 0;
-  i2c_read_reg(s_i2c_port, address->i2c_address, prv_select_reg(address->pin, INPUT0, INPUT1),
-               &gpio_data, 1);
+  prv_read_reg(address, INPUT0, INPUT1, &gpio_data);
 
   // Read the |bit|th bit
   uint8_t bit = prv_pin_bit(address->pin);
