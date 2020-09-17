bbc423380c52027e5d5b8d036c86d9b8d53b6eaa
JarvisWeng <jarvis0910@gmail.com>
2020-09-16 20:50:37 -0400

    added missing docstring

--- projects/baby_driver/scripts/can_message.py
+++ projects/baby_driver/scripts/can_message.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import can
 
 def send_message(msg):
+    """sends can message with 8 bytes. You can change the 2nd one"""
     #change the channel later to can0 or vcan0
     bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=250000)
 
--- projects/baby_driver/scripts/first.py
+++ projects/baby_driver/scripts/first.py
@@ -1,5 +1,8 @@
-#this is just an example file
+#!/usr/bin/env python
+# coding: utf-8
 import can_message
+#this is just an example files
 
 if __name__ == "__main__":
+    #just sends specific can message
     can_message.send_message(1)
