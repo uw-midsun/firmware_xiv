2f501b0594084a18d5111dbc00dcc5fe50011ede
Raiyan Sayeed <raiyan.business@gmail.com>
2020-09-20 04:40:37 +0000

    refactored timer to use only one function, fixed timer error

--- projects/hello_firmware/src/main.c
+++ projects/hello_firmware/src/main.c
@@ -1,47 +1,45 @@
-#include <stdint.h>  // for integer types
+#include <stdint.h>      // for integer types
 
-#include "interrupt.h"  // interrupts are required for soft timers
-#include "log.h"
+#include "interrupt.h"   // interrupts are required for soft timers
+#include "log.h"         // for logging
 #include "soft_timer.h"  // Software timers for scheduling future events.
 #include "wait.h"        // for wait function
 
-#define COUNTER_A_DELAY_MS 500
-#define COUNTER_B_DELAY_MS 1000
+#define COUNTER_DELAY_MS 500
 
 typedef struct Counter {
-  uint8_t counter_a;
-  uint8_t counter_b;
+  uint8_t counters[2];
 } Counter;
 
-static void prv_counter_a_callback(SoftTimerId timer_id, void *context);
-static void prv_counter_b_callback(SoftTimerId timer_id, void *context);
+typedef struct TimerData {
+  Counter main_counter;
+  bool incrementBoth;
+} TimerData;
 
-static void prv_counter_a_callback(SoftTimerId timer_id, void *context) {
-  Counter *main_counter = context;
-  main_counter->counter_a++;
-
-  LOG_DEBUG("Counter A: %d\n", main_counter->counter_a);
-
-  soft_timer_start_millis(COUNTER_A_DELAY_MS, prv_counter_b_callback, main_counter, NULL);
+static void prv_increment_and_print_counter(int counter_num, Counter *main_counter) {
+  main_counter->counters[counter_num]++;
+  LOG_DEBUG("Counter %c: %d\n", counter_num+65, main_counter->counters[counter_num]);
 }
 
-static void prv_counter_b_callback(SoftTimerId timer_id, void *context) {
-  Counter *main_counter = context;
-  main_counter->counter_a++;
-  main_counter->counter_b++;
+static void prv_counter_callback(SoftTimerId timer_id, void *context) {
+  TimerData *timer_data = context;
 
-  LOG_DEBUG("Counter A: %d\n", main_counter->counter_a);
-  LOG_DEBUG("Counter B: %d\n", main_counter->counter_b);
+  prv_increment_and_print_counter(0, &timer_data->main_counter);
+  if (timer_data->incrementBoth) {
+    prv_increment_and_print_counter(1, &timer_data->main_counter);
+  }
+  timer_data->incrementBoth = !timer_data->incrementBoth;
 
-  soft_timer_start_millis(COUNTER_B_DELAY_MS, prv_counter_a_callback, main_counter, NULL);
+  soft_timer_start_millis(COUNTER_DELAY_MS, prv_counter_callback, timer_data, NULL);
 }
 
 int main(void) {
   interrupt_init();
   soft_timer_init();
-  Counter main_counter = { .counter_a = 0, .counter_b = 0 };
+  Counter main_counter = { .counters = { 0 } };
+  TimerData timer_data = { .main_counter = main_counter, .incrementBoth = false};
 
-  soft_timer_start_millis(COUNTER_A_DELAY_MS, prv_counter_a_callback, &main_counter, NULL);
+  soft_timer_start_millis(COUNTER_DELAY_MS, prv_counter_callback, &timer_data, NULL);
 
   while (true) {
     wait();
