7ff3ff46e02ad0661bcd4b040020bed7aacaab94
Cheran Mahalingam <cheranm@outlook.com>
2020-09-24 22:08:15 +0000

    Implementation of soft timer for tutorial 102 homework

---
+++ projects/tutorial_102/src/main.c
@@ -0,0 +1,45 @@
+#include "interrupt.h"
+#include "soft_timer.h"
+#include "log.h"
+#include "wait.h"
+
+
+typedef struct Counters {
+        uint8_t counter_a;
+        uint8_t counter_b;
+    } Counters;
+
+void prv_timer_callback(SoftTimerId timer_id, void *context) {
+    Counters *storage = context;
+    storage->counter_a++;
+
+    LOG_DEBUG("Counter A: %d \n", storage->counter_a);
+    
+    if (storage->counter_a == (storage->counter_b)*2 + 2) {
+        storage->counter_b++;
+        LOG_DEBUG("Counter B: %d \n", storage->counter_b);
+    }
+
+    soft_timer_start_millis(500,
+                            prv_timer_callback,
+                            storage,
+                            NULL);
+}
+
+int main(void) {
+    interrupt_init();
+    soft_timer_init();
+
+    Counters storage = { 0 };
+
+    soft_timer_start_millis(500,
+                            prv_timer_callback,
+                            &storage,
+                            NULL); 
+
+    while (true) {
+        wait();
+    }
+
+    return 0;
+}
