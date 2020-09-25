51416452c1dad4a57bfb285e2fb42e17ed5ac4f0
Ryan Dancy <ryan@keal.ca>
2020-09-25 19:07:26 -0400

    [SOFT-317] Update python to conform to babydriver CAN message format

--- projects/baby_driver/scripts/can_message.py
+++ projects/baby_driver/scripts/can_message.py
@@ -1,14 +1,17 @@
-""" This module sends a CAN message for babydriver"""
+"""This module sends a CAN message for babydriver"""
 
 import can
 
-def send_message(message):
-    """sends can message with 8 bytes. You can change the 2nd one"""
-    # change the channel later to can0 or vcan0
+def send_message(message, device_id=15, msg_id=63):
+    """Send a CAN message."""
+    # TODO(SOFT-317): dynamically change between can0 and vcan0
     bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=500000)
 
+    # our CAN system uses 6 bits of message ID, 1 bit for ACK/DATA, and 4 bits for device ID
+    arbitration_id = (msg_id << 5) | device_id
+
     msg = can.Message(
-        arbitration_id=0x2,
+        arbitration_id=arbitration_id,
         data=message,
         is_extended_id=False
     )
---
+++ projects/baby_driver/scripts/message_defs.py
@@ -0,0 +1,4 @@
+"""An enumeration of babydriver message IDs, the Python counterpart to babydriver_msg_defs.h."""
+
+class BabydriverMessageID(object):
+    STATUS = 0
