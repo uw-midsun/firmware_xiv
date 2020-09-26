8edc33c5e82e338ab00b73a46100e72165d5f2f5
Ryan Dancy <ryan@keal.ca>
2020-09-26 15:30:25 +0000

    [SOFT-317] Address some issues, add Message class

--- projects/baby_driver/scripts/can_util.py
+++ projects/baby_driver/scripts/can_util.py
@@ -19,21 +19,60 @@ def get_bus(channel=None):
     return can.interface.Bus(bustype="socketcan", channel=channel, bitrate=500000)
 
 
-def get_arbitration_id(msg_id, device_id):
-    """Returns the arbitration ID for a CAN message from its message and device ID."""
-    # our CAN system uses 6 bits of message ID, 1 bit for ACK/DATA, and 4 bits for device ID
-    return (msg_id << 5) | device_id
+class Message:
+    """
+    An immutable wrapper over Python-CAN's can.Message to support our message and device ID
+    conventions. See https://python-can.readthedocs.io/en/master/message.html.
 
+    Attributes:
+        msg: The underlying can.Message.
+        message_id: The message ID of the CAN message.
+        device_id: The device ID of the CAN message.
+        data: The data associated with the message.
+    """
 
-def arbitration_to_message_id(arbitration_id):
-    """Extracts and returns the message ID from a CAN message's arbitration ID."""
-    # message ID is bits 5-11
-    return (arbitration_id >> 5) & 0b111111
+    def __init__(self, message_id=0, device_id=0, **kwargs):
+        """Initialize a Message. See Python-CAN's can.Message for more info.
+
+        Args:
+            message_id: The message ID of the CAN message, used if arbitration_id is not passed.
+            device_id: The device ID of the CAN message, used if arbitration_id is not passed.
+        """
+
+        if "arbitration_id" not in kwargs:
+            # our CAN system uses 6 bits of message ID, 1 bit for ACK/DATA, and 4 bits for device ID
+            kwargs["arbitration_id"] = (message_id << 5) | device_id
+
+        self.msg = can.Message(**kwargs)
+
+    @classmethod
+    def from_msg(cls, msg):
+        """Helper to get a Message from a can.Message."""
+        message = cls()
+        message.msg = msg
+        return message
+
+    @property
+    def message_id(self):
+        """The message ID of this CAN message."""
+        # message ID is bits 5-11
+        return (self.msg.arbitration_id >> 5) & 0b111111
+
+    @property
+    def device_id(self):
+        """The device ID of this CAN message."""
+        # device ID is bits 0-3
+        return self.msg.arbitration_id & 0b1111
+
+    @property
+    def data(self):
+        """The data associated with this CAN message."""
+        return self.msg.data
 
 
 def send_message(
-        babydriver_id,
-        data,
+        babydriver_id=None,
+        data=None,
         channel=None,
         msg_id=BABYDRIVER_CAN_MESSAGE_ID,
         device_id=BABYDRIVER_DEVICE_ID,
@@ -41,8 +80,11 @@ def send_message(
     """Sends a CAN message.
 
     Args:
-        babydriver_id: The babydriver ID (first byte of message data) of the message to send.
-        data: Up to 7 bytes (0-255) of data to send in the CAN message.
+        babydriver_id: The babydriver ID (first byte of message data) of the message to send. If
+            None, the first byte of message data isn't overwritten.
+        data: The data to send in the CAN message. Must be a list of bytes (0-255). If babydriver_id
+            is None, this can be up to 8 bytes; otherwise, it can only be up to 7 bytes since the
+            first byte is the babydriver ID.
         channel: The SocketCAN channel on which to send the message.
         msg_id: The CAN message ID to use.
         device_id: The device ID to use.
@@ -51,23 +93,29 @@ def send_message(
         can.CanError: If there was an error in transmitting the message.
     """
 
-    if len(data) > 7:
-        raise ValueError("Only 7 bytes of data may be sent")
-    if len(data) < 7:
-        # pad to 7 bytes so that the firmware project will accept it
-        data += [0] * (7 - len(data))
+    if data is None:
+        data = []
+    if babydriver_id is not None:
+        data = [babydriver_id] + data
 
-    message_data = bytearray([babydriver_id] + data)
+    if len(data) > 8:
+        raise ValueError("Only 8 bytes of data (including babydriver ID) may be sent")
+    if len(data) < 8 and msg_id == BABYDRIVER_CAN_MESSAGE_ID:
+        # pad to 8 bytes so that the firmware project will accept it
+        data += [0] * (8 - len(data))
+
+    data = bytearray(data)
 
     bus = get_bus(channel)
 
-    msg = can.Message(
-        arbitration_id=get_arbitration_id(msg_id, device_id),
-        data=message_data,
+    msg = Message(
+        message_id=msg_id,
+        device_id=device_id,
+        data=data,
         is_extended_id=False
     )
 
-    bus.send(msg)
+    bus.send(msg.msg)
 
 
 def next_message(
@@ -79,20 +127,28 @@ def next_message(
     """Blocks until we receive a babydriver CAN message or we time out.
 
     Args:
-        babydriver_id: If non-None and the received message's babydriver ID (i.e. first byte of
-            message data) isn't equal to this, raise an exception.
+        babydriver_id: A babydriver ID or list of IDs. If non-None and the received message's
+            babydriver ID (i.e. first byte of message data) isn't equal to this or an element of
+            this, raise an exception.
         channel: The SocketCAN channel to send on (can0 or vcan0).
         timeout: Timeout to wait for a message before raising an exception, in seconds.
-        msg_id: The CAN message ID to wait for, defaulting to the babydriver CAN message. All other
-            CAN messages will be ignored.
+        msg_id: The CAN message ID or list of IDs to wait for, defaulting to the babydriver CAN
+            message. All other CAN messages will be ignored. If None, don't check the message ID
+            and return the first CAN message we see.
 
     Returns:
-        A bytearray of data from the CAN message received.
+        A Message object representing the received CAN message.
 
     Raises:
         TimeoutError: if we time out waiting for an appropriate CAN message.
         ValueError: if we receive a CAN message but its babydriver ID does not match.
     """
+
+    # make these iterable to support waiting on one or multiple message/babydriver IDs
+    if isinstance(babydriver_id, int):
+        babydriver_id = (babydriver_id,)
+    if isinstance(msg_id, int):
+        msg_id = (msg_id,)
 
     bus = get_bus(channel)
 
@@ -106,8 +162,8 @@ def next_message(
             # bus.recv timed out
             break
 
-        rx_msg_id = arbitration_to_message_id(msg.arbitration_id)
-        if rx_msg_id == msg_id:
+        msg = Message.from_msg(msg)
+        if msg_id is None or msg.message_id in msg_id:
             break
 
         # ignore messages that we aren't waiting for.
@@ -120,10 +176,10 @@ def next_message(
     if msg is None:
         raise TimeoutError()
 
-    if babydriver_id is not None and (not msg.data or msg.data[0] != babydriver_id):
+    if babydriver_id is not None and (not msg.data or msg.data[0] not in babydriver_id):
         raise ValueError("next_message expected babydriver ID {} but got {}".format(
             babydriver_id,
             msg.data[0] if msg.data else "empty message",
         ))
 
-    return msg.data
+    return msg
