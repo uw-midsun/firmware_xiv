a96907d53c4e5941819dcc510dc74a9b83a05d2b
Avery Chiu <a27chiu@uwaterloo.ca>
2020-09-26 15:36:16 +0000

    Removed redundant .port and .pin

--- libraries/ms-helper/inc/controller_board_pins.h
+++ libraries/ms-helper/inc/controller_board_pins.h
@@ -6,183 +6,183 @@
 #define CONTROLLER_BOARD_ADDR_DEBUG_CAN_RX \
   \
 {                                       \
-    .port = GPIO_PORT_A, .pin = 11         \
+    GPIO_PORT_A, 11                        \
   }
 
 #define CONTROLLER_BOARD_ADDR_DEBUG_CAN_TX \
   \
 {                                       \
-    .port = GPIO_PORT_A, .pin = 12         \
+    GPIO_PORT_A, 12                        \
   }
 
 // USART 1
 #define CONTROLLER_BOARD_ADDR_DEBUG_USART1_TX \
   \
 {                                          \
-    .port = GPIO_PORT_B, .pin = 6             \
+    GPIO_PORT_B, 6                            \
   }
 
 #define CONTROLLER_BOARD_ADDR_DEBUG_USART1_RX \
   \
 {                                          \
-    .port = GPIO_PORT_B, .pin = 7             \
+    GPIO_PORT_B, 7                            \
   }
 
 // USART 2
 #define CONTROLLER_BOARD_ADDR_USART2_TX \
   \
 {                                    \
-    .port = GPIO_PORT_A, .pin = 2       \
+    GPIO_PORT_A, 2                      \
   }
 
 #define CONTROLLER_BOARD_ADDR_USART2_RX \
   \
 {                                    \
-    .port = GPIO_PORT_A, .pin = 3       \
+    GPIO_PORT_A, 3                      \
   }
 
 // USART 3
 #define CONTROLLER_BOARD_ADDR_USART3_TX \
   \
 {                                    \
-    .port = GPIO_PORT_B, .pin = 10      \
+    GPIO_PORT_B, 10                     \
   }
 
 #define CONTROLLER_BOARD_ADDR_USART3_RX \
   \
 {                                    \
-    .port = GPIO_PORT_B, .pin = 11      \
+    GPIO_PORT_B, 11                     \
   }
 
 // SPI 1
 #define CONTROLLER_BOARD_ADDR_SPI1_NSS \
   \
 {                                   \
-    .port = GPIO_PORT_A, .pin = 4      \
+    GPIO_PORT_A, 4                     \
   }
 
 #define CONTROLLER_BOARD_ADDR_SPI1_SCK \
   \
 {                                   \
-    .port = GPIO_PORT_A, .pin = 5      \
+    GPIO_PORT_A, 5                     \
   }
 
 #define CONTROLLER_BOARD_ADDR_SPI1_MISO \
   \
 {                                    \
-    .port = GPIO_PORT_A, .pin = 6       \
+    GPIO_PORT_A, 6                      \
   }
 
 #define CONTROLLER_BOARD_ADDR_SPI1_MOSI \
   \
 {                                    \
-    .port = GPIO_PORT_A, .pin = 7       \
+    GPIO_PORT_A, 7                      \
   }
 
 // SPI 2
 #define CONTROLLER_BOARD_ADDR_SPI2_NSS \
   \
 {                                   \
-    .port = GPIO_PORT_B, .pin = 12     \
+    GPIO_PORT_B, 12                    \
   }
 
 #define CONTROLLER_BOARD_ADDR_SPI2_SCK \
   \
 {                                   \
-    .port = GPIO_PORT_B, .pin = 13     \
+    GPIO_PORT_B, 13                    \
   }
 
 #define CONTROLLER_BOARD_ADDR_SPI2_MISO \
   \
 {                                    \
-    .port = GPIO_PORT_B, .pin = 14      \
+    GPIO_PORT_B, 14                     \
   }
 
 #define CONTROLLER_BOARD_ADDR_SPI2_MOSI \
   \
 {                                    \
-    .port = GPIO_PORT_B, .pin = 15      \
+    GPIO_PORT_B, 15                     \
   }
 
 // I2C 1
 #define CONTROLLER_BOARD_ADDR_I2C1_SCL \
   \
 {                                   \
-    .port = GPIO_PORT_B, .pin = 8      \
+    GPIO_PORT_B, 8                     \
   }
 
 #define CONTROLLER_BOARD_ADDR_I2C1_SDA \
   \
 {                                   \
-    .port = GPIO_PORT_B, .pin = 9      \
+    GPIO_PORT_B, 9                     \
   }
 
 // I2C 2
 #define CONTROLLER_BOARD_ADDR_I2C2_SCL \
   \
 {                                   \
-    .port = GPIO_PORT_B, .pin = 10     \
+    GPIO_PORT_B, 10                    \
   }
 
 #define CONTROLLER_BOARD_ADDR_I2C2_SDA \
   \
 {                                   \
-    .port = GPIO_PORT_B, .pin = 11     \
+    GPIO_PORT_B, 11                    \
   }
 
 // Onboard LEDs
 #define CONTROLLER_BOARD_ADDR_LED_BLUE \
   \
 {                                   \
-    .port = GPIO_PORT_A, .pin = 15     \
+    GPIO_PORT_A, 15                    \
   }
 
 #define CONTROLLER_BOARD_ADDR_LED_RED \
   \
 {                                  \
-    .port = GPIO_PORT_B, .pin = 3     \
+    GPIO_PORT_B, 3                    \
   }
 
 #define CONTROLLER_BOARD_ADDR_LED_YELLOW \
   \
 {                                     \
-    .port = GPIO_PORT_B, .pin = 4        \
+    GPIO_PORT_B, 4                       \
   }
 
 #define CONTROLLER_BOARD_ADDR_LED_GREEN \
   \
 {                                    \
-    .port = GPIO_PORT_B, .pin = 5       \
+    GPIO_PORT_B, 5                      \
   }
 
 // PWM
 #define CONTROLLER_BOARD_ADDR_PWM_TIM1 \
   \
 {                                   \
-    .port = GPIO_PORT_A, .pin = 8      \
+    GPIO_PORT_A, 8                     \
   }
 
 #define CONTROLLER_BOARD_ADDR_PWM_TIM3 \
   \
 {                                   \
-    .port = GPIO_PORT_B, .pin = 0      \
+    GPIO_PORT_B, 0                     \
   }
 
 #define CONTROLLER_BOARD_ADDR_PWM_TIM14 \
   \
 {                                    \
-    .port = GPIO_PORT_A, .pin = 4       \
+    GPIO_PORT_A, 4                      \
   }
 
 // SWD
 #define CONTROLLER_BOARD_ADDR_SWDIO \
   \
 {                                \
-    .port = GPIO_PORT_A, .pin = 13  \
+    GPIO_PORT_A, 13                 \
   }
 
 #define CONTROLLER_BOARD_ADDR_SWCLK \
   \
 {                                \
-    .port = GPIO_PORT_A, .pin = 14  \
+    GPIO_PORT_A, 14                 \
   }
