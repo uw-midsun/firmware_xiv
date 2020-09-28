67761b4c134972ef6a2311d77b591dea21de8cd1
Sudhish Meenakshisundaram <sudhish.3120@gmail.com>
2020-09-28 03:19:42 +0000

    completed making changes - firmware 102 homework

--- projects/usingSoftTimers/src/main.c
+++ projects/usingSoftTimers/src/main.c
@@ -1,12 +1,6 @@
 
-#pragma once
-
 // Created by Sudhish M on 2020-09-25.
-// usingSoftTimers
-
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
+// using_soft_timers
 
 #include "interrupt.h"
 #include "log.h"
@@ -21,27 +15,26 @@ typedef struct counters {
 } Counters;
 
 static void prv_timer_callback(SoftTimerId timer_id, void *context) {
-  // &counterTest=counterPtr
-  Counters *counterPtr = context;
+  Counters *counter_ptr = context;
 
-  counterPtr->counter_a++;
-  LOG_DEBUG("Counter A: %i \n", counterPtr->counter_a);
+  counter_ptr->counter_a++;
+  LOG_DEBUG("Counter A: %i \n", counter_ptr->counter_a);
 
-  if (counterPtr->counter_a % 2 == 0) {
-    counterPtr->counter_b++;
-    LOG_DEBUG("Counter B: %i \n", counterPtr->counter_b);
+  if (counter_ptr->counter_a % 2 == 0) {
+    counter_ptr->counter_b++;
+    LOG_DEBUG("Counter B: %i \n", counter_ptr->counter_b);
   }
 
-  soft_timer_start_millis(COUNTER_PERIOD_MS, prv_timer_callback, counterPtr, NULL);
+  soft_timer_start_millis(COUNTER_PERIOD_MS, prv_timer_callback, counter_ptr, NULL);
 }
 
 int main() {
   interrupt_init();
   soft_timer_init();
 
-  Counters counterTest = { 0 };
+  Counters counter_test = { 0 };
 
-  soft_timer_start_millis(COUNTER_PERIOD_MS, prv_timer_callback, &counterTest, NULL);
+  soft_timer_start_millis(COUNTER_PERIOD_MS, prv_timer_callback, &counter_test, NULL);
 
   while (true) {
     wait();
