8e2643f2c5667325dcf94272cbb25945e2341ca3
Ryan Dancy <ryan@keal.ca>
2020-10-08 01:42:51 +0000

    [SOFT-296] Refactor led_manager more, not sure if it's good

--- projects/centre_console/src/led_manager.c
+++ projects/centre_console/src/led_manager.c
@@ -21,7 +21,12 @@
 #define MCP23008_I2C_PORT I2C_PORT_2
 #define MCP23008_I2C_ADDR 0x27
 
-typedef void (*EventHandler)(Event *e);
+typedef void (*EventHandler)(Event *e, uint16_t context);
+
+typedef struct EventHandlerAndContext {
+  EventHandler handler;
+  uint16_t context;
+} EventHandlerAndContext;
 
 static const Mcp23008GpioAddress s_led_to_address[NUM_CENTRE_CONSOLE_LEDS] = {
   [CENTRE_CONSOLE_LED_BPS] = { MCP23008_I2C_ADDR, 0 },
@@ -41,56 +46,46 @@ static const CentreConsoleLed s_drive_state_leds[] = {
   CENTRE_CONSOLE_LED_PARKING,
 };
 
-static const CentreConsoleLed s_drive_fsm_output_events_to_leds[] = {
-  [DRIVE_FSM_OUTPUT_EVENT_DRIVE] = CENTRE_CONSOLE_LED_DRIVE,
-  [DRIVE_FSM_OUTPUT_EVENT_REVERSE] = CENTRE_CONSOLE_LED_REVERSE,
-  [DRIVE_FSM_OUTPUT_EVENT_NEUTRAL] = CENTRE_CONSOLE_LED_NEUTRAL,
-  [DRIVE_FSM_OUTPUT_EVENT_PARKING] = CENTRE_CONSOLE_LED_PARKING,
-};
-
 static void prv_set_led(CentreConsoleLed led, Mcp23008GpioState state) {
   mcp23008_gpio_set_state(&s_led_to_address[led], state);
 }
 
-static void prv_set_bps_led(Event *e) {
+static void prv_set_bps_led(Event *e, uint16_t context) {
   FaultReason reason = { .raw = e->data };
   if (reason.fields.area == EE_CONSOLE_FAULT_AREA_BPS_HEARTBEAT) {
     prv_set_led(CENTRE_CONSOLE_LED_BPS, MCP23008_GPIO_STATE_HIGH);
   }
 }
 
-static void prv_set_hazards_led(Event *e) {
-  prv_set_led(CENTRE_CONSOLE_LED_HAZARDS,
-              e->id == HAZARD_EVENT_ON ? MCP23008_GPIO_STATE_HIGH : MCP23008_GPIO_STATE_LOW);
+static void prv_set_hazards_led(Event *e, uint16_t context) {
+  Mcp23008GpioState state = context;
+  prv_set_led(CENTRE_CONSOLE_LED_HAZARDS, state);
 }
 
-static void prv_set_power_led(Event *e) {
-  if (e->id == POWER_MAIN_SEQUENCE_EVENT_COMPLETE || e->id == POWER_AUX_SEQUENCE_EVENT_COMPLETE) {
-    prv_set_led(CENTRE_CONSOLE_LED_POWER, MCP23008_GPIO_STATE_HIGH);
-  } else if (e->id == POWER_OFF_SEQUENCE_EVENT_COMPLETE) {
-    prv_set_led(CENTRE_CONSOLE_LED_POWER, MCP23008_GPIO_STATE_LOW);
-  }
+static void prv_set_power_led(Event *e, uint16_t context) {
+  Mcp23008GpioState state = context;
+  prv_set_led(CENTRE_CONSOLE_LED_POWER, state);
 }
 
-static void prv_set_drive_state_leds(Event *e) {
-  CentreConsoleLed led_to_enable = s_drive_fsm_output_events_to_leds[e->id];
+static void prv_set_drive_state_leds(Event *e, uint16_t context) {
+  CentreConsoleLed led_to_enable = context;
   for (size_t i = 0; i < SIZEOF_ARRAY(s_drive_state_leds); i++) {
     CentreConsoleLed led = s_drive_state_leds[i];
     prv_set_led(led, led == led_to_enable ? MCP23008_GPIO_STATE_HIGH : MCP23008_GPIO_STATE_LOW);
   }
 }
 
-static const EventHandler s_event_to_handler[NUM_CENTRE_CONSOLE_EVENTS] = {
-  [CENTRE_CONSOLE_POWER_EVENT_FAULT] = prv_set_bps_led,
-  [HAZARD_EVENT_ON] = prv_set_hazards_led,
-  [HAZARD_EVENT_OFF] = prv_set_hazards_led,
-  [POWER_MAIN_SEQUENCE_EVENT_COMPLETE] = prv_set_power_led,
-  [POWER_AUX_SEQUENCE_EVENT_COMPLETE] = prv_set_power_led,
-  [POWER_OFF_SEQUENCE_EVENT_COMPLETE] = prv_set_power_led,
-  [DRIVE_FSM_OUTPUT_EVENT_DRIVE] = prv_set_drive_state_leds,
-  [DRIVE_FSM_OUTPUT_EVENT_REVERSE] = prv_set_drive_state_leds,
-  [DRIVE_FSM_OUTPUT_EVENT_NEUTRAL] = prv_set_drive_state_leds,
-  [DRIVE_FSM_OUTPUT_EVENT_PARKING] = prv_set_drive_state_leds,
+static const EventHandlerAndContext s_event_to_handler[NUM_CENTRE_CONSOLE_EVENTS] = {
+  [CENTRE_CONSOLE_POWER_EVENT_FAULT] = { prv_set_bps_led },
+  [HAZARD_EVENT_ON] = { prv_set_hazards_led, MCP23008_GPIO_STATE_HIGH },
+  [HAZARD_EVENT_OFF] = { prv_set_hazards_led, MCP23008_GPIO_STATE_LOW },
+  [POWER_MAIN_SEQUENCE_EVENT_COMPLETE] = { prv_set_power_led, MCP23008_GPIO_STATE_HIGH },
+  [POWER_AUX_SEQUENCE_EVENT_COMPLETE] = { prv_set_power_led, MCP23008_GPIO_STATE_HIGH },
+  [POWER_OFF_SEQUENCE_EVENT_COMPLETE] = { prv_set_power_led, MCP23008_GPIO_STATE_LOW },
+  [DRIVE_FSM_OUTPUT_EVENT_DRIVE] = { prv_set_drive_state_leds, CENTRE_CONSOLE_LED_DRIVE },
+  [DRIVE_FSM_OUTPUT_EVENT_REVERSE] = { prv_set_drive_state_leds, CENTRE_CONSOLE_LED_REVERSE },
+  [DRIVE_FSM_OUTPUT_EVENT_NEUTRAL] = { prv_set_drive_state_leds, CENTRE_CONSOLE_LED_NEUTRAL },
+  [DRIVE_FSM_OUTPUT_EVENT_PARKING] = { prv_set_drive_state_leds, CENTRE_CONSOLE_LED_PARKING },
 };
 
 StatusCode led_manager_init(void) {
@@ -107,9 +102,9 @@ StatusCode led_manager_init(void) {
 
 bool led_manager_process_event(Event *e) {
   if (e == NULL || e->id >= NUM_CENTRE_CONSOLE_EVENTS) return false;
-  EventHandler handler = s_event_to_handler[e->id];
-  if (handler != NULL) {
-    handler(e);
+  EventHandlerAndContext handler_and_context = s_event_to_handler[e->id];
+  if (handler_and_context.handler != NULL) {
+    handler_and_context.handler(e, handler_and_context.context);
     return true;
   }
   return false;
