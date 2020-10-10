7cdf07a858780244b7dc4b193795f63622674c88
Het Shah <h46shah@uwaterloo.ca>
2020-10-10 17:44:59 +0000

    created static array for gpio pin addresses

--- projects/smoke_thermistor/src/main.c
+++ projects/smoke_thermistor/src/main.c
@@ -8,20 +8,17 @@
 // Toggle the following to select which data is logged
 #define READ_DATA_RAW true
 #define READ_DATA_CONVERTED false
-#define NUM_ADC_PINS 6
 #define TIME_BETWEEN_READS_IN_MILLIS 200
+
+static GpioAddress s_pin_addresses[] = {
+  { GPIO_PORT_A, 0 }, { GPIO_PORT_A, 1 }, { GPIO_PORT_A, 2 },
+  { GPIO_PORT_A, 3 }, { GPIO_PORT_A, 4 }, { GPIO_PORT_A, 5 }
+};
 
 int main(void) {
   interrupt_init();
   soft_timer_init();
   gpio_init();
-
-  GpioAddress pin_addresses[NUM_ADC_PINS];
-
-  for (uint8_t pin = 0; pin < NUM_ADC_PINS; pin++) {
-    pin_addresses[pin].port = GPIO_PORT_A;
-    pin_addresses[pin].pin = pin;
-  }
 
   GpioSettings settings = {
     .direction = GPIO_DIR_IN,           //
@@ -30,27 +27,27 @@ int main(void) {
     .alt_function = GPIO_ALTFN_ANALOG,  //
   };
 
-  for (uint8_t pin = 0; pin < NUM_ADC_PINS; pin++) {
-    gpio_init_pin(&pin_addresses[pin], &settings);
+  for (uint8_t pin = 0; pin < SIZEOF_ARRAY(s_pin_addresses); pin++) {
+    gpio_init_pin(&s_pin_addresses[pin], &settings);
   }
 
   adc_init(ADC_MODE_SINGLE);
 
-  for (uint8_t pin = 0; pin < NUM_ADC_PINS; pin++) {
-    adc_set_channel_pin(pin_addresses[pin], true);
+  for (uint8_t pin = 0; pin < SIZEOF_ARRAY(s_pin_addresses); pin++) {
+    adc_set_channel_pin(s_pin_addresses[pin], true);
   }
 
   while (true) {
     uint16_t temp_data = 0;
     if (READ_DATA_RAW) {
-      for (uint8_t reading = 0; reading < NUM_ADC_PINS; reading++) {
-        adc_read_raw_pin(pin_addresses[reading], &temp_data);
+      for (uint8_t reading = 0; reading < SIZEOF_ARRAY(s_pin_addresses); reading++) {
+        adc_read_raw_pin(s_pin_addresses[reading], &temp_data);
         LOG_DEBUG("Temp %d raw data: %d\n", reading, temp_data);
       }
     }
     if (READ_DATA_CONVERTED) {
-      for (uint8_t reading = 0; reading < NUM_ADC_PINS; reading++) {
-        adc_read_converted_pin(pin_addresses[reading], &temp_data);
+      for (uint8_t reading = 0; reading < SIZEOF_ARRAY(s_pin_addresses); reading++) {
+        adc_read_converted_pin(s_pin_addresses[reading], &temp_data);
         LOG_DEBUG("Temp %d converted data: %d\n", reading, temp_data);
       }
     }
