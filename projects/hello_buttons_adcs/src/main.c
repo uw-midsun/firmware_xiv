3f423b6709101d9f0877259a4e405b3abfebbdeb
Raiyan Sayeed <raiyan.business@gmail.com>
2020-10-12 23:03:52 +0000

    fixed style nitpicks

--- projects/hello_buttons_adcs/src/main.c
+++ projects/hello_buttons_adcs/src/main.c
@@ -8,8 +8,8 @@
 #include "log.h"
 #include "soft_timer.h"
 
-GpioAddress adc_pin_addr = { .port = GPIO_PORT_A, .pin = 6 };
-GpioAddress button_addr = { .port = GPIO_PORT_B, .pin = 2 };
+static GpioAddress s_adc_pin_addr = { .port = GPIO_PORT_A, .pin = 6 };
+static GpioAddress s_button_addr = { .port = GPIO_PORT_B, .pin = 2 };
 
 static GpioSettings s_adc_pin_settings = {
   .direction = GPIO_DIR_IN,
@@ -26,15 +26,17 @@ static InterruptSettings s_interrupt_settings = {
 static AdcChannel s_adc_pin_channel = NUM_ADC_CHANNELS;
 
 static void prv_button_interrupt_handler(const GpioAddress *address, void *context) {
+  AdcChannel *s_adc_pin_channel = context;
+  
   uint16_t adc_pin_data = 0;
 
-  adc_read_converted(s_adc_pin_channel, &adc_pin_data);
+  adc_read_converted(*s_adc_pin_channel, &adc_pin_data);
   LOG_DEBUG("ADC data: %d\n", adc_pin_data);
 }
 
 static void prv_register_interrupts(void) {
-  gpio_it_register_interrupt(&button_addr, &s_interrupt_settings, INTERRUPT_EDGE_FALLING,
-                             prv_button_interrupt_handler, (void *)0);
+  gpio_it_register_interrupt(&s_button_addr, &s_interrupt_settings, INTERRUPT_EDGE_FALLING,
+                             prv_button_interrupt_handler, &s_adc_pin_channel);
 }
 
 int main(void) {
@@ -45,10 +47,10 @@ int main(void) {
   gpio_it_init();
   prv_register_interrupts();
 
-  gpio_init_pin(&adc_pin_addr, &s_adc_pin_settings);
+  gpio_init_pin(&s_adc_pin_addr, &s_adc_pin_settings);
 
   adc_init(ADC_MODE_SINGLE);
-  adc_get_channel(adc_pin_addr, &s_adc_pin_channel);
+  adc_get_channel(s_adc_pin_addr, &s_adc_pin_channel);
   adc_set_channel(s_adc_pin_channel, true);
 
   while (true) {
