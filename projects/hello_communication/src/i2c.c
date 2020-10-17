129a4a21f4d83f731b6ae99b9f0e1b7b8e711b37
Raiyan Sayeed <raiyan.business@gmail.com>
2020-10-17 00:17:28 +0000

    i2c done(?)

---
+++ projects/hello_communication/src/i2c.c
@@ -0,0 +1,133 @@
+#include <stdbool.h>
+#include "gpio.h"
+#include "i2c.h"
+#include "log.h"
+
+// ==== WRITE PARAMETERS ====
+
+// Set this to true to perform an I2C write.
+#define SHOULD_WRITE true
+
+// Fill in these variables with the port and address to write to.
+#define WRITE_I2C_PORT 4  
+#define WRITE_I2C_ADDRESS 0x48
+
+// Fill in this array with the bytes to write.
+static const uint8_t bytes_to_write[] = { 0b0010010010100110 };
+
+// Set this to true to write to a register or false to write normally.
+#define SHOULD_WRITE_REGISTER true
+
+// If the previous parameter is true, fill in this variable with the register to write to.
+#define REGISTER_TO_WRITE 0b00000001 // config register
+
+// ==== READ PARAMETERS ====
+
+// Set this to true to perform an I2C read.
+#define SHOULD_READ true
+
+// Fill in these variables with the port and address to read from.
+#define READ_I2C_PORT 4  
+#define READ_I2C_ADDRESS 0x48
+
+// Fill in this variable with the number of bytes to read.
+#define NUM_BYTES_TO_READ 16
+
+// Set this to true to read from a register or false to read normally.
+#define SHOULD_READ_REGISTER true
+
+// If the previous parameter is true, fill in this variable with the register to read from.
+#define REGISTER_TO_READ 0b00000000 // conversion register
+
+// ==== END OF PARAMETERS ====
+
+// These are the SDA and SCL ports for the I2C ports 1 and 2.
+#define I2C1_SDA \
+  { .port = 4, .pin = 13 }
+#define I2C1_SCL \
+  { .port = 4, .pin = 14 }
+
+static void prv_initialize(void) {
+  gpio_init();
+
+  I2CSettings i2c1_settings = {
+    .speed = I2C_SPEED_FAST,
+    .sda = I2C1_SDA,
+    .scl = I2C1_SCL,
+  };
+  i2c_init(I2C_PORT_1, &i2c1_settings);
+
+}
+
+int main(void) {
+  prv_initialize();
+
+  if (SHOULD_WRITE) {
+    // Calculate the write length
+    uint16_t tx_len = SIZEOF_ARRAY(bytes_to_write);
+
+    StatusCode status;
+    if (SHOULD_WRITE_REGISTER) {
+      // Perform the write to a register
+      status = i2c_write_reg(WRITE_I2C_PORT, WRITE_I2C_ADDRESS, REGISTER_TO_WRITE, bytes_to_write,
+                             tx_len);
+
+      // Log a successful write
+      if (status == STATUS_CODE_OK) {
+        LOG_DEBUG("Successfully wrote %d bytes to register %x at I2C address %x on I2C_PORT_%d\n",
+                  tx_len, REGISTER_TO_WRITE, WRITE_I2C_ADDRESS,
+                  (WRITE_I2C_PORT == I2C_PORT_1) ? 1 : 2);
+      }
+    } else {
+      // Perform the write normally
+      status = i2c_write(WRITE_I2C_PORT, WRITE_I2C_ADDRESS, bytes_to_write, tx_len);
+
+      // Log a successful write
+      if (status == STATUS_CODE_OK) {
+        LOG_DEBUG("Successfully wrote %d bytes to I2C address %x on I2C_PORT_%d\n", tx_len,
+                  WRITE_I2C_ADDRESS, (WRITE_I2C_PORT == I2C_PORT_1) ? 1 : 2);
+      }
+    }
+
+    // Log an unsuccessful write
+    if (status != STATUS_CODE_OK) {
+      LOG_DEBUG("Write failed: status code %d\n", status);
+    }
+  }
+
+  if (SHOULD_READ) {
+    // Allocate space for the bytes we'll read
+    uint8_t read_bytes[NUM_BYTES_TO_READ] = { 0 };
+
+    StatusCode status;
+    if (SHOULD_READ_REGISTER) {
+      // Perform the read from a register
+      status = i2c_read_reg(READ_I2C_PORT, READ_I2C_ADDRESS, REGISTER_TO_READ, read_bytes,
+                            NUM_BYTES_TO_READ);
+
+      // Log a successful read
+      LOG_DEBUG("Successfully read %d bytes from register %x at I2C address %x on I2C_PORT_%d\n",
+                NUM_BYTES_TO_READ, REGISTER_TO_READ, READ_I2C_ADDRESS,
+                (READ_I2C_PORT == I2C_PORT_1) ? 1 : 2);
+    } else {
+      // Perform the read normally
+      status = i2c_read(READ_I2C_PORT, READ_I2C_ADDRESS, read_bytes, NUM_BYTES_TO_READ);
+
+      // Log a successful read
+      LOG_DEBUG("Successfully read %d bytes from I2C address %x on I2C_PORT_%d\n",
+                NUM_BYTES_TO_READ, READ_I2C_ADDRESS, (READ_I2C_PORT == I2C_PORT_1) ? 1 : 2);
+    }
+
+    if (status == STATUS_CODE_OK) {
+      // Log the bytes we read
+      for (uint16_t i = 0; i < NUM_BYTES_TO_READ; i++) {
+        LOG_DEBUG("Byte %x read: %x\n", i, read_bytes[i]);
+      }
+    } else {
+      // Log an unsucessful read
+      LOG_DEBUG("Read failed: status code %d\n", status);
+    }
+  }
+
+  return 0;
+}
---
+++ projects/hello_communication/src/spi.c
@@ -0,0 +1,79 @@
+/**
+ * Part 1: I2C
+
+Based on the MSXII pedal board schematics, write a function that writes 0b0010010010100110 to the configuration register, then reads the conversion register.
+
+Hint: you’ll need to look at the schematics for the pedal board and the datasheet for the ADC.
+Part 2: SPI
+
+Based on the MSXII charger interface board schematics, write a function that initializes SPI then writes the following information to the CANCTRL 
+register of the CAN controller IC:
+
+    Set loopback mode
+
+    Do not request abort of transmit buffers
+
+    Enable one shot mode
+
+    Disable CLKOUT pin
+
+    Set the CLKOUT prescaler to System Clock / 2.
+
+Then, send the READ STATUS instruction and print the TXB1CNTRL[3] bit from the return.
+Part 3: CAN Communication
+
+Write a function that periodically sends a CAN message with id 0xA and a random uint16_t as the body, and requests an ACK with an OK status. 
+Write another function that periodically sends a CAN message with id 0xB and a different random uint16_t as the body.
+
+Then, register callbacks for both that print the data, but only ACK the 0xA message.
+
+Run the program in two terminals at the same time, and send a screenshot of the output to your lead.
+**/
+
+#include "gpio.h"
+#include "log.h"
+#include "spi.h"
+
+// This project is for smoke testing SPI.
+
+// Fill in the parameters as needed.
+// You can use hex with 0x... or binary with 0b...
+
+// FILL IN THIS PACKAGE WITH THE BYTES TO SEND
+static uint8_t tx_bytes[] = { 0b00000001, 0b00001111 };
+
+// FILL IN THIS VARIABLE WITH THE EXPECTED RESPONSE LENGTH
+#define EXPECTED_RESPONSE_LENGTH 2
+
+// FILL THIS VARIABLE WITH THE DESIRED SPI PORT
+static SpiPort port_to_use = SPI_PORT_2;
+
+const SpiSettings settings_to_use = {
+  .baudrate = 60000,
+  .mode = SPI_MODE_0,
+  // Adjust GPIO pins as needed
+  .mosi = { .port = 3, 7 },
+  .miso = { .port = 3, 8 },
+  .sclk = { .port = 3, 9 },
+  .cs = { .port = 3, 10 },
+};
+
+int main(void) {
+  gpio_init();
+  spi_init(port_to_use, &settings_to_use);
+
+  // Calculate transmission length
+  uint16_t tx_len = SIZEOF_ARRAY(tx_bytes);
+
+  // Allocate space for response
+  uint8_t response[EXPECTED_RESPONSE_LENGTH] = { 0 };
+
+  // Do the exchange
+  spi_exchange(port_to_use, tx_bytes, tx_len, response, EXPECTED_RESPONSE_LENGTH);
+
+  // Log the output
+  for (uint16_t i = 0; i < EXPECTED_RESPONSE_LENGTH; i++) {
+    LOG_DEBUG("Byte %x of response: %x\n", i, response[i]);
+  }
+}
+
