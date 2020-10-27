f79e93fcc12f5d184b73955912531fa8004d4eed
Ryan Dancy <ryan@keal.ca>
2020-10-27 02:16:08 +0000

    [SOFT-350] Fix mux + a bit more in smoke_bts7200

--- projects/smoke_bts7200/src/main.c
+++ projects/smoke_bts7200/src/main.c
@@ -2,14 +2,16 @@
 
 // Periodically take reading from user selected channels and log the result
 // Configurable items: wait time, FRONT or REAR power distro selection, channels to be tested
-#include "bts7200_load_switch.h"
-#include "current_measurement_config.h"
-#include "pca9539r_gpio_expander.h"
 
+#include "bts7200_load_switch.h"
+#include "controller_board_pins.h"
+#include "current_measurement_config.h"
 #include "gpio.h"
+#include "i2c.h"
 #include "interrupt.h"
 #include "log.h"
 #include "mux.h"
+#include "pca9539r_gpio_expander.h"
 #include "soft_timer.h"
 #include "wait.h"
 
@@ -20,6 +22,11 @@
 // Maximum number of channels that can be tested,
 // which is the same number of bts7200 used in front/rear power distribution
 #define MAX_TEST_CHANNELS 8
+
+#define I2C_PORT I2C_PORT_2
+#define PIN_I2C_SCL CONTROLLER_BOARD_ADDR_I2C2_SCL
+#define PIN_I2C_SDA CONTROLLER_BOARD_ADDR_I2C2_SDA
+
 // Set of channels to be tested, the array contains the indices of the BTS7200 in the front/rear HW
 // config Details at
 // https://uwmidsun.atlassian.net/wiki/spaces/ELEC/pages/1419740028/BTS7200+smoke+test+user+guide
@@ -45,6 +52,13 @@ int main() {
   gpio_init();
   interrupt_init();
   soft_timer_init();
+  adc_init(ADC_MODE_SINGLE);
+  I2CSettings i2c_settings = {
+    .speed = I2C_SPEED_FAST,
+    .sda = PIN_I2C_SDA,
+    .scl = PIN_I2C_SCL,
+  };
+  i2c_init(I2C_PORT, &i2c_settings);
 
   PowerDistributionCurrentHardwareConfig s_hw_config =
       IS_FRONT_POWER_DISTRO ? FRONT_POWER_DISTRIBUTION_CURRENT_HW_CONFIG
@@ -52,9 +66,18 @@ int main() {
 
   status_ok_or_return(mux_init(&s_hw_config.mux_address));
 
+  // Initialize the mux enable pin to low - CD74HC4067M96's enable pin is active low on power distro
+  GpioSettings mux_enable_pin_settings = {
+    .direction = GPIO_DIR_OUT,
+    .state = GPIO_STATE_LOW,
+    .resistor = GPIO_RES_NONE,
+    .alt_function = GPIO_ALTFN_NONE,
+  };
+  status_ok_or_return(gpio_init_pin(&s_hw_config.mux_enable_pin, &mux_enable_pin_settings));
+
   // Initialize and start the BTS7200s
   Bts7200Pca9539rSettings bts7200_settings = {
-    .sense_pin = &s_hw_config.mux_address.mux_output_pin,
+    .sense_pin = &s_hw_config.mux_output_pin,
     .i2c_port = s_hw_config.i2c_port,
   };
 
@@ -62,6 +85,16 @@ int main() {
     bts7200_settings.select_pin = &s_hw_config.bts7200s[s_test_channels[i]].dsel_pin;
     status_ok_or_return(bts7200_init_pca9539r(&s_bts7200_storages[i], &bts7200_settings));
   }
+
+  // enabling steering + front left light for convenience in smoke testing
+  Pca9539rGpioAddress steering_en = { .i2c_address = 0x76, .pin = PCA9539R_PIN_IO1_3 };
+  Pca9539rGpioAddress front_left_light_en = { .i2c_address = 0x76, .pin = PCA9539R_PIN_IO0_4 };
+  Pca9539rGpioSettings pca9539r_gpio_settings = {
+    .direction = PCA9539R_GPIO_DIR_OUT,
+    .state = PCA9539R_GPIO_STATE_HIGH,
+  };
+  pca9539r_gpio_init_pin(&steering_en, &pca9539r_gpio_settings);
+  pca9539r_gpio_init_pin(&front_left_light_en, &pca9539r_gpio_settings);
 
   soft_timer_start_millis(CURRENT_MEASURE_INTERVAL_MS, prv_read_and_log, &s_hw_config, NULL);
   while (true) {
