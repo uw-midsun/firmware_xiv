322a81092c9c3d312f5a9a803f57fb7f2b111a80
selinahsu <selinaraindrop@gmail.com>
2020-10-27 01:54:20 +0000

    comment cleanup

--- projects/baby_driver/scripts/adc_read.py
+++ projects/baby_driver/scripts/adc_read.py
@@ -2,10 +2,10 @@ import can_util
 from gpio_port import GpioPort
 from message_defs import BabydriverMessageId
 
-# Port number can be input as either an int or str
+# Port number can be entered as either an int or str
 def adc_read(port, pin, raw):
 
-    # If port is input as a str, convert to int
+    # If port is entered as a str, convert to int
     if isinstance(port, str): 
         port = getattr(GpioPort, port.capitalize())
 
@@ -23,10 +23,10 @@ def adc_read(port, pin, raw):
     # Send CAN message 
     can_util.send_message(data)
 
-    # Wait to receive a CAN message with data
+    # Wait to receive the first CAN message with data
     data_mssg = can_util.next_message()
 
-    # Check if received message is actually an adc read
+    # Check if received message is an adc_read
     if data_mssg.data[0] != BabydriverMessageId.ADC_READ_DATA:
         raise Exception("ERROR: did not receive adc read data") 
 
@@ -34,12 +34,12 @@ def adc_read(port, pin, raw):
     result_low = data_mssg.data[1]
     result_high = data_mssg.data[2]
 
-    # Wait to receive a CAN status message indicating the end of transaction
+    # Wait to receive the second CAN message with status
     status_mssg = can_util.next_message()
 
-    # Check if message status is not ok (nonzero)
+    # Check if status is not ok (if it's nonzero)
     if status_mssg.data[1] != BabydriverMessageId.STATUS:
         raise Exception("ERROR: received STATUS_CODE {}".format(status_mssg.data[1]))
 
-    # return ADC conversion values 
+    # Return ADC reading as a 16-bit int
     return (result_high << 8) | result_low
