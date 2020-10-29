91b57a0f7edf2d42f3177a6643471f1fe5d973df
Hewitt McGaughey <hewittmcgaughey@gmail.com>
2020-10-29 21:20:06 +0000

    saving temp changes

--- projects/mci/src/mci_broadcast.c
+++ projects/mci/src/mci_broadcast.c
@@ -22,7 +22,10 @@ static Mcp2515Storage s_mcp2515_storage;
 static MotorControllerCallbackStorage s_cb_storage;
 
 // Uncomment to test with only one motor controller
-#define RIGHT_MOTOR_CONTROLLER_UNUSED
+//#define RIGHT_MOTOR_CONTROLLER_UNUSED
+
+// get rid of local mcp2515 stuff
+//#define TESTING
 
 static void prv_broadcast_speed(MotorControllerBroadcastStorage *storage) {
   float *measurements = storage->measurements.vehicle_velocity;
@@ -38,6 +41,7 @@ static void prv_broadcast_bus_measurement(MotorControllerBroadcastStorage *stora
                                    (uint16_t)measurements[RIGHT_MOTOR_CONTROLLER].bus_current_a);
 }
 
+#ifndef TESTING
 // Change the MCP2515 filter to filter for the next ID to look for
 static void prv_change_filter(void) {
   if(s_cb_storage.offset == NUM_MCI_BROADCAST_MEASUREMENTS) {
@@ -56,10 +60,12 @@ static void prv_change_filter(void) {
 
 // CB for rx received
 static void prv_process_rx(uint32_t id, bool extended, uint64_t data, size_t dlc, void *context) {
+  LOG_DEBUG("real prv process rx\n");
   LOG_DEBUG("received rx from id: %d\n", (int)id);
   LOG_DEBUG("Data: 0x%x%x\n", (int)data, (int)(data >> 32));
   // this is kinda weird since the enum starts at 1, figure out a better way of doing
-  uint32_t cb_offset = s_cb_storage.motor_controller + s_cb_storage.offset - 1;
+  uint32_t cb_offset = s_cb_storage.motor_controller + s_cb_storage.offset - MCI_BROADCAST_MEASUREMENT_OFFSET;
+  
 
   // Only call CB if it exists
   if(s_cb_storage.callbacks[cb_offset] != NULL) {
@@ -110,7 +116,9 @@ static void prv_handle_bus_measurement_rx(const GenericCanMsg *msg, void *contex
   WaveSculptorCanData can_data = { .raw = msg->data };
 
   for (size_t motor_id = 0; motor_id < NUM_MOTOR_CONTROLLERS; motor_id++) {
+    LOG_DEBUG("can device id %d storage device id %d\n", (int)(can_id.device_id), (int)(storage->ids[motor_id]));
     if (can_id.device_id == storage->ids[motor_id]) {
+      LOG_DEBUG("logging bus rx from motor id %d\n", (int)motor_id);
       bool disabled = critical_section_start();
       measurements[motor_id] = can_data.bus_measurement;
       storage->bus_rx_bitset |= 1 << motor_id;
@@ -121,9 +129,15 @@ static void prv_handle_bus_measurement_rx(const GenericCanMsg *msg, void *contex
 
 static void prv_setup_motor_can(MotorControllerBroadcastStorage *storage) {
   // Set up callback storage
-  s_cb_storage.callbacks[MCI_BROADCAST_STATUS - MCI_BROADCAST_MEASUREMENT_OFFSET] = prv_handle_status_rx;
-  s_cb_storage.callbacks[MCI_BROADCAST_BUS - MCI_BROADCAST_MEASUREMENT_OFFSET] = prv_handle_bus_measurement_rx; 
-  s_cb_storage.callbacks[MCI_BROADCAST_VELOCITY - MCI_BROADCAST_MEASUREMENT_OFFSET] = prv_handle_speed_rx; 
+  if(s_cb_storage.callbacks[MCI_BROADCAST_STATUS - MCI_BROADCAST_MEASUREMENT_OFFSET] == NULL) {
+    s_cb_storage.callbacks[MCI_BROADCAST_STATUS - MCI_BROADCAST_MEASUREMENT_OFFSET] = prv_handle_status_rx;
+  }
+  if(s_cb_storage.callbacks[MCI_BROADCAST_BUS - MCI_BROADCAST_MEASUREMENT_OFFSET] == NULL) {
+    s_cb_storage.callbacks[MCI_BROADCAST_BUS - MCI_BROADCAST_MEASUREMENT_OFFSET] = prv_handle_bus_measurement_rx; 
+  }
+  if(s_cb_storage.callbacks[MCI_BROADCAST_VELOCITY - MCI_BROADCAST_MEASUREMENT_OFFSET] == NULL) {
+    s_cb_storage.callbacks[MCI_BROADCAST_VELOCITY - MCI_BROADCAST_MEASUREMENT_OFFSET] = prv_handle_speed_rx; 
+  }
 
   s_cb_storage.offset = MCI_BROADCAST_STATUS;
   s_cb_storage.motor_controller = LEFT_MOTOR_CONTROLLER;
@@ -148,10 +162,11 @@ static void prv_setup_motor_can(MotorControllerBroadcastStorage *storage) {
   mcp2515_init(&s_mcp2515_storage, &mcp2515_settings);
   mcp2515_register_cbs(&s_mcp2515_storage, prv_process_rx, NULL, storage);
 }
-
+#endif
 
 static void prv_periodic_broadcast_tx(SoftTimerId timer_id, void *context) {
   MotorControllerBroadcastStorage *storage = context;
+  LOG_DEBUG("in here\n");
   if (storage->velocity_rx_bitset == (1 << NUM_MOTOR_CONTROLLERS) - 1) {
     // Received speed from all motor controllers - clear bitset and broadcast
     storage->velocity_rx_bitset = 0;
@@ -162,6 +177,7 @@ static void prv_periodic_broadcast_tx(SoftTimerId timer_id, void *context) {
     storage->bus_rx_bitset = 0;
     prv_broadcast_bus_measurement(storage);
   }
+  LOG_DEBUG("out\n");
   // TODO(SOFT-139): Send status messages here as well
   soft_timer_start_millis(MOTOR_CONTROLLER_BROADCAST_TX_PERIOD_MS, prv_periodic_broadcast_tx,
                           storage, NULL);
@@ -174,8 +190,9 @@ StatusCode mci_broadcast_init(MotorControllerBroadcastStorage *storage,
   }
   storage->velocity_rx_bitset = 0;
   storage->bus_rx_bitset = 0;
-
-  prv_setup_motor_can(storage);
+  #ifndef TESTING
+    prv_setup_motor_can(storage);
+  #endif
   return soft_timer_start_millis(MOTOR_CONTROLLER_BROADCAST_TX_PERIOD_MS, prv_periodic_broadcast_tx,
                                  storage, NULL);
   return STATUS_CODE_OK;
