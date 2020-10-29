a201453f5ebc5ff5ee7fec3f163d15324532c18c
g-jessmuir <e.jessmuir@gmail.com>
2020-10-28 21:07:00 -0400

    added locks to logging

--- libraries/libcore/inc/log.h
+++ libraries/libcore/inc/log.h
@@ -31,12 +31,29 @@ typedef enum {
     }                                                                         \
   } while (0)
 #else
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+static pthread_mutex_t s_sig_lock = PTHREAD_MUTEX_INITIALIZER;
+// signal handler for catching parent
+static void prv_sigusr2(int signo) {
+  pthread_mutex_unlock(&s_sig_lock);
+  return;
+}
 // fflush necessary for printing through pipes
 #define LOG(level, fmt, ...)                                                  \
   do {                                                                        \
     if ((level) >= LOG_LEVEL_VERBOSITY) {                                     \
+      pthread_mutex_lock(&s_sig_lock);                                        \
       printf("[%u] %s:%u: " fmt, (level), __FILE__, __LINE__, ##__VA_ARGS__); \
       fflush(stdout);                                                         \
+      pthread_mutex_lock(&s_sig_lock);                                        \
+      pthread_mutex_unlock(&s_sig_lock);                                      \
     }                                                                         \
   } while (0)
 #endif
