15b9355dc3dbbfe6216d5713ae5c7273ea7c96ed
Max <max.zhu.sss@gmail.com>
2020-10-29 02:53:32 +0000

    test not working

--- mpxe/integration_tests/test_ads1259.py
+++ mpxe/integration_tests/test_ads1259.py
@@ -7,13 +7,21 @@ from mpxe.sims.ads1259 import Ads1259
 class TestAds1259(int_test.IntTest):
     def setUp(self):
         super(TestAds1259, self).setUp()
-        self.ads1259 = self.manager.start('smoke_ads1259', Ads1259())
-        
+        self.ads1259 = self.manager.start('smoke_ads1259', Ads1259())        
 
     def test_ads1259(self):
-        time.sleep(0.5)
-        self.ads1259.sim.update_ads_reading(self.ads1259, 0x9A)        
-        time.sleep(3)
+        time.sleep(1)
+        self.ads1259.sim.update_ads_reading(self.ads1259, 0x9A)       
+        # self.assertEqual(self.ads1259.stores["rx_data_lsb"], 0x9A)
+        time.sleep(4)
+
+        self.ads1259.sim.read_stores(self.ads1259)
+        # self.assertEqual(self.ads1259.sim.set_gpio, 0x9A)
+        print("done")
+
+        # print(self.ads1259.stores.items
+        # print(self.ads1259.
+        # self.assertEqual(self.ads1259.stores["rx_data_msb"], 0x9A)
 
 if __name__ == '__main__':
     unittest.main()
--- mpxe/sims/ads1259.py
+++ mpxe/sims/ads1259.py
@@ -8,6 +8,11 @@ from mpxe.sims import sim
 ADS1259_KEY = (stores_pb2.MxStoreType.ADS1259, 0)
 
 class Ads1259(sim.Sim):
+    def read_stores(self, proj):
+        
+        if ADS1259_KEY in proj.stores:
+            return proj.stores[ADS1259_KEY].rx_data_lsb
+
     def update_ads_reading(self, proj, val):
         ads1259_msg = ads1259_pb2.MxAds1259Store()
         ads1259_msg.rx_data_lsb = val
