f8ebb9eb54da30339b1c57a28dc319bdbe5c63cd
Hewitt McGaughey <hewittmcgaughey@gmail.com>
2020-10-30 20:47:32 +0000

    got periodic broadcasts working, made implementation a bit cleaner

--- projects/mci/src/mci_broadcast.c
+++ projects/mci/src/mci_broadcast.c
@@ -21,6 +21,14 @@ static Mcp2515Storage s_mcp2515_storage;
 
 static MotorControllerCallbackStorage s_cb_storage;
 
+static const MciBroadcastMeasurementType MCI_BROADCAST_MEASUREMENT_OFFSET_LOOKUP[NUM_MCI_BROADCAST_MEASUREMENTS] = {
+  [MCI_BROADCAST_STATUS] = MCI_BROADCAST_STATUS_OFFSET,
+  [MCI_BROADCAST_BUS] = MCI_BROADCAST_BUS_OFFSET, 
+  [MCI_BROADCAST_VELOCITY] = MCI_BROADCAST_VELOCITY_OFFSET, 
+  [MCI_BROADCAST_MOTOR_TEMP] = MCI_BROADCAST_MOTOR_TEMP_OFFSET, 
+  [MCI_BROADCAST_DSP_TEMP] = MCI_BROADCAST_DSP_TEMP_OFFSET 
+};
+
 // Uncomment when using with only the left motor controller
 //#define RIGHT_MOTOR_CONTROLLER_UNUSED
 
@@ -40,15 +48,17 @@ static void prv_broadcast_bus_measurement(MotorControllerBroadcastStorage *stora
 
 // Change the MCP2515 filter to filter for the next ID to look for
 static void prv_change_filter(void) {
-  if(s_cb_storage.offset == NUM_MCI_BROADCAST_MEASUREMENTS) {
-    s_cb_storage.offset = MCI_BROADCAST_STATUS;
+  if(s_cb_storage.cur_measurement == NUM_MCI_BROADCAST_MEASUREMENTS - 1) {
+    s_cb_storage.cur_measurement = MCI_BROADCAST_STATUS;
+    LOG_DEBUG("reset to status broadcast\n");
     #ifndef RIGHT_MOTOR_CONTROLLER_UNUSED 
       s_cb_storage.motor_controller = !s_cb_storage.motor_controller;
     #endif
   } else {
-    s_cb_storage.offset++;
+    s_cb_storage.cur_measurement++;
   }
-  uint32_t filter = (uint32_t)motor_controller_base_addr_lookup(s_cb_storage.motor_controller) + s_cb_storage.offset;
+  uint32_t filter = (uint32_t)MOTOR_CONTROLLER_BASE_ADDR_LOOKUP(s_cb_storage.motor_controller) +
+  (uint32_t)MCI_BROADCAST_MEASUREMENT_OFFSET_LOOKUP[s_cb_storage.cur_measurement];
   LOG_DEBUG("Changing filter to %x\n", (int)filter);
   uint32_t filters[2] = {MCI_ID_UNUSED, filter};
   LOG_DEBUG("Change filter result %d\n", mcp2515_set_filter(&s_mcp2515_storage, filters));
@@ -59,7 +69,7 @@ static void prv_process_rx(uint32_t id, bool extended, uint64_t data, size_t dlc
   LOG_DEBUG("received rx from id: %d\n", (int)id);
   LOG_DEBUG("Data: 0x%x%x\n", (int)data, (int)(data >> 32));
   // this is kinda weird since the enum starts at 1, figure out a better way of doing
-  uint32_t cb_offset = s_cb_storage.motor_controller + s_cb_storage.offset - MCI_BROADCAST_MEASUREMENT_OFFSET;
+  uint32_t cb_offset = (uint32_t)(s_cb_storage.cur_measurement);
   
 
   // Only call CB if it exists
@@ -111,7 +121,7 @@ static void prv_handle_bus_measurement_rx(const GenericCanMsg *msg, void *contex
 
   WaveSculptorCanId can_id = { .raw = msg->id };
   WaveSculptorCanData can_data = { .raw = msg->data };
-
+  LOG_DEBUG("device id %d\n", can_id.device_id);
   for (size_t motor_id = 0; motor_id < NUM_MOTOR_CONTROLLERS; motor_id++) {
     if (can_id.device_id == storage->ids[motor_id]) {
       bool disabled = critical_section_start();
@@ -125,14 +135,14 @@ static void prv_handle_bus_measurement_rx(const GenericCanMsg *msg, void *contex
 static void prv_setup_motor_can(MotorControllerBroadcastStorage *storage) {
   // Set up callbacks and define in storage
   // TODO(SOFT-353): move the cb storage into MotorControllerBroadcastStorage
-  s_cb_storage.callbacks[MCI_BROADCAST_STATUS - MCI_BROADCAST_MEASUREMENT_OFFSET] = prv_handle_status_rx;
-  storage->callbacks[MCI_BROADCAST_STATUS - MCI_BROADCAST_MEASUREMENT_OFFSET] = prv_handle_status_rx;
-  s_cb_storage.callbacks[MCI_BROADCAST_BUS - MCI_BROADCAST_MEASUREMENT_OFFSET] = prv_handle_bus_measurement_rx; 
-  storage->callbacks[MCI_BROADCAST_BUS - MCI_BROADCAST_MEASUREMENT_OFFSET] = prv_handle_bus_measurement_rx; 
-  s_cb_storage.callbacks[MCI_BROADCAST_VELOCITY - MCI_BROADCAST_MEASUREMENT_OFFSET] = prv_handle_speed_rx; 
-  storage->callbacks[MCI_BROADCAST_VELOCITY - MCI_BROADCAST_MEASUREMENT_OFFSET] = prv_handle_speed_rx; 
+  s_cb_storage.callbacks[MCI_BROADCAST_STATUS] = prv_handle_status_rx;
+  storage->callbacks[MCI_BROADCAST_STATUS] = prv_handle_status_rx;
+  s_cb_storage.callbacks[MCI_BROADCAST_BUS] = prv_handle_bus_measurement_rx; 
+  storage->callbacks[MCI_BROADCAST_BUS] = prv_handle_bus_measurement_rx; 
+  s_cb_storage.callbacks[MCI_BROADCAST_VELOCITY] = prv_handle_speed_rx; 
+  storage->callbacks[MCI_BROADCAST_VELOCITY] = prv_handle_speed_rx; 
 
-  s_cb_storage.offset = MCI_BROADCAST_STATUS;
+  s_cb_storage.cur_measurement = MCI_BROADCAST_STATUS;
   s_cb_storage.motor_controller = LEFT_MOTOR_CONTROLLER;
   
   Mcp2515Settings mcp2515_settings = {
@@ -148,7 +158,7 @@ static void prv_setup_motor_can(MotorControllerBroadcastStorage *storage) {
     .loopback = false,
     .filters = {
       [MCP2515_FILTER_ID_RXF0] = {.raw = MCI_ID_UNUSED}, // only want to use one filter
-      [MCP2515_FILTER_ID_RXF1] = {.raw = (uint32_t)(LEFT_MOTOR_CONTROLLER_BASE_ADDR + s_cb_storage.offset)},
+      [MCP2515_FILTER_ID_RXF1] = {.raw = (uint32_t)(LEFT_MOTOR_CONTROLLER_BASE_ADDR + s_cb_storage.cur_measurement)},
     },
   };
   mcp2515_init(&s_mcp2515_storage, &mcp2515_settings);
@@ -156,16 +166,18 @@ static void prv_setup_motor_can(MotorControllerBroadcastStorage *storage) {
 }
 
 static void prv_periodic_broadcast_tx(SoftTimerId timer_id, void *context) {
-  LOG_DEBUG("TXing periodic broadcast\n");
   MotorControllerBroadcastStorage *storage = context;
+  LOG_DEBUG("velocity rx bitset: %d, bus bitset: %d\n", storage->velocity_rx_bitset, storage->bus_rx_bitset);
   if (storage->velocity_rx_bitset == (1 << NUM_MOTOR_CONTROLLERS) - 1) {
     // Received speed from all motor controllers - clear bitset and broadcast
     storage->velocity_rx_bitset = 0;
+    LOG_DEBUG("sending velocity periodic broadcast\n");
     prv_broadcast_speed(storage);
   }
   if (storage->bus_rx_bitset == (1 << NUM_MOTOR_CONTROLLERS) - 1) {
     // Received bus measurements from all motor controllers - clear bitset and broadcast
     storage->bus_rx_bitset = 0;
+    LOG_DEBUG("sending bus periodic broadcast\n");
     prv_broadcast_bus_measurement(storage);
   }
   // TODO(SOFT-139): Send status messages here as well
