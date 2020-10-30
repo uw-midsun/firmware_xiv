03017bc5ae9d2937b41e37fab18fac572947395a
Max <max.zhu.sss@gmail.com>
2020-10-30 03:23:56 +0000

    asserts

--- mpxe/integration_tests/test_ads1259.py
+++ mpxe/integration_tests/test_ads1259.py
@@ -11,8 +11,12 @@ class TestAds1259(int_test.IntTest):
 
     def test_ads1259(self):
         time.sleep(1)
-        self.ads1259.sim.update_ads_reading(self.ads1259, 0x9A)       
-        time.sleep(4)
+        self.ads1259.sim.update_ads_reading(self.ads1259, 0x9A, 0x9B, 0x9C)       
+        time.sleep(4) # smoke test runs
+        self.ads1259.sim.assert_store_value_lsb(self.ads1259, 0x9A)
+        self.ads1259.sim.assert_store_value_mid(self.ads1259, 0x9B)
+        self.ads1259.sim.assert_store_value_msb(self.ads1259, 0x9C)
+
 
 if __name__ == '__main__':
     unittest.main()
--- mpxe/sims/ads1259.py
+++ mpxe/sims/ads1259.py
@@ -8,16 +8,12 @@ from mpxe.sims import sim
 ADS1259_KEY = (stores_pb2.MxStoreType.ADS1259, 0)
 
 class Ads1259(sim.Sim):
-    def read_stores(self, proj):
-        
-        if ADS1259_KEY in proj.stores:
-            return proj.stores[ADS1259_KEY].rx_data_lsb
 
-    def update_ads_reading(self, proj, val):
+    def update_ads_reading(self, proj, lsb_val, mid_val, msb_val):
         ads1259_msg = ads1259_pb2.MxAds1259Store()
-        ads1259_msg.rx_data_lsb = val
-        ads1259_msg.rx_data_mid = val
-        ads1259_msg.rx_data_msb = val
+        ads1259_msg.rx_data_lsb = lsb_val
+        ads1259_msg.rx_data_mid = mid_val
+        ads1259_msg.rx_data_msb = msb_val
 
         ads1259_mask = ads1259_pb2.MxAds1259Store()
         ads1259_mask.rx_data_lsb = 1
@@ -25,3 +21,13 @@ class Ads1259(sim.Sim):
         ads1259_mask.rx_data_msb = 1
 
         proj.write_store(stores_pb2.MxStoreType.ADS1259, ads1259_msg, ads1259_mask)
+
+    def assert_store_value_lsb(self, proj, reading):
+        assert(proj.stores[ADS1259_KEY].rx_data_lsb  == reading) 
+    
+    def assert_store_value_mid(self, proj, reading):
+        assert(proj.stores[ADS1259_KEY].rx_data_mid  == reading) 
+    
+    def assert_store_value_msb(self, proj, reading):
+        assert(proj.stores[ADS1259_KEY].rx_data_msb  == reading) 
+
