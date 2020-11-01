359a20957e87a417b8e024b5a3fe19b5ad6dcef2
vagrant <vagrant@midsunbox>
2020-11-01 02:47:05 +0000

    added description for gpio_set module

--- projects/baby_driver/inc/gpio_set.h
+++ projects/baby_driver/inc/gpio_set.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// This module sets a specific gpio pin to a desired state
+// Requires dispatcher, gpio, and CAN to be initialized.
+
 #include "status.h"
 
 // initialize the module
