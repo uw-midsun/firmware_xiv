1cba37e8d7b0cea0041ae67fcf58999733a6a0be
g-jessmuir <e.jessmuir@gmail.com>
2020-11-08 13:37:04 -0500

    Add logging to precharge

--- projects/mci/src/precharge_control.c
+++ projects/mci/src/precharge_control.c
@@ -18,6 +18,16 @@ PrechargeControlStorage *test_get_storage(void) {
   return &s_precharge_storage;
 }
 
+static void prv_check_pin_states(void) {
+  uint8_t mon = 0;
+  uint8_t mon2 = 0;
+  gpio_get_state(&s_precharge_storage.precharge_monitor, &mon);
+  gpio_get_state(&s_precharge_storage.precharge_monitor2, &mon2);
+  LOG_DEBUG("Precharge monitors %d.%d: %d, %d.%d: %d\n", s_precharge_storage.precharge_monitor.port,
+            s_precharge_storage.precharge_monitor.pin, s_precharge_storage.precharge_monitor2.port,
+            s_precharge_storage.precharge_monitor2.pin, mon, mon2);
+}
+
 StatusCode prv_set_precharge_control(PrechargeControlStorage *storage, const GpioState state) {
   gpio_set_state(&storage->precharge_control, state);
   return STATUS_CODE_OK;
@@ -25,12 +35,16 @@ StatusCode prv_set_precharge_control(PrechargeControlStorage *storage, const Gpi
 
 void prv_precharge_monitor(const GpioAddress *address, void *context) {
   PrechargeControlStorage *storage = context;
+  LOG_DEBUG("precharge interrupt\n");
+  prv_check_pin_states();
   if (storage->state == MCI_PRECHARGE_DISCHARGED) {
     // inconsistent until second precharge result
     storage->state = MCI_PRECHARGE_INCONSISTENT;
+    LOG_DEBUG("precharge state moved to inconsistent\n");
   } else {
     // both pins are in sync
     storage->state = MCI_PRECHARGE_CHARGED;
+    LOG_DEBUG("precharge state moved to complete\n");
     CAN_TRANSMIT_PRECHARGE_COMPLETED();
   }
 }
@@ -43,6 +57,7 @@ StatusCode prv_discharge_rx(const CanMessage *msg, void *context, CanAckStatus *
 
 StatusCode prv_precharge_rx(const CanMessage *msg, void *context, CanAckStatus *ack_reply) {
   PrechargeControlStorage *storage = context;
+  LOG_DEBUG("precharge rx, setting control\n");
   return prv_set_precharge_control(storage, GPIO_STATE_HIGH);
 }
 
