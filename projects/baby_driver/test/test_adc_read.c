b2d792d7ea0340fd1618744f42d5d08a111051d8
Raiyan Sayeed <raiyan.business@gmail.com>
2020-11-20 18:35:38 +0000

    added test for invalid arguments, tests are passing

--- projects/baby_driver/test/test_adc_read.c
+++ projects/baby_driver/test/test_adc_read.c
@@ -51,7 +51,6 @@ StatusCode TEST_MOCK(adc_read_converted_pin)(GpioAddress address, uint16_t *read
   *reading = X86READING;
   s_low = s_mock_reading & 0xff;
   s_high = (s_mock_reading >> 8) & 0xff;
-  LOG_DEBUG("s_low:  %d\n", s_low);
   return STATUS_CODE_OK;
 }
 
@@ -63,23 +62,6 @@ static StatusCode prv_rx_adc_read_callback(uint8_t data[8], void *context, bool 
   return s_status_return;
 }
 
-// static StatusCode prv_test_adc_read_callback_handler(uint8_t data[8], void *context,
-//                                                      bool *tx_result) {
-//   s_received_context = context;
-//   *tx_result = s_should_tx_result;
-
-//   // // todo RAIYAN
-//   // // Two callbacks: one for raw read, another for converted read
-//   // if (s_times_callback_called < 2) {
-//   //   CAN_UNPACK_BABYDRIVER(NULL, NULL, &s_received_data[0], &s_received_data[1],
-//   &s_received_data[2],
-//   //                         &s_received_data[3], NULL, NULL, NULL);
-//   //   s_times_callback_called++;
-//   //   return STATUS_CODE_OK;
-//   // }
-//   // return STATUS_CODE_RESOURCE_EXHAUSTED;
-// }
-
 void setup_test(void) {
   TEST_ASSERT_OK(initialize_can_and_dependencies(&s_can_storage, SYSTEM_CAN_DEVICE_BABYDRIVER,
                                                  TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX,
@@ -90,23 +72,12 @@ void setup_test(void) {
   s_times_callback_called = 0;
   memset(s_received_data, 0, sizeof(s_received_data));
   s_received_context = NULL;
-  // s_low = 0;
-  // s_high = 0;
-
-  // TEST_ASSERT_OK(dispatcher_register_callback(BABYDRIVER_MESSAGE_STATUS,
-  // prv_rx_adc_read_callback, NULL));
-  // TEST_ASSERT_OK(dispatcher_register_callback(BABYDRIVER_MESSAGE_ADC_READ_DATA,
-  // prv_rx_adc_read_callback, NULL));
 }
 
 void teardown_test(void) {}
 
 // Test that raw data command works
 void test_adc_read_raw(void) {
-  // TEST_ASSERT_OK(dispatcher_register_callback(BABYDRIVER_MESSAGE_ADC_READ_COMMAND,
-  //                                             prv_test_adc_read_callback_handler, NULL));
-  s_should_tx_result = true;
-
   TEST_ASSERT_OK(dispatcher_register_callback(BABYDRIVER_MESSAGE_ADC_READ_DATA,
                                               prv_rx_adc_read_callback, NULL));
   TEST_ASSERT_OK(
@@ -148,7 +119,7 @@ void test_adc_read_raw(void) {
   // TEST_ASSERT_EQUAL_INT8(HIGH_BYTE, s_received_data[3]);
   TEST_ASSERT_EQUAL_INT8(HIGH_BYTE, s_high);
 
-  MS_TEST_HELPER_CAN_TX(TEST_CAN_EVENT_TX);
+  // MS_TEST_HELPER_CAN_TX(TEST_CAN_EVENT_TX);
   MS_TEST_HELPER_CAN_RX(TEST_CAN_EVENT_RX);
 
   // RAIYAN change array indices to understandable macros e.g LOW_BYTE = 1
@@ -159,7 +130,7 @@ void test_adc_read_raw(void) {
 
 // Test that converted data command works
 void test_adc_read_converted(void) {
-  s_should_tx_result = true;
+  // s_should_tx_result = true;
 
   TEST_ASSERT_OK(dispatcher_register_callback(BABYDRIVER_MESSAGE_ADC_READ_DATA,
                                               prv_rx_adc_read_callback, NULL));
@@ -194,16 +165,67 @@ void test_adc_read_converted(void) {
   MS_TEST_HELPER_CAN_TX(TEST_CAN_EVENT_TX);
   MS_TEST_HELPER_CAN_RX(TEST_CAN_EVENT_RX);
 
-  TEST_ASSERT_EQUAL(BABYDRIVER_MESSAGE_STATUS, s_received_data[0]);
+  TEST_ASSERT_EQUAL(BABYDRIVER_MESSAGE_ADC_READ_DATA, s_received_data[0]);
   TEST_ASSERT_EQUAL(BABYDRIVER_MESSAGE_ADC_READ_DATA, s_received_data[1]);
 
-  TEST_ASSERT_EQUAL_INT8(1, s_received_data[2]);  // confused on what to put for "expected" here.
-  TEST_ASSERT_EQUAL_INT8(1, s_received_data[3]);
+  TEST_ASSERT_EQUAL_INT8(LOW_BYTE,
+                         s_received_data[2]);  // confused on what to put for "expected" here.
+  TEST_ASSERT_EQUAL_INT8(HIGH_BYTE, s_received_data[3]);
 
-  MS_TEST_HELPER_CAN_TX(TEST_CAN_EVENT_TX);
+  // MS_TEST_HELPER_CAN_TX(TEST_CAN_EVENT_TX);
   MS_TEST_HELPER_CAN_RX(TEST_CAN_EVENT_RX);
 
   TEST_ASSERT_EQUAL(2, s_times_callback_called);
 }
 
-// Test that dispatch sends error if not pin data is incorrect
+// Test that dispatch sends error if pin data is incorrect
+void test_invalid_read(void) {
+  TEST_ASSERT_OK(dispatcher_register_callback(BABYDRIVER_MESSAGE_ADC_READ_DATA,
+                                              prv_rx_adc_read_callback, NULL));
+  TEST_ASSERT_OK(
+      dispatcher_register_callback(BABYDRIVER_MESSAGE_STATUS, prv_rx_adc_read_callback, NULL));
+
+  TEST_ASSERT_EQUAL(0, s_times_callback_called);
+
+  uint8_t inv_port_data[7] = { NUM_GPIO_PORTS,
+                               1,  // random pin on invalid port
+                               0,  // converted read
+                               0,
+                               0,
+                               0,
+                               0 };
+
+  uint8_t inv_pin_data[7] = { 1, GPIO_TOTAL_PINS, 0, 0, 0, 0, 0 };
+
+  // invalid port
+  CAN_TRANSMIT_BABYDRIVER(BABYDRIVER_MESSAGE_ADC_READ_COMMAND, inv_port_data[0], inv_port_data[1],
+                          inv_port_data[2], inv_port_data[3], inv_port_data[4], inv_port_data[5],
+                          inv_port_data[6]);
+
+  MS_TEST_HELPER_CAN_TX(TEST_CAN_EVENT_TX);
+  MS_TEST_HELPER_CAN_RX(TEST_CAN_EVENT_RX);
+
+  MS_TEST_HELPER_CAN_TX(TEST_CAN_EVENT_TX);
+  MS_TEST_HELPER_CAN_RX(TEST_CAN_EVENT_RX);
+
+  TEST_ASSERT_EQUAL(BABYDRIVER_MESSAGE_STATUS, s_received_data[0]);
+  TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, s_received_data[1]);
+
+  // reset data to make sure second CAN message truly has invalid status code
+  s_received_data[0] = NUM_BABYDRIVER_MESSAGES;
+  s_received_data[1] = NUM_STATUS_CODES;
+
+  // invalid pin
+  CAN_TRANSMIT_BABYDRIVER(BABYDRIVER_MESSAGE_ADC_READ_COMMAND, inv_pin_data[0], inv_pin_data[1],
+                          inv_pin_data[2], inv_pin_data[3], inv_pin_data[4], inv_pin_data[5],
+                          inv_pin_data[6]);
+
+  MS_TEST_HELPER_CAN_TX(TEST_CAN_EVENT_TX);
+  MS_TEST_HELPER_CAN_RX(TEST_CAN_EVENT_RX);
+
+  MS_TEST_HELPER_CAN_TX(TEST_CAN_EVENT_TX);
+  MS_TEST_HELPER_CAN_RX(TEST_CAN_EVENT_RX);
+
+  TEST_ASSERT_EQUAL(BABYDRIVER_MESSAGE_STATUS, s_received_data[0]);
+  TEST_ASSERT_EQUAL(STATUS_CODE_INVALID_ARGS, s_received_data[1]);
+}
