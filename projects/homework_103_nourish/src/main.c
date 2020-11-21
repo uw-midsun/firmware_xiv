22b8cac3ec97be101ca7eac4da53eb28d5a1ecae
Nourish <nourishnew@gmail.com>
2020-11-21 18:37:25 +0000

    final changed

--- projects/homework_103_nourish/src/main.c
+++ projects/homework_103_nourish/src/main.c
@@ -14,38 +14,38 @@ static void prv_interrupt_callback_fn(const GpioAddress *address, void *context)
 }
 
 int main(void) {
-interrupt_init();
-gpio_init();
-gpio_it_init();
-soft_timer_init();
-GpioAddress adc_addr = { .port = GPIO_PORT_A, .pin = 6 };
-GpioAddress button_addr = { .port = GPIO_PORT_B, .pin = 2 };
-GpioSettings adc_settings = {
+  interrupt_init();
+  gpio_init();
+  gpio_it_init();
+  soft_timer_init();
+  GpioAddress adc_addr = { .port = GPIO_PORT_A, .pin = 6 };
+  GpioAddress button_addr = { .port = GPIO_PORT_B, .pin = 2 };
+  GpioSettings adc_settings = {
     .direction = GPIO_DIR_IN,
     .state = GPIO_STATE_LOW,
     .alt_function = GPIO_ALTFN_ANALOG,
     .resistor = GPIO_RES_NONE,
-};
-GpioSettings button_settings = {
+  };
+  GpioSettings button_settings = {
     .direction = GPIO_DIR_IN,
     .state = GPIO_STATE_LOW,
     .alt_function = GPIO_ALTFN_NONE,
     .resistor = GPIO_RES_NONE,
-};
-InterruptSettings interrupt_settings = {
-        .type = INTERRUPT_TYPE_INTERRUPT,
-        .priority = INTERRUPT_PRIORITY_NORMAL,
-};
-gpio_init_pin(&adc_addr, &adc_settings);
-gpio_init_pin(&button_addr, &button_settings);
-adc_init(ADC_MODE_SINGLE);
-AdcChannel adc_channel = NUM_ADC_CHANNELS;
-adc_get_channel(adc_addr, &adc_channel);
-adc_set_channel(adc_channel, true);
-gpio_it_register_interrupt(&button_addr, &interrupt_settings, INTERRUPT_EDGE_FALLING,
-                           prv_interrupt_callback_fn, &adc_addr);
-while (true) {
-wait();
-}
-return 0;
+  };
+  InterruptSettings interrupt_settings = {
+    .type = INTERRUPT_TYPE_INTERRUPT,
+    .priority = INTERRUPT_PRIORITY_NORMAL,
+  };
+  gpio_init_pin(&adc_addr, &adc_settings);
+  gpio_init_pin(&button_addr, &button_settings);
+  adc_init(ADC_MODE_SINGLE);
+  AdcChannel adc_channel = NUM_ADC_CHANNELS;
+  adc_get_channel(adc_addr, &adc_channel);
+  adc_set_channel(adc_channel, true);
+  gpio_it_register_interrupt(&button_addr, &interrupt_settings, INTERRUPT_EDGE_FALLING,
+                             prv_interrupt_callback_fn, &adc_addr);
+  while (true) {
+    wait();
+  }
+  return 0;
 }
