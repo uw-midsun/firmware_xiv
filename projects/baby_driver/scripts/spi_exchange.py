07ab654a097fe9ee6d7eb03887f08a31cfa158c4
Raiyan Sayeed <raiyan.business@gmail.com>
2020-11-25 04:00:45 +0000

    wrote data definitions

--- projects/baby_driver/scripts/spi_exchange.py
+++ projects/baby_driver/scripts/spi_exchange.py
@@ -17,17 +17,50 @@ def spi_exchange(tx_bytes, rx_len, spi_port=1, spi_mode=0, baudrate=6000000, cs=
     Raises: 
         AttributeError: if port parameter is entered as a string and is invalid 
     """
-
-    assert (spi_port == 1) or (spi_port == 2)
-    assert (spi_mode >= 0) and (spi_mode <= 3)
  
     if isinstance(port, str):
         port = getattr(GpioPort, port.capitalize())
 
-    if port < 0 or port >= GpioPort.NUM_GPIO_PORTS:
+    if spi_port < 0 or port >= GpioPort.NUM_GPIO_PORTS:
         raise ValueError("Expected port between A and {}".format(
             chr(GpioPort.NUM_GPIO_PORTS + ord('A') - 1)
         ))
+    if spi_mode < 0 or spi_mode > 3:
+        raise ValueError("Expected mode between 0 and 3")
+
+    if cs == None:
+        cs_port = 0
+        cs_pin = 0
+        use_cs = 0
+    else:
+        cs_port = cs[0]
+        cs_pin = cs[1] 
+        use_cs = 1
+
+    data1 = [
+        (10, 1), # id
+        (spi_port, 1), 
+        (spi_mode, 1),
+        (len(tx_bytes), 1), # tx_len
+        (rx_len, 1),
+        (cs_port, 1),
+        (cs_pin, 1),
+        (use_cs, 1)
+    ]
+
+    data2 = [
+        (11, 1), # id
+        (baudrate, 4)
+    ]
+
+    data1 = can_util.can_pack(data1)
+    data2 = can_util.can_pack(data2)
+
+    # loop with step of len(tx_bytes)
+
+    can_util.send_message(
+        babydriver_id=BabydriverMessageId.SPI_EXCHANGE_METADATA_1
+    )
 
     
 
