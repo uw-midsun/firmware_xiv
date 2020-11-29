db30abde5f19d40ed9077adc2ac1ddff774fe0b7
Nourish <nourishnew@gmail.com>
2020-11-29 18:11:15 +0000

    smoke test for drv120

--- projects/smoke_drv120/src/main.c
+++ projects/smoke_drv120/src/main.c
@@ -23,13 +23,13 @@ static void prv_error_handler(void *context) {
 static void prv_timer_callback(SoftTimerId timer_id, void *context) {
   bool *toggle = context;
 
-  if (toggle) {
+  if (*toggle) {
     drv120_relay_open();
-    toggle = false;
+    *toggle = false;
 
   } else {
     drv120_relay_close();
-    toggle = true;
+    *toggle = true;
   }
 
   soft_timer_start_millis(TIME_INTERVAL, prv_timer_callback, toggle, NULL);
@@ -39,16 +39,16 @@ int main(void) {
   soft_timer_init();
   gpio_it_init();
   gpio_init();
-  GpioAddress *status_pin_address = &STATUS_PIN;
-  GpioAddress *enable_pin_address = &ENABLE_PIN;
+  GpioAddress status_pin_address = STATUS_PIN;
+  GpioAddress enable_pin_address = ENABLE_PIN;
   bool toggle = true;
 
-  Drv120RelaySettings *relay_settings = { .enable_pin = enable_pin_address,
-                                          .status_pin = status_pin_address,
-                                          .error_handler = prv_error_handler,
-                                          null };
+  Drv120RelaySettings relay_settings = { .enable_pin = &enable_pin_address,
+                                         .status_pin = &status_pin_address,
+                                         .error_handler = prv_error_handler,
+                                         NULL };
 
-  drv120_relay_init(relay_settings);
+  drv120_relay_init(&relay_settings);
   soft_timer_start_millis(TIME_INTERVAL, prv_timer_callback, &toggle, NULL);
 
   while (true) {
