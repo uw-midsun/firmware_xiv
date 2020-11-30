111f964ac1ebef0d1afd425ba92ea6e722a21280
Cheran Mahalingam <cheranm@outlook.com>
2020-11-30 02:20:22 +0000

    i2c with test issues

--- projects/baby_driver/src/i2c_write.c
+++ projects/baby_driver/src/i2c_write.c
@@ -7,6 +7,7 @@
 #include "gpio.h"
 #include "i2c.h"
 #include "log.h"
+#include "wait.h"
 
 #define I2C1_SDA \
     {.port = GPIO_PORT_B, .pin = 9}
@@ -18,22 +19,46 @@
     {.port = GPIO_PORT_B, .pin = 10}
 
 static uint8_t message_count = 0;
-static uint8_t max_messages = UINT8_MAX;
+static uint8_t i2c_command[8];
 static uint8_t *i2c_metadata;
 
 static StatusCode prv_i2c_write_data_callback(uint8_t data[8], void *context, bool *tx_result);
 
 static StatusCode prv_i2c_write_data_callback(uint8_t data[8], void *context, bool *tx_result) {
     LOG_DEBUG("REAL CALLBACK!\n");
-    if(message_count == max_messages) {
+    if(message_count == 0) {
+        if(data[0] != BABYDRIVER_MESSAGE_I2C_WRITE_COMMAND || (data[1] != 0 && data[1] != 1)) {
+            return STATUS_CODE_INVALID_ARGS;
+        }
+
+        i2c_metadata = malloc(ceil(data[3]/7)*7*sizeof(uint8_t));
+        LOG_DEBUG("FIRST MESSAGE\n");
+        *tx_result = false;
+        message_count++;
+        for(uint8_t i = 0; i < 8; i++) {
+            i2c_command[i] = data[i];
+        }
+
+        return STATUS_CODE_OK;
+    } else if(message_count == ceil(i2c_command[3]/7)) {
+        if(data[0] != BABYDRIVER_MESSAGE_I2C_WRITE_DATA) {
+            return STATUS_CODE_INVALID_ARGS;
+        }
+
         LOG_DEBUG("FINAL CALLBACK!\n");
         *tx_result = true;
+
+        uint8_t metadata_index = (message_count - 1)*7;
+        for(uint8_t i = 0; i < 7; ++i) {
+            i2c_metadata[metadata_index + i] = data[i + 1];
+        }
+
         message_count = 0;
-        I2CPort port = i2c_metadata[1];
-        uint8_t addr = i2c_metadata[2];
-        size_t tx_len = i2c_metadata[3];
-        uint8_t is_reg = i2c_metadata[4];
-        uint8_t reg = i2c_metadata[5];
+        I2CPort port = i2c_command[1];
+        uint8_t addr = i2c_command[2];
+        size_t tx_len = i2c_command[3];
+        uint8_t is_reg = i2c_command[4];
+        uint8_t reg = i2c_command[5];
 
         if(port == I2C_PORT_1) {
             I2CSettings i2c1_settings = {
@@ -42,15 +67,13 @@ static StatusCode prv_i2c_write_data_callback(uint8_t data[8], void *context, bo
                 .scl = I2C1_SCL,
             };
             i2c_init(I2C_PORT_1, &i2c1_settings);
-        } else if(port == I2C_PORT_2) {
+        } else {
             I2CSettings i2c2_settings = {
                 .speed = I2C_SPEED_FAST,
                 .sda = I2C2_SDA,
                 .scl = I2C2_SCL,
             };
             i2c_init(I2C_PORT_2, &i2c2_settings);
-        } else {
-            return STATUS_CODE_OK;
         }
 
         if(is_reg) {
@@ -63,27 +86,23 @@ static StatusCode prv_i2c_write_data_callback(uint8_t data[8], void *context, bo
 
         return STATUS_CODE_OK;
     } else {
-        if(message_count == 0) {
-            i2c_metadata = malloc(ceil(data[3]/7)*8*sizeof(uint8_t));
-            max_messages = ceil(data[3]/7);
-            LOG_DEBUG("FIRST MESSAGE\n");
-            LOG_DEBUG("MAX MESSAGES = %u\n", max_messages);
-            *tx_result = false;
-            message_count++;
-        } else {
-            uint8_t metadata_index = (message_count - 1)*8;
-            message_count++;
-
-            for(uint8_t i = 0; i < 8; i++) {
-                i2c_metadata[metadata_index + i] = data[i];
-            }
+        if(data[0] != BABYDRIVER_MESSAGE_I2C_WRITE_DATA) {
+            return STATUS_CODE_INVALID_ARGS;
         }
 
-        return dispatcher_register_callback(BABYDRIVER_MESSAGE_I2C_WRITE_DATA, prv_i2c_write_data_callback, NULL);
+        uint8_t metadata_index = (message_count - 1)*7;
+        message_count++;
+
+        for(uint8_t i = 0; i < 7; i++) {
+            i2c_metadata[metadata_index + i] = data[i+1];
+        }
+
+        return STATUS_CODE_OK;
     }
 }
 
 StatusCode i2c_write_init(void) {
     LOG_DEBUG("Initialized!\n");
-    return dispatcher_register_callback(BABYDRIVER_MESSAGE_I2C_WRITE_COMMAND, prv_i2c_write_data_callback, NULL);
+    status_ok_or_return(dispatcher_register_callback(BABYDRIVER_MESSAGE_I2C_WRITE_COMMAND, prv_i2c_write_data_callback, NULL));
+    return dispatcher_register_callback(BABYDRIVER_MESSAGE_I2C_WRITE_DATA, prv_i2c_write_data_callback, NULL);
 }
--- projects/baby_driver/test/test_i2c_write.c
+++ projects/baby_driver/test/test_i2c_write.c
@@ -10,7 +10,6 @@
 #include "gpio.h"
 #include "i2c.h"
 #include "log.h"
-#include <math.h>
 #include "ms_test_helper_can.h"
 #include "ms_test_helpers.h"
 #include "test_helpers.h"
@@ -44,6 +43,9 @@ static StatusCode prv_rx_i2c_write_callback(uint8_t data[8], void *context, bool
     memcpy(s_received_data, data, 8);
     s_received_context = context;
     *tx_result = false;
+    for(uint8_t i = 0; i < 8; ++i) {
+        LOG_DEBUG("data %u\n", s_received_data[i]);
+    }
     return s_status_return;
 }
 
@@ -62,21 +64,87 @@ void setup_test(void) {
 
 void teardown_test(void) {}
 
+//Test that data is correctly written over I2C
 void test_writing_over_i2c(void) {
-    uint8_t data[8] = {BABYDRIVER_MESSAGE_I2C_WRITE_COMMAND, I2C_PORT_1, 0x74, 7, 0, 0, 0, 0};
-    uint8_t data_write[8] = {BABYDRIVER_MESSAGE_I2C_WRITE_DATA, 0, 0, 0, 0, 0, 0, 0};
+    //Test receiving multiple messages
+    uint8_t command[8] = {BABYDRIVER_MESSAGE_I2C_WRITE_COMMAND, I2C_PORT_1, 0x74, 14, 2, 3, 0, 0};
+    uint8_t data_1[8] = {BABYDRIVER_MESSAGE_I2C_WRITE_DATA, 10, 22, 5, 4, 3, 76, 120};
+    uint8_t data_2[8] = {BABYDRIVER_MESSAGE_I2C_WRITE_DATA, 43, 2, 3, 4, 5, 1, 8};
 
-    CAN_TRANSMIT_BABYDRIVER(data[0], data[1], data[2],
-                            data[3], data[4], data[5], data[6], data[7]);
-    CAN_TRANSMIT_BABYDRIVER(data_write[0], data_write[1], data_write[2], data_write[3],
-                            data_write[4], data_write[5], data_write[6], data_write[7]);
-
-    MS_TEST_HELPER_CAN_TX_RX(TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX);
-
+    CAN_TRANSMIT_BABYDRIVER(command[0], command[1], command[2], command[3], command[4],
+                            command[5], command[6], command[7]);
 
     MS_TEST_HELPER_CAN_TX_RX(TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX);
 
-    LOG_DEBUG("WORKS?\n");
+    CAN_TRANSMIT_BABYDRIVER(data_1[0], data_1[1], data_1[2], data_1[3], data_1[4],
+                            data_1[5], data_1[6], data_1[7]);
 
+    MS_TEST_HELPER_CAN_TX_RX(TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX);
+    MS_TEST_HELPER_CAN_TX_RX(TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX);
+
+    CAN_TRANSMIT_BABYDRIVER(data_2[0], data_2[1], data_2[2], data_2[3], data_2[4],
+                            data_2[5], data_2[6], data_2[7]);
+
+    MS_TEST_HELPER_CAN_TX_RX(TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX);
+    MS_TEST_HELPER_CAN_TX_RX(TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX);
+
+    //Note that the command message does not count toward count
     TEST_ASSERT_EQUAL(2, s_times_callback_called);
+
+    //Test receiving a single message
+    CAN_TRANSMIT_BABYDRIVER(command[0], command[1], command[2], 6, command[4],
+                            command[5], command[6], command[7]);
+
+    MS_TEST_HELPER_CAN_TX_RX(TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX);    
+}
+
+void test_invalid_args_i2c(void) {
+    s_status_return = STATUS_CODE_INVALID_ARGS;
+    uint8_t command[8] = {BABYDRIVER_MESSAGE_I2C_WRITE_COMMAND, I2C_PORT_1, 10, 7, 0, 0, 0, 0};
+
+    //invalid babydriver_id for command message
+    uint8_t inv_1[8] = {BABYDRIVER_MESSAGE_I2C_WRITE_DATA, I2C_PORT_2, 0, 0, 0, 0, 0, 0};
+
+    CAN_TRANSMIT_BABYDRIVER(inv_1[0], inv_1[1], inv_1[2], inv_1[3], inv_1[4],
+                            inv_1[5], inv_1[6], inv_1[7]);
+
+    MS_TEST_HELPER_CAN_TX_RX(TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX);
+
+    TEST_ASSERT_EQUAL(0, s_times_callback_called);
+    TEST_ASSERT_EQUAL(BABYDRIVER_MESSAGE_STATUS, s_received_data[0]);
+    TEST_ASSERT_EQUAL(s_status_return, s_received_data[1]);
+
+    //invalid babydriver_id for data messages
+    uint8_t inv_2[8] = {BABYDRIVER_MESSAGE_I2C_WRITE_COMMAND, 0, 0, 0, 0, 0, 0, 0};
+
+    CAN_TRANSMIT_BABYDRIVER(command[0], command[1], command[2], command[3], command[4],
+                            command[5], command[6], command[7]);
+
+    MS_TEST_HELPER_CAN_TX_RX(TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX);
+
+    CAN_TRANSMIT_BABYDRIVER(inv_2[0], inv_2[1], inv_2[2], inv_2[3], inv_2[4],
+                            inv_2[5], inv_2[6], inv_2[7]);
+
+    MS_TEST_HELPER_CAN_TX_RX(TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX);
+    MS_TEST_HELPER_CAN_TX_RX(TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX);
+
+    TEST_ASSERT_EQUAL(0, s_times_callback_called);
+    TEST_ASSERT_EQUAL(BABYDRIVER_MESSAGE_STATUS, s_received_data[0]);
+    TEST_ASSERT_EQUAL(s_status_return, s_received_data[1]);
+
+    //invalid port for command message
+    uint8_t inv_3[8] = {BABYDRIVER_MESSAGE_I2C_WRITE_COMMAND, 2, 0, 0, 0, 0, 0, 0};
+
+    CAN_TRANSMIT_BABYDRIVER(inv_3[0], inv_3[1], inv_3[2], inv_3[3], inv_3[4],
+                            inv_3[5], inv_3[6], inv_3[7]);
+
+    MS_TEST_HELPER_CAN_TX_RX(TEST_CAN_EVENT_TX, TEST_CAN_EVENT_RX);
+
+    TEST_ASSERT_EQUAL(0, s_times_callback_called);
+    TEST_ASSERT_EQUAL(BABYDRIVER_MESSAGE_STATUS, s_received_data[0]);
+    TEST_ASSERT_EQUAL(s_status_return, s_received_data[1]);
+}
+
+void test_timeout_error_i2c(void) {
+    
 }
