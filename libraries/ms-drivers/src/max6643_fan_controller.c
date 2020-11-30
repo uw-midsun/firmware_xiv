a75f05a9615686e2d9b30a6d07b97d46e756ea39
Max <max.zhu.sss@gmail.com>
2020-11-30 03:06:31 +0000

    address comments

--- libraries/ms-drivers/src/max6643_fan_controller.c
+++ libraries/ms-drivers/src/max6643_fan_controller.c
@@ -1,35 +1,27 @@
+#include "max6643_fan_controller.h"
+
 #include <stddef.h>
 #include <string.h>
 
 #include "gpio.h"
 #include "gpio_it.h"
 #include "interrupt.h"
-#include "max6643_fan_controller.h"
 
-StatusCode max6643_init(Max6643Storage *storage, Max6643Settings *settings) {
-  if (storage == NULL || settings == NULL) {
+StatusCode max6643_init(Max6643Settings *settings) {
+  if (settings == NULL) {
     return status_code(STATUS_CODE_INVALID_ARGS);
   }
 
-  memset(storage, 0, sizeof(Max6643Storage));
-
-  storage->overtemp_pin = settings->overtemp_pin;
-  storage->fanfail_pin = settings->fanfail_pin;
-  storage->overtemp_callback = settings->overtemp_callback;
-  storage->fanfail_callback = settings->fanfail_callback;
-  storage->fanfail_callback_context = settings->fanfail_callback_context;
-  storage->overtemp_callback_context = settings->overtemp_callback_context;
-
-  static InterruptSettings s_interrupt_settings = {
+  InterruptSettings s_interrupt_settings = {
     .type = INTERRUPT_TYPE_INTERRUPT,
     .priority = INTERRUPT_PRIORITY_NORMAL,
   };
 
-  gpio_it_register_interrupt(&storage->overtemp_pin, &s_interrupt_settings, INTERRUPT_EDGE_FALLING,
-                             storage->overtemp_callback, storage->overtemp_callback_context);
+  gpio_it_register_interrupt(&settings->overtemp_pin, &s_interrupt_settings, INTERRUPT_EDGE_FALLING,
+                             settings->overtemp_callback, settings->overtemp_callback_context);
 
-  gpio_it_register_interrupt(&storage->fanfail_pin, &s_interrupt_settings, INTERRUPT_EDGE_FALLING,
-                             storage->fanfail_callback, storage->fanfail_callback_context);
+  gpio_it_register_interrupt(&settings->fanfail_pin, &s_interrupt_settings, INTERRUPT_EDGE_FALLING,
+                             settings->fanfail_callback, settings->fanfail_callback_context);
 
   return STATUS_CODE_OK;
 }
--- libraries/ms-drivers/test/test_max6643_fan_controller.c
+++ libraries/ms-drivers/test/test_max6643_fan_controller.c
@@ -1,47 +1,51 @@
+#include "max6643_fan_controller.h"
+
 #include "interrupt.h"
 #include "log.h"
-#include "max6643_fan_controller.h"
 #include "test_helpers.h"
 #include "unity.h"
 #include "wait.h"
 
-typedef struct Max6643MockStatus {
-  uint8_t FANFAIL_INTERRUPT_STATUS;  // 0 if not triggered, 1 otherwise
-  uint8_t OVERTEMP_INTERRUPT_STATUS;
-} Max6643MockStatus;
-
-static Max6643MockStatus s_mock_statuses;
-static Max6643Storage s_mock_storage;
-static Max6643Storage s_storage;
+static uint8_t s_times_fanfail_callback_called;
+static uint8_t s_times_overtemp_callback_called;
+static uint8_t s_fanfail_callback_context;
+static uint8_t s_overtemp_callback_context;
+static const GpioAddress test_fanfail_pin = { .port = GPIO_PORT_A, .pin = 0 };
+static const GpioAddress test_overtemp_pin = { .port = GPIO_PORT_A, .pin = 1 };
 
 static void prv_test_max6643_fanfail_interrupt_callback(const GpioAddress *address, void *context) {
   LOG_DEBUG("FANFAIL INTERRUPT CALLBACK TRIGGERED\n");
 
-  TEST_ASSERT_EQUAL(1, s_mock_statuses.FANFAIL_INTERRUPT_STATUS);
+  uint8_t *fanfail_context = (uint8_t *)context;
+  TEST_ASSERT_EQUAL(0xA, *fanfail_context);
+
+  s_times_fanfail_callback_called++;
 }
 
 static void prv_test_max6643_overtemp_interrupt_callback(const GpioAddress *address,
                                                          void *context) {
   LOG_DEBUG("OVERTEMP INTERRUPT CALLBACK TRIGGERED\n");
 
-  TEST_ASSERT_EQUAL(1, s_mock_statuses.OVERTEMP_INTERRUPT_STATUS);
+  uint8_t *overtemp_context = (uint8_t *)context;
+  TEST_ASSERT_EQUAL(0xB, *overtemp_context);
+
+  s_times_overtemp_callback_called++;
 }
 
 void setup_test(void) {
   gpio_init();
   interrupt_init();
   gpio_it_init();
-  s_mock_statuses.FANFAIL_INTERRUPT_STATUS = 0;
-  s_mock_statuses.OVERTEMP_INTERRUPT_STATUS = 0;
+  s_times_fanfail_callback_called = 0;
+  s_times_overtemp_callback_called = 0;
+  s_fanfail_callback_context = 0xA;
+  s_overtemp_callback_context = 0xB;
 }
 
 void teardown_test(void) {}
 
 // Test initializing with valid parameters
 void test_max6643_init_works(void) {
-  GpioAddress test_fanfail_pin = { .port = GPIO_PORT_A, .pin = 0 };
-  GpioAddress test_overtemp_pin = { .port = GPIO_PORT_A, .pin = 1 };
-
   Max6643Settings valid_settings = { .fanfail_pin = test_fanfail_pin,
                                      .overtemp_pin = test_overtemp_pin,
                                      .fanfail_callback = NULL,
@@ -49,51 +53,27 @@ void test_max6643_init_works(void) {
                                      .fanfail_callback_context = NULL,
                                      .overtemp_callback_context = NULL };
 
-  s_mock_storage.fanfail_pin = valid_settings.fanfail_pin;
-  s_mock_storage.overtemp_pin = valid_settings.overtemp_pin;
-  s_mock_storage.fanfail_callback = valid_settings.fanfail_callback;
-  s_mock_storage.overtemp_callback = valid_settings.overtemp_callback;
-  s_mock_storage.fanfail_callback_context = valid_settings.fanfail_callback_context;
-  s_mock_storage.overtemp_callback_context = valid_settings.overtemp_callback_context;
-
-  TEST_ASSERT_OK(max6643_init(&s_storage, &valid_settings));
-
-  TEST_ASSERT_EQUAL(s_mock_storage.fanfail_pin.pin, s_storage.fanfail_pin.pin);
-  TEST_ASSERT_EQUAL(s_mock_storage.fanfail_pin.port, s_storage.fanfail_pin.port);
-  TEST_ASSERT_EQUAL(s_mock_storage.overtemp_pin.pin, s_storage.overtemp_pin.pin);
-  TEST_ASSERT_EQUAL(s_mock_storage.overtemp_pin.port, s_storage.overtemp_pin.port);
-  TEST_ASSERT_EQUAL(s_mock_storage.fanfail_callback, s_storage.fanfail_callback);
-  TEST_ASSERT_EQUAL(s_mock_storage.fanfail_callback_context, s_storage.fanfail_callback_context);
-  TEST_ASSERT_EQUAL(s_mock_storage.overtemp_callback, s_storage.overtemp_callback);
-  TEST_ASSERT_EQUAL(s_mock_storage.overtemp_callback_context, s_storage.overtemp_callback_context);
+  TEST_ASSERT_OK(max6643_init(&valid_settings));
 }
 
 // test that interrupts work
 void test_max6643_interrupts(void) {
-  GpioAddress test_fanfail_pin = { .port = GPIO_PORT_A, .pin = 0 };
-  GpioAddress test_overtemp_pin = { .port = GPIO_PORT_A, .pin = 1 };
-
   Max6643Settings valid_settings = {
     .fanfail_pin = test_fanfail_pin,
     .overtemp_pin = test_overtemp_pin,
     .fanfail_callback = (GpioItCallback)prv_test_max6643_fanfail_interrupt_callback,
     .overtemp_callback = (GpioItCallback)prv_test_max6643_overtemp_interrupt_callback,
-    .fanfail_callback_context = NULL,
-    .overtemp_callback_context = NULL
+    .fanfail_callback_context = &s_fanfail_callback_context,
+    .overtemp_callback_context = &s_overtemp_callback_context
   };
 
-  s_mock_storage.fanfail_pin = valid_settings.fanfail_pin;
-  s_mock_storage.overtemp_pin = valid_settings.overtemp_pin;
-  s_mock_storage.fanfail_callback = valid_settings.fanfail_callback;
-  s_mock_storage.overtemp_callback = valid_settings.overtemp_callback;
-
   // add callback to storage
-  TEST_ASSERT_OK(max6643_init(&s_storage, &valid_settings));
+  TEST_ASSERT_OK(max6643_init(&valid_settings));
 
   // trigger interrupt and fetch data
-  s_mock_statuses.FANFAIL_INTERRUPT_STATUS = 1;
   gpio_it_trigger_interrupt(&test_fanfail_pin);
+  TEST_ASSERT_EQUAL(1, s_times_fanfail_callback_called);
 
-  s_mock_statuses.OVERTEMP_INTERRUPT_STATUS = 1;
   gpio_it_trigger_interrupt(&test_overtemp_pin);
+  TEST_ASSERT_EQUAL(1, s_times_overtemp_callback_called);
 }
