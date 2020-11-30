3c281edc77099e512a7234741e90d743c0c0e682
Patrick Kim <s524kim@uwaterloo.ca>
2020-11-30 05:21:12 +0000

    soft_363 Patrick Kim

--- platform/stm32f0xx/scripts/pretty_size.sh
+++ platform/stm32f0xx/scripts/pretty_size.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-
-#./arm-none-eabi-size
 size_data_raw=$($1 $2)
-#echo "$size_data_raw"
+echo "$size_data_raw"
 
-#bytes_used="$bss + $data"
-#bytes_total=16384 #16kb available
-#bytes_remaining="$bytes_total - $bytes_used"
-#bytes_threshold=2048 #need 2kb static memory left
+read -ra temp <<< "$(echo -e "$size_data_raw" | sed -n '2p')"
 
-#echo "power distribution used ${bytes_used}/${bytes_total} bytes of static memory,"
-#echo "leaving $bytes_remaining bytes for stack space."
+data="${temp[1]}"
+bss="${temp[2]}"
 
-#if (bytes_remaining <= bytes_threshold){
- # echo "Warning: This may not be enough for the program to run!"
-#}
+bytes_used=$(("$bss + $data"))
+bytes_total=16384 #16kb available
+bytes_remaining=$(("$bytes_total - $bytes_used"))
+bytes_threshold=2048 #need 2kb static memory left
+
+echo "Power distribution used ${bytes_used}/${bytes_total} bytes of static memory," \
+     "leaving $bytes_remaining bytes for stack space."
+
+if [ "$bytes_remaining" -lt "$bytes_threshold" ]
+then
+  echo "Warning: This may not be enough for the program to run!"
+fi
+
