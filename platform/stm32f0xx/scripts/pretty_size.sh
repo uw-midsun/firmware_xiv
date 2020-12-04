0b552eefde81821b86ff42e87fa06fd63114aea4
Patrick Kim <s524kim@uwaterloo.ca>
2020-12-04 06:15:22 +0000

    soft_363 revision

--- platform/stm32f0xx/scripts/pretty_size.sh
+++ platform/stm32f0xx/scripts/pretty_size.sh
@@ -7,13 +7,15 @@ read -ra temp <<< "$(echo -e "$size_data_raw" | sed -n '2p')"
 
 data="${temp[1]}"
 bss="${temp[2]}"
+program_name="$(echo $2 | sed -E 's/\.elf//g')"
+program_name="$(echo "$program_name" | rev | cut -d'/' -f 1 | rev)"
 
 bytes_used=$(("$bss + $data"))
-bytes_total=16384 #16kb available
+bytes_total=16384 # 16kb available
 bytes_remaining=$(("$bytes_total - $bytes_used"))
-bytes_threshold=2048 #need 2kb static memory left
+bytes_threshold=2048 # need 2kb static memory left
 
-echo "Power distribution used ${bytes_used}/${bytes_total} bytes of static memory," \
+echo "${program_name} used ${bytes_used}/${bytes_total} bytes of static memory," \
      "leaving $bytes_remaining bytes for stack space."
 
 if [ "$bytes_remaining" -lt "$bytes_threshold" ]
