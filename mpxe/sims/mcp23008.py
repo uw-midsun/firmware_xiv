d62cbc020d5fdaab6b3728319d266b930f7d96f7
mitchellostler <ostlermitchell44@gmail.com>
2020-12-10 00:29:02 +0000

    Format :P

--- mpxe/sims/mcp23008.py
+++ mpxe/sims/mcp23008.py
@@ -13,15 +13,15 @@ class Mcp23008(sim.Sim):
     def handle_update(self, pm, proj):
         stores = proj.stores
         if MCP23008_KEY in stores:
-            mcp         = stores[MCP23008_KEY]
+            mcp          = stores[MCP23008_KEY]
             self.states  = [bool(mcp.state[i]) for i in range(len(mcp.state)) if i < NUM_MCP_PINS]
 
     # Update the store with a new pin state
     def update_pin_state(self, proj, pin, state):
-        mcp23008_msg = mcp23008_pb2.MxMcp23008Store()
-        mcp23008_msg.state[pin] = state
+        mcp23008_msg             = mcp23008_pb2.MxMcp23008Store()
+        mcp23008_msg.state[pin]  = state
 
-        mcp23008_mask = mcp23008_pb2.MxMcp23008Store()
+        mcp23008_mask            = mcp23008_pb2.MxMcp23008Store()
         mcp23008_mask.state[pin] = 1
     
         proj.write_store(mcp23008_msg, mcp23008_mask, stores_pb2.MxStoreType.MCP23008)
@@ -30,3 +30,4 @@ class Mcp23008(sim.Sim):
     def assert_store_value_reading(self, proj, pin, state):
         for i in range(NUM_MCP_PINS):
             assert(self.states[pin] == state)
+
