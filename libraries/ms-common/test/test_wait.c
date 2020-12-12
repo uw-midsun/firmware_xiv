b530dc03755b399fab9da873b126328bbe85fc77
Max <max.zhu.sss@gmail.com>
2020-12-12 02:47:36 +0000

    initial commit

---
+++ libraries/ms-common/test/test_wait.c
@@ -0,0 +1,56 @@
+#include "wait.h"
+
+#include "gpio.h"
+#include "gpio_it.h"
+#include "interrupt.h"
+#include "log.h"
+#include "soft_timer.h"
+#include "test_helpers.h"
+#include "unity.h"
+
+static int s_num_wait_cycles_timer;
+static int s_num_wait_cycles_gpio;
+
+#define WAIT_INTERVAL_S 2
+#define EXPECTED_INTERRUPT_CYCLES 2
+
+static void prv_test_wait_interrupt_callback(SoftTimerId id, void *context) {
+  s_num_wait_cycles_timer++;
+}
+
+void setup_test(void) {
+  gpio_init();
+  interrupt_init();
+  gpio_it_init();
+  soft_timer_init();
+  s_num_wait_cycles_timer = 0;
+}
+
+void teardown_test(void) {}
+
+void test_wait_init_works_timer(void) {
+  int done = 0;
+  while (!done) {
+    soft_timer_start_seconds(WAIT_INTERVAL_S, prv_test_wait_interrupt_callback, NULL, NULL);
+
+    wait();
+
+    if (s_num_wait_cycles_timer > EXPECTED_INTERRUPT_CYCLES - 1) {
+      break;
+    }
+  }
+  TEST_ASSERT_EQUAL(EXPECTED_INTERRUPT_CYCLES, s_num_wait_cycles_timer);
+}
+
+void test_wait_init_works_gpio(void) {
+  int done = 0;
+  while (!done) {
+    wait();
+    s_num_wait_cycles_gpio++;
+
+    if (s_num_wait_cycles_gpio > 1) {
+      break;
+    }
+  }
+  TEST_ASSERT_EQUAL(1, s_num_wait_cycles_gpio);
+}
