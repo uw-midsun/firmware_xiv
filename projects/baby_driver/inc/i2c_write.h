442aa0dbd5e28797531afe43f06ec93bd13a3051
Ryan Dancy <ryan@keal.ca>
2020-12-12 22:39:17 -0500

    Add soft timers to i2c_write's dependencies

--- projects/baby_driver/inc/i2c_write.h
+++ projects/baby_driver/inc/i2c_write.h
@@ -2,7 +2,7 @@
 
 // This module allows data to be written over i2c. It listens for a series of i2c_write
 // babydriver CAN messages and stores the data before writing them over i2c.
-// Requires dispatcher, interrupts, gpio, the event queue, and CAN to be initialized.
+// Requires dispatcher, interrupts, soft timers, gpio, the event queue, and CAN to be initialized.
 
 #include "i2c.h"
 #include "status.h"
