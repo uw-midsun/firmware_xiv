3aac098eeb2b4af10fcdc8668976fd91c8df1b5e
AveryChiu64 <a27chiu@uwaterloo.ca>
2020-12-29 03:14:45 +0000

    Implemented read time

--- libraries/ms-drivers/src/pcf8523_rtc.c
+++ libraries/ms-drivers/src/pcf8523_rtc.c
@@ -1,4 +1,5 @@
 #include "pcf8523_rtc.h"
+#include "bcd.h"
 #include "gpio.h"
 #include "i2c.h"
 #include "pcf8523_rtc_defs.h"
@@ -26,6 +27,23 @@ StatusCode pcf8523_init(Pcf8523Settings *settings) {
   i2c_write(port, I2C_ADDR, data, (sizeof(data)));
 }
 
-StatusCode pcf8523_get_time(Pcf8523Time *time) {}
+StatusCode pcf8523_get_time(Pcf8523Time *time) {
+  // Set starting register address (this will auto-increment)
+  i2c_write(port, I2C_ADDR, SECONDS, 1);
+
+  // Read time registers
+  uint8_t data[NUM_TIME_REG];
+  i2c_read(port, I2C_ADDR, data, (sizeof(data)));
+
+  // Store time
+  time->seconds = bcd_to_dec(data[0]);
+  time->minutes = bcd_to_dec(data[1]);
+  time->hours = bcd_to_dec(data[2]);
+  time->days = bcd_to_dec(data[3]);
+  time->weekdays = bcd_to_dec(data[4]);
+  time->months = bcd_to_dec(data[5]);
+  time->years = bcd_to_dec(data[6]);
+  return STATUS_CODE_OK;
+}
 
 StatusCode pcf8523_set_time(Pcf8523Time *time) {}
