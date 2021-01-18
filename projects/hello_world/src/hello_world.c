142294886b0d4c90cafbeaa3e9b7989c7a48b9fd
Henry <henryzhou000@gmail.com>
2021-01-17 20:21:31 -0800

    revised code for review

--- projects/hello_world/src/hello_world.c
+++ projects/hello_world/src/hello_world.c
@@ -1,35 +1,33 @@
-//
-// Created by henry on 2021-01-09.
-//
 #include <stdint.h>
 
 #include "interrupt.h"
 #include "log.h"
 #include "soft_timer.h"
+#include "wait.h"
+#define INTERVAL 0.5
 
 typedef struct Counters {
   uint8_t counter_a;
   uint8_t counter_b;
 } Counters;
-void callback(SoftTimerId id, void *context) {
-  static bool b_increment = false;
-  Counters *Counter = context;
-  LOG_DEBUG("%d\n", ++Counter->counter_a);
-  if (b_increment) {
-    LOG_DEBUG("%d\n", ++Counter->counter_b);
-    b_increment = false;
-  } else {
-    b_increment = true;
+void prv_callback(SoftTimerId id, void *context) {
+  Counters *counter = context;
+  counter->counter_a++;
+  LOG_DEBUG("Counter A: %i\n", counter->counter_a);
+  if (counter->counter_a % 2 == 0) {
+    counter->counter_b++;
+    LOG_DEBUG("Counter B: %i\n", counter->counter_b);
   }
+  soft_timer_start_seconds(INTERVAL, prv_callback, counter, NULL);
 }
+
 int main(void) {
-  Counters Counter = { 0 };
+  Counters counter = { 0 };
   interrupt_init();
   soft_timer_init();
+  soft_timer_start_seconds(INTERVAL, prv_callback, &counter, NULL);
   while (true) {
-    if(!soft_timer_inuse()){
-      soft_timer_start(0.5 * 1000000, callback, &Counter, NULL);
-    }
+    wait();
   }
   return 0;
 }
