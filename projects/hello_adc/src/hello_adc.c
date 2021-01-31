a5ddd8aa4278ede5fe011f21d31c081f363e3634
Henry <henryzhou000@gmail.com>
2021-01-31 10:22:41 -0800

    Made changes based on feedback

--- projects/hello_adc/src/hello_adc.c
+++ projects/hello_adc/src/hello_adc.c
@@ -6,23 +6,22 @@
 #include "log.h"
 #include "soft_timer.h"
 
-#define BUTTON_PIN 178
-#define READ_FROM_PIN 166
+static void prv_log_pin_data(GpioAddress *button_addr, void *context);
 
-GpioItCallback prv_log_pin_data(GpioAddress *button_addr, void *context);
-
-int main() {
+int main(void) {
   interrupt_init();
   soft_timer_init();
   gpio_init();
-  GpioAddress button_addr = { GPIO_PORT_A, BUTTON_PIN };
-  GpioAddress read_addr = { GPIO_PORT_A, READ_FROM_PIN };
+  GpioAddress button_addr = { GPIO_PORT_A, 6 };
+  GpioAddress read_addr = { GPIO_PORT_B, 2 };
   // Setting pin value is not necessary
-  GpioSettings null_settings = {};
-  gpio_init_pin(&button_addr, &null_settings);
-  gpio_init_pin(&read_addr, &null_settings);
+  GpioSettings pin_settings = { .direction = GPIO_DIR_IN, .alt_function = GPIO_ALTFN_ANALOG };
+  gpio_init_pin(&button_addr, &pin_settings);
+  gpio_init_pin(&read_addr, &pin_settings);
   adc_init(ADC_MODE_SINGLE);
-  InterruptSettings button_interrupt = { INTERRUPT_TYPE_EVENT, INTERRUPT_PRIORITY_NORMAL };
+  adc_set_channel_pin(read_addr, true);
+  InterruptSettings button_interrupt = { .type = INTERRUPT_TYPE_EVENT,
+                                         .priority = INTERRUPT_PRIORITY_NORMAL };
   gpio_it_register_interrupt(&button_addr, &button_interrupt, INTERRUPT_EDGE_FALLING,
                              (GpioItCallback)prv_log_pin_data, &read_addr);
   while (true) {
@@ -30,10 +29,9 @@ int main() {
   return 0;
 }
 
-GpioItCallback prv_log_pin_data(GpioAddress *button_addr, void *context) {
+static void prv_log_pin_data(GpioAddress *button_addr, void *context) {
   GpioAddress *read_addr = context;
   uint16_t pin_data = 0;
   adc_read_converted_pin(*read_addr, &pin_data);
   LOG_DEBUG("Pin A6 data: %d\n", pin_data);
-  return NULL;
 }
