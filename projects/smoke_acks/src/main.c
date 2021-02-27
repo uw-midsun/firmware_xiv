532edbb00fe70882630b0c24b2ce378e8fd87d31
Jess Muir <e.jessmuir@gmail.com>
2021-02-27 16:38:51 -0500

    Improve and comment smoke_acks

--- projects/smoke_acks/src/main.c
+++ projects/smoke_acks/src/main.c
@@ -1,3 +1,14 @@
+// This is a utility that acknowledges all messages with a configurable id.
+// It was originally intended to be used for startup sequence testing,
+// so it also transmits PRECHARGE_COMPLETED when it receives
+// SYSTEM_CAN_MESSAGE_BEGIN_PRECHARGE.
+
+// To run from a laptop, use this command in a separate terminal:
+// make run PROJECT=smoke_acks PLATFORM=x86 DEFINE=CAN_HW_DEV_USE_CAN0
+
+// IMPORTANT USAGE NOTE: you may have to increase the CAN_ACK_TIMEOUT_MS
+// in can_ack.h, since laptops tend to be slow. A value of 250 ms should be safe.
+
 #include "can.h"
 #include "can_ack.h"
 #include "can_msg_defs.h"
@@ -9,12 +20,17 @@
 #include "log.h"
 #include "wait.h"
 
+// Change this id to whatever device you want.
+#define ACK_DEVICE_ID SYSTEM_CAN_DEVICE_BABYDRIVER
+
 static CanStorage s_can_storage;
 
 static StatusCode prv_handle_msg(const CanMessage *msg, void *context, CanAckStatus *ack) {
+  // Only messages with ID <= 16 get acknowledged.
   if (msg->msg_id <= (1 << 4)) {
     LOG_DEBUG("acked %d\n", msg->msg_id);
   }
+  // Mock precharge for ease of use in power sequencing. Comment this out if unnecessary.
   if (msg->msg_id == SYSTEM_CAN_MESSAGE_BEGIN_PRECHARGE) {
     CAN_TRANSMIT_PRECHARGE_COMPLETED();
   }
@@ -27,7 +43,7 @@ int main(void) {
   event_queue_init();
 
   CanSettings can_settings = {
-    .device_id = SYSTEM_CAN_DEVICE_BABYDRIVER,
+    .device_id = ACK_DEVICE_ID,
     .bitrate = CAN_HW_BITRATE_500KBPS,
     .rx_event = 1,
     .tx_event = 2,
