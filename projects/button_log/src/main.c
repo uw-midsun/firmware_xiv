2dd88dfa1fa200a163958e07d4976e8f91e4402c
Marko Renic <mrenic@uwaterloo.ca>
2021-03-01 18:03:00 +0000

    Completed 103HW I did guess some of the settings and not sure how to test?

---
+++ projects/button_log/src/main.c
@@ -0,0 +1,16 @@
+// Hints:
+// assume the button is active-high
+// register an interrupt with the appropriate InterruptEdge settings (google this or ask someone /
+// your lead)
+#include <stdbool.h>
+#include "adc.h"
+#include "gpio.h"
+#include "gpio_it.h"
+#include "interrupt.h"
+#include "interrupt_def.h"
+#include "log.h"
+
+
+int main(void){
+return 0;
+}
