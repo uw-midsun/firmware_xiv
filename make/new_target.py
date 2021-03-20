ebb6469d524bafab7f468b9a9f446b24e2cd0b5f
Abdullah <abdullahshahid20022002@gmail.com>
2021-03-20 19:04:34 +0000

    Formatting (Automated commit)

--- make/new_target.py
+++ make/new_target.py
@@ -50,11 +50,10 @@ def new_target(target_type, name):
 
     projects/libraries
     └── name
-        ├── inc
-        ├── README.md
-        ├── rules.mk
-        ├── src
-        └── test
+                                                                                                                                                                                                     ├── inc
+                                                                                                                                                                                                     ├── rules.mk
+                                                                                                                                                                                                     ├── src
+                                                                                                                                                                                                     └── test
 
     where rules.mk is required for the project or library to be valid.
 
