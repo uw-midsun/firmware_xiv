e1f0e74f1eaa47aca4d15b30cffce520adb3fc40
Mitchell Ostler <mlostler@uwaterloo.ca>
2021-03-24 22:57:54 +0000

    INITIAL CONDITIONS WORK, WITH MULTIPLE DRIVERS HELL YEAH

--- mpxe/integration_tests/test_pca9539r.py
+++ mpxe/integration_tests/test_pca9539r.py
@@ -3,12 +3,13 @@ import time
 
 from mpxe.integration_tests import int_test
 from mpxe.sims.pca9539r import Pca9539r
+from mpxe.integration_tests.init_cond import pca9539r_init_conditions
 
 
 class TestPca9539r(int_test.IntTest):
     def setUp(self):
         super().setUp()
-        self.pca9539r = self.manager.start('smoke_pca9539r', Pca9539r())
+        self.pca9539r = self.manager.start('smoke_pca9539r', Pca9539r(), pca9539r_init_conditions())
 
     def test_pca9539r(self):
         time.sleep(0.1)
--- projects/smoke_pca9539r/src/main.c
+++ projects/smoke_pca9539r/src/main.c
@@ -17,6 +17,7 @@
 
 #define PCA9539_I2C_ADDRESS 0x74  // PCA9539 address
 #define I2C_PORT I2C_PORT_2
+
 // I2C_PORT_1 has SDA on PB9 and SCL on PB8
 // I2C_PORT_2 has SDA on PB11 and SCL on PB10
 
@@ -33,6 +34,27 @@ void setup_test(void) {
   };
   i2c_init(I2C_PORT, &i2c_settings);
   pca9539r_gpio_init(I2C_PORT, PCA9539_I2C_ADDRESS);
+#ifdef MPXE  // used to test double initial conditions
+  Pca9539rGpioState in_state;
+  Pca9539rGpioAddress address = { .i2c_address = PCA9539_I2C_ADDRESS };
+  for (Pca9539rPinAddress pin = PCA9539R_PIN_IO0_0; pin < NUM_PCA9539R_GPIO_PINS; pin++) {
+    address.pin = pin;
+    pca9539r_gpio_get_state(&address, &in_state);
+    LOG_DEBUG("State for First PCA9539R pin %d_%d == %d\n", address.pin / 8, address.pin % 8,
+              in_state);
+  }
+
+#define PCA9539_I2C_ADDRESS2 0x75  // PCA9539 address 2
+  pca9539r_gpio_init(I2C_PORT, PCA9539_I2C_ADDRESS2);
+  address.i2c_address = PCA9539_I2C_ADDRESS2;
+  for (Pca9539rPinAddress pin = PCA9539R_PIN_IO0_0; pin < NUM_PCA9539R_GPIO_PINS; pin++) {
+    address.pin = pin;
+    pca9539r_gpio_get_state(&address, &in_state);
+    LOG_DEBUG("State for Second PCA9539R pin %d_%d == %d\n", address.pin / 8, address.pin % 8,
+              in_state);
+  }
+
+#endif
 }
 
 // initialize all pins to in/out - must be called after pca9539r_gpio_init
