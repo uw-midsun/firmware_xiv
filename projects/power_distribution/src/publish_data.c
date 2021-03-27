83983fc2ac080b0b8f145bf103fbc5cbbe4721d7
Ryan Dancy <ryan@keal.ca>
2021-03-26 23:36:35 -0400

    Output-ify publish_data

--- projects/power_distribution/src/publish_data.c
+++ projects/power_distribution/src/publish_data.c
@@ -1,9 +1,10 @@
 #include "publish_data.h"
 
-#include <stddef.h>
+#include <stdint.h>
 
 #include "log.h"
 #include "misc.h"
+#include "output.h"
 #include "soft_timer.h"
 #include "status.h"
 
@@ -14,14 +15,13 @@ static PowerDistributionPublishConfig s_config = { 0 };
 static uint16_t *s_current_measurements = NULL;
 
 StatusCode power_distribution_publish_data_init(PowerDistributionPublishConfig config) {
-  // check that the transmitter and the currents aren't null
-  if (!config.transmitter || !config.currents_to_publish) {
+  if (config.transmitter == NULL || config.outputs_to_publish == NULL) {
     return status_code(STATUS_CODE_INVALID_ARGS);
   }
 
-  // check that each current we're to publish is valid to avoid segfaults
-  for (uint16_t c = 0; c < config.num_currents_to_publish; c++) {
-    if (config.currents_to_publish[c] >= NUM_CURRENTS) {
+  // check that each output we're to publish is valid to avoid segfaults
+  for (uint16_t c = 0; c < config.num_outputs_to_publish; c++) {
+    if (config.outputs_to_publish[c] >= NUM_OUTPUTS) {
       return status_code(STATUS_CODE_INVALID_ARGS);
     }
   }
@@ -39,9 +39,9 @@ static void prv_partially_publish(SoftTimerId timer_id, void *context) {
   // We use a uintptr_t for the index so we can pass it through the context
   uintptr_t index = (uintptr_t)context;
 
-  uint16_t transmit_up_to = MIN(index + NUM_MSG_PER_ITERATION, s_config.num_currents_to_publish);
+  uint16_t transmit_up_to = MIN(index + NUM_MSG_PER_ITERATION, s_config.num_outputs_to_publish);
   while (index < transmit_up_to) {
-    PowerDistributionCurrent current_id = s_config.currents_to_publish[index];
+    Output current_id = s_config.outputs_to_publish[index];
     StatusCode tx_code = s_config.transmitter(current_id, s_current_measurements[current_id]);
     if (!status_ok(tx_code)) {
       LOG_WARN("Failed to TX current %d: status code %d\n", current_id, tx_code);
@@ -49,7 +49,7 @@ static void prv_partially_publish(SoftTimerId timer_id, void *context) {
     index++;
   }
 
-  if (index < s_config.num_currents_to_publish) {
+  if (index < s_config.num_outputs_to_publish) {
     StatusCode code =
         soft_timer_start_millis(ITERATION_WAIT_MS, prv_partially_publish, (void *)index, NULL);
     if (!status_ok(code)) {
@@ -58,8 +58,8 @@ static void prv_partially_publish(SoftTimerId timer_id, void *context) {
   }
 }
 
-StatusCode power_distribution_publish_data_publish(uint16_t current_measurements[NUM_CURRENTS]) {
-  if (!s_config.currents_to_publish) {
+StatusCode power_distribution_publish_data_publish(uint16_t current_measurements[NUM_OUTPUTS]) {
+  if (s_config.outputs_to_publish == NULL) {
     return status_code(STATUS_CODE_UNINITIALIZED);
   }
 
--- projects/power_distribution/src/publish_data_config.c
+++ projects/power_distribution/src/publish_data_config.c
@@ -1,58 +1,55 @@
 #include "publish_data_config.h"
 #include "can_transmit.h"
 
-static StatusCode prv_publish_front_current_measurement(PowerDistributionCurrent current_id,
-                                                        uint16_t current_data) {
-  return CAN_TRANSMIT_FRONT_CURRENT_MEASUREMENT((uint16_t)current_id, current_data);
+static StatusCode prv_publish_front_current_measurement(Output output, uint16_t current_data) {
+  return CAN_TRANSMIT_FRONT_CURRENT_MEASUREMENT((uint16_t)output, current_data);
 }
 
-static StatusCode prv_publish_rear_current_measurement(PowerDistributionCurrent current_id,
-                                                       uint16_t current_data) {
-  return CAN_TRANSMIT_REAR_CURRENT_MEASUREMENT((uint16_t)current_id, current_data);
+static StatusCode prv_publish_rear_current_measurement(Output output, uint16_t current_data) {
+  return CAN_TRANSMIT_REAR_CURRENT_MEASUREMENT((uint16_t)output, current_data);
 }
+
+// TODO(SOFT_396): these are the same as current_measurement...
 
 const PowerDistributionPublishConfig FRONT_POWER_DISTRIBUTION_PUBLISH_DATA_CONFIG = {
   .transmitter = prv_publish_front_current_measurement,
-  .currents_to_publish =
-      (PowerDistributionCurrent[]){
-          FRONT_CURRENT_CENTRE_CONSOLE,
-          FRONT_CURRENT_PEDAL,
-          FRONT_CURRENT_STEERING,
-          FRONT_CURRENT_LEFT_CAMERA,
-          FRONT_CURRENT_RIGHT_CAMERA,
-          FRONT_CURRENT_DRIVER_DISPLAY,
-          FRONT_CURRENT_INFOTAINMENT_DISPLAY,
-          FRONT_CURRENT_LEFT_DISPLAY,
-          FRONT_CURRENT_RIGHT_DISPLAY,
-          FRONT_CURRENT_REAR_DISPLAY,
-          FRONT_CURRENT_MAIN_PI,
-          FRONT_CURRENT_LEFT_FRONT_TURN_LIGHT,
-          FRONT_CURRENT_RIGHT_FRONT_TURN_LIGHT,
-          FRONT_CURRENT_DAYTIME_RUNNING_LIGHTS,
-          FRONT_CURRENT_SPEAKER,
-          FRONT_CURRENT_HORN,
-          FRONT_CURRENT_FAN_1,
-          FRONT_CURRENT_FAN_2,
+  .outputs_to_publish =
+      (Output[]){
+          FRONT_OUTPUT_CENTRE_CONSOLE,
+          FRONT_OUTPUT_PEDAL,
+          FRONT_OUTPUT_STEERING,
+          FRONT_OUTPUT_LEFT_CAMERA,
+          FRONT_OUTPUT_RIGHT_CAMERA,
+          FRONT_OUTPUT_DRIVER_DISPLAY,
+          FRONT_OUTPUT_INFOTAINMENT_DISPLAY,
+          FRONT_OUTPUT_REAR_DISPLAY,
+          FRONT_OUTPUT_LEFT_DISPLAY,
+          FRONT_OUTPUT_RIGHT_DISPLAY,
+          FRONT_OUTPUT_MAIN_PI,
+          FRONT_OUTPUT_LEFT_FRONT_TURN_LIGHT,
+          FRONT_OUTPUT_RIGHT_FRONT_TURN_LIGHT,
+          FRONT_OUTPUT_DAYTIME_RUNNING_LIGHTS,
+          FRONT_OUTPUT_SPEAKER,
+          FRONT_OUTPUT_UV_VBAT,
       },
-  .num_currents_to_publish = 18,
+  .num_outputs_to_publish = 16,
 };
 
 const PowerDistributionPublishConfig REAR_POWER_DISTRIBUTION_PUBLISH_DATA_CONFIG = {
   .transmitter = prv_publish_rear_current_measurement,
-  .currents_to_publish =
-      (PowerDistributionCurrent[]){
-          REAR_CURRENT_BMS,
-          REAR_CURRENT_MCI,
-          REAR_CURRENT_CHARGER,
-          REAR_CURRENT_SOLAR_SENSE,
-          REAR_CURRENT_TELEMETRY,
-          REAR_CURRENT_REAR_CAMERA,
-          REAR_CURRENT_LEFT_REAR_TURN_LIGHT,
-          REAR_CURRENT_RIGHT_REAR_TURN_LIGHT,
-          REAR_CURRENT_BRAKE_LIGHT,
-          REAR_CURRENT_STROBE,
-          REAR_CURRENT_FAN_1,
-          REAR_CURRENT_FAN_2,
+  .outputs_to_publish =
+      (Output[]){
+          REAR_OUTPUT_BMS,
+          REAR_OUTPUT_MCI,
+          REAR_OUTPUT_CHARGER,
+          REAR_OUTPUT_SOLAR_SENSE,
+          REAR_OUTPUT_REAR_CAMERA,
+          REAR_OUTPUT_LEFT_REAR_TURN_LIGHT,
+          REAR_OUTPUT_RIGHT_REAR_TURN_LIGHT,
+          REAR_OUTPUT_BRAKE_LIGHT,
+          REAR_OUTPUT_STROBE_LIGHT,
+          REAR_OUTPUT_FAN_1,
+          REAR_OUTPUT_FAN_2,
       },
-  .num_currents_to_publish = 12,
+  .num_outputs_to_publish = 11,
 };
