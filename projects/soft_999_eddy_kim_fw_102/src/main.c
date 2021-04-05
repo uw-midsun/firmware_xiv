7e5543791530fa3e42c07d6c9bc18c816d466ae1
Eddy Kim <Eddy.M.K@outlook.com>
2021-04-05 15:34:28 +0000

    Removed hello_world and debug_test; made changes to the actual project

--- projects/soft_999_eddy_kim_fw_102/src/main.c
+++ projects/soft_999_eddy_kim_fw_102/src/main.c
@@ -6,35 +6,30 @@
 #include "soft_timer.h"
 #include "wait.h"
 
-#define COUNTER_A_WAIT_TIME 500
+#define COUNTER_A_WAIT_TIME_MS 500
 
-void prv_timer_callback(SoftTimerId timer_id, void *context) {
-  typedef struct Counters {
-    uint8_t counter_a;
-    uint8_t counter_b;
-  } Counters;
+typedef struct Counters {
+  uint8_t counter_a;
+  uint8_t counter_b;
+} Counters;
 
+static void prv_timer_callback(SoftTimerId timer_id, void *context) {
   Counters *counters = context;
   LOG_DEBUG("Counter A: %d\n", ++(counters->counter_a));
   if (counters->counter_a % 2 == 0) {
     LOG_DEBUG("Counter B: %d\n", ++(counters->counter_b));
   }
 
-  soft_timer_start_millis(COUNTER_A_WAIT_TIME, prv_timer_callback, counters, NULL);
+  soft_timer_start_millis(COUNTER_A_WAIT_TIME_MS, prv_timer_callback, counters, NULL);
 }
 
 int main(void) {
-  typedef struct Counters {
-    uint8_t counter_a;
-    uint8_t counter_b;
-  } Counters;
-
   interrupt_init();
   soft_timer_init();
 
   Counters counters = { 0 };
 
-  soft_timer_start_millis(COUNTER_A_WAIT_TIME, prv_timer_callback, &counters, NULL);
+  soft_timer_start_millis(COUNTER_A_WAIT_TIME_MS, prv_timer_callback, &counters, NULL);
 
   while (true) {
     wait();
