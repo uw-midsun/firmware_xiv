35a69f777ddb8b6101a66f45dd8eab7744c603a9
Faraz Khoubsirat <farazkhoubsirat80@gmail.com>
2021-04-11 22:28:13 +0000

    added spi_exchange and i2c_write

--- mpxe/integration_tests/test_babydriver.py
+++ mpxe/integration_tests/test_babydriver.py
@@ -10,7 +10,8 @@ import adc_read
 import can_util
 import gpio_get
 import gpio_interrupts
-import gpio_port
+import spi_exchange
+import i2c_write
 import gpio_set
 import repl_setup
 
@@ -29,14 +30,16 @@ class TestBabyDriver(int_test.IntTest):
     def test_gpio_get(self):
         time.sleep(1)
         gpio_get.gpio_get('A', 6)
-
+        
     def test_gpio_set(self):
+        time.sleep(1)
+        self.babydriver.sim.get_gpio(proj=self.babydriver, port='A', pin=5) # To get intial states
+        
         time.sleep(1)
         gpio_set.gpio_set('A', 5, True)
         
-        time.sleep(0.5)
-        self.babydriver.sim.get_gpio(proj=self.babydriver, port='A', pin=5)
-        ''' Throws an error of KeyError: (1,0) '''
+        time.sleep(1)
+        self.babydriver.sim.get_gpio(proj=self.babydriver, port='A', pin=5) # To get states after the set
 
     def test_gpio_interrupts(self):
         time.sleep(1)
@@ -44,6 +47,14 @@ class TestBabyDriver(int_test.IntTest):
         
         time.sleep(1)
         gpio_interrupts.unregister_gpio_interrupt(port='A', pin=3)
+        
+    def test_i2c_write(self):
+        time.sleep(1)
+        i2c_write.i2c_write(port=1, address=200, tx_bytes=[1,2,3,4,5,6], reg=None)
+    
+    def test_spi_exchange(self):
+        time.sleep(1)
+        spi_exchange.spi_exchange(tx_bytes=[1,2,3,4,5,6], rx_len=6, spi_port=1, spi_mode=0, baudrate=6000000, cs=None)
         
 if __name__ == '__main__':
     unittest.main()
