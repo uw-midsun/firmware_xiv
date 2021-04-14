25452f4195ec24dfbc7b530c57ae36b3fad9f9ba
Eddy Kim <Eddy.M.K@outlook.com>
2021-04-14 02:03:21 +0000

    Resolved issues and formatting inconsistencies

---
+++ projects/soft_999_eddy_kim_fw_102/src/main.c
@@ -0,0 +1,30 @@
+#include "soft_timer.h"
+#include "wait.h"
+
+#define COUNTER_A_WAIT_TIME_MS 500
+
+typedef struct Counters {
+  uint8_t counter_a;
+  uint8_t counter_b;
+} Counters;
+
+static void prv_timer_callback(SoftTimerId timer_id, void *context) {
+  Counters *counters = context;
+  LOG_DEBUG("Counter A: %d\n", ++(counters->counter_a));
+  if (counters->counter_a % 2 == 0) {
+    LOG_DEBUG("Counter B: %d\n", ++(counters->counter_b));
+  }
+
+  soft_timer_start_millis(COUNTER_A_WAIT_TIME_MS, prv_timer_callback, counters, NULL);
+}
+
+int main(void) {
+  interrupt_init();
+  soft_timer_init();
+
+  Counters counters = { 0 };
+  
+  soft_timer_start_millis(COUNTER_A_WAIT_TIME_MS, prv_timer_callback, &counters, NULL);
+
+  while (true) {
+    wait();
--- projects/soft_999_eddy_kim_fw_103/src/main.c
+++ projects/soft_999_eddy_kim_fw_103/src/main.c
@@ -6,40 +6,54 @@
 #include "soft_timer.h"
 #include "wait.h"
 
-static void callback(const GpioAddress *address, void *context) {
-  // Is it right to pass the GpioAddress A6 by *context? I'm unsure what *address is supposed to
-  // return
+static void prv_callback(const GpioAddress *address, void *context) {
   GpioAddress *A6 = context;
   uint16_t A6_data = 0;
-  adc_read_raw_pin(*A6, &A6_data);
+  adc_read_converted_pin(*A6, &A6_data);
   LOG_DEBUG("%d\n", A6_data);
-  return;
 }
 
 int main(void) {
   gpio_init();
+  gpio_it_init();
   interrupt_init();
 
-  // Unsure if these are defined properly
-  GpioAddress B2 = { .port = GPIO_PORT_B, .pin = 2 };
-  GpioAddress A6 = { .port = GPIO_PORT_A, .pin = 6 };
+  GpioAddress button_pin = {
+    .port = GPIO_PORT_B,
+    .pin = 2,
+  };
 
-  // Unsure if these are defined properly as well
-  GpioSettings B2_settings = { GPIO_DIR_IN, GPIO_STATE_LOW, GPIO_RES_NONE, GPIO_ALTFN_ANALOG };
+  GpioAddress adc_reading = {
+    .port = GPIO_PORT_A,
+    .pin = 6,
+  };
 
-  GpioSettings A6_settings = { GPIO_DIR_IN, GPIO_STATE_LOW, GPIO_RES_NONE, GPIO_ALTFN_ANALOG };
+  GpioSettings button_pin_settings = {
+    .direction = GPIO_DIR_IN,
+    .state = GPIO_STATE_LOW,
+    .resistor = GPIO_RES_NONE,
+    .alt_function = GPIO_ALTFN_NONE,
+  };
+
+  GpioSettings adc_reading_settings = {
+    .direction = GPIO_DIR_IN,
+    .state = GPIO_STATE_LOW,
+    .resistor = GPIO_RES_NONE,
+    .alt_function = GPIO_ALTFN_ANALOG,
+  };
 
   adc_init(ADC_MODE_SINGLE);
 
-  adc_set_channel_pin(A6, true);
+  adc_set_channel_pin(adc_reading, true);
 
-  // Unsure if it's necessary to set channel pin for B2 as well
-  adc_set_channel_pin(B2, true);
+  InterruptSettings settings = {
+    INTERRUPT_TYPE_INTERRUPT,
+    INTERRUPT_PRIORITY_NORMAL,
+  };
 
-  InterruptSettings settings = { INTERRUPT_TYPE_INTERRUPT, INTERRUPT_PRIORITY_NORMAL };
-  InterruptType edge = { INTERRUPT_EDGE_FALLING };
+  InterruptEdge edge = INTERRUPT_EDGE_FALLING;
 
-  gpio_it_register_interrupt(&B2, &settings, edge, callback, &A6);
+  gpio_it_register_interrupt(&button_pin, &settings, edge, prv_callback, &adc_reading);
 
   while (true) {
     wait();
