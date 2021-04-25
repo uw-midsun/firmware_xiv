949cfa89c70be0b83e0ddfe9f015614fa884a271
Hewitt McGaughey <hewittmcgaughey@gmail.com>
2021-04-25 17:04:31 +0000

    clean up voltage_to_res func

--- projects/power_select/inc/power_select_thermistor.h
+++ projects/power_select/inc/power_select_thermistor.h
@@ -4,4 +4,17 @@
 
 double resistance_to_temp(double resistance);
 
-#define voltage_to_res(r) 30000.0 / (double)((r) / 1000.0) - 10000
+#define POWER_SELECT_THERMISTOR_VREF 3000.0f
+
+// Determined experimentally through testing
+#define POWER_SELECT_VREF_SCALING 10.0f
+
+#define POWER_SELECT_THERMISTOR_VREF_SCALED \
+  (POWER_SELECT_THERMISTOR_VREF * POWER_SELECT_VREF_SCALING)
+
+#define MV_TO_V 1000.0f
+
+#define POWER_SELECT_THERMISTOR_RESISTOR 10000
+
+#define voltage_to_res(v) \
+  POWER_SELECT_THERMISTOR_VREF_SCALED / (double)((v) / MV_TO_V) - POWER_SELECT_THERMISTOR_RESISTOR
