b146fac657cf1aa5b14c4e1c34eee084b180c706
Michael Ding <m28ding@uwaterloo.ca>
2021-04-30 22:47:24 +0000

    update 102 hw

--- projects/firmware_homework102/src/main.c
+++ projects/firmware_homework102/src/main.c
@@ -1,56 +1,40 @@
 #include <stdint.h>
 #include <stdlib.h>
-#include <time.h>
 
+#include "delay.h"
 #include "interrupt.h"
 #include "log.h"
 #include "soft_timer.h"
 #include "wait.h"
 
-struct Counters {
+#define COIN_FLIP_PERIOD_MS 500
+
+typedef struct Counters {
   uint8_t counter_a;
   uint8_t counter_b;
-};
+} Counters;
 
-struct Counters *counter_init() {
-  struct Counters *out = malloc(sizeof(struct Counters));
-  out->counter_a = 0;
-  out->counter_b = 0;
-  return out;
-}
-
-void delay(unsigned milliseconds) {
-  clock_t pause;
-  clock_t start;
-
-  pause = milliseconds * (CLOCKS_PER_SEC / 1000);
-  start = clock();
-  while ((clock() - start) < pause) {
-    continue;
-  }
-}
-
-void increment(SoftTimerId timer_id, void *context) {
-  struct Counters *temp = context;
-  temp->counter_a += 1;
+static void prv_increment(SoftTimerId timer_id, void *context) {
+  Counters *temp = context;
+  temp->counter_a++;
   LOG_DEBUG("Counter A: %d\n", temp->counter_a);
-  delay(0.5);
-  temp->counter_a += 1;
+  delay_s(0.5);
+  temp->counter_a++;
   LOG_DEBUG("Counter A: %d\n", temp->counter_a);
 
-  temp->counter_b += 1;
+  temp->counter_b++;
   LOG_DEBUG("Counter B: %d\n", temp->counter_b);
 
-  soft_timer_start_millis(500, increment, temp, NULL);
+  soft_timer_start_millis(COIN_FLIP_PERIOD_MS, prv_increment, temp, NULL);
 }
 
 int main(void) {
   interrupt_init();
   soft_timer_init();
 
-  struct Counters *temp = counter_init();
+  Counters temp = { 0 };
 
-  soft_timer_start_millis(500, increment, temp, NULL);
+  soft_timer_start_millis(COIN_FLIP_PERIOD_MS, prv_increment, &temp, NULL);
 
   while (1) {
     wait();
