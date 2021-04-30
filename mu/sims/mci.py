e1c8a19ac7caa935addfea73920518c3ac0134d6
Faraz Khoubsirat <farazkhoubsirat80@gmail.com>
2021-04-30 10:32:08 +0000

    [SOFT 450] reverted back the mci.py typing errors

--- mu/sims/mci.py
+++ mu/sims/mci.py
@@ -9,7 +9,7 @@ class Mci(BoardSim):
 
     def handle_store(self, store, key):
         # Upon precharge enable (PA9) going high, set PB0 and PA10 to mock precharge
-        if not self.precharge_enable and self.'a', 9):
-            self.precharge_enable = self.'a', 9)
+        if not self.precharge_enable and self.get_gpio('a', 9):
+            self.precharge_enable = self.get_gpio('a', 9)
             self.set_timer(0.1, self.set_gpio, ['b', 0, True])
             self.set_timer(0.2, self.set_gpio, ['a', 10, True])
