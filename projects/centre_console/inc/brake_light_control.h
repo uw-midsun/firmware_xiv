44d73df3a767ccafd40cf8d27dc60f7dee0107ef
FourRhythm <FourRhythm@users.noreply.github.com>
2021-05-01 11:12:32 -0400

    [SOFT-456] Brake Light Control
    In centre console, pedal_monitor.c receives updates from pedal and decides whether the pedal is pressed or released. Use this to tell power distribution when to turn the brake light on and off.
    
    To get PD to turn the brake light off, transmit a SYSTEM_CAN_MESSAGE_LIGHTS with the lights ID (first u8) as EE_LIGHTS_TYPE_BRAKES and the state (second u8) as either EE_LIGHTS_STATE_OFF or EE_LIGHTS_STATE_ON.
    
    To save bandwidth on the CAN network, only transmit this when the state changes.

---
+++ projects/centre_console/inc/brake_light_control.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// Retrieves pedal state from pedal_monitor.c and transmits CAN messages
+// to control brake lights
+
+// Requires pedal_monitor, CAN, GPIO, soft timers, event queue, and interrupts to be initialized.
+
+#define PEDAL_STATE_UPDATE_FREQUENCY_MS 100
+
+typedef enum {
+  BRAKE_LIGHT_CONTROL_CAN_RX = 0,
+  BRAKE_LIGHT_CONTROL_CAN_TX,
+  BRAKE_LIGHT_CONTROL_FAULT,
+  NUM_BRAKE_LIGHT_CONTROL_CAN_EVENTS
+} BrakeLightControlEvent;
+
+StatusCode brake_light_control_init(void);
---
+++ projects/centre_console/src/brake_light_control.c
@@ -0,0 +1,41 @@
+#include "brake_light_control.h"
+
+#include "can.h"
+#include "can_transmit.h"
+#include "centre_console_events.h"
+#include "gpio.h"
+#include "gpio_it.h"
+#include "log.h"
+#include "pedal_monitor.h"
+#include "pedal_rx.h"
+#include "soft_timer.h"
+
+static PedalState s_prev_pedal_state = PEDAL_STATE_RELEASED;
+
+// Called when brake light should change
+// Will transmit the appropriate CanMessage to turn brake lights on/off
+static void prv_can_transmit_brake_light_change(PedalState current_state) {
+  if (current_state == PEDAL_STATE_RELEASED) {
+    CAN_TRANSMIT_LIGHTS(SYSTEM_CAN_MESSAGE_LIGHTS, EE_LIGHT_STATE_OFF);
+
+  } else if (current_state == PEDAL_STATE_PRESSED) {
+    CAN_TRANSMIT_LIGHTS(SYSTEM_CAN_MESSAGE_LIGHTS, EE_LIGHT_STATE_ON);
+  }
+}
+
+// Callback function for soft timer
+// Checks whether or not a CAN message should be transmitted
+static void prv_update_brake_lights(SoftTimerId timer_id, void *context) {
+  PedalState current_state = get_pedal_state();
+  if (current_state != s_prev_pedal_state) {
+    prv_can_transmit_brake_light_change(current_state);
+  }
+  status_ok_or_return(soft_timer_start_millis(PEDAL_STATE_UPDATE_FREQUENCY_MS,
+                                              prv_update_brake_lights, NULL, NULL));
+}
+
+StatusCode brake_light_control_init(void) {
+  status_ok_or_return(soft_timer_start_millis(PEDAL_STATE_UPDATE_FREQUENCY_MS,
+                                              prv_update_brake_lights, NULL, NULL));
+  return STATUS_CODE_OK;
+}
