7f59c32bb94b6d3859fae9821139a3c640e5b014
selinahsu <selinaraindrop@gmail.com>
2021-05-05 18:21:29 +0000

    delay_us implemented without soft timer

--- libraries/ms-common/src/delay.c
+++ libraries/ms-common/src/delay.c
@@ -2,19 +2,15 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <time.h>
 
 #include "soft_timer.h"
 #include "wait.h"
 
-static void prv_delay_it(SoftTimerId timer_id, void *context) {
-  volatile bool *block = context;
-  *block = false;
-}
-
 void delay_us(uint32_t t) {
-  volatile bool block = true;
-  soft_timer_start(t, prv_delay_it, (void *)&block, NULL);
-  while (block) {
-    wait();
+  clock_t end = t * (CLOCKS_PER_SEC / 1000000);
+  clock_t start = clock();
+
+  while ((clock() - start) < end) {
   }
 }
