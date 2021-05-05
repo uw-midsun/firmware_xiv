55e614a7439103503a017446b7b6630ed864d4b4
Frank Yan <frankyan2351@gmail.com>
2021-05-05 00:44:47 +0000

    Update interrupt event, add data checking, and fix commenting

--- projects/firmware_homework/src/main.c
+++ projects/firmware_homework/src/main.c
@@ -9,20 +9,25 @@
 // Interrupt callback function
 static void prv_gpio_callback(const GpioAddress *address, void *context) {
   // Context should be the address of A6 as we want to read from this pin
-  GpioAddress *adc_channel = context;
+  GpioAddress *adc_reading = context;
   uint16_t adc_data = 0;
   // Read the values from the ADC channel and output the reading
-  adc_read_converted_pin(*adc_channel, &adc_data);
-  LOG_DEBUG("ADC Reading: %i\n", adc_data);
+  // Check the data to see if it is valid first
+  if (adc_read_converted_pin(*adc_reading, &adc_data) == STATUS_CODE_OK) {
+    LOG_DEBUG("ADC Reading: %i\n", adc_data);
+  } else {
+    LOG_DEBUG("Invalid Reading: %i\n");
+    // I'll just put invalid reading for anything else than STATUS CODE OK
+  }
 }
 
 int main(void) {
-  /*Initializations*/
+  // Initializations
   interrupt_init();  // Enable interrupts
   gpio_init();       // Set up all GPIO pins
   gpio_it_init();    // Set GPIO pins for interrupts
 
-  /*Pin Defintions*/
+  // Pin Defintions
   // GPIO pin (B2) that is connected to the button and "triggers" the interrupt
   GpioAddress button_pin = {
     .port = GPIO_PORT_B,
@@ -51,14 +56,14 @@ int main(void) {
   gpio_init_pin(&button_pin, &button_pin_settings);
   gpio_init_pin(&adc_pin, &adc_pin_settings);
 
-  /*ADC Functions*/
+  // ADC Functions
   adc_init(ADC_MODE_SINGLE);
   adc_set_channel_pin(adc_pin, true);  // Set up the ADC to read from pin A6
 
-  /*Interrupt*/
+  // Interrupt
   // Default settings
   InterruptSettings settings = {
-    .type = INTERRUPT_TYPE_EVENT,
+    .type = INTERRUPT_TYPE_INTERRUPT,
     .priority = INTERRUPT_PRIORITY_NORMAL,
   };
   // Want to detect the falling edge for a button (when it is released)
@@ -66,7 +71,7 @@ int main(void) {
   // Set Pin B2 to register interrupts and call the interrupt function when the button is pressed
   gpio_it_register_interrupt(&button_pin, &settings, edge, prv_gpio_callback, &adc_pin);
 
-  /*Infinite loop*/
+  // Infinite loop
   while (true) {
     wait();
   }
