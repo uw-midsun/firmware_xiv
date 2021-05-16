fec94fee909c1d289e15d1ac212c39c27ccb9846
ShiCheng Lu <shichenglu0708@gmail.com>
2021-05-16 22:54:24 +0000

    completed fw 102 counter homework

---
+++ projects/soft_999_shicheng_lu_fw_102/src/main.c
@@ -0,0 +1,39 @@
+#include <stdint.h>
+
+#include "interrupt.h"
+#include "log.h"
+#include "soft_timer.h"
+#include "wait.h"
+
+#define DELAY_MS 500
+
+typedef struct Counters {
+  uint8_t counter_a;
+  uint8_t counter_b;
+} Counters;
+
+void increment_counters(SoftTimerId id, void *context) {
+  Counters *counter = context;
+
+  counter->counter_a++;
+  LOG_DEBUG("Counter A: %i\n", counter->counter_a);
+  if (counter->counter_a % 2 == 0) {
+    counter->counter_b++;
+    LOG_DEBUG("Counter B: %i\n", counter->counter_b);
+  }
+
+  soft_timer_start_millis(DELAY_MS, increment_counters, context, NULL);
+}
+
+int main() {
+  interrupt_init();
+  soft_timer_init();
+
+  Counters counter = { 0 };
+
+  soft_timer_start_millis(DELAY_MS, increment_counters, &counter, NULL);
+  while (true) {
+    wait();
+  }
+  return 0;
+}
