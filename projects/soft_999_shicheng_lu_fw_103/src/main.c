9d75ad8e9b19f1f374951b34f5c433412a9cbc79
ShiCheng Lu <shichenglu0708@gmail.com>
2021-05-18 20:51:18 +0000

    changed adc pin and log adc function's names

--- projects/soft_999_shicheng_lu_fw_103/src/main.c
+++ projects/soft_999_shicheng_lu_fw_103/src/main.c
@@ -7,11 +7,11 @@
 #include "wait.h"
 
 // LOG_DEBUG the adc converted value of context pin when called (context is A6 pin)
-void log_adc_a6(const GpioAddress *address, void *context) {
-  GpioAddress *pin_a6_addr = context;
-  uint16_t pin_a6_data = 0;
-  adc_read_converted_pin(*pin_a6_addr, &pin_a6_data);
-  LOG_DEBUG("adc converted pin A6 data: %d\n", pin_a6_data);
+static void priv_log_adc_value(const GpioAddress *address, void *context) {
+  GpioAddress *adc_addr = context;
+  uint16_t adc_data = 0;
+  adc_read_converted_pin(*adc_addr, &adc_data);
+  LOG_DEBUG("adc converted pin A6 data: %d\n", adc_data);
 }
 
 int main(void) {
@@ -20,17 +20,24 @@ int main(void) {
   adc_init(ADC_MODE_SINGLE);
 
   // output gpio pin A6
-  GpioAddress analog_pin_a6_addr = { .port = GPIO_PORT_A, .pin = 6 };
-  GpioSettings analog_pin_a6_settings = {
+  GpioAddress adc_addr = {
+    .port = GPIO_PORT_A,
+    .pin = 6,
+  };
+  GpioSettings adc_settings = {
     GPIO_DIR_IN,
     GPIO_STATE_LOW,
     GPIO_RES_NONE,
     GPIO_ALTFN_ANALOG,
   };
-  gpio_init_pin(&analog_pin_a6_addr, &analog_pin_a6_settings);
+  gpio_init_pin(&adc_addr, &adc_settings);
+  adc_set_channel_pin(adc_addr);
 
   // button gpio pin
-  GpioAddress button_addr = { .port = GPIO_PORT_B, .pin = 2 };
+  GpioAddress button_addr = {
+    .port = GPIO_PORT_B,
+    .pin = 2,
+  };
   GpioSettings button_settings = {
     GPIO_DIR_IN,
     GPIO_STATE_LOW,
@@ -41,12 +48,12 @@ int main(void) {
 
   InterruptSettings interrupt_settings = {
     INTERRUPT_TYPE_INTERRUPT,
-    INTERRUPT_PRIORITY_HIGH,
+    INTERRUPT_PRIORITY_NORMAL,
   };
 
   // calls log_adc_output on the falling edge of button press
   gpio_it_register_interrupt(&button_addr, &interrupt_settings, INTERRUPT_EDGE_FALLING, log_adc_a6,
-                             &analog_pin_a6_addr);
+                             &adc_addr);
 
   while (true) {
     wait();
