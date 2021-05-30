d2f796e9572cdd4ef3ea27a6b98b6e4deaa36320
ShiCheng Lu <shichenglu0708@gmail.com>
2021-05-30 22:33:46 +0000

    fixed build error, I'm dumb

--- projects/soft_999_shicheng_lu_fw_102/src/main.c
+++ projects/soft_999_shicheng_lu_fw_102/src/main.c
@@ -15,11 +15,11 @@ typedef struct Counters {
 static void prv_increment_counters(SoftTimerId id, void *context) {
   Counters *counters = context;
 
-  counter->counter_a++;
-  LOG_DEBUG("Counter A: %i\n", counter->counter_a);
-  if (counter->counter_a % 2 == 0) {
-    counter->counter_b++;
-    LOG_DEBUG("Counter B: %i\n", counter->counter_b);
+  counters->counter_a++;
+  LOG_DEBUG("Counter A: %i\n", counters->counter_a);
+  if (counters->counter_a % 2 == 0) {
+    counters->counter_b++;
+    LOG_DEBUG("Counter B: %i\n", counters->counter_b);
   }
 
   soft_timer_start_millis(DELAY_MS, prv_increment_counters, context, NULL);
@@ -31,7 +31,7 @@ int main() {
 
   Counters counters = { 0 };
 
-  soft_timer_start_millis(DELAY_MS, prv_increment_counters, &counter, NULL);
+  soft_timer_start_millis(DELAY_MS, prv_increment_counters, &counters, NULL);
   while (true) {
     wait();
   }
