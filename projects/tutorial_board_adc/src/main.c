cd2cfbb02e40e430e7d9c491d4fac4d427b3b6b5
Avery Chiu <avery.chiu1@uwaterloo.ca>
2021-06-04 19:35:44 -0400

    Fixed tutorial board adc

--- projects/tutorial_board_adc/src/main.c
+++ projects/tutorial_board_adc/src/main.c
@@ -10,31 +10,26 @@ int main(void) {
   soft_timer_init();
   gpio_init();
 
-  GpioAddress potentiometer_addr = { .port = GPIO_PORT_A, .pin = 0 };
+  GpioAddress potentiometer_addr = {
+    .port = GPIO_PORT_A,
+    .pin = 0,
+  };
 
   GpioSettings pot_settings = {
-    GPIO_DIR_IN,        //
-    GPIO_STATE_LOW,     //
-    GPIO_RES_NONE,      //
-    GPIO_ALTFN_ANALOG,  //
+    .direction = GPIO_DIR_IN,
+    .state = GPIO_STATE_LOW,
+    .resistor = GPIO_RES_NONE,
+    .alt_function = GPIO_ALTFN_ANALOG,
   };
 
   gpio_init_pin(&potentiometer_addr, &pot_settings);
   adc_init(ADC_MODE_SINGLE);
 
-  AdcChannel pot_channel = NUM_ADC_CHANNELS;
-
-  adc_get_channel(potentiometer_addr, &pot_channel);
-  adc_set_channel(pot_channel, true);
-  adc_set_channel(ADC_CHANNEL_TEMP, true);
-
-  LOG_DEBUG("Potentiometer ADC Channel: %d\n", pot_channel);
+  adc_set_channel_pin(potentiometer_addr, true);
 
   while (true) {
     uint16_t potentiometer_data = 0;
-    uint16_t temperature_data = 0;
-    adc_read_raw(pot_channel, &potentiometer_data);
-    adc_read_raw(ADC_CHANNEL_TEMP, &temperature_data);
+    adc_read_raw_pin(potentiometer_addr, &potentiometer_data);
     LOG_DEBUG("potentiometer data: %d\n", potentiometer_data);
     delay_ms(200);
   }
