e7d36719f08c4bdbd446e5a4a70f1e45a43bac4d
Ben Lin <lbhan1221@gmail.com>
2021-06-10 03:17:04 +0000

    made fixes

--- projects/adc_challenge/src/main.c
+++ projects/adc_challenge/src/main.c
@@ -3,28 +3,26 @@
 #include "gpio_it.h"
 #include "interrupt.h"
 #include "log.h"
-#include "soft_timer.h"
 #include "wait.h"
 
 // reads from the specified address and logs the value
 static void prv_read_from_adc_addr(const GpioAddress *address, void *context) {
-  GpioAddress addr = {
-    .port = address->port,  //
-    .pin = address->pin     //
-  };
+  GpioAddress *addr = context;
   uint16_t adc_data = 0;
-  adc_read_converted_pin(addr, &adc_data);
+  adc_read_converted_pin(*addr, &adc_data);
 
-  LOG_DEBUG("ADC pin has value %d", adc_data);
+  LOG_DEBUG("ADC pin has value %d\n", adc_data);
 }
 
 int main() {
   interrupt_init();
-  soft_timer_init();
   gpio_init();
   gpio_it_init();
 
-  GpioAddress input_addr = { .port = GPIO_PORT_A, .pin = 6 };
+  GpioAddress input_addr = {
+    .port = GPIO_PORT_A,
+    .pin = 6,
+  };
 
   GpioSettings input_settings = {
     .direction = GPIO_DIR_IN,           //
@@ -37,7 +35,10 @@ int main() {
   adc_init(ADC_MODE_SINGLE);
   adc_set_channel_pin(input_addr, true);
 
-  GpioAddress button_addr = { .port = GPIO_PORT_B, .pin = 2 };
+  GpioAddress button_addr = {
+    .port = GPIO_PORT_B,
+    .pin = 2,
+  };
 
   GpioSettings button_settings = {
     .direction = GPIO_DIR_IN,         //
