5554c43650482ea70858730c152ee97ab2cb843e
Logan <loganyu439@gmail.com>
2021-06-15 02:10:51 +0000

    completed 102 homework: hello_world and blinking_led

---
+++ projects/blinking_led/src/main.c
@@ -0,0 +1,37 @@
+#include "interrupt.h"   // apparently soft timers needs this
+#include "log.h"         // similar to prinf but indicates the line #
+#include "soft_timer.h"  // soft timers
+#include "wait.h"        // for wait function
+
+#define COUNTER_PERIOD_MS 500  // the time between each count in ms
+
+typedef struct Counters {
+  uint8_t counter_a;
+  uint8_t counter_b;
+} Counters;
+
+void SoftTimerCallCounter(SoftTimerId timer_id, void *context) {
+  Counters *storage = context;
+  if ((storage->counter_a + 1) % 2 != 0 || storage->counter_a == 0) {
+    storage->counter_a++;
+    LOG_DEBUG("Counter A: %i\n", storage->counter_a);
+  } else {
+    storage->counter_a++;
+    storage->counter_b++;
+    LOG_DEBUG("Counter A: %i\n", storage->counter_a);
+    LOG_DEBUG("Counter B: %i\n", storage->counter_b);
+  }
+  soft_timer_start_millis(COUNTER_PERIOD_MS, SoftTimerCallCounter, storage, NULL);
+}
+
+int main() {
+  interrupt_init();
+  soft_timer_init();
+  Counters storage = { 0 };
+  soft_timer_start_millis(COUNTER_PERIOD_MS, SoftTimerCallCounter, &storage, NULL);
+
+  while (true) {
+    wait();
+  }
+  return 0;
+}
