00f7385ad319ab885e1e11c3277b24653ea7b87e
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2021-06-16 03:37:06 +0000

    New changes to to firmware 103 suggested by Avery, 2021-06-15

--- projects/button_challenge/src/main.c
+++ projects/button_challenge/src/main.c
@@ -22,23 +22,35 @@ int main() {
   gpio_it_init();
 
   // Addresses
-  const GpioAddress button_address = { .port = GPIO_PORT_B, .pin = 2 };
+  const GpioAddress button_address = {
+    .port = GPIO_PORT_B,
+    .pin = 2,
+  };
 
-  const GpioAddress adc_address = { .port = GPIO_PORT_A, .pin = 6 };
+  const GpioAddress adc_address = {
+    .port = GPIO_PORT_A,
+    .pin = 6,
+  };
 
   // Settings
-  GpioSettings button_settings = { .direction = GPIO_DIR_IN,
-                                   .state = GPIO_STATE_HIGH,
-                                   .alt_function = GPIO_ALTFN_NONE,
-                                   .resistor = GPIO_RES_PULLDOWN };
+  GpioSettings button_settings = {
+    .direction = GPIO_DIR_IN,
+    .state = GPIO_STATE_HIGH,
+    .alt_function = GPIO_ALTFN_NONE,
+    .resistor = GPIO_RES_PULLDOWN,
+  };
 
-  GpioSettings adc_settings = { .direction = GPIO_DIR_OUT,
-                                .state = GPIO_STATE_LOW,
-                                .alt_function = GPIO_ALTFN_ANALOG,
-                                .resistor = GPIO_RES_NONE };
+  GpioSettings adc_settings = {
+    .direction = GPIO_DIR_OUT,
+    .state = GPIO_STATE_LOW,
+    .alt_function = GPIO_ALTFN_ANALOG,
+    .resistor = GPIO_RES_NONE,
+  };
 
-  InterruptSettings interrupt_settings = { .type = INTERRUPT_TYPE_INTERRUPT,
-                                           .priority = INTERRUPT_PRIORITY_NORMAL };
+  InterruptSettings interrupt_settings = {
+    .type = INTERRUPT_TYPE_INTERRUPT,
+    .priority = INTERRUPT_PRIORITY_NORMAL,
+  };
 
   // Initializations
   gpio_init_pin(&button_address, &button_settings);
@@ -46,12 +58,6 @@ int main() {
   adc_init(ADC_MODE_SINGLE);
 
   adc_set_channel_pin(adc_address, true);
-
-  // Outdated ADC reading
-  // AdcChannel adc_channel = NUM_ADC_CHANNELS;
-  // adc_get_channel(adc_address, &adc_channel);
-  // adc_set_channel(adc_channel, true);
-  // adc_set_channel(ADC_CHANNEL_TEMP, true);
 
   gpio_it_register_interrupt(&button_address, &interrupt_settings, INTERRUPT_EDGE_RISING,
                              prv_adc_callback, &adc_address);
