80ec1a7d1480b0813da4e36cc5a73be0bf252a9e
Ryan Dancy <ryan@keal.ca>
2021-06-16 20:25:43 -0400

    Don't commit merge conflict markers (oops)

--- mu/ctl/req.py
+++ mu/ctl/req.py
@@ -1,11 +1,6 @@
 import requests
 from mu.srv.server import TCP_PORT
 
-<<<<<<< HEAD
-
-def send(route, body=None):
-=======
 def send(route, params=None, body=None):
->>>>>>> master
     url = 'http://localhost:{}/{}'.format(TCP_PORT, route)
     return requests.get(url, params=params, json=body)
