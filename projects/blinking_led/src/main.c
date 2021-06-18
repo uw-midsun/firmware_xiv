cdd222193ad241680fe91ef04f764732c0aaecff
Logan Yu <loganyu438@gmail.com>
2021-06-18 19:34:02 -0400

    updated blinking_led

--- projects/blinking_led/src/main.c
+++ projects/blinking_led/src/main.c
@@ -10,25 +10,22 @@ typedef struct Counters {
   uint8_t counter_b;
 } Counters;
 
-void SoftTimerCallCounter(SoftTimerId timer_id, void *context) {
+static void prv_SoftTimerCallCounter(SoftTimerId timer_id, void *context) {
   Counters *storage = context;
-  if ((storage->counter_a + 1) % 2 != 0 || storage->counter_a == 0) {
-    storage->counter_a++;
-    LOG_DEBUG("Counter A: %i\n", storage->counter_a);
-  } else {
-    storage->counter_a++;
+  storage->counter_a++;
+  LOG_DEBUG("Counter A: %i\n", storage->counter_a);
+  if ((storage->counter_a) % 2 == 0) {
     storage->counter_b++;
-    LOG_DEBUG("Counter A: %i\n", storage->counter_a);
     LOG_DEBUG("Counter B: %i\n", storage->counter_b);
   }
-  soft_timer_start_millis(COUNTER_PERIOD_MS, SoftTimerCallCounter, storage, NULL);
+  soft_timer_start_millis(COUNTER_PERIOD_MS, prv_SoftTimerCallCounter, storage, NULL);
 }
 
 int main() {
   interrupt_init();
   soft_timer_init();
   Counters storage = { 0 };
-  soft_timer_start_millis(COUNTER_PERIOD_MS, SoftTimerCallCounter, &storage, NULL);
+  soft_timer_start_millis(COUNTER_PERIOD_MS, prv_SoftTimerCallCounter, &storage, NULL);
 
   while (true) {
     wait();
