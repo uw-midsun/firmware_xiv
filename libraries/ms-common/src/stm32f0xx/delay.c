4587930453e3bcede38faf03053431301f21004e
ShiCheng Lu <shichenglu0708@gmail.com>
2021-06-21 19:52:06 +0000

    use soft_timer when not in critical section, use clock timer when in critical section (interrupts disabled), jank fix, but delay really shouldn't be used in critical section anyway

--- libraries/ms-common/src/stm32f0xx/delay.c
+++ libraries/ms-common/src/stm32f0xx/delay.c
@@ -1,10 +1,16 @@
 #include "delay.h"
+
 #include <stdbool.h>
+#include <stddef.h>
+
+#include "soft_timer.h"
 #include "stm32f0xx.h"
+#include "stm32f0xx_misc.h"
+#include "wait.h"
 
 // needs soft_timer to be initalized
 
-void delay_us(uint32_t t) {
+static void prv_delay_us_sys_clock(uint32_t t) {
   uint32_t current_time = TIM_GetCounter(TIM2);  // use TIM2
   uint32_t end_time = current_time + t;
   // since t is uint32, there is a max of 1 rollover
@@ -17,5 +23,28 @@ void delay_us(uint32_t t) {
       rollover = false;
     }
     current_time = time;
+  }
+}
+
+static void prv_delay_it(SoftTimerId timer_id, void *context) {
+  volatile bool *block = context;
+  *block = false;
+}
+
+static void prv_delay_us_soft_timer(uint32_t t) {
+  volatile bool block = true;
+  soft_timer_start(t, prv_delay_it, (void *)&block, NULL);
+  while (block) {
+    wait();
+  }
+}
+
+void delay_us(uint32_t t) {
+  // use softtimer if interrupts are not disabled
+  // use clock delay if iterrupts are disabled
+  if (__get_PRIMASK()) {  // interrupts disabled
+    prv_delay_us_sys_clock(t);
+  } else {
+    prv_delay_us_soft_timer(t);
   }
 }
--- libraries/ms-common/src/x86/delay.c
+++ libraries/ms-common/src/x86/delay.c
@@ -6,9 +6,11 @@
 #include <sys/time.h>
 #include <time.h>
 
+#include "soft_timer.h"
 #include "wait.h"
+#include "x86_interrupt.h"
 
-void delay_us(uint32_t t) {
+static void prv_delay_us_sys_clock(uint32_t t) {
   volatile struct timespec timer;
 
   // Start clock
@@ -21,5 +23,50 @@ void delay_us(uint32_t t) {
 
   while (timer.tv_sec < end_sec || (timer.tv_sec == end_sec && timer.tv_nsec < end_nano)) {
     clock_gettime(CLOCK_MONOTONIC_RAW, &timer);
+  }
+}
+
+// static uint32_t prv_get_time_us() {
+//   struct timespec timer;
+//   clock_gettime(CLOCK_MONOTONIC_RAW, &timer);
+//   return (timer.tv_sec * 1000000 + timer.tv_nsec / 1000);
+// }
+
+// void delay_us(uint32_t t) {
+//   uint32_t current_time = prv_get_time_us();
+//   uint32_t end_time = current_time + t;
+//   // since t is uint32, there is a max of 1 rollover
+//   bool rollover = (end_time < current_time);
+
+//   while (rollover || current_time < end_time) {
+//     // update time
+//     uint32_t time = prv_get_time_us();
+//     if (time < current_time) {  // rollover detection
+//       rollover = false;
+//     }
+//     current_time = time;
+//   }
+// }
+
+static void prv_delay_it(SoftTimerId timer_id, void *context) {
+  volatile bool *block = context;
+  *block = false;
+}
+
+static void prv_delay_us_soft_timer(uint32_t t) {
+  volatile bool block = true;
+  soft_timer_start(t, prv_delay_it, (void *)&block, NULL);
+  while (block) {
+    wait();
+  }
+}
+
+void delay_us(uint32_t t) {
+  // use softtimer if interrupts are not disabled
+  // use clock delay if iterrupts are disabled
+  if (x86_interrupt_in_handler()) {  // interrupts disabled
+    prv_delay_us_sys_clock(t);
+  } else {
+    prv_delay_us_soft_timer(t);
   }
 }
