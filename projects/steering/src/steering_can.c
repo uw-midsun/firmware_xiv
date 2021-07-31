ef216c278d3856d333d19fa93e4d08d146e87ba7
JarvisWeng <jarvis0910@gmail.com>
2021-07-30 20:45:55 -0400

    update steering

--- projects/steering/src/steering_can.c
+++ projects/steering/src/steering_can.c
@@ -7,16 +7,19 @@
 #include "steering_digital_input.h"
 #include "steering_events.h"
 
+static bool turning = false;
+
 StatusCode steering_can_process_event(Event *e) {
   switch (e->id) {
     case STEERING_INPUT_HORN_EVENT:
       CAN_TRANSMIT_HORN((EEHornState)e->data);
       break;
     case STEERING_DRL_1_EVENT:
-      CAN_TRANSMIT_LIGHTS(EE_LIGHT_TYPE_DRL, (EELightState)e->data);
+      if (turning == false)
+        CAN_TRANSMIT_LIGHTS(EE_LIGHT_TYPE_DRL, EE_LIGHT_STATE_ON);
       break;
     case STEERING_DRL_2_EVENT:
-      CAN_TRANSMIT_LIGHTS(EE_LIGHT_TYPE_DRL, (EELightState)e->data);
+      CAN_TRANSMIT_LIGHTS(EE_LIGHT_TYPE_DRL, EE_LIGHT_STATE_OFF);
       break;
     case STEERING_DIGITAL_INPUT_CC_TOGGLE_PRESSED_EVENT:
       CAN_TRANSMIT_CRUISE_CONTROL_COMMAND(EE_CRUISE_CONTROL_COMMAND_TOGGLE);
@@ -29,9 +32,21 @@ StatusCode steering_can_process_event(Event *e) {
       break;
     case STEERING_CONTROL_STALK_EVENT_LEFT_SIGNAL:
       CAN_TRANSMIT_LIGHTS(EE_LIGHT_TYPE_SIGNAL_LEFT, (EELightState)e->data);
+      if ((EELightState)e->data == EE_LIGHT_STATE_ON)
+      {
+        turning = true;
+      } else {
+        turning = false;
+      }
       break;
     case STEERING_CONTROL_STALK_EVENT_RIGHT_SIGNAL:
       CAN_TRANSMIT_LIGHTS(EE_LIGHT_TYPE_SIGNAL_RIGHT, (EELightState)e->data);
+      if ((EELightState)e->data == EE_LIGHT_STATE_ON)
+      {
+        turning = true;
+      } else {
+        turning = false;
+      }
       break;
     case STEERING_REGEN_BRAKE_EVENT:
       CAN_TRANSMIT_REGEN_BRAKING_TOGGLE_REQUEST();
