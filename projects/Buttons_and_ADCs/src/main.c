54d8807c302bc99e0fa8327ab02bc3f7752a78bd
Logan Yu <loganyu438@gmail.com>
2021-08-18 22:50:20 -0400

    made more changed

--- projects/Buttons_and_ADCs/src/main.c
+++ projects/Buttons_and_ADCs/src/main.c
@@ -5,41 +5,57 @@
 #include "gpio_it.h"
 #include "interrupt.h"
 #include "log.h"
-#include "soft_timer.h"
 
-
-typedef enum { BUTTON_B2 = 0, NUM_BUTTONS } Button;
-
-typedef enum { SENSOR_A6 = 0, NUM_SENSORS } Sensor;
-
-static GpioAddress button_addresses[] = {
-    [BUTTON_B2] = { .port = GPIO_PORT_B, .pin = 2}
-    };
-
-static GpioAddress sensor_addresses[] = { 
-    [SENSOR_A6] = { .port = GPIO_PORT_A, .pin = 6} 
-    };
-
-static GpioSettings button_setting= { 
-    .direction = GPIO_DIR_IN,
-    .alt_function = GPIO_ALTFN_NONE,
-    .resistor = GPIO_RES_NONE,
-    };
-
-static GpioSettings sensor_setting = { 
-    .direction = GPIO_DIR_IN,
-    .state = GPIO_STATE_HIGH,
-    .alt_function = GPIO_ALTFN_NONE,
-    .resistor = GPIO_RES_NONE 
-    };
-
-static InterruptSettings s_interrupt_settings = {
-  .type = INTERRUPT_TYPE_INTERRUPT,       
-  .priority = INTERRUPT_PRIORITY_NORMAL,  
-    };
-
-static void prv_button_interrupt_handler(const GpioAddress *address, void *context) {
-  GpioAddress *button_address = (GpioAddress *)context;
-  gpio_toggle_state(button_address);
+static void prv_adc_reading(const GpioAddress *address, void *context){
+    GpioAddress *address = context;
+    uint16_t adc = 0;
+    adc_read_converted_pin(*address, &adc);
+    LOG_DEBUG("ADC reading: %d\n", adc);
 }
 
+int main(void){
+    interrupt_init();
+    gpio_init();
+    gpio_it();
+
+    typedef enum { BUTTON_B2 = 0, NUM_BUTTONS } Button;
+
+    typedef enum { ADC_A6 = 0, NUM_SENSORS } ADC;
+
+    static GpioAddress button_addresses[] = {
+        [BUTTON_B2] = { .port = GPIO_PORT_B, .pin = 2}
+        };
+        
+    static GpioSettings button_setting= { 
+        .direction = GPIO_DIR_IN,
+        .state = GPIO_STATE_LOW,
+        .alt_function = GPIO_ALTFN_NONE,
+        .resistor = GPIO_RES_NONE,
+        };
+
+    static GpioAddress sensor_addresses[] = { 
+        [ADC_A6] = { .port = GPIO_PORT_A, .pin = 6} 
+        };
+
+    static GpioSettings adc_setting = { 
+        .direction = GPIO_DIR_IN,
+        .state = GPIO_STATE_LOW,
+        .alt_function = GPIO_ALTFN_NONE,
+        .resistor = GPIO_RES_NONE 
+        };
+
+    static InterruptSettings s_interrupt_settings = {
+    .type = INTERRUPT_TYPE_INTERRUPT,       
+    .priority = INTERRUPT_PRIORITY_NORMAL,  
+        };
+    
+    //gpio_init_pin(&potentiometer_addr, &pot_settings);
+    //adc_set_channel_pin(potentiometer_addr, true);
+    //adc_init(ADC_MODE_SINGLE);
+    //gpio_it_register_interrupt(&button_addresses[BUTTON_GREEN], &s_interrupt_settings,
+    //                        INTERRUPT_EDGE_RISING, prv_button_interrupt_handler,
+    //                        &led_addresses[LED_BLUE]);
+    while (true) {
+        wait();
+    }
+}
