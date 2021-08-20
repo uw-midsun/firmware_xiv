adcb91aaf36de3d9e9368e0cd9fc5df2976f5eb9
Logan Yu <loganyu438@gmail.com>
2021-08-19 20:30:34 -0400

    updated more on buttons and adc

--- projects/Buttons_and_ADCs/src/main.c
+++ projects/Buttons_and_ADCs/src/main.c
@@ -1,16 +1,16 @@
-#include "wait.h"     
 #include "adc.h"
 #include "delay.h"
 #include "gpio.h"
 #include "gpio_it.h"
 #include "interrupt.h"
 #include "log.h"
+#include "soft_timer.h"
 
-static void prv_adc_reading(const GpioAddress *address, void *context){
-    GpioAddress *address = context;
+static void prv_button_interrupt_handler(const GpioAddress *adc_address, void *context){
+    GpioAddress *adc_address = context;
     uint16_t adc = 0;
-    adc_read_converted_pin(*address, &adc);
-    LOG_DEBUG("ADC reading: %d\n", adc);
+    adc_read_converted_pin(*adc_address, &adc);
+    LOG_DEBUG("ADC reading is: %d\n", adc);
 }
 
 int main(void){
@@ -22,10 +22,13 @@ int main(void){
 
     typedef enum { ADC_A6 = 0, NUM_SENSORS } ADC;
 
-    static GpioAddress button_addresses[] = {
-        [BUTTON_B2] = { .port = GPIO_PORT_B, .pin = 2}
+    //Button address
+    static GpioAddress button_addresses = {
+        .port = GPIO_PORT_B, 
+        .pin = 2,
         };
-        
+    
+    //Button setting
     static GpioSettings button_setting= { 
         .direction = GPIO_DIR_IN,
         .state = GPIO_STATE_LOW,
@@ -33,29 +36,35 @@ int main(void){
         .resistor = GPIO_RES_NONE,
         };
 
-    static GpioAddress sensor_addresses[] = { 
-        [ADC_A6] = { .port = GPIO_PORT_A, .pin = 6} 
+    //Adc address
+    static GpioAddress adc_addresses = { 
+        .port = GPIO_PORT_A, 
+        .pin = 6,
         };
 
+    //Adc setting
     static GpioSettings adc_setting = { 
         .direction = GPIO_DIR_IN,
         .state = GPIO_STATE_LOW,
-        .alt_function = GPIO_ALTFN_NONE,
+        .alt_function = GPIO_ALTFN_ANALOG,
         .resistor = GPIO_RES_NONE 
         };
 
     static InterruptSettings s_interrupt_settings = {
     .type = INTERRUPT_TYPE_INTERRUPT,       
     .priority = INTERRUPT_PRIORITY_NORMAL,  
-        };
-    
-    //gpio_init_pin(&potentiometer_addr, &pot_settings);
-    //adc_set_channel_pin(potentiometer_addr, true);
-    //adc_init(ADC_MODE_SINGLE);
-    //gpio_it_register_interrupt(&button_addresses[BUTTON_GREEN], &s_interrupt_settings,
-    //                        INTERRUPT_EDGE_RISING, prv_button_interrupt_handler,
-    //                        &led_addresses[LED_BLUE]);
+    };
+
+    gpio_init_pin(&adc_addresses, &adc_setting);
+    adc_init(ADC_MODE_SINGLE);
+    adc_set_channel_pin(adc_addresses, true);
+
+    gpio_it_register_interrupt(&button_addresses, &s_interrupt_settings,
+                            INTERRUPT_EDGE_RISING, prv_button_interrupt_handler,
+                            &adc_addresses);
     while (true) {
         wait();
     }
+    return 0;
 }
+
