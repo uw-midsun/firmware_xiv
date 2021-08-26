68c9821d5141ac38241a3d2ce907c0f26bfffe75
Logan Yu <loganyu438@gmail.com>
2021-08-26 12:27:16 -0400

    made fixed according to the comments

--- projects/Buttons_and_ADCs/src/main.c
+++ projects/Buttons_and_ADCs/src/main.c
@@ -3,64 +3,62 @@
 #include "gpio.h"
 #include "gpio_it.h"
 #include "interrupt.h"
-#include "wait.h"
 #include "log.h"
+#include "wait.h"
 
-static void prv_button_interrupt_handler(const GpioAddress *adc_address, void *context){
-    GpioAddress *adc_position = context;
-    uint16_t adc = 0;
-    adc_read_converted_pin(*adc_position, &adc);
-    LOG_DEBUG("ADC reading is: %d\n", adc);
+static void prv_button_interrupt_handler(const GpioAddress *adc_address, void *context) {
+  GpioAddress *adc_position = context;
+  uint16_t adc = 0;
+  adc_read_converted_pin(*adc_position, &adc);
+  LOG_DEBUG("ADC reading is: %d\n", adc);
 }
 
-int main(void){
-    interrupt_init();
-    gpio_init();
-    gpio_it_init();
+int main(void) {
+  interrupt_init();
+  gpio_init();
+  gpio_it_init();
 
-    //Button address
-    static GpioAddress button_addresses = {
-        .port = GPIO_PORT_B, 
-        .pin = 2,
-        };
-    
-    //Button setting
-    static GpioSettings button_setting= { 
-        .direction = GPIO_DIR_IN,
-        .state = GPIO_STATE_LOW,
-        .alt_function = GPIO_ALTFN_NONE,
-        .resistor = GPIO_RES_NONE,
-        };
+  // Button address
+  GpioAddress button_addresses = {
+    .port = GPIO_PORT_B,
+    .pin = 2,
+  };
 
-    //Adc address
-    static GpioAddress adc_addresses = { 
-        .port = GPIO_PORT_A, 
-        .pin = 6,
-        };
+  // Button setting
+  GpioSettings button_setting = {
+    .direction = GPIO_DIR_IN,
+    .state = GPIO_STATE_LOW,
+    .alt_function = GPIO_ALTFN_NONE,
+    .resistor = GPIO_RES_NONE,
+  };
 
-    //Adc setting
-    static GpioSettings adc_setting = { 
-        .direction = GPIO_DIR_IN,
-        .state = GPIO_STATE_LOW,
-        .alt_function = GPIO_ALTFN_ANALOG,
-        .resistor = GPIO_RES_NONE 
-        };
+  // Adc address
+  GpioAddress adc_addresses = {
+    .port = GPIO_PORT_A,
+    .pin = 6,
+  };
 
-    static InterruptSettings s_interrupt_settings = {
-    .type = INTERRUPT_TYPE_INTERRUPT,       
-    .priority = INTERRUPT_PRIORITY_NORMAL,  
-    };
+  // Adc setting
+  GpioSettings adc_setting = {
+    .direction = GPIO_DIR_IN,
+    .state = GPIO_STATE_LOW,
+    .alt_function = GPIO_ALTFN_ANALOG,
+    .resistor = GPIO_RES_NONE,
+  };
 
-    gpio_init_pin(&adc_addresses, &adc_setting);
-    adc_init(ADC_MODE_SINGLE);
-    adc_set_channel_pin(adc_addresses, true);
+  static InterruptSettings s_interrupt_settings = {
+    .type = INTERRUPT_TYPE_INTERRUPT,
+    .priority = INTERRUPT_PRIORITY_NORMAL,
+  };
 
-    gpio_it_register_interrupt(&button_addresses, &s_interrupt_settings,
-                            INTERRUPT_EDGE_FALLING, prv_button_interrupt_handler,
-                            &adc_addresses);
-    while (true) {
-        wait();
-    }
-    return 0;
+  gpio_init_pin(&adc_addresses, &adc_setting);
+  adc_init(ADC_MODE_SINGLE);
+  adc_set_channel_pin(adc_addresses, true);
+
+  gpio_it_register_interrupt(&button_addresses, &s_interrupt_settings, INTERRUPT_EDGE_FALLING,
+                             prv_button_interrupt_handler, &adc_addresses);
+  while (true) {
+    wait();
+  }
+  return 0;
 }
-
