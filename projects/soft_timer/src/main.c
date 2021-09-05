d066223a023485dd8e1588ffc88a0e22059917a0
Daniel Qu <danielq987@gmail.com>
2021-09-05 04:03:40 +0000

    firmware 102 soft timer homework

---
+++ projects/soft_timer/src/main.c
@@ -0,0 +1,41 @@
+#include "interrupt.h"
+#include "soft_timer.h"
+#include "log.h"
+
+#include <stdlib.h>
+#include <stdint.h>
+
+// Values are in milliseconds.
+const int COUNTER_A_DELAY = 500;
+const int COUNTER_B_DELAY = 1000;
+
+typedef struct counter {
+	uint8_t counter_a;
+	uint8_t counter_b;
+} Counters;
+
+void prv_timer_a_callback(SoftTimerId timer_id, void* context) {
+	Counters* counters = context;
+	counters->counter_a++;
+	LOG_DEBUG("Counter A: %d\n", counters->counter_a);
+	soft_timer_start_millis(COUNTER_A_DELAY, prv_timer_a_callback, counters, NULL);
+}
+
+void prv_timer_b_callback(SoftTimerId timer_id, void* context) {
+	Counters* counters = context;
+	counters->counter_b++;
+	LOG_DEBUG("Counter B: %d\n", counters->counter_b);
+	soft_timer_start_millis(COUNTER_B_DELAY, prv_timer_b_callback, counters, NULL);
+}
+
+int main() {
+	interrupt_init();
+	soft_timer_init();
+
+	Counters counters = { 0 };
+
+	soft_timer_start_millis(COUNTER_A_DELAY, prv_timer_a_callback, &counters, NULL);
+	soft_timer_start_millis(COUNTER_B_DELAY, prv_timer_b_callback, &counters, NULL);
+
+	while (true) {}
+}
