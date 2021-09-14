317256a5fd234c94fbb5094676f903139dd537fa
Daniel Qu <danielq987@gmail.com>
2021-09-14 22:01:09 +0000

    fix PR

--- projects/button_adc/src/main.c
+++ projects/button_adc/src/main.c
@@ -1,19 +1,18 @@
-#include <stdbool.h>
+#include "adc.h"
 #include "gpio.h"
 #include "gpio_it.h"
-#include "inc/adc.h"
 #include "interrupt.h"
 #include "log.h"
 #include "wait.h"
 
 // Interrupt callback
-static void prv_adc_callback(GpioAddress *address, void *context) {
+static void prv_adc_callback(const GpioAddress *address, void *context) {
   GpioAddress *adc_address = context;
 
   uint16_t adc_reading = 0;
   adc_read_raw_pin(*adc_address, &adc_reading);
 
-  LOG_DEBUG("Read ADC value");
+  LOG_DEBUG("ADC Reading: %d", adc_reading);
 }
 
 int main(void) {
@@ -28,10 +27,15 @@ int main(void) {
     .pin = 2,             //
   };
 
-  GpioSettings gpio_settings = {
+  GpioSettings adc_settings = {
     .direction = GPIO_DIR_OUT,          //
     .state = GPIO_STATE_HIGH,           //
     .alt_function = GPIO_ALTFN_ANALOG,  //
+  };
+
+  GpioSettings button_settings = {
+    .direction = GPIO_DIR_OUT,  //
+    .state = GPIO_STATE_HIGH,   //
   };
 
   InterruptSettings interrupt_settings = {
@@ -46,13 +50,13 @@ int main(void) {
   adc_init(ADC_MODE_SINGLE);
 
   // Pin Inits
-  gpio_init_pin(&adc_address, &gpio_settings);
-  gpio_init_pin(&button_address, &gpio_settings);
+  gpio_init_pin(&adc_address, &adc_settings);
+  gpio_init_pin(&button_address, &button_settings);
 
   adc_set_channel_pin(adc_address, true);
 
   // Interrupt Code
-  gpio_it_register_interrupt(&button_address, &interrupt_settings, INTERRUPT_EDGE_RISING_FALLING,
+  gpio_it_register_interrupt(&button_address, &interrupt_settings, INTERRUPT_EDGE_FALLING,
                              prv_adc_callback, &adc_address);
 
   // Infinite loop
