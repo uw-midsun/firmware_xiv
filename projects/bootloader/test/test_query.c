1c39d239660b70e5ad12b2d93a5a8008b5a9bed7
ShiCheng Lu <shichenglu0708@gmail.com>
2021-09-18 20:35:41 +0000

    [SOFT-535] refactored query response/no-response assertions

--- projects/bootloader/test/test_query.c
+++ projects/bootloader/test/test_query.c
@@ -40,6 +40,57 @@ static uint8_t s_destination_nodes[DGRAM_MAX_DEST_NODES_SIZE];
 static uint8_t s_rx_data[DGRAM_MAX_DATA_SIZE];
 static uint16_t s_rx_data_len;
 
+// send a query and assert the response matches the expected responses
+void prv_test_response(uint8_t *query, size_t query_len, uint8_t *expected, size_t expected_len) {
+  CanDatagramTxConfig tx_config = {
+    .dgram_type = BOOTLOADER_DATAGRAM_QUERY_COMMAND,
+    .destination_nodes_len = 1,
+    .destination_nodes = &s_client_id,
+    .data_len = query_len,
+    .data = query,
+    .tx_cmpl_cb = tx_cmpl_cb,
+  };
+
+  CanDatagramRxConfig rx_config = {
+    .destination_nodes = s_destination_nodes,
+    .data = s_rx_data,
+    .node_id = 0,  // listen to all
+    .rx_cmpl_cb = NULL,
+  };
+
+  dgram_helper_mock_tx_datagram(&tx_config);
+  dgram_helper_mock_rx_datagram(&rx_config);
+
+  TEST_ASSERT_EQUAL(DATAGRAM_STATUS_RX_COMPLETE, can_datagram_get_status());
+
+  TEST_ASSERT_EQUAL(BOOTLOADER_DATAGRAM_QUERY_RESPONSE, rx_config.dgram_type);
+  TEST_ASSERT_EQUAL(rx_config.data_len, expected_len);
+  for (int i = 0; i < rx_config.data_len; ++i) {
+    TEST_ASSERT_EQUAL(rx_config.data[i], expected[i]);
+  }
+}
+
+// send a query and assert that no response is sent
+void prv_test_no_response(uint8_t *query, size_t query_len) {
+  CanDatagramTxConfig tx_config = {
+    .dgram_type = BOOTLOADER_DATAGRAM_QUERY_COMMAND,
+    .destination_nodes_len = 1,
+    .destination_nodes = &s_board_id,
+    .data_len = query_len,
+    .data = query,
+    .tx_cmpl_cb = tx_cmpl_cb,
+  };
+  CanDatagramRxConfig rx_config = {
+    .destination_nodes = s_destination_nodes,
+    .data = s_rx_data,
+    .node_id = 0,  // listen to all
+    .rx_cmpl_cb = NULL,
+  };
+
+  dgram_helper_mock_tx_datagram(&tx_config);
+  dgram_helper_assert_no_response();
+}
+
 void setup_test(void) {
   event_queue_init();
   interrupt_init();
@@ -95,32 +146,6 @@ void test_query(void) {
   // response
   size_t expected_len = len;
   uint8_t *expected_response = data;
-
-  CanDatagramTxConfig tx_config = {
-    .dgram_type = BOOTLOADER_DATAGRAM_QUERY_COMMAND,
-    .destination_nodes_len = 1,
-    .destination_nodes = &s_client_id,
-    .data_len = len,
-    .data = data,
-    .tx_cmpl_cb = tx_cmpl_cb,
-  };
-
-  CanDatagramRxConfig rx_config = {
-    .destination_nodes = s_destination_nodes,
-    .data = s_rx_data,
-    .node_id = 0,  // listen to all
-    .rx_cmpl_cb = NULL,
-  };
-  dgram_helper_mock_tx_datagram(&tx_config);
-  dgram_helper_mock_rx_datagram(&rx_config);
-
-  TEST_ASSERT_EQUAL(DATAGRAM_STATUS_RX_COMPLETE, can_datagram_get_status());
-
-  TEST_ASSERT_EQUAL(BOOTLOADER_DATAGRAM_QUERY_RESPONSE, rx_config.dgram_type);
-  TEST_ASSERT_EQUAL(rx_config.data_len, expected_len);
-  for (int i = 0; i < rx_config.data_len; ++i) {
-    TEST_ASSERT_EQUAL(rx_config.data[i], expected_response[i]);
-  }
 }
 
 void test_repeated_field(void) {
@@ -139,8 +164,8 @@ void test_repeated_field(void) {
   // "current_project" : [ "pedal board", "centre console" ]
   // "project_info" : ["testing"]
   // "git_version" : []
-  size_t len = 58;
-  uint8_t data[58] = {
+  size_t query_len = 58;
+  uint8_t query[58] = {
     0x08, 0x01, 0x08, 0x02, 0x08, 0x03, 0x12, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x12, 0x05,
     0x77, 0x6F, 0x72, 0x6C, 0x64, 0x1A, 0x0B, 0x70, 0x65, 0x64, 0x61, 0x6C, 0x20, 0x62, 0x6F,
     0x61, 0x72, 0x64, 0x1A, 0x0E, 0x63, 0x65, 0x6E, 0x74, 0x72, 0x65, 0x20, 0x63, 0x6F, 0x6E,
@@ -149,37 +174,13 @@ void test_repeated_field(void) {
 
   // bootloader config data in protobuf
   size_t expected_len = 47;
-  uint8_t expected_response[47] = {
+  uint8_t expected[47] = {
     0x08, 0x02, 0x12, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x1A, 0x0E, 0x63, 0x65, 0x6E, 0x74, 0x72,
     0x65, 0x20, 0x63, 0x6F, 0x6E, 0x73, 0x6F, 0x6C, 0x65, 0x22, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69,
     0x6E, 0x67, 0x2A, 0x0B, 0x72, 0x61, 0x6E, 0x64, 0x6F, 0x6D, 0x20, 0x69, 0x6E, 0x66, 0x6F,
   };
 
-  CanDatagramTxConfig tx_config = {
-    .dgram_type = BOOTLOADER_DATAGRAM_QUERY_COMMAND,
-    .destination_nodes_len = 1,
-    .destination_nodes = &s_client_id,
-    .data_len = len,
-    .data = data,
-    .tx_cmpl_cb = tx_cmpl_cb,
-  };
-  CanDatagramRxConfig rx_config = {
-    .destination_nodes = s_destination_nodes,
-    .data = s_rx_data,
-    .node_id = 0,  // listen to all
-    .rx_cmpl_cb = NULL,
-  };
-
-  dgram_helper_mock_tx_datagram(&tx_config);
-  dgram_helper_mock_rx_datagram(&rx_config);
-
-  TEST_ASSERT_EQUAL(DATAGRAM_STATUS_RX_COMPLETE, can_datagram_get_status());
-
-  TEST_ASSERT_EQUAL(BOOTLOADER_DATAGRAM_QUERY_RESPONSE, rx_config.dgram_type);
-  TEST_ASSERT_EQUAL(rx_config.data_len, expected_len);
-  for (int i = 0; i < rx_config.data_len; ++i) {
-    TEST_ASSERT_EQUAL(rx_config.data[i], expected_response[i]);
-  }
+  prv_test_response(query, query_len, expected, expected_len);
 }
 
 void test_empty_query(void) {
@@ -202,37 +203,12 @@ void test_empty_query(void) {
 
   // bootloader config data in protobuf
   size_t expected_len = 47;
-  uint8_t expected_response[47] = {
+  uint8_t expected[47] = {
     0x08, 0x02, 0x12, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x1A, 0x0E, 0x63, 0x65, 0x6E, 0x74, 0x72,
     0x65, 0x20, 0x63, 0x6F, 0x6E, 0x73, 0x6F, 0x6C, 0x65, 0x22, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69,
     0x6E, 0x67, 0x2A, 0x0B, 0x72, 0x61, 0x6E, 0x64, 0x6F, 0x6D, 0x20, 0x69, 0x6E, 0x66, 0x6F,
   };
-
-  CanDatagramTxConfig tx_config = {
-    .dgram_type = BOOTLOADER_DATAGRAM_QUERY_COMMAND,
-    .destination_nodes_len = 1,
-    .destination_nodes = &s_board_id,
-    .data_len = 0,
-    .data = NULL,
-    .tx_cmpl_cb = tx_cmpl_cb,
-  };
-  CanDatagramRxConfig rx_config = {
-    .destination_nodes = s_destination_nodes,
-    .data = s_rx_data,
-    .node_id = 0,  // listen to all
-    .rx_cmpl_cb = NULL,
-  };
-
-  dgram_helper_mock_tx_datagram(&tx_config);
-  dgram_helper_mock_rx_datagram(&rx_config);
-
-  TEST_ASSERT_EQUAL(DATAGRAM_STATUS_RX_COMPLETE, can_datagram_get_status());
-
-  TEST_ASSERT_EQUAL(BOOTLOADER_DATAGRAM_QUERY_RESPONSE, rx_config.dgram_type);
-  TEST_ASSERT_EQUAL(rx_config.data_len, expected_len);
-  for (int i = 0; i < rx_config.data_len; ++i) {
-    TEST_ASSERT_EQUAL(rx_config.data[i], expected_response[i]);
-  }
+  prv_test_response(NULL, 0, expected, expected_len);
 }
 
 void test_no_match(void) {
@@ -251,29 +227,13 @@ void test_no_match(void) {
   // "current_project" : []
   // "project_info" : [ "no match" ]
   // "git_version" : []
-  size_t len = 30;
-  uint8_t data[30] = {
+  size_t query_len = 30;
+  uint8_t query[30] = {
     0x08, 0x01, 0x08, 0x02, 0x08, 0x03, 0x12, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x12, 0x05,
     0x77, 0x6F, 0x72, 0x6C, 0x64, 0x22, 0x08, 0x6E, 0x6F, 0x20, 0x6D, 0x61, 0x74, 0x63, 0x68,
   };
 
-  CanDatagramTxConfig tx_config = {
-    .dgram_type = BOOTLOADER_DATAGRAM_QUERY_COMMAND,
-    .destination_nodes_len = 1,
-    .destination_nodes = &s_board_id,
-    .data_len = len,
-    .data = data,
-    .tx_cmpl_cb = tx_cmpl_cb,
-  };
-  CanDatagramRxConfig rx_config = {
-    .destination_nodes = s_destination_nodes,
-    .data = s_rx_data,
-    .node_id = 0,  // listen to all
-    .rx_cmpl_cb = NULL,
-  };
-
-  dgram_helper_mock_tx_datagram(&tx_config);
-  dgram_helper_assert_no_response();
+  prv_test_no_response(query, query_len);
 }
 
 void test_no_project_on_board(void) {
@@ -291,42 +251,18 @@ void test_no_project_on_board(void) {
   // "current_project" : []
   // "project_info" : [ "no match" ]
   // "git_version" : []
-  size_t len = 30;
-  uint8_t data[30] = {
+  size_t query_len = 20;
+  uint8_t query[20] = {
     0x08, 0x01, 0x08, 0x02, 0x08, 0x03, 0x12, 0x05, 0x68, 0x65,
     0x6C, 0x6C, 0x6F, 0x12, 0x05, 0x77, 0x6F, 0x72, 0x6C, 0x64,
   };
 
   size_t expected_len = 9;
-  uint8_t expected_response[9] = {
+  uint8_t expected[9] = {
     0x08, 0x02, 0x12, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F,
   };
 
-  CanDatagramTxConfig tx_config = {
-    .dgram_type = BOOTLOADER_DATAGRAM_QUERY_COMMAND,
-    .destination_nodes_len = 1,
-    .destination_nodes = &s_board_id,
-    .data_len = len,
-    .data = data,
-    .tx_cmpl_cb = tx_cmpl_cb,
-  };
-  CanDatagramRxConfig rx_config = {
-    .destination_nodes = s_destination_nodes,
-    .data = s_rx_data,
-    .node_id = 0,  // listen to all
-    .rx_cmpl_cb = NULL,
-  };
-
-  dgram_helper_mock_tx_datagram(&tx_config);
-  dgram_helper_mock_rx_datagram(&rx_config);
-
-  TEST_ASSERT_EQUAL(DATAGRAM_STATUS_RX_COMPLETE, can_datagram_get_status());
-
-  TEST_ASSERT_EQUAL(BOOTLOADER_DATAGRAM_QUERY_RESPONSE, rx_config.dgram_type);
-  TEST_ASSERT_EQUAL(rx_config.data_len, expected_len);
-  for (int i = 0; i < rx_config.data_len; ++i) {
-    TEST_ASSERT_EQUAL(rx_config.data[i], expected_response[i]);
-  }
+  prv_test_response(query, query_len, expected, expected_len);
 }
 
 void test_no_project_on_board_no_response(void) {
@@ -342,27 +278,11 @@ void test_no_project_on_board_no_response(void) {
   // "current_project" : []
   // "project_info" : [ "no match" ]
   // "git_version" : []
-  size_t len = 30;
-  uint8_t data[30] = {
+  size_t query_len = 30;
+  uint8_t query[30] = {
     0x08, 0x01, 0x08, 0x02, 0x08, 0x03, 0x12, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x12, 0x05,
     0x77, 0x6F, 0x72, 0x6C, 0x64, 0x22, 0x08, 0x6E, 0x6F, 0x20, 0x6D, 0x61, 0x74, 0x63, 0x68,
   };
 
-  CanDatagramTxConfig tx_config = {
-    .dgram_type = BOOTLOADER_DATAGRAM_QUERY_COMMAND,
-    .destination_nodes_len = 1,
-    .destination_nodes = &s_board_id,
-    .data_len = len,
-    .data = data,
-    .tx_cmpl_cb = tx_cmpl_cb,
-  };
-  CanDatagramRxConfig rx_config = {
-    .destination_nodes = s_destination_nodes,
-    .data = s_rx_data,
-    .node_id = 0,  // listen to all
-    .rx_cmpl_cb = NULL,
-  };
-
-  dgram_helper_mock_tx_datagram(&tx_config);
-  dgram_helper_assert_no_response();
+  prv_test_no_response(query, query_len);
 }
