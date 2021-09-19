7171ca8e9e12b5fc1dec28e99e3463a26316385b
Elvis <elvis.cheung8@gmail.com>
2021-09-19 00:03:32 -0400

    fw 102

---
+++ projects/102_homework/src/main.c
@@ -0,0 +1,45 @@
+#include "interrupt.h"
+#include "log.h"
+#include "soft_timer.h"
+#include "wait.h"
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#define DELAY_COUNTER_A_MS 500
+
+typedef struct Counters {
+  uint8_t counter_a;
+  uint8_t counter_b;
+} Counters;
+
+void soft_timer_callback(SoftTimerId timer_id, void *context) {
+  Counters *increment = context;
+
+  increment->counter_a++;
+
+  LOG_DEBUG("Counter A: %i\n", increment->counter_a);
+
+  if (increment->counter_a % 2 == 0) {
+    increment->counter_b++;
+
+    LOG_DEBUG("Counter B: %i\n", increment->counter_b);
+  }
+
+  soft_timer_start_millis(DELAY_COUNTER_A_MS, soft_timer_callback, increment, NULL);
+}
+
+int main(void) {
+  Counters storage = { 0 };
+
+  interrupt_init();
+  soft_timer_init();
+
+  soft_timer_start_millis(DELAY_COUNTER_A_MS, soft_timer_callback, &storage, NULL);
+
+  while (true) {
+    wait();
+  }
+
+  return 0;
+}
