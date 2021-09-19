971a9dea087ff7dd0725e79bec9a10037f12736f
vagrant <vagrant@midsunbox>
2021-09-19 16:02:22 +0000

    formatting updates

--- projects/mod2_fw_102/src/main.c
+++ projects/mod2_fw_102/src/main.c
@@ -1,65 +1,56 @@
-//Library inclusions:
+// Library inclusions:
+#include <stdint.h>
+#include "interrupt.h"
 #include "log.h"
-#include "interrupt.h" 
 #include "soft_timer.h"
 #include "wait.h"
-#include <stdint.h> 
 
-//Counters Struct
-typedef struct Counters{
+// Counters Struct
+typedef struct Counters {
   uint8_t counter_a;
   uint8_t counter_b;
 } Counters;
 
-//Counter functions:
+// Counter functions:
 void counter_a_func(SoftTimerId timer_id, void *context) {
   Counters *storage = context;
   uint8_t counter_a = 1;
-  
+
   LOG_DEBUG("Counter A: %i\n", storage->counter_a++);
-  
-  soft_timer_start(500000,
-                   counter_a_func, 
-                   storage, 
-                   NULL);
+
+  soft_timer_start(500000, counter_a_func, storage, NULL);
 }
 
 void counter_b_func(SoftTimerId timer_id, void *context) {
   Counters *storage = context;
   uint8_t counter_b = 1;
-  
+
   LOG_DEBUG("Counter B: %i\n", storage->counter_b++);
-  
-  soft_timer_start(1000000,
-                   counter_b_func, 
-                   storage, 
-                   NULL);
+
+  soft_timer_start(1000000, counter_b_func, storage, NULL);
 }
 
-//Main Loop:
+// Main Loop:
 
-int main (void) {
-    //Library initializations:
-    interrupt_init();
-    soft_timer_init();
+int main(void) {
+  // Library initializations:
+  interrupt_init();
+  soft_timer_init();
 
-    //Context initialization:
-    Counters storage = { 0 };
-  
+  // Context initialization:
+  Counters storage = { 0 };
 
-    //Initialize timer loop:
-    soft_timer_start(10000, 
-                    counter_a_func, //function callback
-                    &storage, 
-                    NULL);
+  // Initialize timer loop:
+  soft_timer_start(10000,
+                   counter_a_func,  // function callback
+                   &storage, NULL);
 
-     soft_timer_start(50000, 
-                      counter_b_func, //functioncallback
-                      &storage, 
-                      NULL);
+  soft_timer_start(50000,
+                   counter_b_func,  // functioncallback
+                   &storage, NULL);
 
-    while (true) {
-        wait(); //let functions run themselves indefinetly
-    }
-    return 0;
+  while (true) {
+    wait();  // let functions run themselves indefinetly
+  }
+  return 0;
 }
