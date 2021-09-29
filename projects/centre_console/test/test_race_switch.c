949f9b3ac61452148772f31868f2aeb96f7edb2e
Raiyan Sayeed <raiyan.business@gmail.com>
2021-09-29 02:37:18 +0000

    [SOFT-524] tested data in can message, cleaned up some code

--- projects/centre_console/test/test_race_switch.c
+++ projects/centre_console/test/test_race_switch.c
@@ -1,6 +1,7 @@
 #include <string.h>
 
 #include "can_transmit.h"
+#include "can_unpack.h"
 #include "centre_console_events.h"
 #include "event_queue.h"
 #include "gpio.h"
@@ -19,9 +20,17 @@ static RaceSwitchFsmStorage s_race_switch_fsm_storage;
 static GpioState s_returned_state;
 static GpioAddress s_race_switch_address = { .port = GPIO_PORT_A, .pin = 4 };
 static GpioAddress s_voltage_monitor_address = { .port = GPIO_PORT_B, .pin = 7 };
+static uint8_t s_can_race_mode_on = false;
 
 StatusCode TEST_MOCK(gpio_get_state)(const GpioAddress *address, GpioState *state) {
   *state = s_returned_state;
+  return STATUS_CODE_OK;
+}
+
+// verifies correct data is being sent on the race switch status message
+static StatusCode prv_race_status_callback(const CanMessage *msg, void *context,
+                                           CanAckStatus *ack_reply) {
+  CAN_UNPACK_RACE_NORMAL_STATUS(msg, &s_can_race_mode_on);
   return STATUS_CODE_OK;
 }
 
@@ -37,6 +46,8 @@ void setup_test(void) {
                                   CENTRE_CONSOLE_EVENT_CAN_TX, CENTRE_CONSOLE_EVENT_CAN_RX,
                                   CENTRE_CONSOLE_EVENT_CAN_FAULT);
   gpio_it_init();
+  TEST_ASSERT_OK(can_register_rx_handler(SYSTEM_CAN_MESSAGE_RACE_NORMAL_STATUS,
+                                         prv_race_status_callback, NULL));
   memset(&s_race_switch_fsm_storage, 0, sizeof(s_race_switch_fsm_storage));
   TEST_ASSERT_OK(race_switch_fsm_init(&s_race_switch_fsm_storage));
 }
@@ -51,17 +62,13 @@ void test_can_state_on(void) {
   prv_assert_current_race_state(RACE_STATE_OFF);
 
   CAN_TRANSMIT_RACE_NORMAL_SWITCH_MODE(RACE_STATE_ON);
-
-  // sending CAN message to switch race mode
-  MS_TEST_HELPER_CAN_TX(CENTRE_CONSOLE_EVENT_CAN_TX);
-  MS_TEST_HELPER_CAN_RX(CENTRE_CONSOLE_EVENT_CAN_RX);
+  MS_TEST_HELPER_CAN_TX_RX(CENTRE_CONSOLE_EVENT_CAN_TX, CENTRE_CONSOLE_EVENT_CAN_RX);
 
   TEST_ASSERT_TRUE(prv_process_fsm_event_manually());
+  // sending CAN message out to notify other devices e.g telemetry
+  MS_TEST_HELPER_CAN_TX_RX(CENTRE_CONSOLE_EVENT_CAN_TX, CENTRE_CONSOLE_EVENT_CAN_RX);
 
-  // sending CAN message out to notify others e.g telemetry
-  MS_TEST_HELPER_CAN_TX(CENTRE_CONSOLE_EVENT_CAN_TX);
-  MS_TEST_HELPER_CAN_RX(CENTRE_CONSOLE_EVENT_CAN_RX);
-
+  TEST_ASSERT_TRUE(s_can_race_mode_on);
   TEST_ASSERT_EQUAL(RACE_STATE_ON, race_switch_fsm_get_current_state(&s_race_switch_fsm_storage));
 }
 
@@ -69,27 +76,21 @@ void test_can_state_off(void) {
   prv_assert_current_race_state(RACE_STATE_OFF);
 
   CAN_TRANSMIT_RACE_NORMAL_SWITCH_MODE(RACE_STATE_ON);
-
-  MS_TEST_HELPER_CAN_TX(CENTRE_CONSOLE_EVENT_CAN_TX);
-  MS_TEST_HELPER_CAN_RX(CENTRE_CONSOLE_EVENT_CAN_RX);
+  MS_TEST_HELPER_CAN_TX_RX(CENTRE_CONSOLE_EVENT_CAN_TX, CENTRE_CONSOLE_EVENT_CAN_RX);
 
   TEST_ASSERT_TRUE(prv_process_fsm_event_manually());
+  MS_TEST_HELPER_CAN_TX_RX(CENTRE_CONSOLE_EVENT_CAN_TX, CENTRE_CONSOLE_EVENT_CAN_RX);
 
-  MS_TEST_HELPER_CAN_TX(CENTRE_CONSOLE_EVENT_CAN_TX);
-  MS_TEST_HELPER_CAN_RX(CENTRE_CONSOLE_EVENT_CAN_RX);
-
+  TEST_ASSERT_TRUE(s_can_race_mode_on);
   prv_assert_current_race_state(RACE_STATE_ON);
 
   CAN_TRANSMIT_RACE_NORMAL_SWITCH_MODE(RACE_STATE_OFF);
-
-  MS_TEST_HELPER_CAN_TX(CENTRE_CONSOLE_EVENT_CAN_TX);
-  MS_TEST_HELPER_CAN_RX(CENTRE_CONSOLE_EVENT_CAN_RX);
+  MS_TEST_HELPER_CAN_TX_RX(CENTRE_CONSOLE_EVENT_CAN_TX, CENTRE_CONSOLE_EVENT_CAN_RX);
 
   TEST_ASSERT_TRUE(prv_process_fsm_event_manually());
+  MS_TEST_HELPER_CAN_TX_RX(CENTRE_CONSOLE_EVENT_CAN_TX, CENTRE_CONSOLE_EVENT_CAN_RX);
 
-  MS_TEST_HELPER_CAN_TX(CENTRE_CONSOLE_EVENT_CAN_TX);
-  MS_TEST_HELPER_CAN_RX(CENTRE_CONSOLE_EVENT_CAN_RX);
-
+  TEST_ASSERT_FALSE(s_can_race_mode_on);
   TEST_ASSERT_EQUAL(RACE_STATE_OFF, race_switch_fsm_get_current_state(&s_race_switch_fsm_storage));
 }
 
@@ -98,24 +99,18 @@ void test_transition_to_race(void) {
   // Initially the module begins in normal mode so PA4 is low
   s_returned_state = GPIO_STATE_LOW;
 
-  // MS_TEST_HELPER_CAN_TX(CENTRE_CONSOLE_EVENT_CAN_TX);
-  // MS_TEST_HELPER_CAN_RX(CENTRE_CONSOLE_EVENT_CAN_RX);
-
   prv_assert_current_race_state(RACE_STATE_OFF);
 
   // Mock rising edge on race switch pin
   s_returned_state = GPIO_STATE_HIGH;
 
-  // MS_TEST_HELPER_CAN_TX(CENTRE_CONSOLE_EVENT_CAN_TX);
-  // MS_TEST_HELPER_CAN_RX(CENTRE_CONSOLE_EVENT_CAN_RX);
-
   // Trigger interrupt to change fsm state from normal to race
   TEST_ASSERT_OK(gpio_it_trigger_interrupt(&s_race_switch_address));
   TEST_ASSERT_TRUE(prv_process_fsm_event_manually());
 
-  MS_TEST_HELPER_CAN_TX(CENTRE_CONSOLE_EVENT_CAN_TX);
-  MS_TEST_HELPER_CAN_RX(CENTRE_CONSOLE_EVENT_CAN_RX);
+  MS_TEST_HELPER_CAN_TX_RX(CENTRE_CONSOLE_EVENT_CAN_TX, CENTRE_CONSOLE_EVENT_CAN_RX);
 
+  TEST_ASSERT_TRUE(s_can_race_mode_on);
   prv_assert_current_race_state(RACE_STATE_ON);
 
   // Test if no error when interrupt is triggered with same edge multiple times
@@ -141,9 +136,9 @@ void test_transition_to_normal(void) {
   TEST_ASSERT_OK(gpio_it_trigger_interrupt(&s_race_switch_address));
   TEST_ASSERT_TRUE(prv_process_fsm_event_manually());
 
-  MS_TEST_HELPER_CAN_TX(CENTRE_CONSOLE_EVENT_CAN_TX);
-  // MS_TEST_HELPER_CAN_RX(CENTRE_CONSOLE_EVENT_CAN_RX);
+  MS_TEST_HELPER_CAN_TX_RX(CENTRE_CONSOLE_EVENT_CAN_TX, CENTRE_CONSOLE_EVENT_CAN_RX);
 
+  TEST_ASSERT_FALSE(s_can_race_mode_on);
   prv_assert_current_race_state(RACE_STATE_OFF);
 }
 
@@ -164,9 +159,9 @@ void test_voltage_during_transition(void) {
   TEST_ASSERT_OK(gpio_it_trigger_interrupt(&s_race_switch_address));
   TEST_ASSERT_TRUE(prv_process_fsm_event_manually());
 
-  MS_TEST_HELPER_CAN_TX(CENTRE_CONSOLE_EVENT_CAN_TX);
-  MS_TEST_HELPER_CAN_RX(CENTRE_CONSOLE_EVENT_CAN_RX);
+  MS_TEST_HELPER_CAN_TX_RX(CENTRE_CONSOLE_EVENT_CAN_TX, CENTRE_CONSOLE_EVENT_CAN_RX);
 
+  TEST_ASSERT_TRUE(s_can_race_mode_on);
   prv_assert_current_race_state(RACE_STATE_ON);
 
   // Mock low state on voltage monitor pin
@@ -178,9 +173,9 @@ void test_voltage_during_transition(void) {
   TEST_ASSERT_OK(gpio_it_trigger_interrupt(&s_race_switch_address));
   TEST_ASSERT_TRUE(prv_process_fsm_event_manually());
 
-  MS_TEST_HELPER_CAN_TX(CENTRE_CONSOLE_EVENT_CAN_TX);
-  MS_TEST_HELPER_CAN_RX(CENTRE_CONSOLE_EVENT_CAN_RX);
+  MS_TEST_HELPER_CAN_TX_RX(CENTRE_CONSOLE_EVENT_CAN_TX, CENTRE_CONSOLE_EVENT_CAN_RX);
 
+  TEST_ASSERT_FALSE(s_can_race_mode_on);
   prv_assert_current_race_state(RACE_STATE_OFF);
 
   // Mock high state on voltage monitor pin
