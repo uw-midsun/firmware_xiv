3c6babbdad2fcb91567f3c8f4327ebe7a939600b
Ryan Dancy <ryan@keal.ca>
2021-10-06 22:27:07 +0000

    [SOFT-552] Fix format

--- projects/power_distribution/src/smoketests_pd.c
+++ projects/power_distribution/src/smoketests_pd.c
@@ -14,14 +14,16 @@ static SmokeTestFunction s_smoke_tests[NUM_SMOKE_TESTS] = {
 void smoketests_pd_run(SmokeTest smoke_test, const char *smoke_test_name) {
   if (smoke_test >= NUM_SMOKE_TESTS) {
     LOG_CRITICAL(
-      "Invalid smoke test! Please set PD_SMOKE_TEST to a valid value from the SmokeTest enum, "
-      "or comment it out to run PD normally. (Name=%s, value=%d)\n", smoke_test_name, smoke_test);
+        "Invalid smoke test! Please set PD_SMOKE_TEST to a valid value from the SmokeTest enum, "
+        "or comment it out to run PD normally. (Name=%s, value=%d)\n",
+        smoke_test_name, smoke_test);
     return;
   }
   if (s_smoke_tests[smoke_test] == NULL) {
     LOG_CRITICAL(
-      "Smoke test '%s' (%d) has no entry in lookup table! Please add it to s_smoke_tests "
-      "in smoketests_pd.c\n", smoke_test_name, smoke_test);
+        "Smoke test '%s' (%d) has no entry in lookup table! Please add it to s_smoke_tests "
+        "in smoketests_pd.c\n",
+        smoke_test_name, smoke_test);
     return;
   }
 
