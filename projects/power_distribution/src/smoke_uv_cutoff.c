4c8cf475fcb512469d71aeb7024434c8debf5cf3
Logan Yu <loganyu438@gmail.com>
2021-10-07 01:36:04 +0000

    updated smoke_uv_cutoff.c

--- projects/power_distribution/src/smoke_uv_cutoff.c
+++ projects/power_distribution/src/smoke_uv_cutoff.c
@@ -1,4 +1,5 @@
-#include "bug.h"
+#include "smoke_uv_cutoff.h"
+
 #include "can_transmit.h"
 #include "front_uv_detector.h"
 #include "gpio.h"
@@ -9,31 +10,30 @@
 #include "status.h"
 #include "wait.h"
 
+static void prv_pin_interrupt_handler(const GpioAddress *address, void *context) {
+  CAN_TRANSMIT_UV_CUTOFF_NOTIFICATION();
+
+  LOG_WARN("UV lockout was triggered\n");
+}
+
+StatusCode front_uv_detector_init(GpioAddress *detector_pin) {
+  InterruptSettings interrupt_settings = {
+    .type = INTERRUPT_TYPE_INTERRUPT,
+    .priority = INTERRUPT_PRIORITY_NORMAL,
+  };
+
+  // callback occurs when pin turns low i.e falling
+  status_ok_or_return(gpio_it_register_interrupt(
+      detector_pin, &interrupt_settings, INTERRUPT_EDGE_FALLING, prv_pin_interrupt_handler, NULL));
+
+  return STATUS_CODE_OK;
+}
+
 void smoke_uv_cutoff_perform(void) {
   gpio_init();
   gpio_it_init();
   event_queue_init();
   interrupt_init();
-
-  static void prv_pin_interrupt_handler(const GpioAddress *address, void *context) {
-    CAN_TRANSMIT_UV_CUTOFF_NOTIFICATION();
-
-    LOG_WARN("UV lockout was triggered\n");
-  }
-
-  StatusCode front_uv_detector_init(GpioAddress * detector_pin) {
-    InterruptSettings interrupt_settings = {
-      .type = INTERRUPT_TYPE_INTERRUPT,
-      .priority = INTERRUPT_PRIORITY_NORMAL,
-    };
-
-    // callback occurs when pin turns low i.e falling
-    status_ok_or_return(gpio_it_register_interrupt(detector_pin, &interrupt_settings,
-                                                   INTERRUPT_EDGE_FALLING,
-                                                   prv_pin_interrupt_handler, NULL));
-
-    return STATUS_CODE_OK;
-  }
 
   Event e = { 0 };
   while (true) {
