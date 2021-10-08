2ed41d0f4ff6f9ef697fe15b8b20082473e65e19
Elvis <elvis.cheung8@gmail.com>
2021-10-08 16:06:05 -0400

    made into private function, called gpio_init() and interrupt_init() before gpio_it_init(), removed variable capitalization

--- projects/103_homework/src/main.c
+++ projects/103_homework/src/main.c
@@ -1,29 +1,31 @@
 #include "adc.h"
 #include "delay.h"
 #include "gpio.h"
+#include "interrupt.h"
 #include "gpio_it.h"
 #include "log.h"
 #include "soft_timer.h"
 #include "wait.h"
 
 // interrupt call back function
-void interrupt_callback(const GpioAddress *address, void *context) {
-  GpioAddress *ADC_address = context;
+static void prv_interrupt_callback(const GpioAddress *address, void *context) {
+  GpioAddress *adc_address = context;
 
-  uint16_t ADC_reading = 0;
-  if (adc_read_raw_pin(*ADC_address, &ADC_reading) == STATUS_CODE_OK) {
-    LOG_DEBUG("Output: &d\n", ADC_reading);
+  uint16_t adc_reading = 0;
+  if (adc_read_raw_pin(*adc_address, &adc_reading) == STATUS_CODE_OK) {
+    LOG_DEBUG("Output: &d\n", adc_reading);
   } else {
     LOG_DEBUG("Failed to read.");
   }
 }
 
 int main(void) {
+  gpio_init();
+  interrupt_init();
   gpio_it_init();
   soft_timer_init();
-  gpio_init();
 
-  GpioAddress ADC_addr = {
+  GpioAddress adc_addr = {
     .port = GPIO_PORT_A,
     .pin = 6,
   };
@@ -45,13 +47,13 @@ int main(void) {
     .priority = INTERRUPT_PRIORITY_NORMAL,
   };
 
-  gpio_init_pin(&ADC_addr, &pot_settings);
+  gpio_init_pin(&adc_addr, &pot_settings);
   gpio_init_pin(&button_addr, &pot_settings);
-  adc_set_channel_pin(ADC_addr, true);
+  adc_set_channel_pin(adc_addr, true);
 
   // waits for GPIO to fall from high to low (button released) as the button is active-high
   gpio_it_register_interrupt(&button_addr, &interrupt_settings, INTERRUPT_EDGE_FALLING,
-                             interrupt_callback, &ADC_addr);
+                             prv_interrupt_callback, &adc_addr);
 
   while (true) {
     wait();
