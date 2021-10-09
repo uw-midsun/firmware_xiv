c043876d3fe1a359b38c5f9be94eccdf580354b2
Brian Gao <bhgao20014@gmail.com>
2021-10-09 15:19:24 +0000

    [SOFT-999] Added base files

---
+++ projects/fw_103_hw/src/main.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "interrupt.h"
+#include "log.h"
+#include "soft_timer.h"
+#include "wait.h"
+
+int main(void) {
+  LOG_DEBUG("Does this work?");
+  return 0;
+}
