718f7f816917ebc0ade70c026d36eb9dc73ffdf9
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2021-10-10 18:49:28 +0000

    [SOFT-468] included string library to jump header

--- projects/bootloader/inc/jump.h
+++ projects/bootloader/inc/jump.h
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <stdnoreturn.h>
+#include <string.h>
 
 // Reset main stack pointer to main stack pointer, and jump to program counter
 noreturn __attribute__((naked)) void perform_jump(uint32_t sp, uint32_t pc);
