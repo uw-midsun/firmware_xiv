f76ca4bb6e3f04cd1ddb5578956b64a407bf4c20
Brian Gao <bhgao20014@gmail.com>
2021-11-01 20:18:24 +0000

    Wrote read instructions and receiving data back

--- projects/fw_104_hw_p1_p2/src/spi.c
+++ projects/fw_104_hw_p1_p2/src/spi.c
@@ -3,9 +3,12 @@
 #include "gpio.h"
 #include "log.h"
 
-static SpiPort port_to_use = SPI_PORT_2;
+static const uint8_t canctrl_info_tx[] = { 0b01001001 };
+static const uint8_t read_status_tx[] = { 0b00000011 };
 
-static const uint8_t tx_bytes[] = { 0b01001001 };
+#define EXPECTED_RESPONSE_LENGTH 2
+
+static SpiPort port_to_use = SPI_PORT_2;
 
 const SpiSettings settings_to_use = {
   .baudrate = 6000000,
@@ -21,9 +24,12 @@ int main(void) {
   spi_init(port_to_use, &settings_to_use);
 
   StatusCode status;
-  const uint16_t tx_len = SIZEOF_ARRAY(tx_bytes);
 
-  status = spi_tx(port_to_use, tx_bytes, tx_len);
+  uint16_t tx_len = SIZEOF_ARRAY(canctrl_info_tx);
+
+  const uint8_t response[EXPECTED_RESPONSE_LENGTH] = { 0 };
+
+  status = spi_tx(port_to_use, canctrl_info_tx, tx_len);
 
   if (status == STATUS_CODE_OK) {
     LOG_DEBUG("Successfully wrote %d byte(s) to spi port %d\n", tx_len,
@@ -31,4 +37,10 @@ int main(void) {
   } else {
     LOG_DEBUG("Error: Could not write to spi port %d\n", port_to_use);
   }
+
+  tx_len = SIZEOF_ARRAY(read_status_tx);
+
+  status = spi_exchange(port_to_use, read_status_tx, tx_len, response, EXPECTED_RESPONSE_LENGTH);
+
+  print("%")
 }
