ea8915205675242dda708fc023acce0ac7b02a59
Brian Gao <bhgao20014@gmail.com>
2021-11-10 01:17:14 +0000

    [SOFT-560] Added comments/descriptions and tweaked code; ready for draft PR

--- projects/bootloader/scripts/jump_application.py
+++ projects/bootloader/scripts/jump_application.py
@@ -1,34 +1,40 @@
 """This client script handles the jump-to-application process of controller boards."""
 
 import time
+import can
 
 from can_datagram import Datagram
 from can_datagram import DatagramSender
 from can_datagram import DatagramListener
 
+DATAGRAM_TYPE_ID = 5
+DATAGRAM_DATA = []
+
 
 def jump_to_application(node_ids):
-    '''Sends datagram to specified boards, receives response, then returns status'''
+    """This function sends jump-to-application datagrams to specified boards,
+    receives, response, then returns status"""
     sender = DatagramSender(receive_own_messages=True)
-
     listener = DatagramListener(trigger_callback)
-    notifier = can.Notifier(sender.bus, [listener])
+
+    can.Notifier(sender.bus, [listener])
 
     message = Datagram(
-        datagram_type_id=5,
+        datagram_type_id=DATAGRAM_TYPE_ID,
         node_ids=node_ids,
-        data=[])
+        data=DATAGRAM_DATA)
     sender.send(message)
-    
+
     timeout = time.time() + 10
     listener_message = listener.get_message()
+    # Retrieves response datagrams until there are none or timeout
     while listener_message is not None:
         if time.time() > timeout:
-                break
+            break
         listener_message = listener.get_message()
 
 
-def trigger_callback(datagram):
-    '''Returns datagram status from boards'''
-    print("Response status code is {}".format(datagram.data))
-    return datagram.data
+def trigger_callback(msg):
+    """Returns status of response datagrams from specified boards"""
+    print(f"Response status code of datagram id {msg.datagram_type_id} is {msg.data}")
+    return msg.data
--- projects/bootloader/scripts/test_jump_application.py
+++ projects/bootloader/scripts/test_jump_application.py
@@ -1,49 +1,54 @@
-# """This Module Tests the functions in jump_application.py"""
+"""This Module Tests the functions in jump_application.py"""
 
-# import unittest
-# import can
+import unittest
+import time
+import can
 
-# from can_datagram import Datagram
-# from can_datagram import DatagramSender
-# from can_datagram import DatagramListener
+from can_datagram import Datagram
+from can_datagram import DatagramSender
+from can_datagram import DatagramListener
 
-# TEST_CHANNEL = "vcan0"
+TEST_CHANNEL = "vcan0"
 
-# TEST_PROTOCOL_VERSION = 1
-# TEST_DATAGRAM_TYPE_ID = 1
-# TEST_NODES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# TEST_DATA = []
+TEST_DATAGRAM_TYPE_ID = 5
+TEST_NODES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+TEST_DATA = []
 
-# class TestJumpApplication(unittest.TestCase):
-#   """Tests jump application functions"""
+TEST_RESPONSE_DATAGRAM_TYPE_ID = 0
 
-#     def test_jump_application(self):
-#         """Test the node ids jump to application"""
-#         self.callback_triggered = False
-#         self.message = []
-
-#         sender = DatagramSender(channel=TEST_CHANNEL, receive_own_messages=True)
-#         listener = DatagramListener(self.triggerCallback)
-#         notifier = can.Notifier(sender.bus, [listener])
-
-#         message = Datagram(
-#             datagram_type_id=TEST_DATAGRAM_TYPE_ID,
-#             node_ids=TEST_NODES,
-#             data=bytearray(TEST_DATA))
-#         sender.send(message)
-
-#         timeout = time.time() + 10
-#         while not self.callback_triggered:
-#             if time.time() > timeout:
-#                 break
-
-#         self.assertEqual(self.message.serialize(), message.serialize())
-#         self.assertEqual(self.callback_triggered, True)
-
-#     def triggerCallback(self, msg):
-#         self.message = msg
-#         self.callback_triggered = True
+STATUS_CODE_OK = 0
 
 
-# if __name__ == "__main__":
-#   unittest.main()
+class TestJumpApplication(unittest.TestCase):
+    """Test Jump Application"""
+
+    def test_jump_application(self):
+        """Test the jump to application feature"""
+        sender = DatagramSender(channel=TEST_CHANNEL, receive_own_messages=True)
+        listener = DatagramListener(self.callback)
+
+        can.Notifier(sender.bus, [listener])
+
+        message = Datagram(
+            datagram_type_id=TEST_DATAGRAM_TYPE_ID,
+            node_ids=TEST_NODES,
+            data=bytearray(TEST_DATA))
+        sender.send(message)
+
+        timeout = time.time() + 10
+        listener_message = listener.get_message()
+        while listener_message is not None:
+            if time.time() > timeout:
+                break
+            listener_message = listener.get_message()
+
+    def callback(self, msg):
+        """Received datagram should have correct status and id"""
+        if msg.datagram_type_id == TEST_DATAGRAM_TYPE_ID:
+            return
+        self.assertEqual(msg.datagram_type_id, TEST_RESPONSE_DATAGRAM_TYPE_ID)
+        self.assertEqual(msg.data, bytearray(STATUS_CODE_OK))
+
+
+if __name__ == "__main__":
+    unittest.main()
