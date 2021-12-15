959026a369570e2d16bf4174907ece0c8472cd78
ShiCheng Lu <shichenglu0708@gmail.com>
2021-12-15 16:19:32 +0000

    [SOFT-565] fixed some small issues

--- projects/bootloader/src/flash_application_code.c
+++ projects/bootloader/src/flash_application_code.c
@@ -23,8 +23,8 @@ static uint32_t s_app_crc;
 static uint32_t s_remaining_size;
 static StatusCode status;
 
-static char name[64];
-static char git_version[64];
+static char s_name[64];
+static char s_git_version[64];
 
 static bool prv_decode_string(pb_istream_t *stream, const pb_field_iter_t *field, void **arg) {
   size_t str_len = MIN(stream->bytes_left, (size_t)64);
@@ -33,7 +33,7 @@ static bool prv_decode_string(pb_istream_t *stream, const pb_field_iter_t *field
 }
 
 static StatusCode prv_start_flash(uint8_t *data, uint16_t data_len, void *context) {
-  LOG_DEBUG("recieved flash metadata");
+  LOG_DEBUG("received flash metadata\n");
 
   pb_istream_t pb_istream = pb_istream_from_buffer(data, data_len);
   if (!pb_decode(&pb_istream, FlashApplicationCode_fields, &s_meta_data)) {
@@ -58,8 +58,8 @@ static StatusCode prv_flash_complete() {
     config_commit(&s_updated_config);
     return status_response(STATUS_CODE_INTERNAL_ERROR, tx_cmpl_cb);
   } else {
-    strncpy(s_updated_config.project_name, name, 64);
-    strncpy(s_updated_config.git_version, git_version, 64);
+    strncpy(s_updated_config.project_name, s_name, 64);
+    strncpy(s_updated_config.git_version, s_git_version, 64);
     s_updated_config.application_crc32 = s_meta_data.application_crc;
     s_updated_config.application_size = s_meta_data.size;
 
@@ -68,30 +68,28 @@ static StatusCode prv_flash_complete() {
 }
 
 static StatusCode prv_flash_page(uint8_t *data, uint16_t data_len, void *context) {
-  // # This process assumes |data_len| less than FLASH_PAGE_BYTES #
   if (s_remaining_size < data_len) {
     return STATUS_CODE_OUT_OF_RANGE;
   }
-  // flash page, set data_len to the next multiple of 4
-  // |flash_write_len| will never exceed DATA_LEN_MAX
-  uint16_t flash_write_len = (data_len + 3) / FLASH_MCU_WRITE_BYTES * FLASH_MCU_WRITE_BYTES;
-
-  // erase then write data on page
-  for (uint16_t i = 0; i < data_len; i += FLASH_PAGE_BYTES) {
-    StatusCode status = flash_erase(s_page + i / FLASH_PAGE_BYTES);
+  // number of bytes written is the next multiple of FLASH_MCU_WRITE_BYTES
+  uint16_t flash_write_len =
+      (data_len + FLASH_MCU_WRITE_BYTES - 1) / FLASH_MCU_WRITE_BYTES * FLASH_MCU_WRITE_BYTES;
+  // number pages that are being written is the ceil(data_len / FLASH_PAGE_BYTES)
+  uint16_t flash_pages_len = (data_len + FLASH_PAGE_BYTES - 1) / FLASH_PAGE_BYTES;
+  // erase all pages that would be written on
+  for (uint16_t i = 0; i < flash_pages_len; i++) {
+    StatusCode status = flash_erase(s_page + i);
     if (status != STATUS_CODE_OK) {
       return status_response(status, tx_cmpl_cb);
     }
   }
-
+  // write
   StatusCode status = flash_write(FLASH_PAGE_TO_ADDR(s_page), data, flash_write_len);
   if (status != STATUS_CODE_OK) {
     return status_response(status, tx_cmpl_cb);
   }
 
-  for (uint16_t i = 0; i < data_len; i += FLASH_PAGE_BYTES) {
-    s_page++;
-  }
+  s_page += flash_pages_len;
 
   // add to crc
   s_app_crc = crc32_append_arr(data, data_len, s_app_crc);
@@ -105,8 +103,8 @@ static StatusCode prv_flash_page(uint8_t *data, uint16_t data_len, void *context
 }
 
 StatusCode flash_application_init() {
-  s_meta_data.git_version.arg = git_version;
-  s_meta_data.name.arg = name;
+  s_meta_data.git_version.arg = s_git_version;
+  s_meta_data.name.arg = s_name;
   s_meta_data.git_version.funcs.decode = prv_decode_string;
   s_meta_data.name.funcs.decode = prv_decode_string;
 
