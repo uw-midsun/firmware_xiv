724cf5fd07298cefaa328c4ef38059dd949d13d0
Adrian Jendo <ajendo@uwaterloo.ca>
2022-01-20 00:52:41 +0000

    [SOFT-999] Update code format

--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -8,7 +8,7 @@
 #include "log.h"
 #include "soft_timer.h"
 
-void readADC(const GpioAddress *btn_addr_ptr, void *context) {
+static void prv_read_adc(const GpioAddress *btn_addr_ptr, void *context) {
   GpioAddress pot_addr = *(GpioAddress *)context;
   uint16_t pot_data = 0;
   adc_read_raw_pin(pot_addr, &pot_data);
@@ -23,7 +23,10 @@ int main(void) {
 
   // Potentiometer
   GpioAddress pot_addr = { .port = GPIO_PORT_A, .pin = 6 };
-  GpioSettings pot_settings = { GPIO_DIR_IN, GPIO_STATE_LOW, GPIO_RES_NONE, GPIO_ALTFN_ANALOG };
+  GpioSettings pot_settings = { .direction = GPIO_DIR_IN,
+                                .state = GPIO_STATE_LOW,
+                                .resistor = GPIO_RES_NONE,
+                                .alt_function = GPIO_ALTFN_ANALOG };
 
   gpio_init_pin(&pot_addr, &pot_settings);
   adc_init(ADC_MODE_SINGLE);
@@ -31,9 +34,11 @@ int main(void) {
 
   // Button
   GpioAddress btn_addr = { .port = GPIO_PORT_B, .pin = 2 };
-  InterruptSettings btn_intrpt = { INTERRUPT_TYPE_INTERRUPT, INTERRUPT_PRIORITY_NORMAL };
+  InterruptSettings btn_intrpt = { .type = INTERRUPT_TYPE_INTERRUPT,
+                                   .priority = INTERRUPT_PRIORITY_NORMAL };
 
-  gpio_it_register_interrupt(&btn_addr, &btn_intrpt, INTERRUPT_EDGE_FALLING, readADC, &pot_addr);
+  gpio_it_register_interrupt(&btn_addr, &btn_intrpt, INTERRUPT_EDGE_FALLING, prv_read_adc,
+                             &pot_addr);
 
   while (true) {
     gpio_it_trigger_interrupt(&btn_addr);
