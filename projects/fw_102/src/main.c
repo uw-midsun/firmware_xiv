28e584497f32eef11e938bfd2ca5c17cadb32453
Nourish <nourishnew@gmail.com>
2022-01-21 05:35:01 +0000

    [SOFT-999] fw102

---
+++ projects/fw_102/src/main.c
@@ -0,0 +1,38 @@
+
+#include "interrupt.h"
+#include "log.h"
+#include "soft_timer.h"
+#include "wait.h"
+
+#define COUNTER_DURATION 500
+
+typedef struct {
+  uint8_t counter_a;
+  uint8_t counter_b;
+  int turn;
+} Counters;
+
+void prv_timer_callback(SoftTimerId timer_id, void *context) {
+  Counters *counters = context;
+  if (counters->turn % 2 == 0) {
+    counters->counter_a++;
+    LOG_DEBUG("Counter A %d\n", counters->counter_a);
+  } else {
+    counters->counter_a++;
+    counters->counter_b++;
+    LOG_DEBUG("Counter A: %d\n", counters->counter_a);
+    LOG_DEBUG("Counter B: %d\n", counters->counter_b);
+  }
+  counters->turn++;
+  soft_timer_start_millis(COUNTER_DURATION, prv_timer_callback, counters, NULL);
+}
+
+int main(void) {
+  interrupt_init();
+  soft_timer_init();
+  Counters c = { 0, 0, 0 };
+  soft_timer_start_millis(COUNTER_DURATION, prv_timer_callback, &c, NULL);
+  while (true) {
+    wait();
+  }
+}
