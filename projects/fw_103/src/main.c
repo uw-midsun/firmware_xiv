df68f760d88ea34979e2f1586a8f69c90dba147d
Abhinav <abhiraja1012@gmail.com>
2022-01-31 23:49:45 -0500

    [SOFT-999] fixes

--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -7,14 +7,14 @@
 #include "log.h"        // for printing
 #include "wait.h"       // for wait function
 
-GpioAddress adc_addr = {
+static GpioAddress s_adc_addr = {
   .port = GPIO_PORT_A,
   .pin = 6,
 };
 
 static void prv_read_adc(const GpioAddress *btn_addr_ptr, void *context) {
   uint16_t adc_data = 0;
-  if (adc_read_converted_pin(adc_addr, &adc_data) == STATUS_CODE_OK) {
+  if (adc_read_converted_pin(s_adc_addr, &adc_data) == STATUS_CODE_OK) {
     LOG_DEBUG("ADC data: %d\n", adc_data);
   } else {
     LOG_DEBUG("Failed to read adc data");
@@ -22,9 +22,9 @@ static void prv_read_adc(const GpioAddress *btn_addr_ptr, void *context) {
 }
 
 int main(void) {
+  interrupt_init();
   gpio_init();
   gpio_it_init();
-  interrupt_init();
 
   GpioAddress btn_addr = {
     .port = GPIO_PORT_B,
@@ -39,14 +39,14 @@ int main(void) {
   GpioSettings btn_gpio_settings = {
     .direction = GPIO_DIR_IN,
     .state = GPIO_STATE_LOW,
-    .resistor = GPIO_RES_PULLDOWN,
+    .resistor = GPIO_RES_NONE,
   };
 
-  gpio_init_pin(&adc_addr, &adc_gpio_settings);
+  gpio_init_pin(&s_adc_addr, &adc_gpio_settings);
   gpio_init_pin(&btn_addr, &btn_gpio_settings);
   adc_init(ADC_MODE_SINGLE);
 
-  adc_set_channel_pin(adc_addr, true);
+  adc_set_channel_pin(s_adc_addr, true);
 
   InterruptSettings settings = { .priority = INTERRUPT_PRIORITY_NORMAL,
                                  .type = INTERRUPT_TYPE_INTERRUPT };
