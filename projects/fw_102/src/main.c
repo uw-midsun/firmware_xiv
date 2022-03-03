8830a4f99b16b120459920ea610ec74e1dc62108
NOURISH CHERISH <nourishnew@gmail.com>
2022-03-02 21:54:13 -0500

    updated the main file

--- projects/fw_102/src/main.c
+++ projects/fw_102/src/main.c
@@ -4,17 +4,16 @@
 #include "soft_timer.h"
 #include "wait.h"
 
-#define COUNTER_DURATION 500
+#define COUNTER_DURATION_MS 500
 
 typedef struct {
   uint8_t counter_a;
   uint8_t counter_b;
-  int turn;
 } Counters;
 
-void prv_timer_callback(SoftTimerId timer_id, void *context) {
+static void prv_timer_callback(SoftTimerId timer_id, void *context) {
   Counters *counters = context;
-  if (counters->turn % 2 == 0) {
+  if (counters->counter_a % 2 == 0) {
     counters->counter_a++;
     LOG_DEBUG("Counter A %d\n", counters->counter_a);
   } else {
@@ -23,14 +22,13 @@ void prv_timer_callback(SoftTimerId timer_id, void *context) {
     LOG_DEBUG("Counter A: %d\n", counters->counter_a);
     LOG_DEBUG("Counter B: %d\n", counters->counter_b);
   }
-  counters->turn++;
   soft_timer_start_millis(COUNTER_DURATION, prv_timer_callback, counters, NULL);
 }
 
 int main(void) {
   interrupt_init();
   soft_timer_init();
-  Counters c = { 0, 0, 0 };
+  Counters c = { 0 };
   soft_timer_start_millis(COUNTER_DURATION, prv_timer_callback, &c, NULL);
   while (true) {
     wait();
